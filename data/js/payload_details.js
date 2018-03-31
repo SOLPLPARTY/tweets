7bd3c8301e3acf2982055c05e7dec272bbb3fad6
솔플파티 <solplparty@gmail.com>
2018-04-01 00:14:30 +0900

    Added 4 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
-var payload_details =  {
-  "tweets" : 431,
-  "created_at" : "2018-03-30 17:11:12 +0000",
-  "lang" : "ko"
+var payload_details = {
+	"tweets": 435,
+	"created_at": "Sat Mar 31 15:14:29 +0000 2018",
+	"lang": "ko"
 }
