332ea15cbcb9a68d644d6ac344a966ee4f19a230
솔플파티 <solplparty@gmail.com>
2018-06-25 00:22:25 +0900

    Added 5 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
-var payload_details =  {
-  "tweets" : 4311,
-  "created_at" : "2018-06-24 11:23:39 +0000",
-  "lang" : "ko"
+var payload_details = {
+	"tweets": 4316,
+	"created_at": "Sun Jun 24 15:22:25 +0000 2018",
+	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -1,31 +1,37 @@
-var tweet_index =  [ {
-  "file_name" : "data\/js\/tweets\/2018_06.js",
-  "year" : 2018,
-  "var_name" : "tweets_2018_06",
-  "tweet_count" : 1718,
-  "month" : 6
-}, {
-  "file_name" : "data\/js\/tweets\/2018_05.js",
-  "year" : 2018,
-  "var_name" : "tweets_2018_05",
-  "tweet_count" : 1472,
-  "month" : 5
-}, {
-  "file_name" : "data\/js\/tweets\/2018_04.js",
-  "year" : 2018,
-  "var_name" : "tweets_2018_04",
-  "tweet_count" : 686,
-  "month" : 4
-}, {
-  "file_name" : "data\/js\/tweets\/2018_03.js",
-  "year" : 2018,
-  "var_name" : "tweets_2018_03",
-  "tweet_count" : 432,
-  "month" : 3
-}, {
-  "file_name" : "data\/js\/tweets\/2018_02.js",
-  "year" : 2018,
-  "var_name" : "tweets_2018_02",
-  "tweet_count" : 1,
-  "month" : 2
-} ]
+var tweet_index = [
+	{
+		"file_name": "data/js/tweets/2018_06.js",
+		"year": 2018,
+		"var_name": "tweets_2018_06",
+		"tweet_count": 1723,
+		"month": 6
+	},
+	{
+		"file_name": "data/js/tweets/2018_05.js",
+		"year": 2018,
+		"var_name": "tweets_2018_05",
+		"tweet_count": 1472,
+		"month": 5
+	},
+	{
+		"file_name": "data/js/tweets/2018_04.js",
+		"year": 2018,
+		"var_name": "tweets_2018_04",
+		"tweet_count": 686,
+		"month": 4
+	},
+	{
+		"file_name": "data/js/tweets/2018_03.js",
+		"year": 2018,
+		"var_name": "tweets_2018_03",
+		"tweet_count": 432,
+		"month": 3
+	},
+	{
+		"file_name": "data/js/tweets/2018_02.js",
+		"year": 2018,
+		"var_name": "tweets_2018_02",
+		"tweet_count": 1,
+		"month": 2
+	}
+]
