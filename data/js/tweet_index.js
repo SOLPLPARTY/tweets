19be837bdaa17721a5d083150c48a0f5fbf6bea3
solplparty <solplparty@gmail.com>
2018-07-23 01:46:54 +0900

    Revert "Merge branch 'master' of https://github.com/solplparty/tweets"
    This reverts commit 7e4dc4fab0574b9903cc120f1bc4a318fa277b78, reversing
    changes made to 7b76270ca82456ed1e8522af3f876e4bb5e0eeb3.

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var tweet_index =  [ {
   "file_name" : "data\/js\/tweets\/2018_07.js",
   "year" : 2018,
@@ -36,42 +35,3 @@ var tweet_index =  [ {
   "tweet_count" : 1,
   "month" : 2
 } ]
-=======
-var tweet_index = [
-	{
-		"file_name": "data/js/tweets/2018_06.js",
-		"year": 2018,
-		"var_name": "tweets_2018_06",
-		"tweet_count": 2285,
-		"month": 6
-	},
-	{
-		"file_name": "data/js/tweets/2018_05.js",
-		"year": 2018,
-		"var_name": "tweets_2018_05",
-		"tweet_count": 1472,
-		"month": 5
-	},
-	{
-		"file_name": "data/js/tweets/2018_04.js",
-		"year": 2018,
-		"var_name": "tweets_2018_04",
-		"tweet_count": 686,
-		"month": 4
-	},
-	{
-		"file_name": "data/js/tweets/2018_03.js",
-		"year": 2018,
-		"var_name": "tweets_2018_03",
-		"tweet_count": 432,
-		"month": 3
-	},
-	{
-		"file_name": "data/js/tweets/2018_02.js",
-		"year": 2018,
-		"var_name": "tweets_2018_02",
-		"tweet_count": 1,
-		"month": 2
-	}
-]
->>>>>>> 599c2cf6c135ebf898aa17536f49de2f3ca0098c
