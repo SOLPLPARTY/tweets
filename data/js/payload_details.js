6bc61fd2ec472ffb6d20cfc96b5080500fc24a72
솔플파티 <solplparty@gmail.com>
2019-03-26 16:17:53 +0900

    Added 4 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13321,
-	"created_at": "Tue Mar 26 06:17:54 +0000 2019",
+	"tweets": 13325,
+	"created_at": "Tue Mar 26 07:17:52 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2019_03.js",
 		"year": 2019,
 		"var_name": "tweets_2019_03",
-		"tweet_count": 1581,
+		"tweet_count": 1585,
 		"month": 3
 	},
 	{
