7810971396fd2903f6fa7b704bd7ecbb1162cd40
솔플파티 <solplparty@gmail.com>
2019-04-06 21:17:39 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14235,
-	"created_at": "Sat Apr 06 10:17:37 +0000 2019",
+	"tweets": 14236,
+	"created_at": "Sat Apr 06 12:17:38 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,209 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Sat Apr 06 12:11:43 +0000 2019",
+		"id": 1114493321978601500,
+		"id_str": "1114500930886623232",
+		"full_text": "#클럽빵 #인디 #홍대앞 ##liveclubbbang https://t.co/42bOZHtxed",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			29
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "클럽빵",
+					"indices": [
+						0,
+						4
+					]
+				},
+				{
+					"text": "인디",
+					"indices": [
+						5,
+						8
+					]
+				},
+				{
+					"text": "홍대앞",
+					"indices": [
+						9,
+						13
+					]
+				},
+				{
+					"text": "liveclubbbang",
+					"indices": [
+						15,
+						29
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1114493312843411500,
+					"id_str": "1114493312843411456",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/D3d5w2CUcAAlr8b.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3d5w2CUcAAlr8b.jpg",
+					"url": "https://t.co/42bOZHtxed",
+					"display_url": "pic.twitter.com/42bOZHtxed",
+					"expanded_url": "https://twitter.com/club_bbang/status/1114493321978601473/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 542,
+							"h": 516,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 542,
+							"h": 516,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 542,
+							"h": 516,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1114493312843411500,
+					"id_str": "1114493312843411456",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/D3d5w2CUcAAlr8b.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3d5w2CUcAAlr8b.jpg",
+					"url": "https://t.co/42bOZHtxed",
+					"display_url": "pic.twitter.com/42bOZHtxed",
+					"expanded_url": "https://twitter.com/club_bbang/status/1114493321978601473/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 542,
+							"h": 516,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 542,
+							"h": 516,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 542,
+							"h": 516,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1387140522,
+			"id_str": "1387140522",
+			"name": "클럽 빵",
+			"screen_name": "club_bbang",
+			"location": "서울 마포 서교동 327-18 지하",
+			"description": "모던록 라이브클럽 빵, 공연일정을 알려드려요! 문의 010-8910-1089, cafebbang@hanmail.net",
+			"url": "https://t.co/ZROqsiLHaK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ZROqsiLHaK",
+							"expanded_url": "http://cafe.daum.net/cafebbang",
+							"display_url": "cafe.daum.net/cafebbang",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2672,
+			"friends_count": 942,
+			"listed_count": 51,
+			"created_at": "Sun Apr 28 14:57:43 +0000 2013",
+			"favourites_count": 64,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1761,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3617429436/49aa1ea87a3bdf4e12ef8ab826e7b229_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3617429436/49aa1ea87a3bdf4e12ef8ab826e7b229_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1387140522/1367815826",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "#클럽빵 #인디 #홍대앞 ##liveclubbbang https://t.co/42bOZHtxed"
+	},
+	{
 		"created_at": "Sat Apr 06 10:11:52 +0000 2019",
 		"id": 1114466672407015400,
 		"id_str": "1114470769784967170",
