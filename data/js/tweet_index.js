942d001ebb254d48b4313d70f1d4f4cdf66b1aaa
솔플파티 <solplparty@gmail.com>
2019-04-18 15:17:45 +0900

    Added 3 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2019_04.js",
 		"year": 2019,
 		"var_name": "tweets_2019_04",
-		"tweet_count": 1290,
+		"tweet_count": 1293,
 		"month": 4
 	},
 	{
