d37cea50c24b4c892304e160903f06c7951c5621
솔플파티 <solplparty@gmail.com>
2019-05-04 06:17:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16072,
-	"created_at": "Fri May 03 17:17:39 +0000 2019",
+	"tweets": 16073,
+	"created_at": "Fri May 03 21:17:35 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,10 +1,237 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Fri May 03 20:59:18 +0000 2019",
+		"id": 1124418174244540400,
+		"id_str": "1124418174244540416",
+		"full_text": "【 #두인디 】\n쩜오구데이: .59 단독공연 at (2019/05/09 20:00)\n▶ https://t.co/Kt68td2vm2\n　\n#Doindie #공연일정 #티켓공지\n#솔플파티 #solplparty https://t.co/P1gE7BmUuZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			113
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "두인디",
+					"indices": [
+						2,
+						6
+					]
+				},
+				{
+					"text": "Doindie",
+					"indices": [
+						75,
+						83
+					]
+				},
+				{
+					"text": "공연일정",
+					"indices": [
+						84,
+						89
+					]
+				},
+				{
+					"text": "티켓공지",
+					"indices": [
+						90,
+						95
+					]
+				},
+				{
+					"text": "솔플파티",
+					"indices": [
+						96,
+						101
+					]
+				},
+				{
+					"text": "solplparty",
+					"indices": [
+						102,
+						113
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Kt68td2vm2",
+					"expanded_url": "http://bit.ly/2LoNGdE",
+					"display_url": "bit.ly/2LoNGdE",
+					"indices": [
+						49,
+						72
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1124418172994641900,
+					"id_str": "1124418172994641920",
+					"indices": [
+						114,
+						137
+					],
+					"media_url": "http://pbs.twimg.com/media/D5q8XvRW4AAMTww.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5q8XvRW4AAMTww.jpg",
+					"url": "https://t.co/P1gE7BmUuZ",
+					"display_url": "pic.twitter.com/P1gE7BmUuZ",
+					"expanded_url": "https://twitter.com/solplparty/status/1124418174244540416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1124418172994641900,
+					"id_str": "1124418172994641920",
+					"indices": [
+						114,
+						137
+					],
+					"media_url": "http://pbs.twimg.com/media/D5q8XvRW4AAMTww.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5q8XvRW4AAMTww.jpg",
+					"url": "https://t.co/P1gE7BmUuZ",
+					"display_url": "pic.twitter.com/P1gE7BmUuZ",
+					"expanded_url": "https://twitter.com/solplparty/status/1124418174244540416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 968706450779463700,
+			"id_str": "968706450779463682",
+			"name": "솔플파티",
+			"screen_name": "solplparty",
+			"location": "",
+			"description": "🌈 문화예술 커뮤니티, 솔플파티\n\n공연일정 / 티켓예매 공지 / 페스티벌 / 인디밴드 / 내한공연 / 콘서트 / 홍대문화 / 영화제 / 전시회 / 북토크 / 모임\n\n문화예술정보 @EntRecruitKR\n문의 : contact@solplparty.com",
+			"url": "https://t.co/GCEfwO8QQD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GCEfwO8QQD",
+							"expanded_url": "http://solplparty.com",
+							"display_url": "solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 76,
+			"friends_count": 304,
+			"listed_count": 1,
+			"created_at": "Wed Feb 28 04:36:30 +0000 2018",
+			"favourites_count": 215,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 30204,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/968706450779463682/1519792691",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "【 #두인디 】\n쩜오구데이: .59 단독공연 at (2019/05/09 20:00)\n▶ https://t.co/Kt68td2vm2\n　\n#Doindie #공연일정 #티켓공지\n#솔플파티 #solplparty https://t.co/P1gE7BmUuZ"
+	},
+	{
 		"created_at": "Fri May 03 17:11:42 +0000 2019",
 		"id": 1124347378775887900,
 		"id_str": "1124360899777658881",
-		"full_text": "[위댄스르륵 마켓 @초원서점]\n2019.5.4(토) PM 1:00-7:00 \n\n🎪이상하고 재미있는 위댄스르륵 마켓이 올해는 초원서점으로 갑니다!\n\n🤹‍♀️판매품목: 초원서점의 책들 + 위댄스 언픽스드 앨범과 티셔츠, 빈티지 의류, 음료, 쿠키, 각종 재미난 물건들 \n\n🥳초원서점에서 즐거운 한때 같이 보내요! https://t.co/pSuNm7pGNZ",
+		"full_text": "[위댄스르륵 마켓 @초원서점]\n2019.5.4(토) PM 1:00-7:00 \n\n🎪이상하고 재미있는 위댄스르륵 마켓이 올해는 초원서점으로 갑니다!\n\n🤹♀️판매품목: 초원서점의 책들 + 위댄스 언픽스드 앨범과 티셔츠, 빈티지 의류, 음료, 쿠키, 각종 재미난 물건들 \n\n🥳초원서점에서 즐거운 한때 같이 보내요! https://t.co/pSuNm7pGNZ",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -172,7 +399,7 @@ Grailbird.data.tweets_2019_05 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "[위댄스르륵 마켓 @초원서점]\n2019.5.4(토) PM 1:00-7:00 \n\n🎪이상하고 재미있는 위댄스르륵 마켓이 올해는 초원서점으로 갑니다!\n\n🤹‍♀️판매품목: 초원서점의 책들 + 위댄스 언픽스드 앨범과 티셔츠, 빈티지 의류, 음료, 쿠키, 각종 재미난 물건들 \n\n🥳초원서점에서 즐거운 한때 같이 보내요! https://t.co/pSuNm7pGNZ"
+		"text": "[위댄스르륵 마켓 @초원서점]\n2019.5.4(토) PM 1:00-7:00 \n\n🎪이상하고 재미있는 위댄스르륵 마켓이 올해는 초원서점으로 갑니다!\n\n🤹♀️판매품목: 초원서점의 책들 + 위댄스 언픽스드 앨범과 티셔츠, 빈티지 의류, 음료, 쿠키, 각종 재미난 물건들 \n\n🥳초원서점에서 즐거운 한때 같이 보내요! https://t.co/pSuNm7pGNZ"
 	},
 	{
 		"created_at": "Fri May 03 15:18:16 +0000 2019",
