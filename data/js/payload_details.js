7101ef4994b9a2fb5246b7102428ca1f5580a366
솔플파티 <solplparty@gmail.com>
2019-05-05 00:17:37 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16091,
-	"created_at": "Sat May 04 12:17:35 +0000 2019",
+	"tweets": 16092,
+	"created_at": "Sat May 04 15:17:36 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,247 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Sat May 04 14:22:14 +0000 2019",
+		"id": 1124680639024959500,
+		"id_str": "1124680639024959489",
+		"full_text": "【 #상상마당 】\n[콘서트] 민트페스타 vol. 63 ~ Corvus (2019.05.26~2019.05.26) at 라이브홀\n▶ https://t.co/BbWUcggR8x\n　\n#상상마당홍대 #라이브홀 #LIVE_HALL\n#공연일정 #티켓공지 #솔플파티 #solplparty https://t.co/P3K9pK48iQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			153
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "상상마당",
+					"indices": [
+						2,
+						7
+					]
+				},
+				{
+					"text": "상상마당홍대",
+					"indices": [
+						99,
+						106
+					]
+				},
+				{
+					"text": "라이브홀",
+					"indices": [
+						107,
+						112
+					]
+				},
+				{
+					"text": "LIVE_HALL",
+					"indices": [
+						113,
+						123
+					]
+				},
+				{
+					"text": "공연일정",
+					"indices": [
+						124,
+						129
+					]
+				},
+				{
+					"text": "티켓공지",
+					"indices": [
+						130,
+						135
+					]
+				},
+				{
+					"text": "솔플파티",
+					"indices": [
+						136,
+						141
+					]
+				},
+				{
+					"text": "solplparty",
+					"indices": [
+						142,
+						153
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/BbWUcggR8x",
+					"expanded_url": "http://bit.ly/2DJIBpR",
+					"display_url": "bit.ly/2DJIBpR",
+					"indices": [
+						73,
+						96
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1124680637808554000,
+					"id_str": "1124680637808553984",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/D5urFM5WsAA5i8k.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5urFM5WsAA5i8k.jpg",
+					"url": "https://t.co/P3K9pK48iQ",
+					"display_url": "pic.twitter.com/P3K9pK48iQ",
+					"expanded_url": "https://twitter.com/solplparty/status/1124680639024959489/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 400,
+							"h": 566,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 400,
+							"h": 566,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 400,
+							"h": 566,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1124680637808554000,
+					"id_str": "1124680637808553984",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/D5urFM5WsAA5i8k.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5urFM5WsAA5i8k.jpg",
+					"url": "https://t.co/P3K9pK48iQ",
+					"display_url": "pic.twitter.com/P3K9pK48iQ",
+					"expanded_url": "https://twitter.com/solplparty/status/1124680639024959489/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 400,
+							"h": 566,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 400,
+							"h": 566,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 400,
+							"h": 566,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 968706450779463700,
+			"id_str": "968706450779463682",
+			"name": "솔플파티",
+			"screen_name": "solplparty",
+			"location": "",
+			"description": "🌈 문화예술 커뮤니티, 솔플파티\n\n공연일정 / 티켓예매 공지 / 페스티벌 / 인디밴드 / 내한공연 / 콘서트 / 홍대문화 / 영화제 / 전시회 / 북토크 / 모임\n\n문화예술정보 @EntRecruitKR\n문의 : contact@solplparty.com",
+			"url": "https://t.co/GCEfwO8QQD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GCEfwO8QQD",
+							"expanded_url": "http://solplparty.com",
+							"display_url": "solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 76,
+			"friends_count": 304,
+			"listed_count": 1,
+			"created_at": "Wed Feb 28 04:36:30 +0000 2018",
+			"favourites_count": 215,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 30229,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/968706450779463682/1519792691",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "【 #상상마당 】\n[콘서트] 민트페스타 vol. 63 ~ Corvus (2019.05.26~2019.05.26) at 라이브홀\n▶ https://t.co/BbWUcggR8x\n　\n#상상마당홍대 #라이브홀 #LIVE_HALL\n#공연일정 #티켓공지 #솔플파티 #solplparty https://t.co/P3K9pK48iQ"
+	},
+	{
 		"created_at": "Sat May 04 12:12:01 +0000 2019",
 		"id": 1124646878203662300,
 		"id_str": "1124647867208151040",
