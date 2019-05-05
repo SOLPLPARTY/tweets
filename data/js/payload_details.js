97baa1edf4ef48a8a867b93a782b9b960c917297
솔플파티 <solplparty@gmail.com>
2019-05-05 18:17:37 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16102,
-	"created_at": "Sun May 05 08:17:35 +0000 2019",
+	"tweets": 16103,
+	"created_at": "Sun May 05 09:17:36 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,238 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Sun May 05 08:18:16 +0000 2019",
+		"id": 1124937817841328100,
+		"id_str": "1124951432669560833",
+		"full_text": "[랄라스윗] 오늘 어반소스에서 열린 프라이빗 라이브에 함께해주신 분들께 감사드립니다💕 모두 좋은 주말 되세요 :)  \n\n#랄라스윗 #프라이빗라이브 #어반소스 https://t.co/kvhnlqvj6e",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			87
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "랄라스윗",
+					"indices": [
+						67,
+						72
+					]
+				},
+				{
+					"text": "프라이빗라이브",
+					"indices": [
+						73,
+						81
+					]
+				},
+				{
+					"text": "어반소스",
+					"indices": [
+						82,
+						87
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1124937809381380100,
+					"id_str": "1124937809381380096",
+					"indices": [
+						88,
+						111
+					],
+					"media_url": "http://pbs.twimg.com/media/D5yU-jqVUAARqq3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5yU-jqVUAARqq3.jpg",
+					"url": "https://t.co/kvhnlqvj6e",
+					"display_url": "pic.twitter.com/kvhnlqvj6e",
+					"expanded_url": "https://twitter.com/YourSummerKR/status/1124937817841328128/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1124937809381380100,
+					"id_str": "1124937809381380096",
+					"indices": [
+						88,
+						111
+					],
+					"media_url": "http://pbs.twimg.com/media/D5yU-jqVUAARqq3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5yU-jqVUAARqq3.jpg",
+					"url": "https://t.co/kvhnlqvj6e",
+					"display_url": "pic.twitter.com/kvhnlqvj6e",
+					"expanded_url": "https://twitter.com/YourSummerKR/status/1124937817841328128/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1124937809385611300,
+					"id_str": "1124937809385611264",
+					"indices": [
+						88,
+						111
+					],
+					"media_url": "http://pbs.twimg.com/media/D5yU-jrV4AA6tAe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5yU-jrV4AA6tAe.jpg",
+					"url": "https://t.co/kvhnlqvj6e",
+					"display_url": "pic.twitter.com/kvhnlqvj6e",
+					"expanded_url": "https://twitter.com/YourSummerKR/status/1124937817841328128/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 404720294,
+			"id_str": "404720294",
+			"name": "당신의 여름",
+			"screen_name": "YourSummerKR",
+			"location": "한국",
+			"description": "당신의 여름(Your Summer)은 가을방학, 계피, 김사월, 김사월X김해원, 랄라스윗, 못(Mot), 성진환, 아마도이자람밴드, 오지은, 우주히피, 이지형, 이하윤, 전기뱀장어, 최낙타, 홍이삭과 함께합니다.",
+			"url": "https://t.co/O7n52oEN1K",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/O7n52oEN1K",
+							"expanded_url": "http://www.instagram.com/yoursummernet",
+							"display_url": "instagram.com/yoursummernet",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1432,
+			"friends_count": 30,
+			"listed_count": 11,
+			"created_at": "Fri Nov 04 09:50:32 +0000 2011",
+			"favourites_count": 229,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3472,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/978966061050609669/wmNssQkU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/978966061050609669/wmNssQkU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/404720294/1522238671",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[랄라스윗] 오늘 어반소스에서 열린 프라이빗 라이브에 함께해주신 분들께 감사드립니다💕 모두 좋은 주말 되세요 :)  \n\n#랄라스윗 #프라이빗라이브 #어반소스 https://t.co/kvhnlqvj6e"
+	},
+	{
 		"created_at": "Sun May 05 07:19:54 +0000 2019",
 		"id": 1124929050990956500,
 		"id_str": "1124936743986368513",
