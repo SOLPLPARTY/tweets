a7e7b9206425017b0e104c61c2304f084fcb6a89
솔플파티 <solplparty@gmail.com>
2019-05-06 19:17:38 +0900

    Added 1 tweets

--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,127 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Mon May 06 09:38:36 +0000 2019",
+		"id": 1125334037541863400,
+		"id_str": "1125334037541863425",
+		"full_text": "#서울재즈페스티벌 #Seoul_Jazz_Festival\n[SJF19 A Message From Artist] Car, the gardenvvv\nhttps://t.co/Z3tGkvg8zR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			103
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "서울재즈페스티벌",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"text": "Seoul_Jazz_Festival",
+					"indices": [
+						10,
+						30
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Z3tGkvg8zR",
+					"expanded_url": "https://youtu.be/3s6_ls31YRw",
+					"display_url": "youtu.be/3s6_ls31YRw",
+					"indices": [
+						80,
+						103
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 968706450779463700,
+			"id_str": "968706450779463682",
+			"name": "솔플파티",
+			"screen_name": "solplparty",
+			"location": "",
+			"description": "🌈 문화예술 커뮤니티, 솔플파티\n\n공연일정 / 티켓예매 공지 / 페스티벌 / 인디밴드 / 내한공연 / 콘서트 / 홍대문화 / 영화제 / 전시회 / 북토크 / 모임\n\n문화예술정보 @EntRecruitKR\n문의 : contact@solplparty.com",
+			"url": "https://t.co/GCEfwO8QQD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GCEfwO8QQD",
+							"expanded_url": "http://solplparty.com",
+							"display_url": "solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 76,
+			"friends_count": 304,
+			"listed_count": 1,
+			"created_at": "Wed Feb 28 04:36:30 +0000 2018",
+			"favourites_count": 215,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 30268,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/968706450779463682/1519792691",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#서울재즈페스티벌 #Seoul_Jazz_Festival\n[SJF19 A Message From Artist] Car, the gardenvvv\nhttps://t.co/Z3tGkvg8zR"
+	},
+	{
 		"created_at": "Mon May 06 07:23:18 +0000 2019",
 		"id": 1125299985132683300,
 		"id_str": "1125299985132683265",
@@ -299,7 +420,7 @@ Grailbird.data.tweets_2019_05 =
 		"created_at": "Mon May 06 07:19:54 +0000 2019",
 		"id": 1125286763537358800,
 		"id_str": "1125299131793182722",
-		"full_text": "다시 생각해 봐도, 친구랑 노는게 제일 재밋음.\n일탈의 일상화, #월디페!!!\n지혜 하고싶은거 다해~\n.\n검증된 즐거움, 월디페!!!⁣\n6월 최고의 경험을 준비하세요.⁣\n예매처 : https://t.co/pFmo1AWpe0⁣\n.⁣\n\n문화를 더하다. life with culture.⁣\n㈜비이피씨탄젠트⁣ https://t.co/zallplyGAJ",
+		"full_text": "다시 생각해 봐도, 친구랑 노는게 제일 재밋음.\n일탈의 일상화, #월디페!!!\n지혜 하고싶은거 다해~\n.\n검증된 즐거움, 월디페!!!\n6월 최고의 경험을 준비하세요.\n예매처 : https://t.co/pFmo1AWpe0\n.\n\n문화를 더하다. life with culture.\n㈜비이피씨탄젠트 https://t.co/zallplyGAJ",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -516,7 +637,7 @@ Grailbird.data.tweets_2019_05 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "다시 생각해 봐도, 친구랑 노는게 제일 재밋음.\n일탈의 일상화, #월디페!!!\n지혜 하고싶은거 다해~\n.\n검증된 즐거움, 월디페!!!⁣\n6월 최고의 경험을 준비하세요.⁣\n예매처 : https://t.co/pFmo1AWpe0⁣\n.⁣\n\n문화를 더하다. life with culture.⁣\n㈜비이피씨탄젠트⁣ https://t.co/zallplyGAJ"
+		"text": "다시 생각해 봐도, 친구랑 노는게 제일 재밋음.\n일탈의 일상화, #월디페!!!\n지혜 하고싶은거 다해~\n.\n검증된 즐거움, 월디페!!!\n6월 최고의 경험을 준비하세요.\n예매처 : https://t.co/pFmo1AWpe0\n.\n\n문화를 더하다. life with culture.\n㈜비이피씨탄젠트 https://t.co/zallplyGAJ"
 	},
 	{
 		"created_at": "Mon May 06 06:46:42 +0000 2019",
