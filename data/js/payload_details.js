06634286407542f6d2ce148742bcb3dad8043d63
솔플파티 <solplparty@gmail.com>
2019-05-09 04:17:40 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16304,
-	"created_at": "Wed May 08 16:17:37 +0000 2019",
+	"tweets": 16305,
+	"created_at": "Wed May 08 19:17:39 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,215 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Wed May 08 18:18:16 +0000 2019",
+		"id": 1126179182680010800,
+		"id_str": "1126189591545950208",
+		"full_text": "#20190509\n#정준일 #JungJoonil\n앨범 녹음을 위해 \n피아노를 녹음실로\n#JoonilJung #피아노 #Piano https://t.co/btw4FP2TAD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			71
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "정준일",
+					"indices": [
+						10,
+						14
+					]
+				},
+				{
+					"text": "JungJoonil",
+					"indices": [
+						15,
+						26
+					]
+				},
+				{
+					"text": "JoonilJung",
+					"indices": [
+						48,
+						59
+					]
+				},
+				{
+					"text": "피아노",
+					"indices": [
+						60,
+						64
+					]
+				},
+				{
+					"text": "Piano",
+					"indices": [
+						65,
+						71
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1126179169589575700,
+					"id_str": "1126179169589575680",
+					"indices": [
+						72,
+						95
+					],
+					"media_url": "http://pbs.twimg.com/media/D6D9_OzUwAApDfs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6D9_OzUwAApDfs.jpg",
+					"url": "https://t.co/btw4FP2TAD",
+					"display_url": "pic.twitter.com/btw4FP2TAD",
+					"expanded_url": "https://twitter.com/messenger_yoon/status/1126179182680010752/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1126179169589575700,
+					"id_str": "1126179169589575680",
+					"indices": [
+						72,
+						95
+					],
+					"media_url": "http://pbs.twimg.com/media/D6D9_OzUwAApDfs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6D9_OzUwAApDfs.jpg",
+					"url": "https://t.co/btw4FP2TAD",
+					"display_url": "pic.twitter.com/btw4FP2TAD",
+					"expanded_url": "https://twitter.com/messenger_yoon/status/1126179182680010752/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 150566482,
+			"id_str": "150566482",
+			"name": "엠와이뮤직(MYmusic Ent)",
+			"screen_name": "messenger_yoon",
+			"location": "Artist Manager (South Korea)",
+			"description": "[Musician] 정준일, 낭만유랑악단, 나인(Nine9), 디어클라우드, 권영찬, 위아영 //mymusicent@naver.com//+82)2.6013.0102",
+			"url": "https://t.co/1IfWMp2Je9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1IfWMp2Je9",
+							"expanded_url": "http://www.mymusicent.co.kr",
+							"display_url": "mymusicent.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2875,
+			"friends_count": 397,
+			"listed_count": 34,
+			"created_at": "Tue Jun 01 07:29:53 +0000 2010",
+			"favourites_count": 12652,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5082,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/540007978649128960/jSz5oRO7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/540007978649128960/jSz5oRO7_normal.jpeg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#20190509\n#정준일 #JungJoonil\n앨범 녹음을 위해 \n피아노를 녹음실로\n#JoonilJung #피아노 #Piano https://t.co/btw4FP2TAD"
+	},
+	{
 		"created_at": "Wed May 08 16:11:59 +0000 2019",
 		"id": 1126143807588786200,
 		"id_str": "1126157809001824257",
