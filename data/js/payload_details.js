05dcad8064a1d73c79c7e45d7d675bb312fdcb6b
솔플파티 <solplparty@gmail.com>
2019-05-11 18:17:42 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16517,
-	"created_at": "Sat May 11 08:17:40 +0000 2019",
+	"tweets": 16518,
+	"created_at": "Sat May 11 09:17:40 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,296 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Sat May 11 09:11:43 +0000 2019",
+		"id": 1127127756091748400,
+		"id_str": "1127139208110514176",
+		"full_text": "#상상마당시네마\n영화도 보고, 선물도 받고!\n지금, 상상마당 시네마에서는\n영화를 보면 영화의 감성이 담긴 \n굿즈도 받아가실 수 있다구요!!\n.\n더비도 만들면서 받고 싶었던 다양한 굿즈들...★\n이벤트 참여 방법과 기간을 확인하고,\n굿즈를 받을 수 있는 기회! 놓치지 마세요 :-) https://t.co/GtSCuohTyq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			156
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "상상마당시네마",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1127127731819278300,
+					"id_str": "1127127731819278336",
+					"indices": [
+						157,
+						180
+					],
+					"media_url": "http://pbs.twimg.com/media/D6Rcs0eUcAAldjh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6Rcs0eUcAAldjh.jpg",
+					"url": "https://t.co/GtSCuohTyq",
+					"display_url": "pic.twitter.com/GtSCuohTyq",
+					"expanded_url": "https://twitter.com/SSmadang/status/1127127756091748352/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1127127731819278300,
+					"id_str": "1127127731819278336",
+					"indices": [
+						157,
+						180
+					],
+					"media_url": "http://pbs.twimg.com/media/D6Rcs0eUcAAldjh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6Rcs0eUcAAldjh.jpg",
+					"url": "https://t.co/GtSCuohTyq",
+					"display_url": "pic.twitter.com/GtSCuohTyq",
+					"expanded_url": "https://twitter.com/SSmadang/status/1127127756091748352/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1127127731785715700,
+					"id_str": "1127127731785715712",
+					"indices": [
+						157,
+						180
+					],
+					"media_url": "http://pbs.twimg.com/media/D6Rcs0WUUAAeIMp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6Rcs0WUUAAeIMp.jpg",
+					"url": "https://t.co/GtSCuohTyq",
+					"display_url": "pic.twitter.com/GtSCuohTyq",
+					"expanded_url": "https://twitter.com/SSmadang/status/1127127756091748352/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1127127731815116800,
+					"id_str": "1127127731815116800",
+					"indices": [
+						157,
+						180
+					],
+					"media_url": "http://pbs.twimg.com/media/D6Rcs0dU8AAGnHn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6Rcs0dU8AAGnHn.jpg",
+					"url": "https://t.co/GtSCuohTyq",
+					"display_url": "pic.twitter.com/GtSCuohTyq",
+					"expanded_url": "https://twitter.com/SSmadang/status/1127127756091748352/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1127127731823468500,
+					"id_str": "1127127731823468545",
+					"indices": [
+						157,
+						180
+					],
+					"media_url": "http://pbs.twimg.com/media/D6Rcs0fUYAEfW1V.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6Rcs0fUYAEfW1V.jpg",
+					"url": "https://t.co/GtSCuohTyq",
+					"display_url": "pic.twitter.com/GtSCuohTyq",
+					"expanded_url": "https://twitter.com/SSmadang/status/1127127756091748352/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 110358280,
+			"id_str": "110358280",
+			"name": "KT&G 상상마당",
+			"screen_name": "SSmadang",
+			"location": "서울 홍대 (Hongdae, Seoul)",
+			"description": "[복합문화공간] #상상마당 영화ㅣ공연ㅣ디자인ㅣ전시ㅣ교육ㅣ카페",
+			"url": "https://t.co/zkpyp6uMPi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zkpyp6uMPi",
+							"expanded_url": "http://sangsangmadang.com",
+							"display_url": "sangsangmadang.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 19688,
+			"friends_count": 2035,
+			"listed_count": 0,
+			"created_at": "Mon Feb 01 07:45:21 +0000 2010",
+			"favourites_count": 256,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9169,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1061898245721403392/H_SOoD3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1061898245721403392/H_SOoD3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/110358280/1421402986",
+			"profile_link_color": "242D8C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "595959",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#상상마당시네마\n영화도 보고, 선물도 받고!\n지금, 상상마당 시네마에서는\n영화를 보면 영화의 감성이 담긴 \n굿즈도 받아가실 수 있다구요!!\n.\n더비도 만들면서 받고 싶었던 다양한 굿즈들...★\n이벤트 참여 방법과 기간을 확인하고,\n굿즈를 받을 수 있는 기회! 놓치지 마세요 :-) https://t.co/GtSCuohTyq"
+	},
+	{
 		"created_at": "Sat May 11 08:11:42 +0000 2019",
 		"id": 1127120873494683600,
 		"id_str": "1127124107995353089",
