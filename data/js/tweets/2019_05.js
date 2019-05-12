8cd195b718abb55738fa9c9cc11cbc75e0dcd257
솔플파티 <solplparty@gmail.com>
2019-05-12 17:17:42 +0900

    Added 1 tweets

--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,233 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Sun May 12 07:21:17 +0000 2019",
+		"id": 1127473806346465300,
+		"id_str": "1127473806346465280",
+		"full_text": "【 #플랫폼창동61 】\n[대관프로그램] \"일상에서 즐기는 예술 - 뚱보새의 5월\" : \"일상에서 즐기는 예술\" (2019년 5월 22일(수), 20시)\n▶ https://t.co/dBhQKfsMrK\n　\n#Platform61 #공연일정 #티켓공지\n#솔플파티 #solplparty https://t.co/NShrKhNM0J",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			154
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "플랫폼창동61",
+					"indices": [
+						2,
+						10
+					]
+				},
+				{
+					"text": "Platform61",
+					"indices": [
+						113,
+						124
+					]
+				},
+				{
+					"text": "공연일정",
+					"indices": [
+						125,
+						130
+					]
+				},
+				{
+					"text": "티켓공지",
+					"indices": [
+						131,
+						136
+					]
+				},
+				{
+					"text": "솔플파티",
+					"indices": [
+						137,
+						142
+					]
+				},
+				{
+					"text": "solplparty",
+					"indices": [
+						143,
+						154
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/dBhQKfsMrK",
+					"expanded_url": "http://bit.ly/2JeujSD",
+					"display_url": "bit.ly/2JeujSD",
+					"indices": [
+						87,
+						110
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1127473805306282000,
+					"id_str": "1127473805306281985",
+					"indices": [
+						155,
+						178
+					],
+					"media_url": "http://pbs.twimg.com/media/D6WXc8rWwAEGSfo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6WXc8rWwAEGSfo.jpg",
+					"url": "https://t.co/NShrKhNM0J",
+					"display_url": "pic.twitter.com/NShrKhNM0J",
+					"expanded_url": "https://twitter.com/solplparty/status/1127473806346465280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 280,
+							"h": 267,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 280,
+							"h": 267,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 280,
+							"h": 267,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1127473805306282000,
+					"id_str": "1127473805306281985",
+					"indices": [
+						155,
+						178
+					],
+					"media_url": "http://pbs.twimg.com/media/D6WXc8rWwAEGSfo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6WXc8rWwAEGSfo.jpg",
+					"url": "https://t.co/NShrKhNM0J",
+					"display_url": "pic.twitter.com/NShrKhNM0J",
+					"expanded_url": "https://twitter.com/solplparty/status/1127473806346465280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 280,
+							"h": 267,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 280,
+							"h": 267,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 280,
+							"h": 267,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 968706450779463700,
+			"id_str": "968706450779463682",
+			"name": "솔플파티",
+			"screen_name": "solplparty",
+			"location": "",
+			"description": "🌈 문화예술 커뮤니티, 솔플파티\n\n공연일정 / 티켓예매 공지 / 페스티벌 / 인디밴드 / 내한공연 / 콘서트 / 홍대문화 / 영화제 / 전시회 / 북토크 / 모임\n\n문화예술정보 @EntRecruitKR\n문의 : contact@solplparty.com",
+			"url": "https://t.co/GCEfwO8QQD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GCEfwO8QQD",
+							"expanded_url": "http://solplparty.com",
+							"display_url": "solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 76,
+			"friends_count": 304,
+			"listed_count": 1,
+			"created_at": "Wed Feb 28 04:36:30 +0000 2018",
+			"favourites_count": 215,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 30749,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/968706450779463682/1519792691",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "【 #플랫폼창동61 】\n[대관프로그램] \"일상에서 즐기는 예술 - 뚱보새의 5월\" : \"일상에서 즐기는 예술\" (2019년 5월 22일(수), 20시)\n▶ https://t.co/dBhQKfsMrK\n　\n#Platform61 #공연일정 #티켓공지\n#솔플파티 #solplparty https://t.co/NShrKhNM0J"
+	},
+	{
 		"created_at": "Sun May 12 05:18:23 +0000 2019",
 		"id": 1127437486991466500,
 		"id_str": "1127442875971788802",
