48d973e4980ed01955e1d21fd00c66c92454ea73
솔플파티 <solplparty@gmail.com>
2019-06-07 23:17:40 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18282,
-	"created_at": "Fri Jun 07 13:17:44 +0000 2019",
+	"tweets": 18283,
+	"created_at": "Fri Jun 07 14:17:38 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,275 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Fri Jun 07 13:19:54 +0000 2019",
+		"id": 1136972393459531800,
+		"id_str": "1136986140815364096",
+		"full_text": "🙌🏻 https://t.co/NhoWQONyQ0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			2
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1136972333237727200,
+					"id_str": "1136972333237727232",
+					"indices": [
+						3,
+						26
+					],
+					"media_url": "http://pbs.twimg.com/media/D8dWUCoU8AAO3x-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8dWUCoU8AAO3x-.jpg",
+					"url": "https://t.co/NhoWQONyQ0",
+					"display_url": "pic.twitter.com/NhoWQONyQ0",
+					"expanded_url": "https://twitter.com/UMFKorea/status/1136972393459531776/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 682,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 682,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1136972333237727200,
+					"id_str": "1136972333237727232",
+					"indices": [
+						3,
+						26
+					],
+					"media_url": "http://pbs.twimg.com/media/D8dWUCoU8AAO3x-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8dWUCoU8AAO3x-.jpg",
+					"url": "https://t.co/NhoWQONyQ0",
+					"display_url": "pic.twitter.com/NhoWQONyQ0",
+					"expanded_url": "https://twitter.com/UMFKorea/status/1136972393459531776/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 682,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 682,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1136972333413851100,
+					"id_str": "1136972333413851137",
+					"indices": [
+						3,
+						26
+					],
+					"media_url": "http://pbs.twimg.com/media/D8dWUDSUYAEl2j0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8dWUDSUYAEl2j0.jpg",
+					"url": "https://t.co/NhoWQONyQ0",
+					"display_url": "pic.twitter.com/NhoWQONyQ0",
+					"expanded_url": "https://twitter.com/UMFKorea/status/1136972393459531776/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 682,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 682,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1136972333560655900,
+					"id_str": "1136972333560655872",
+					"indices": [
+						3,
+						26
+					],
+					"media_url": "http://pbs.twimg.com/media/D8dWUD1UcAABjSq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8dWUD1UcAABjSq.jpg",
+					"url": "https://t.co/NhoWQONyQ0",
+					"display_url": "pic.twitter.com/NhoWQONyQ0",
+					"expanded_url": "https://twitter.com/UMFKorea/status/1136972393459531776/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 682,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 682,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1136972333820702700,
+					"id_str": "1136972333820702720",
+					"indices": [
+						3,
+						26
+					],
+					"media_url": "http://pbs.twimg.com/media/D8dWUEzUcAAd8yQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8dWUEzUcAAd8yQ.jpg",
+					"url": "https://t.co/NhoWQONyQ0",
+					"display_url": "pic.twitter.com/NhoWQONyQ0",
+					"expanded_url": "https://twitter.com/UMFKorea/status/1136972393459531776/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 682,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 682,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 279948669,
+			"id_str": "279948669",
+			"name": "Ultra Korea",
+			"screen_name": "UMFKorea",
+			"location": "Seoul, Korea",
+			"description": "the OFFICIAL Twitter for Ultra Korea",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20312,
+			"friends_count": 24,
+			"listed_count": 123,
+			"created_at": "Sun Apr 10 10:03:04 +0000 2011",
+			"favourites_count": 185,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2234,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1057091149008011266/x_jnAL8B_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1057091149008011266/x_jnAL8B_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/279948669/1557480241",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 15,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "🙌🏻 https://t.co/NhoWQONyQ0"
+	},
+	{
 		"created_at": "Fri Jun 07 13:11:42 +0000 2019",
 		"id": 1136974045587492900,
 		"id_str": "1136984077226168321",
