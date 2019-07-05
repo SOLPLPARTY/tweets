58af780f431daf24a50e4692eccf900c67c8e7bd
솔플파티 <solplparty@gmail.com>
2019-07-05 21:17:37 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20024,
-	"created_at": "Fri Jul 05 11:17:36 +0000 2019",
+	"tweets": 20025,
+	"created_at": "Fri Jul 05 12:17:36 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,215 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Fri Jul 05 12:11:42 +0000 2019",
+		"id": 1147103147648700400,
+		"id_str": "1147115837838508032",
+		"full_text": "7/12(fri) [clubFF - Treasure]\n카프 Calf\n바투 Batu\n티치 Tiech\n더 블랑 The blanc\n먼데이필링 Monday feeling\n너드커넥션 Nerd connection\n오렌지팡팡보이즈 Orange fang fang boys\n\nStart 7:30pm\n*공연 순서는 추후 공개됩니다. https://t.co/cAv9yjwY54",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			175
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1147103136839917600,
+					"id_str": "1147103136839917568",
+					"indices": [
+						176,
+						199
+					],
+					"media_url": "http://pbs.twimg.com/media/D-tUObAUYAAV2mz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-tUObAUYAAV2mz.jpg",
+					"url": "https://t.co/cAv9yjwY54",
+					"display_url": "pic.twitter.com/cAv9yjwY54",
+					"expanded_url": "https://twitter.com/clubFF_/status/1147103147648700416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1147103136839917600,
+					"id_str": "1147103136839917568",
+					"indices": [
+						176,
+						199
+					],
+					"media_url": "http://pbs.twimg.com/media/D-tUObAUYAAV2mz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-tUObAUYAAV2mz.jpg",
+					"url": "https://t.co/cAv9yjwY54",
+					"display_url": "pic.twitter.com/cAv9yjwY54",
+					"expanded_url": "https://twitter.com/clubFF_/status/1147103147648700416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1147103136844111900,
+					"id_str": "1147103136844111873",
+					"indices": [
+						176,
+						199
+					],
+					"media_url": "http://pbs.twimg.com/media/D-tUObBUYAEQFxa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-tUObBUYAEQFxa.jpg",
+					"url": "https://t.co/cAv9yjwY54",
+					"display_url": "pic.twitter.com/cAv9yjwY54",
+					"expanded_url": "https://twitter.com/clubFF_/status/1147103147648700416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 455224965,
+			"id_str": "455224965",
+			"name": "클럽에프에프",
+			"screen_name": "clubFF_",
+			"location": "서울 마포구 서교동 407-8 지하1층",
+			"description": "홍대 최고의 NO.1 Indie experience 라이브 & 댄스클럽 에프에프입니다!! 엪엪의 대관 및 오디션 문의(프로필,음원,연락처 필수)는  010-9025-3407 과 ianbrown@naver.com 으로 .",
+			"url": "https://t.co/dmU2vJP8pS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dmU2vJP8pS",
+							"expanded_url": "http://clubff.modoo.at",
+							"display_url": "clubff.modoo.at",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6836,
+			"friends_count": 7,
+			"listed_count": 100,
+			"created_at": "Wed Jan 04 21:41:48 +0000 2012",
+			"favourites_count": 554,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3450,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2263880419/image_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2263880419/image_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "7/12(fri) [clubFF - Treasure]\n카프 Calf\n바투 Batu\n티치 Tiech\n더 블랑 The blanc\n먼데이필링 Monday feeling\n너드커넥션 Nerd connection\n오렌지팡팡보이즈 Orange fang fang boys\n\nStart 7:30pm\n*공연 순서는 추후 공개됩니다. https://t.co/cAv9yjwY54"
+	},
+	{
 		"created_at": "Fri Jul 05 11:11:45 +0000 2019",
 		"id": 1147099792645869600,
 		"id_str": "1147100748083335168",
