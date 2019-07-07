53b7411a19e52f912405175e13992ebe99bacea2
솔플파티 <solplparty@gmail.com>
2019-07-07 16:17:39 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20058,
-	"created_at": "Sun Jul 07 06:17:36 +0000 2019",
+	"tweets": 20059,
+	"created_at": "Sun Jul 07 07:17:38 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,144 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Sun Jul 07 06:18:26 +0000 2019",
+		"id": 1147745731744112600,
+		"id_str": "1147751708195545089",
+		"full_text": "*권순관 소극장 콘서트\n'변하지 않는 것들'\n일시: 6월 29일 - 7월 14일(금 8시, 토 6시, 일 5시)\n장소: 벨로주 홍대 (서교동 372-6)\n.\n.\n#권순관 #소극장콘서트 #노리플라이 #변하지않는것들 #벨로주 #홍대",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			127
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "권순관",
+					"indices": [
+						90,
+						94
+					]
+				},
+				{
+					"text": "소극장콘서트",
+					"indices": [
+						95,
+						102
+					]
+				},
+				{
+					"text": "노리플라이",
+					"indices": [
+						103,
+						109
+					]
+				},
+				{
+					"text": "변하지않는것들",
+					"indices": [
+						110,
+						118
+					]
+				},
+				{
+					"text": "벨로주",
+					"indices": [
+						119,
+						123
+					]
+				},
+				{
+					"text": "홍대",
+					"indices": [
+						124,
+						127
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1147745726882955300,
+		"in_reply_to_status_id_str": "1147745726882955265",
+		"in_reply_to_user_id": 63414926,
+		"in_reply_to_user_id_str": "63414926",
+		"in_reply_to_screen_name": "HappyRobotKR",
+		"user": {
+			"id": 63414926,
+			"id_str": "63414926",
+			"name": "Happy Robot Records",
+			"screen_name": "HappyRobotKR",
+			"location": "Seoul, Korea",
+			"description": "데이브레이크, 솔루션스(THE SOLUTIONS), 소란, 노리플라이, 쏜애플(THORNAPPLE), 라이프 앤 타임(Life and Time), 램씨(LambC), 설(SURL), 아월(OurR)",
+			"url": "https://t.co/b6z6U7PUtR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/b6z6U7PUtR",
+							"expanded_url": "http://www.happyrobot.co.kr",
+							"display_url": "happyrobot.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 13850,
+			"friends_count": 2799,
+			"listed_count": 306,
+			"created_at": "Thu Aug 06 08:13:58 +0000 2009",
+			"favourites_count": 65,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 12714,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "B2DFDA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1101138907528257537/2sNzE372_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1101138907528257537/2sNzE372_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/63414926/1514732649",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "*권순관 소극장 콘서트\n'변하지 않는 것들'\n일시: 6월 29일 - 7월 14일(금 8시, 토 6시, 일 5시)\n장소: 벨로주 홍대 (서교동 372-6)\n.\n.\n#권순관 #소극장콘서트 #노리플라이 #변하지않는것들 #벨로주 #홍대"
+	},
+	{
 		"created_at": "Sun Jul 07 06:04:35 +0000 2019",
 		"id": 1147748223706173400,
 		"id_str": "1147748223706173442",
