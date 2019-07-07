fef7f8bf6b874698ef3d149363a9d843e6200c72
솔플파티 <solplparty@gmail.com>
2019-07-07 21:17:39 +0900

    Added 1 tweets

--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,121 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Sun Jul 07 12:11:48 +0000 2019",
+		"id": 1147826256047685600,
+		"id_str": "1147840639247278086",
+		"full_text": "유쾌한 모습을 보여주고 있는 더 바코드의 모습을 영상에서 확인해보세요 📹 \n\n🎧BGM: 더 바코드 - Purple Ocean 🎵\n\n페이스북 ▶️https://t.co/rtKXo3JiVC\n유튜브 ▶️https://t.co/uHrvkwFtZt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			133
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/rtKXo3JiVC",
+					"expanded_url": "http://reurl.kr/2C33D682RX",
+					"display_url": "reurl.kr/2C33D682RX",
+					"indices": [
+						80,
+						103
+					]
+				},
+				{
+					"url": "https://t.co/uHrvkwFtZt",
+					"expanded_url": "https://youtu.be/eQeDLsQQXSc",
+					"display_url": "youtu.be/eQeDLsQQXSc",
+					"indices": [
+						110,
+						133
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1147826253015052300,
+		"in_reply_to_status_id_str": "1147826253015052288",
+		"in_reply_to_user_id": 1220731080,
+		"in_reply_to_user_id_str": "1220731080",
+		"in_reply_to_screen_name": "xindieticket",
+		"user": {
+			"id": 1220731080,
+			"id_str": "1220731080",
+			"name": "씬디 티켓라운지",
+			"screen_name": "xindieticket",
+			"location": "서울 마포구 어울마당로 41-1 (홍대주차장길)",
+			"description": "공연 정보를 한 눈에 확인할 수 있는 온&오프라인 티켓 판매처\n(운영시간 : 1PM - 9PM, 저녁시간 6PM - 7PM)\n☎ 02-322-2218",
+			"url": "https://t.co/dqX6O3D21p",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dqX6O3D21p",
+							"expanded_url": "http://xindieticket.kr",
+							"display_url": "xindieticket.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2667,
+			"friends_count": 2566,
+			"listed_count": 22,
+			"created_at": "Tue Feb 26 07:24:04 +0000 2013",
+			"favourites_count": 4360,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 8539,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1062234533746008064/ktqPo5Xl_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1062234533746008064/ktqPo5Xl_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1220731080/1542091439",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "유쾌한 모습을 보여주고 있는 더 바코드의 모습을 영상에서 확인해보세요 📹 \n\n🎧BGM: 더 바코드 - Purple Ocean 🎵\n\n페이스북 ▶️https://t.co/rtKXo3JiVC\n유튜브 ▶️https://t.co/uHrvkwFtZt"
+	},
+	{
 		"created_at": "Sun Jul 07 10:18:16 +0000 2019",
 		"id": 1147807198765912000,
 		"id_str": "1147812066998267905",
