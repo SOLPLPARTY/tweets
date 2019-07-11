54061cecf961d01c4167d1f9598e6a51e6270fc9
솔플파티 <solplparty@gmail.com>
2019-07-12 00:17:42 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20384,
-	"created_at": "Thu Jul 11 14:17:40 +0000 2019",
+	"tweets": 20385,
+	"created_at": "Thu Jul 11 15:17:41 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,10 +1,251 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Thu Jul 11 14:40:40 +0000 2019",
+		"id": 1149327652798500900,
+		"id_str": "1149327652798500864",
+		"full_text": "【 #상상마당 】\n[콘서트] NEW WAVE (2019.07.13~2019.07.13) at 라이브홀\n▶ https://t.co/JxcKXVJc2Z\n　\n#상상마당홍대 #라이브홀 #LIVE_HALL\n#공연일정 #티켓공지 #솔플파티 #solplparty https://t.co/5vA137Ea3w",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			139
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "상상마당",
+					"indices": [
+						2,
+						7
+					]
+				},
+				{
+					"text": "상상마당홍대",
+					"indices": [
+						85,
+						92
+					]
+				},
+				{
+					"text": "라이브홀",
+					"indices": [
+						93,
+						98
+					]
+				},
+				{
+					"text": "LIVE_HALL",
+					"indices": [
+						99,
+						109
+					]
+				},
+				{
+					"text": "공연일정",
+					"indices": [
+						110,
+						115
+					]
+				},
+				{
+					"text": "티켓공지",
+					"indices": [
+						116,
+						121
+					]
+				},
+				{
+					"text": "솔플파티",
+					"indices": [
+						122,
+						127
+					]
+				},
+				{
+					"text": "solplparty",
+					"indices": [
+						128,
+						139
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/JxcKXVJc2Z",
+					"expanded_url": "http://bit.ly/32oOx2C",
+					"display_url": "bit.ly/32oOx2C",
+					"indices": [
+						59,
+						82
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1149327651791933400,
+					"id_str": "1149327651791933440",
+					"indices": [
+						140,
+						163
+					],
+					"media_url": "http://pbs.twimg.com/media/D_M7aO-XkAAQ9KI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D_M7aO-XkAAQ9KI.jpg",
+					"url": "https://t.co/5vA137Ea3w",
+					"display_url": "pic.twitter.com/5vA137Ea3w",
+					"expanded_url": "https://twitter.com/solplparty/status/1149327652798500864/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 400,
+							"h": 568,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 400,
+							"h": 568,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 400,
+							"h": 568,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1149327651791933400,
+					"id_str": "1149327651791933440",
+					"indices": [
+						140,
+						163
+					],
+					"media_url": "http://pbs.twimg.com/media/D_M7aO-XkAAQ9KI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D_M7aO-XkAAQ9KI.jpg",
+					"url": "https://t.co/5vA137Ea3w",
+					"display_url": "pic.twitter.com/5vA137Ea3w",
+					"expanded_url": "https://twitter.com/solplparty/status/1149327652798500864/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 400,
+							"h": 568,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 400,
+							"h": 568,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 400,
+							"h": 568,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 968706450779463700,
+			"id_str": "968706450779463682",
+			"name": "솔플파티",
+			"screen_name": "solplparty",
+			"location": "",
+			"description": "🌈 문화예술 커뮤니티, 솔플파티\n\n공연일정 / 티켓예매 공지 / 페스티벌 / 인디밴드 / 내한공연 / 콘서트 / 홍대문화 / 영화제 / 전시회 / 북토크 / 모임\n\n문화예술정보 @EntRecruitKR\n문의 : contact@solplparty.com",
+			"url": "https://t.co/GCEfwO8QQD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GCEfwO8QQD",
+							"expanded_url": "http://solplparty.com",
+							"display_url": "solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 96,
+			"friends_count": 308,
+			"listed_count": 1,
+			"created_at": "Wed Feb 28 04:36:30 +0000 2018",
+			"favourites_count": 214,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 35229,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/968706450779463682/1519792691",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "【 #상상마당 】\n[콘서트] NEW WAVE (2019.07.13~2019.07.13) at 라이브홀\n▶ https://t.co/JxcKXVJc2Z\n　\n#상상마당홍대 #라이브홀 #LIVE_HALL\n#공연일정 #티켓공지 #솔플파티 #solplparty https://t.co/5vA137Ea3w"
+	},
+	{
 		"created_at": "Thu Jul 11 14:11:42 +0000 2019",
 		"id": 1149310086835101700,
 		"id_str": "1149320363744083970",
-		"full_text": "#십란한밤 #로고송비디오\n십란한밤 프로필 촬영 현장 비하인드가 담겨있는 로고송 비디오가 나왔어요❤️\n\n권정열&amp;고영배 - 나가 (십란한밤 로고송) \n뮤직비디오(?) 많이 봐주세요🙇🏻‍♀️\n#권정열 #고영배 #캐스퍼라디오 \nhttps://t.co/XtFmAwcw7m",
+		"full_text": "#십란한밤 #로고송비디오\n십란한밤 프로필 촬영 현장 비하인드가 담겨있는 로고송 비디오가 나왔어요❤️\n\n권정열&amp;고영배 - 나가 (십란한밤 로고송) \n뮤직비디오(?) 많이 봐주세요🙇🏻♀️\n#권정열 #고영배 #캐스퍼라디오 \nhttps://t.co/XtFmAwcw7m",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -139,7 +380,7 @@ Grailbird.data.tweets_2019_07 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "#십란한밤 #로고송비디오\n십란한밤 프로필 촬영 현장 비하인드가 담겨있는 로고송 비디오가 나왔어요❤️\n\n권정열&amp;고영배 - 나가 (십란한밤 로고송) \n뮤직비디오(?) 많이 봐주세요🙇🏻‍♀️\n#권정열 #고영배 #캐스퍼라디오 \nhttps://t.co/XtFmAwcw7m"
+		"text": "#십란한밤 #로고송비디오\n십란한밤 프로필 촬영 현장 비하인드가 담겨있는 로고송 비디오가 나왔어요❤️\n\n권정열&amp;고영배 - 나가 (십란한밤 로고송) \n뮤직비디오(?) 많이 봐주세요🙇🏻♀️\n#권정열 #고영배 #캐스퍼라디오 \nhttps://t.co/XtFmAwcw7m"
 	},
 	{
 		"created_at": "Thu Jul 11 13:29:49 +0000 2019",
