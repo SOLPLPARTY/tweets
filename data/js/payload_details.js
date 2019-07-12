8309e4794a514d64c61cabbe31023692a2a3b2c2
솔플파티 <solplparty@gmail.com>
2019-07-12 21:17:41 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20454,
-	"created_at": "Fri Jul 12 11:17:42 +0000 2019",
+	"tweets": 20455,
+	"created_at": "Fri Jul 12 12:17:40 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,240 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Fri Jul 12 11:18:17 +0000 2019",
+		"id": 1149634048030994400,
+		"id_str": "1149639106948788224",
+		"full_text": "[#아마도이자람밴드] 오늘 제주 함덕해수욕장에서 진행된 &lt;2019 스테핑 스톤 페스티벌&gt; 아마도이자람밴드의 무대 즐거우셨나요?🏝\n아마도이자람밴드 멤버들은 정말 재미있게 즐기고 왔답니다✨\n이번주 일요일 단독 공연 &lt;칵테일&gt;에서 다시 만나요!\n-\n#아마도이자람밴드 #제주 #스테핑스톤페스티벌 https://t.co/qS6qorJIlh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			172
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "아마도이자람밴드",
+					"indices": [
+						1,
+						10
+					]
+				},
+				{
+					"text": "아마도이자람밴드",
+					"indices": [
+						148,
+						157
+					]
+				},
+				{
+					"text": "제주",
+					"indices": [
+						158,
+						161
+					]
+				},
+				{
+					"text": "스테핑스톤페스티벌",
+					"indices": [
+						162,
+						172
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1149633958293864400,
+					"id_str": "1149633958293864448",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1149633958293864448/pu/img/DNd9vFSzGsxFRQZz.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1149633958293864448/pu/img/DNd9vFSzGsxFRQZz.jpg",
+					"url": "https://t.co/qS6qorJIlh",
+					"display_url": "pic.twitter.com/qS6qorJIlh",
+					"expanded_url": "https://twitter.com/YourSummerKR/status/1149634048030994433/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1149633958293864400,
+					"id_str": "1149633958293864448",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1149633958293864448/pu/img/DNd9vFSzGsxFRQZz.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1149633958293864448/pu/img/DNd9vFSzGsxFRQZz.jpg",
+					"url": "https://t.co/qS6qorJIlh",
+					"display_url": "pic.twitter.com/qS6qorJIlh",
+					"expanded_url": "https://twitter.com/YourSummerKR/status/1149634048030994433/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 60620,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1149633958293864448/pu/vid/480x270/uC94Lk5euGOiafu3.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1149633958293864448/pu/vid/640x360/BaVUug_LpG9Nwa8L.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1149633958293864448/pu/pl/uxeAZjFTzNUWLQf1.m3u8?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1149633958293864448/pu/vid/960x540/m3Ee9qzOjPW9grQM.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 404720294,
+			"id_str": "404720294",
+			"name": "당신의 여름",
+			"screen_name": "YourSummerKR",
+			"location": "한국",
+			"description": "당신의 여름(Your Summer)은 가을방학, 계피, 김사월, 김사월X김해원, 랄라스윗, 못(Mot), 성진환, 아마도이자람밴드, 오지은, 우주왕복선싸이드미러, 우주히피, 이지형, 이하윤, 전기뱀장어, 최낙타, 홍이삭과 함께합니다.",
+			"url": "https://t.co/O7n52oEN1K",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/O7n52oEN1K",
+							"expanded_url": "http://www.instagram.com/yoursummernet",
+							"display_url": "instagram.com/yoursummernet",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1517,
+			"friends_count": 25,
+			"listed_count": 11,
+			"created_at": "Fri Nov 04 09:50:32 +0000 2011",
+			"favourites_count": 237,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3853,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/978966061050609669/wmNssQkU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/978966061050609669/wmNssQkU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/404720294/1522238671",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#아마도이자람밴드] 오늘 제주 함덕해수욕장에서 진행된 &lt;2019 스테핑 스톤 페스티벌&gt; 아마도이자람밴드의 무대 즐거우셨나요?🏝\n아마도이자람밴드 멤버들은 정말 재미있게 즐기고 왔답니다✨\n이번주 일요일 단독 공연 &lt;칵테일&gt;에서 다시 만나요!\n-\n#아마도이자람밴드 #제주 #스테핑스톤페스티벌 https://t.co/qS6qorJIlh"
+	},
+	{
 		"created_at": "Fri Jul 12 11:05:22 +0000 2019",
 		"id": 1149635857785057300,
 		"id_str": "1149635857785057281",
