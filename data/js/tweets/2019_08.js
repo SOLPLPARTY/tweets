2287ca60d94b405181ef8d6c51461cb8dae4408e
솔플파티 <solplparty@gmail.com>
2019-08-03 23:17:36 +0900

    Added 1 tweets

--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,238 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Sat Aug 03 13:18:20 +0000 2019",
+		"id": 1157640718866718700,
+		"id_str": "1157641855439388672",
+		"full_text": "[계피] ‘클라우드x열정도’ 스페셜데이 🎤 더운 날씨에도 너무 좋은 무대였어요 ♥️ 함께해주신 분들 고맙습니다 :) 다음에 또 만나요! \n-\n#계피 #열정도 #클라우드 https://t.co/pjlZBP6AkP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			93
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "계피",
+					"indices": [
+						79,
+						82
+					]
+				},
+				{
+					"text": "열정도",
+					"indices": [
+						83,
+						87
+					]
+				},
+				{
+					"text": "클라우드",
+					"indices": [
+						88,
+						93
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1157640700952842200,
+					"id_str": "1157640700952842240",
+					"indices": [
+						94,
+						117
+					],
+					"media_url": "http://pbs.twimg.com/media/EBDEFYXUEAAMLdp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBDEFYXUEAAMLdp.jpg",
+					"url": "https://t.co/pjlZBP6AkP",
+					"display_url": "pic.twitter.com/pjlZBP6AkP",
+					"expanded_url": "https://twitter.com/YourSummerKR/status/1157640718866718721/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1157640700952842200,
+					"id_str": "1157640700952842240",
+					"indices": [
+						94,
+						117
+					],
+					"media_url": "http://pbs.twimg.com/media/EBDEFYXUEAAMLdp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBDEFYXUEAAMLdp.jpg",
+					"url": "https://t.co/pjlZBP6AkP",
+					"display_url": "pic.twitter.com/pjlZBP6AkP",
+					"expanded_url": "https://twitter.com/YourSummerKR/status/1157640718866718721/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1157640700973838300,
+					"id_str": "1157640700973838337",
+					"indices": [
+						94,
+						117
+					],
+					"media_url": "http://pbs.twimg.com/media/EBDEFYcUcAE4E-X.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBDEFYcUcAE4E-X.jpg",
+					"url": "https://t.co/pjlZBP6AkP",
+					"display_url": "pic.twitter.com/pjlZBP6AkP",
+					"expanded_url": "https://twitter.com/YourSummerKR/status/1157640718866718721/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 404720294,
+			"id_str": "404720294",
+			"name": "당신의 여름",
+			"screen_name": "YourSummerKR",
+			"location": "한국",
+			"description": "당신의 여름(Your Summer)은 가을방학, 계피, 김사월, 김사월X김해원, 랄라스윗, 못(Mot), 성진환, 아마도이자람밴드, 오지은, 우주왕복선싸이드미러, 우주히피, 이지형, 이하윤, 전기뱀장어, 최낙타, 홍이삭과 함께합니다.",
+			"url": "https://t.co/O7n52oEN1K",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/O7n52oEN1K",
+							"expanded_url": "http://www.instagram.com/yoursummernet",
+							"display_url": "instagram.com/yoursummernet",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1576,
+			"friends_count": 25,
+			"listed_count": 11,
+			"created_at": "Fri Nov 04 09:50:32 +0000 2011",
+			"favourites_count": 267,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3947,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/978966061050609669/wmNssQkU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/978966061050609669/wmNssQkU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/404720294/1522238671",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[계피] ‘클라우드x열정도’ 스페셜데이 🎤 더운 날씨에도 너무 좋은 무대였어요 ♥️ 함께해주신 분들 고맙습니다 :) 다음에 또 만나요! \n-\n#계피 #열정도 #클라우드 https://t.co/pjlZBP6AkP"
+	},
+	{
 		"created_at": "Sat Aug 03 13:13:29 +0000 2019",
 		"id": 1157634482553274400,
 		"id_str": "1157640635106701313",
