2a73ece7eeb33adc7fd0dac6c3cb8c5a570d5617
솔플파티 <solplparty@gmail.com>
2019-08-05 06:17:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 21906,
-	"created_at": "Sun Aug 04 18:17:35 +0000 2019",
+	"tweets": 21907,
+	"created_at": "Sun Aug 04 21:17:35 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,233 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Sun Aug 04 20:36:39 +0000 2019",
+		"id": 1158114546290241500,
+		"id_str": "1158114546290241536",
+		"full_text": "【 #PlayDB 】 #올림픽공원\n2019 케이월드 페스타 - 케이힙합 콘서트 (2019.08.21 ~ 2019.08.21) at 올림픽공원 SK올림픽핸드볼경기장 (구 펜싱경기장)\n▶ https://t.co/t06WJpGlOP\n　\n#플레이DB #플레이디비\n#공연일정 #티켓공지 https://t.co/Nf0UTDkcd3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			154
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "PlayDB",
+					"indices": [
+						2,
+						9
+					]
+				},
+				{
+					"text": "올림픽공원",
+					"indices": [
+						12,
+						18
+					]
+				},
+				{
+					"text": "플레이DB",
+					"indices": [
+						129,
+						135
+					]
+				},
+				{
+					"text": "플레이디비",
+					"indices": [
+						136,
+						142
+					]
+				},
+				{
+					"text": "공연일정",
+					"indices": [
+						143,
+						148
+					]
+				},
+				{
+					"text": "티켓공지",
+					"indices": [
+						149,
+						154
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/t06WJpGlOP",
+					"expanded_url": "http://bit.ly/336oo93",
+					"display_url": "bit.ly/336oo93",
+					"indices": [
+						103,
+						126
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1158114544729960400,
+					"id_str": "1158114544729960451",
+					"indices": [
+						155,
+						178
+					],
+					"media_url": "http://pbs.twimg.com/media/EBJzCt_XsAMsR0W.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBJzCt_XsAMsR0W.jpg",
+					"url": "https://t.co/Nf0UTDkcd3",
+					"display_url": "pic.twitter.com/Nf0UTDkcd3",
+					"expanded_url": "https://twitter.com/solplparty/status/1158114546290241536/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 90,
+							"h": 110,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 90,
+							"h": 90,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 90,
+							"h": 110,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 90,
+							"h": 110,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1158114544729960400,
+					"id_str": "1158114544729960451",
+					"indices": [
+						155,
+						178
+					],
+					"media_url": "http://pbs.twimg.com/media/EBJzCt_XsAMsR0W.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBJzCt_XsAMsR0W.jpg",
+					"url": "https://t.co/Nf0UTDkcd3",
+					"display_url": "pic.twitter.com/Nf0UTDkcd3",
+					"expanded_url": "https://twitter.com/solplparty/status/1158114546290241536/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 90,
+							"h": 110,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 90,
+							"h": 90,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 90,
+							"h": 110,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 90,
+							"h": 110,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 968706450779463700,
+			"id_str": "968706450779463682",
+			"name": "솔플파티",
+			"screen_name": "solplparty",
+			"location": "",
+			"description": "🌈 문화예술 커뮤니티, 솔플파티\n\n공연일정 / 티켓예매 공지 / 페스티벌 / 인디밴드 / 내한공연 / 콘서트 / 홍대문화 / 영화제 / 전시회 / 북토크 / 모임\n\n문화예술정보 @EntRecruitKR\n문의 : contact@solplparty.com",
+			"url": "https://t.co/GCEfwO8QQD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GCEfwO8QQD",
+							"expanded_url": "http://solplparty.com",
+							"display_url": "solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 116,
+			"friends_count": 314,
+			"listed_count": 1,
+			"created_at": "Wed Feb 28 04:36:30 +0000 2018",
+			"favourites_count": 270,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 36977,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/968706450779463682/1519792691",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "【 #PlayDB 】 #올림픽공원\n2019 케이월드 페스타 - 케이힙합 콘서트 (2019.08.21 ~ 2019.08.21) at 올림픽공원 SK올림픽핸드볼경기장 (구 펜싱경기장)\n▶ https://t.co/t06WJpGlOP\n　\n#플레이DB #플레이디비\n#공연일정 #티켓공지 https://t.co/Nf0UTDkcd3"
+	},
+	{
 		"created_at": "Sun Aug 04 17:48:45 +0000 2019",
 		"id": 1158072294591336400,
 		"id_str": "1158072294591336448",
