98fc97bff67e3f02bb929f14d4813707b1e13271
솔플파티 <solplparty@gmail.com>
2019-08-10 22:17:39 +0900

    Added 1 tweets

--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,238 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Sat Aug 10 12:18:16 +0000 2019",
+		"id": 1160156936345510000,
+		"id_str": "1160163454042497024",
+		"full_text": "[최낙타] 오늘 저녁, 롤링홀에서 진행된 &lt;ROLLING HOT SUMMER MUSIC PARTY&gt;와 함께했어요🍑 무더운 날씨에도 함께 즐겨주신 분들 모두 감사합니다 :) 즐거운 여름 밤 되었길 바래요!🔥\n-\n#최낙타 #롤링홀 #롤링핫썸머뮤직파티 https://t.co/QoZtr8B2Ok",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			143
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "최낙타",
+					"indices": [
+						123,
+						127
+					]
+				},
+				{
+					"text": "롤링홀",
+					"indices": [
+						128,
+						132
+					]
+				},
+				{
+					"text": "롤링핫썸머뮤직파티",
+					"indices": [
+						133,
+						143
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1160156902346518500,
+					"id_str": "1160156902346518528",
+					"indices": [
+						144,
+						167
+					],
+					"media_url": "http://pbs.twimg.com/media/EBm0jlAU8AA9MT9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBm0jlAU8AA9MT9.jpg",
+					"url": "https://t.co/QoZtr8B2Ok",
+					"display_url": "pic.twitter.com/QoZtr8B2Ok",
+					"expanded_url": "https://twitter.com/YourSummerKR/status/1160156936345509889/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1160156902346518500,
+					"id_str": "1160156902346518528",
+					"indices": [
+						144,
+						167
+					],
+					"media_url": "http://pbs.twimg.com/media/EBm0jlAU8AA9MT9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBm0jlAU8AA9MT9.jpg",
+					"url": "https://t.co/QoZtr8B2Ok",
+					"display_url": "pic.twitter.com/QoZtr8B2Ok",
+					"expanded_url": "https://twitter.com/YourSummerKR/status/1160156936345509889/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1160156902350659600,
+					"id_str": "1160156902350659586",
+					"indices": [
+						144,
+						167
+					],
+					"media_url": "http://pbs.twimg.com/media/EBm0jlBUIAI6iuw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBm0jlBUIAI6iuw.jpg",
+					"url": "https://t.co/QoZtr8B2Ok",
+					"display_url": "pic.twitter.com/QoZtr8B2Ok",
+					"expanded_url": "https://twitter.com/YourSummerKR/status/1160156936345509889/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 404720294,
+			"id_str": "404720294",
+			"name": "당신의 여름",
+			"screen_name": "YourSummerKR",
+			"location": "한국",
+			"description": "당신의 여름(Your Summer)은 가을방학, 계피, 김사월, 김사월X김해원, 랄라스윗, 못(Mot), 성진환, 아마도이자람밴드, 오지은, 우주왕복선싸이드미러, 우주히피, 이지형, 이하윤, 전기뱀장어, 최낙타, 홍이삭과 함께합니다.",
+			"url": "https://t.co/O7n52oEN1K",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/O7n52oEN1K",
+							"expanded_url": "http://www.instagram.com/yoursummernet",
+							"display_url": "instagram.com/yoursummernet",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1592,
+			"friends_count": 25,
+			"listed_count": 11,
+			"created_at": "Fri Nov 04 09:50:32 +0000 2011",
+			"favourites_count": 269,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3972,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/978966061050609669/wmNssQkU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/978966061050609669/wmNssQkU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/404720294/1522238671",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[최낙타] 오늘 저녁, 롤링홀에서 진행된 &lt;ROLLING HOT SUMMER MUSIC PARTY&gt;와 함께했어요🍑 무더운 날씨에도 함께 즐겨주신 분들 모두 감사합니다 :) 즐거운 여름 밤 되었길 바래요!🔥\n-\n#최낙타 #롤링홀 #롤링핫썸머뮤직파티 https://t.co/QoZtr8B2Ok"
+	},
+	{
 		"created_at": "Sat Aug 10 11:13:27 +0000 2019",
 		"id": 1160136153657311200,
 		"id_str": "1160147141731463169",
