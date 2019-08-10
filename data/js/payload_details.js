f67c873a9c584385d91800e6e4858bc0a9ee0ef5
솔플파티 <solplparty@gmail.com>
2019-08-10 20:17:41 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 22418,
-	"created_at": "Sat Aug 10 10:17:39 +0000 2019",
+	"tweets": 22419,
+	"created_at": "Sat Aug 10 11:17:39 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,288 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Sat Aug 10 11:13:27 +0000 2019",
+		"id": 1160136153657311200,
+		"id_str": "1160147141731463169",
+		"full_text": "[신규입고]\n📎 엔분의일 슬로건\n밴드 이름 의미에 '우리 젊음을 나눠 먹자 엔분의일로' 라고 담겨있는 엔분의일의 슬로건이 입고되었습니다~! 🙌\n더운 날씨인데도 직접 가져다주신 팬분들께 정말 감사드립니다!🙏\n젊음이 담긴 엔분의일 슬로건을 씬디에서 느껴보세요!💟 https://t.co/ozODx9QIV4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			144
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1160136132664807400,
+					"id_str": "1160136132664807424",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/EBmhqn6U0AASV2K.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBmhqn6U0AASV2K.jpg",
+					"url": "https://t.co/ozODx9QIV4",
+					"display_url": "pic.twitter.com/ozODx9QIV4",
+					"expanded_url": "https://twitter.com/xindieticket/status/1160136153657311233/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1160136132664807400,
+					"id_str": "1160136132664807424",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/EBmhqn6U0AASV2K.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBmhqn6U0AASV2K.jpg",
+					"url": "https://t.co/ozODx9QIV4",
+					"display_url": "pic.twitter.com/ozODx9QIV4",
+					"expanded_url": "https://twitter.com/xindieticket/status/1160136153657311233/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1160136132673192000,
+					"id_str": "1160136132673191936",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/EBmhqn8UwAAL__S.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBmhqn8UwAAL__S.jpg",
+					"url": "https://t.co/ozODx9QIV4",
+					"display_url": "pic.twitter.com/ozODx9QIV4",
+					"expanded_url": "https://twitter.com/xindieticket/status/1160136153657311233/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1160136132664811500,
+					"id_str": "1160136132664811521",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/EBmhqn6U4AEFHQt.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBmhqn6U4AEFHQt.jpg",
+					"url": "https://t.co/ozODx9QIV4",
+					"display_url": "pic.twitter.com/ozODx9QIV4",
+					"expanded_url": "https://twitter.com/xindieticket/status/1160136153657311233/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1160136132669014000,
+					"id_str": "1160136132669014016",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/EBmhqn7VAAA7Pdm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBmhqn7VAAA7Pdm.jpg",
+					"url": "https://t.co/ozODx9QIV4",
+					"display_url": "pic.twitter.com/ozODx9QIV4",
+					"expanded_url": "https://twitter.com/xindieticket/status/1160136153657311233/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1220731080,
+			"id_str": "1220731080",
+			"name": "씬디 티켓라운지",
+			"screen_name": "xindieticket",
+			"location": "서울 마포구 어울마당로 41-1 (홍대주차장길)",
+			"description": "공연 정보를 한 눈에 확인할 수 있는 온오프라인 티켓 판매처\n(운영시간 : 1-9PM, 저녁시간 : 6-7PM)\n☎ 02-322-2218",
+			"url": "https://t.co/dqX6O3D21p",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dqX6O3D21p",
+							"expanded_url": "http://xindieticket.kr",
+							"display_url": "xindieticket.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2683,
+			"friends_count": 2574,
+			"listed_count": 23,
+			"created_at": "Tue Feb 26 07:24:04 +0000 2013",
+			"favourites_count": 4459,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 8615,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1062234533746008064/ktqPo5Xl_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1062234533746008064/ktqPo5Xl_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1220731080/1542091439",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[신규입고]\n📎 엔분의일 슬로건\n밴드 이름 의미에 '우리 젊음을 나눠 먹자 엔분의일로' 라고 담겨있는 엔분의일의 슬로건이 입고되었습니다~! 🙌\n더운 날씨인데도 직접 가져다주신 팬분들께 정말 감사드립니다!🙏\n젊음이 담긴 엔분의일 슬로건을 씬디에서 느껴보세요!💟 https://t.co/ozODx9QIV4"
+	},
+	{
 		"created_at": "Sat Aug 10 09:18:41 +0000 2019",
 		"id": 1160115279805812700,
 		"id_str": "1160118257451487232",
