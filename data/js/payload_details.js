af6d32a02a87c220ed5192c80b93b2953d7ff8ee
솔플파티 <solplparty@gmail.com>
2019-09-13 21:17:42 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 24500,
-	"created_at": "Fri Sep 13 09:17:41 +0000 2019",
+	"tweets": 24501,
+	"created_at": "Fri Sep 13 12:17:41 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,215 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Fri Sep 13 12:13:33 +0000 2019",
+		"id": 1172482584405667800,
+		"id_str": "1172483451519459328",
+		"full_text": "9/22(sat) [clubFF - Learn to fry]\n뷰렛 Biuret\nABTB\n묘리 Myori\n버스터즈 Bursters\n더 사운드 The Sound\n플라잉독 Flying dog\n\nstart 8:00pm\n*공연 순서는 추후 공개됩니다. https://t.co/JvWiGLfoMY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			135
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1172482574083428400,
+					"id_str": "1172482574083428352",
+					"indices": [
+						136,
+						159
+					],
+					"media_url": "http://pbs.twimg.com/media/EEV-sGWUEAAsJhH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EEV-sGWUEAAsJhH.jpg",
+					"url": "https://t.co/JvWiGLfoMY",
+					"display_url": "pic.twitter.com/JvWiGLfoMY",
+					"expanded_url": "https://twitter.com/clubFF_/status/1172482584405667840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1172482574083428400,
+					"id_str": "1172482574083428352",
+					"indices": [
+						136,
+						159
+					],
+					"media_url": "http://pbs.twimg.com/media/EEV-sGWUEAAsJhH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EEV-sGWUEAAsJhH.jpg",
+					"url": "https://t.co/JvWiGLfoMY",
+					"display_url": "pic.twitter.com/JvWiGLfoMY",
+					"expanded_url": "https://twitter.com/clubFF_/status/1172482584405667840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1172482574091862000,
+					"id_str": "1172482574091862016",
+					"indices": [
+						136,
+						159
+					],
+					"media_url": "http://pbs.twimg.com/media/EEV-sGYUwAAAI9v.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EEV-sGYUwAAAI9v.jpg",
+					"url": "https://t.co/JvWiGLfoMY",
+					"display_url": "pic.twitter.com/JvWiGLfoMY",
+					"expanded_url": "https://twitter.com/clubFF_/status/1172482584405667840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 455224965,
+			"id_str": "455224965",
+			"name": "클럽에프에프",
+			"screen_name": "clubFF_",
+			"location": "서울 마포구 서교동 407-8 지하1층",
+			"description": "홍대 최고의 NO.1 Indie experience 라이브 & 댄스클럽 에프에프입니다!! 엪엪의 대관 및 오디션 문의(프로필,음원,연락처 필수)는  010-9025-3407 과 ianbrown@naver.com 으로 .",
+			"url": "https://t.co/dmU2vJP8pS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dmU2vJP8pS",
+							"expanded_url": "http://clubff.modoo.at",
+							"display_url": "clubff.modoo.at",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6723,
+			"friends_count": 7,
+			"listed_count": 98,
+			"created_at": "Wed Jan 04 21:41:48 +0000 2012",
+			"favourites_count": 553,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3510,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2263880419/image_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2263880419/image_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "9/22(sat) [clubFF - Learn to fry]\n뷰렛 Biuret\nABTB\n묘리 Myori\n버스터즈 Bursters\n더 사운드 The Sound\n플라잉독 Flying dog\n\nstart 8:00pm\n*공연 순서는 추후 공개됩니다. https://t.co/JvWiGLfoMY"
+	},
+	{
 		"created_at": "Fri Sep 13 09:04:39 +0000 2019",
 		"id": 1172435914305392600,
 		"id_str": "1172435914305392640",
