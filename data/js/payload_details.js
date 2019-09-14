c33331652a6b119ce437886630fdc7dce99a1619
솔플파티 <solplparty@gmail.com>
2019-09-14 21:17:42 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 24516,
-	"created_at": "Sat Sep 14 09:17:40 +0000 2019",
+	"tweets": 24517,
+	"created_at": "Sat Sep 14 12:17:40 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,215 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Sat Sep 14 12:13:27 +0000 2019",
+		"id": 1172837371139821600,
+		"id_str": "1172845817243324417",
+		"full_text": "9/22(sun) [clubFF - There's honey]\n헤이맨 Hey men\n엔분의일 1/n\nF밴드 F band\n후디니 Houdini\n원디비 1DB\n\nstart 6:00pm\n*공연 순서는 추후 공개됩니다. https://t.co/5G2N8KzbFU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			118
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1172837362151260200,
+					"id_str": "1172837362151260160",
+					"indices": [
+						119,
+						142
+					],
+					"media_url": "http://pbs.twimg.com/media/EEbBXe5VAAAyP5V.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EEbBXe5VAAAyP5V.jpg",
+					"url": "https://t.co/5G2N8KzbFU",
+					"display_url": "pic.twitter.com/5G2N8KzbFU",
+					"expanded_url": "https://twitter.com/clubFF_/status/1172837371139821569/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1172837362151260200,
+					"id_str": "1172837362151260160",
+					"indices": [
+						119,
+						142
+					],
+					"media_url": "http://pbs.twimg.com/media/EEbBXe5VAAAyP5V.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EEbBXe5VAAAyP5V.jpg",
+					"url": "https://t.co/5G2N8KzbFU",
+					"display_url": "pic.twitter.com/5G2N8KzbFU",
+					"expanded_url": "https://twitter.com/clubFF_/status/1172837371139821569/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1172837362151215000,
+					"id_str": "1172837362151215104",
+					"indices": [
+						119,
+						142
+					],
+					"media_url": "http://pbs.twimg.com/media/EEbBXe5UUAAN0S8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EEbBXe5UUAAN0S8.jpg",
+					"url": "https://t.co/5G2N8KzbFU",
+					"display_url": "pic.twitter.com/5G2N8KzbFU",
+					"expanded_url": "https://twitter.com/clubFF_/status/1172837371139821569/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 455224965,
+			"id_str": "455224965",
+			"name": "클럽에프에프",
+			"screen_name": "clubFF_",
+			"location": "서울 마포구 서교동 407-8 지하1층",
+			"description": "홍대 최고의 NO.1 Indie experience 라이브 & 댄스클럽 에프에프입니다!! 엪엪의 대관 및 오디션 문의(프로필,음원,연락처 필수)는  010-9025-3407 과 ianbrown@naver.com 으로 .",
+			"url": "https://t.co/dmU2vJP8pS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dmU2vJP8pS",
+							"expanded_url": "http://clubff.modoo.at",
+							"display_url": "clubff.modoo.at",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6720,
+			"friends_count": 7,
+			"listed_count": 98,
+			"created_at": "Wed Jan 04 21:41:48 +0000 2012",
+			"favourites_count": 553,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3512,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2263880419/image_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2263880419/image_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "9/22(sun) [clubFF - There's honey]\n헤이맨 Hey men\n엔분의일 1/n\nF밴드 F band\n후디니 Houdini\n원디비 1DB\n\nstart 6:00pm\n*공연 순서는 추후 공개됩니다. https://t.co/5G2N8KzbFU"
+	},
+	{
 		"created_at": "Sat Sep 14 09:13:17 +0000 2019",
 		"id": 1172800474082369500,
 		"id_str": "1172800474082369536",
