4d679d849b0ed19408f6c6515bd289ad2b6312a8
솔플파티 <solplparty@gmail.com>
2019-09-14 15:17:42 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 24510,
-	"created_at": "Sat Sep 14 05:17:43 +0000 2019",
+	"tweets": 24511,
+	"created_at": "Sat Sep 14 06:17:41 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,144 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Sat Sep 14 05:18:18 +0000 2019",
+		"id": 1172730140079476700,
+		"id_str": "1172741341564026881",
+		"full_text": "브이앱 혹은 유투브/아리랑라디오 앱으로 청취 가능해요 🙂\n\n• V app\n➡️ Search for “Arirang radio”\n➡️ “Music Access” click\n\n• Youtube, Arirang radio app\n➡️ 3시부터 시청 가능\n-\n#홍이삭 #musicaccess #arirangradio #뮤직엑세스 #아리랑라디오 #isaachong",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			198
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "홍이삭",
+					"indices": [
+						141,
+						145
+					]
+				},
+				{
+					"text": "musicaccess",
+					"indices": [
+						146,
+						158
+					]
+				},
+				{
+					"text": "arirangradio",
+					"indices": [
+						159,
+						172
+					]
+				},
+				{
+					"text": "뮤직엑세스",
+					"indices": [
+						173,
+						179
+					]
+				},
+				{
+					"text": "아리랑라디오",
+					"indices": [
+						180,
+						187
+					]
+				},
+				{
+					"text": "isaachong",
+					"indices": [
+						188,
+						198
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1172730107066118100,
+		"in_reply_to_status_id_str": "1172730107066118147",
+		"in_reply_to_user_id": 404720294,
+		"in_reply_to_user_id_str": "404720294",
+		"in_reply_to_screen_name": "YourSummerKR",
+		"user": {
+			"id": 404720294,
+			"id_str": "404720294",
+			"name": "당신의 여름",
+			"screen_name": "YourSummerKR",
+			"location": "한국",
+			"description": "당신의 여름(Your Summer)은 가을방학, 계피, 김사월, 김사월X김해원, 랄라스윗, 못(Mot), 성진환, 아마도이자람밴드, 오지은, 우주왕복선싸이드미러, 우주히피, 이지형, 이하윤, 전기뱀장어, 최낙타, 홍이삭과 함께합니다.",
+			"url": "https://t.co/O7n52oEN1K",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/O7n52oEN1K",
+							"expanded_url": "http://www.instagram.com/yoursummernet",
+							"display_url": "instagram.com/yoursummernet",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1683,
+			"friends_count": 25,
+			"listed_count": 12,
+			"created_at": "Fri Nov 04 09:50:32 +0000 2011",
+			"favourites_count": 273,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4122,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/978966061050609669/wmNssQkU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/978966061050609669/wmNssQkU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/404720294/1522238671",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "브이앱 혹은 유투브/아리랑라디오 앱으로 청취 가능해요 🙂\n\n• V app\n➡️ Search for “Arirang radio”\n➡️ “Music Access” click\n\n• Youtube, Arirang radio app\n➡️ 3시부터 시청 가능\n-\n#홍이삭 #musicaccess #arirangradio #뮤직엑세스 #아리랑라디오 #isaachong"
+	},
+	{
 		"created_at": "Sat Sep 14 04:19:54 +0000 2019",
 		"id": 1172715024915128300,
 		"id_str": "1172726644236308480",
