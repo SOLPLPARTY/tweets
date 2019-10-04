ea0dd90403e7a8edf67a4ecf15f584f208f7b6b8
솔플파티 <solplparty@gmail.com>
2019-10-04 23:17:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 25997,
-	"created_at": "Fri Oct 04 13:17:37 +0000 2019",
+	"tweets": 25998,
+	"created_at": "Fri Oct 04 14:17:36 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,209 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Fri Oct 04 13:18:16 +0000 2019",
+		"id": 1180096775916408800,
+		"id_str": "1180109886350397440",
+		"full_text": "#우주왕복선싸이드미러 EP [고민은 배송을 늦출 뿐] D-7 \n\n장거리에서 연인뿐만 아니라 사람 간의 마음을 전하는 택배는 그 이름만으로도 설렘입니다.\n택배는 사랑 아닐까요?\n\n우주왕복선싸이드미러 EP [고민은 배송을 늦출 뿐]\n2019.10.11 (금) 정오 발매\n\n#우싸미 #EP #고민은배송을늦출뿐 https://t.co/ZOfTOVUiaU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			169
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "우주왕복선싸이드미러",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"text": "우싸미",
+					"indices": [
+						150,
+						154
+					]
+				},
+				{
+					"text": "EP",
+					"indices": [
+						155,
+						158
+					]
+				},
+				{
+					"text": "고민은배송을늦출뿐",
+					"indices": [
+						159,
+						169
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1180096767980789800,
+					"id_str": "1180096767980789761",
+					"indices": [
+						170,
+						193
+					],
+					"media_url": "http://pbs.twimg.com/media/EGCLwhAU4AEYOjG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGCLwhAU4AEYOjG.jpg",
+					"url": "https://t.co/ZOfTOVUiaU",
+					"display_url": "pic.twitter.com/ZOfTOVUiaU",
+					"expanded_url": "https://twitter.com/YourSummerKR/status/1180096775916408837/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1180096767980789800,
+					"id_str": "1180096767980789761",
+					"indices": [
+						170,
+						193
+					],
+					"media_url": "http://pbs.twimg.com/media/EGCLwhAU4AEYOjG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGCLwhAU4AEYOjG.jpg",
+					"url": "https://t.co/ZOfTOVUiaU",
+					"display_url": "pic.twitter.com/ZOfTOVUiaU",
+					"expanded_url": "https://twitter.com/YourSummerKR/status/1180096775916408837/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 404720294,
+			"id_str": "404720294",
+			"name": "당신의 여름",
+			"screen_name": "YourSummerKR",
+			"location": "한국",
+			"description": "당신의 여름(Your Summer)은 가을방학, 계피, 김사월, 김사월X김해원, 랄라스윗, 못(Mot), 성진환, 아마도이자람밴드, 오지은, 우주왕복선싸이드미러, 우주히피, 이지형, 이하윤, 전기뱀장어, 최낙타, 홍이삭과 함께합니다.",
+			"url": "https://t.co/O7n52oEN1K",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/O7n52oEN1K",
+							"expanded_url": "http://www.instagram.com/yoursummernet",
+							"display_url": "instagram.com/yoursummernet",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1713,
+			"friends_count": 25,
+			"listed_count": 12,
+			"created_at": "Fri Nov 04 09:50:32 +0000 2011",
+			"favourites_count": 276,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4209,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/978966061050609669/wmNssQkU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/978966061050609669/wmNssQkU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/404720294/1522238671",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#우주왕복선싸이드미러 EP [고민은 배송을 늦출 뿐] D-7 \n\n장거리에서 연인뿐만 아니라 사람 간의 마음을 전하는 택배는 그 이름만으로도 설렘입니다.\n택배는 사랑 아닐까요?\n\n우주왕복선싸이드미러 EP [고민은 배송을 늦출 뿐]\n2019.10.11 (금) 정오 발매\n\n#우싸미 #EP #고민은배송을늦출뿐 https://t.co/ZOfTOVUiaU"
+	},
+	{
 		"created_at": "Fri Oct 04 12:18:22 +0000 2019",
 		"id": 1180082053099880400,
 		"id_str": "1180094808376975360",
