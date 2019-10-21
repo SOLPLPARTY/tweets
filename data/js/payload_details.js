900a49bcce33a6dd86a99af5bc50321bcbe8276d
솔플파티 <solplparty@gmail.com>
2019-10-21 13:17:48 +0900

    Added 5 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 27116,
-	"created_at": "Mon Oct 21 03:17:48 +0000 2019",
+	"tweets": 27121,
+	"created_at": "Mon Oct 21 04:17:47 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2019_10.js",
 		"year": 2019,
 		"var_name": "tweets_2019_10",
-		"tweet_count": 1415,
+		"tweet_count": 1420,
 		"month": 10
 	},
 	{
