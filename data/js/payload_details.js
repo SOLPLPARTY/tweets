461e1217991a3a1d8bf45b6c37a89f4742e697c7
솔플파티 <solplparty@gmail.com>
2019-11-15 14:17:51 +0900

    Added 9 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29032,
-	"created_at": "Fri Nov 15 04:17:46 +0000 2019",
+	"tweets": 29041,
+	"created_at": "Fri Nov 15 05:17:49 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2019_11.js",
 		"year": 2019,
 		"var_name": "tweets_2019_11",
-		"tweet_count": 1037,
+		"tweet_count": 1046,
 		"month": 11
 	},
 	{
