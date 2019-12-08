8ba35e2ec8ea7f7d1d79b6c49d0f6939dd46640d
솔플파티 <solplparty@gmail.com>
2019-12-09 03:17:42 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30395,
-	"created_at": "Sun Dec 08 13:17:39 +0000 2019",
+	"tweets": 30396,
+	"created_at": "Sun Dec 08 18:17:40 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,248 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Sun Dec 08 17:18:16 +0000 2019",
+		"id": 1203714514073448400,
+		"id_str": "1203725495373910022",
+		"full_text": "이 순위가 맞는거죠\n팬덤이 많은 팀이 일반이용자가 적은 시간엔\n당연히 순위가 높아야하는데\n언젠가부터 팬덤이 전혀 없는팀이 지붕킥을 하니...\n잠깐 이러다말면 다시는 찾을수 없을지 모릅니다\n내 아티스트의 순위를 지켜주세요\n#엑소 #EXO  #당연한1위 #국민청원 \nhttps://t.co/0Sm10f6qn6 https://t.co/VuKhJWfe7Z",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			170
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "엑소",
+					"indices": [
+						123,
+						126
+					]
+				},
+				{
+					"text": "EXO",
+					"indices": [
+						127,
+						131
+					]
+				},
+				{
+					"text": "당연한1위",
+					"indices": [
+						133,
+						139
+					]
+				},
+				{
+					"text": "국민청원",
+					"indices": [
+						140,
+						145
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/0Sm10f6qn6",
+					"expanded_url": "https://www1.president.go.kr/petitions/583907",
+					"display_url": "www1.president.go.kr/petitions/5839…",
+					"indices": [
+						147,
+						170
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1203714511712055300,
+					"id_str": "1203714511712055296",
+					"indices": [
+						171,
+						194
+					],
+					"media_url": "http://pbs.twimg.com/media/ELRz-ImUcAAaGNU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELRz-ImUcAAaGNU.jpg",
+					"url": "https://t.co/VuKhJWfe7Z",
+					"display_url": "pic.twitter.com/VuKhJWfe7Z",
+					"expanded_url": "https://twitter.com/messenger_yoon/status/1203714514073448454/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 543,
+							"h": 543,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 543,
+							"h": 543,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 543,
+							"h": 543,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1203714511712055300,
+					"id_str": "1203714511712055296",
+					"indices": [
+						171,
+						194
+					],
+					"media_url": "http://pbs.twimg.com/media/ELRz-ImUcAAaGNU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELRz-ImUcAAaGNU.jpg",
+					"url": "https://t.co/VuKhJWfe7Z",
+					"display_url": "pic.twitter.com/VuKhJWfe7Z",
+					"expanded_url": "https://twitter.com/messenger_yoon/status/1203714514073448454/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 543,
+							"h": 543,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 543,
+							"h": 543,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 543,
+							"h": 543,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 150566482,
+			"id_str": "150566482",
+			"name": "엠와이뮤직(MYmusic Ent)",
+			"screen_name": "messenger_yoon",
+			"location": "Artist Manager (South Korea)",
+			"description": "[Musician] 정준일, 낭만유랑악단, 나인(Nine9), 디어클라우드, 권영찬, 위아영 //mymusicent@naver.com//+82)2.6013.0102",
+			"url": "https://t.co/1IfWMp2Je9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1IfWMp2Je9",
+							"expanded_url": "http://www.mymusicent.co.kr",
+							"display_url": "mymusicent.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2849,
+			"friends_count": 368,
+			"listed_count": 34,
+			"created_at": "Tue Jun 01 07:29:53 +0000 2010",
+			"favourites_count": 16541,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5366,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/540007978649128960/jSz5oRO7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/540007978649128960/jSz5oRO7_normal.jpeg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "이 순위가 맞는거죠\n팬덤이 많은 팀이 일반이용자가 적은 시간엔\n당연히 순위가 높아야하는데\n언젠가부터 팬덤이 전혀 없는팀이 지붕킥을 하니...\n잠깐 이러다말면 다시는 찾을수 없을지 모릅니다\n내 아티스트의 순위를 지켜주세요\n#엑소 #EXO  #당연한1위 #국민청원 \nhttps://t.co/0Sm10f6qn6 https://t.co/VuKhJWfe7Z"
+	},
+	{
 		"created_at": "Sun Dec 08 13:13:41 +0000 2019",
 		"id": 1203656113142395000,
 		"id_str": "1203663941857140736",
