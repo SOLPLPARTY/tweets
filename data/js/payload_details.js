c3dc8ce531ac43082a066cd48fb03c81b8a31af9
솔플파티 <solplparty@gmail.com>
2019-12-08 19:17:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30390,
-	"created_at": "Sun Dec 08 09:17:39 +0000 2019",
+	"tweets": 30391,
+	"created_at": "Sun Dec 08 10:17:37 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,241 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Sun Dec 08 10:04:03 +0000 2019",
+		"id": 1203616219212861400,
+		"id_str": "1203616219212861440",
+		"full_text": "@msbsound 최정윤님 유튜브 프로필 사진 사라졌어요. https://t.co/cpKuS593nV https://t.co/XQpp4YXrhi",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			56
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "msbsound",
+					"name": "매스사 김딸기",
+					"id": 263751129,
+					"id_str": "263751129",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/cpKuS593nV",
+					"expanded_url": "https://www.youtube.com/channel/UCJRYtjXPaxk08gi9G01VVRQ",
+					"display_url": "youtube.com/channel/UCJRYt…",
+					"indices": [
+						33,
+						56
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1203616215001788400,
+					"id_str": "1203616215001788416",
+					"indices": [
+						57,
+						80
+					],
+					"media_url": "http://pbs.twimg.com/media/ELQakg0U8AAX_iS.png",
+					"media_url_https": "https://pbs.twimg.com/media/ELQakg0U8AAX_iS.png",
+					"url": "https://t.co/XQpp4YXrhi",
+					"display_url": "pic.twitter.com/XQpp4YXrhi",
+					"expanded_url": "https://twitter.com/solplparty/status/1203616219212861440/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 117,
+							"h": 117,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 505,
+							"h": 117,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 505,
+							"h": 117,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 505,
+							"h": 117,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1203616215001788400,
+					"id_str": "1203616215001788416",
+					"indices": [
+						57,
+						80
+					],
+					"media_url": "http://pbs.twimg.com/media/ELQakg0U8AAX_iS.png",
+					"media_url_https": "https://pbs.twimg.com/media/ELQakg0U8AAX_iS.png",
+					"url": "https://t.co/XQpp4YXrhi",
+					"display_url": "pic.twitter.com/XQpp4YXrhi",
+					"expanded_url": "https://twitter.com/solplparty/status/1203616219212861440/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 117,
+							"h": 117,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 505,
+							"h": 117,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 505,
+							"h": 117,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 505,
+							"h": 117,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 263751129,
+		"in_reply_to_user_id_str": "263751129",
+		"in_reply_to_screen_name": "msbsound",
+		"user": {
+			"id": 968706450779463700,
+			"id_str": "968706450779463682",
+			"name": "솔플파티",
+			"screen_name": "solplparty",
+			"location": "",
+			"description": "🌈 문화예술 커뮤니티, 솔플파티\n\n공연일정 / 티켓예매 공지 / 페스티벌 / 인디밴드 / 내한공연 / 콘서트 / 홍대문화 / 영화제 / 전시회 / 북토크\n\n문화예술정보 @EntRecruitKR\n문의 contact@solplparty.com\n\n▶ https://t.co/mr16mdiyD3",
+			"url": "https://t.co/GCEfwO8QQD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GCEfwO8QQD",
+							"expanded_url": "http://solplparty.com",
+							"display_url": "solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/mr16mdiyD3",
+							"expanded_url": "http://join.solplparty.com",
+							"display_url": "join.solplparty.com",
+							"indices": [
+								137,
+								160
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 183,
+			"friends_count": 315,
+			"listed_count": 2,
+			"created_at": "Wed Feb 28 04:36:30 +0000 2018",
+			"favourites_count": 263,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 46397,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/968706450779463682/1519792691",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "@msbsound 최정윤님 유튜브 프로필 사진 사라졌어요. https://t.co/cpKuS593nV https://t.co/XQpp4YXrhi"
+	},
+	{
 		"created_at": "Sun Dec 08 08:18:17 +0000 2019",
 		"id": 1203582163100172300,
 		"id_str": "1203589600570224640",
