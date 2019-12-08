e43592309e45dcef49fecb7c21caf5579624ca70
솔플파티 <solplparty@gmail.com>
2019-12-08 16:17:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30365,
-	"created_at": "Sun Dec 08 05:17:39 +0000 2019",
+	"tweets": 30366,
+	"created_at": "Sun Dec 08 07:17:37 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Sun Dec 08 06:18:16 +0000 2019",
+		"id": 1203555032177762300,
+		"id_str": "1203559400826384384",
+		"full_text": "[공연안내] 권순관 연말콘서트 ‘Connected’ Day2✨\n⠀⠀⠀⠀⠀\n티켓 부스 오픈 3:30PM\n관객 입장 시작 4:30PM\n공연 시작 5PM\n장소 - 이화여대 삼성홀\n⠀⠀⠀⠀⠀\n✔️현장 구매 가능합니다\n✔️콘서트 증정품은 티켓부스와 민트샵 부스에서 수령 가능합니다 https://t.co/qtbIsNmDnA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			151
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1203555017233272800,
+					"id_str": "1203555017233272832",
+					"indices": [
+						152,
+						175
+					],
+					"media_url": "http://pbs.twimg.com/media/ELPi6VXUEAA1Mbc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELPi6VXUEAA1Mbc.jpg",
+					"url": "https://t.co/qtbIsNmDnA",
+					"display_url": "pic.twitter.com/qtbIsNmDnA",
+					"expanded_url": "https://twitter.com/HappyRobotKR/status/1203555032177762305/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1203555017233272800,
+					"id_str": "1203555017233272832",
+					"indices": [
+						152,
+						175
+					],
+					"media_url": "http://pbs.twimg.com/media/ELPi6VXUEAA1Mbc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELPi6VXUEAA1Mbc.jpg",
+					"url": "https://t.co/qtbIsNmDnA",
+					"display_url": "pic.twitter.com/qtbIsNmDnA",
+					"expanded_url": "https://twitter.com/HappyRobotKR/status/1203555032177762305/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 63414926,
+			"id_str": "63414926",
+			"name": "Happy Robot Records",
+			"screen_name": "HappyRobotKR",
+			"location": "Seoul, Korea",
+			"description": "데이브레이크(DAYBREAK), 솔루션스(THE SOLUTIONS), 소란, 노리플라이, 쏜애플(THORNAPPLE), 램씨(LambC), 설(SURL), 아월(OurR)",
+			"url": "https://t.co/b6z6U7PUtR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/b6z6U7PUtR",
+							"expanded_url": "http://www.happyrobot.co.kr",
+							"display_url": "happyrobot.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 13660,
+			"friends_count": 2482,
+			"listed_count": 299,
+			"created_at": "Thu Aug 06 08:13:58 +0000 2009",
+			"favourites_count": 212,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 12905,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "B2DFDA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1101138907528257537/2sNzE372_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1101138907528257537/2sNzE372_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/63414926/1514732649",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[공연안내] 권순관 연말콘서트 ‘Connected’ Day2✨\n⠀⠀⠀⠀⠀\n티켓 부스 오픈 3:30PM\n관객 입장 시작 4:30PM\n공연 시작 5PM\n장소 - 이화여대 삼성홀\n⠀⠀⠀⠀⠀\n✔️현장 구매 가능합니다\n✔️콘서트 증정품은 티켓부스와 민트샵 부스에서 수령 가능합니다 https://t.co/qtbIsNmDnA"
+	},
+	{
 		"created_at": "Sun Dec 08 04:18:17 +0000 2019",
 		"id": 1203514970765328400,
 		"id_str": "1203529203825741824",
