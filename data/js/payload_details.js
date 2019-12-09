470022f83c36aa285cf09a8240c774033fe29401
솔플파티 <solplparty@gmail.com>
2019-12-10 01:17:43 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30451,
-	"created_at": "Mon Dec 09 14:17:44 +0000 2019",
+	"tweets": 30452,
+	"created_at": "Mon Dec 09 16:17:41 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,324 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Mon Dec 09 16:13:36 +0000 2019",
+		"id": 1204043697043595300,
+		"id_str": "1204071606173274114",
+		"full_text": "&lt;1LO Paradise vol. 3 : Winter &gt;\n서도밴드\n김아현\n로와\nTicket:https://t.co/clV0pWWCvZ https://t.co/rB1TLhOaUT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			80
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/clV0pWWCvZ",
+					"expanded_url": "https://forms.gle/9Zu3gssWympNjRdb9",
+					"display_url": "forms.gle/9Zu3gssWympNjR…",
+					"indices": [
+						57,
+						80
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1204043679725314000,
+					"id_str": "1204043679725314048",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/ELWfWPAU4AAs1-0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELWfWPAU4AAs1-0.jpg",
+					"url": "https://t.co/rB1TLhOaUT",
+					"display_url": "pic.twitter.com/rB1TLhOaUT",
+					"expanded_url": "https://twitter.com/cafe_unplugged/status/1204043697043595264/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 612,
+									"y": 223,
+									"h": 41,
+									"w": 41
+								},
+								{
+									"x": 313,
+									"y": 549,
+									"h": 85,
+									"w": 85
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 864,
+									"y": 316,
+									"h": 58,
+									"w": 58
+								},
+								{
+									"x": 442,
+									"y": 776,
+									"h": 120,
+									"w": 120
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 864,
+									"y": 316,
+									"h": 58,
+									"w": 58
+								},
+								{
+									"x": 442,
+									"y": 776,
+									"h": 120,
+									"w": 120
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 864,
+									"y": 316,
+									"h": 58,
+									"w": 58
+								},
+								{
+									"x": 442,
+									"y": 776,
+									"h": 120,
+									"w": 120
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1204043679725314000,
+					"id_str": "1204043679725314048",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/ELWfWPAU4AAs1-0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELWfWPAU4AAs1-0.jpg",
+					"url": "https://t.co/rB1TLhOaUT",
+					"display_url": "pic.twitter.com/rB1TLhOaUT",
+					"expanded_url": "https://twitter.com/cafe_unplugged/status/1204043697043595264/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 612,
+									"y": 223,
+									"h": 41,
+									"w": 41
+								},
+								{
+									"x": 313,
+									"y": 549,
+									"h": 85,
+									"w": 85
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 864,
+									"y": 316,
+									"h": 58,
+									"w": 58
+								},
+								{
+									"x": 442,
+									"y": 776,
+									"h": 120,
+									"w": 120
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 864,
+									"y": 316,
+									"h": 58,
+									"w": 58
+								},
+								{
+									"x": 442,
+									"y": 776,
+									"h": 120,
+									"w": 120
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 864,
+									"y": 316,
+									"h": 58,
+									"w": 58
+								},
+								{
+									"x": 442,
+									"y": 776,
+									"h": 120,
+									"w": 120
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 295474993,
+			"id_str": "295474993",
+			"name": "카페 언플러그드",
+			"screen_name": "cafe_unplugged",
+			"location": "서울시 마포구 서교동 326-18 1층, 지하 1층",
+			"description": "070-4200-9251",
+			"url": "https://t.co/9lPmySTRmo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9lPmySTRmo",
+							"expanded_url": "http://cafe.naver.com/cafeunplugged",
+							"display_url": "cafe.naver.com/cafeunplugged",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1921,
+			"friends_count": 1137,
+			"listed_count": 48,
+			"created_at": "Mon May 09 03:05:19 +0000 2011",
+			"favourites_count": 235,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4018,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/950439490631352320/q6E5We2L_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/950439490631352320/q6E5We2L_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/295474993/1525645770",
+			"profile_link_color": "FF2450",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "&lt;1LO Paradise vol. 3 : Winter &gt;\n서도밴드\n김아현\n로와\nTicket:https://t.co/clV0pWWCvZ https://t.co/rB1TLhOaUT"
+	},
+	{
 		"created_at": "Mon Dec 09 14:13:27 +0000 2019",
 		"id": 1204023449762418700,
 		"id_str": "1204041372858306566",
