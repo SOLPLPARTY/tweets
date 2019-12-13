9034c60eecc4025cdaf95ccf05f7cc4a83706f29
솔플파티 <solplparty@gmail.com>
2019-12-13 21:17:43 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30669,
-	"created_at": "Fri Dec 13 11:17:40 +0000 2019",
+	"tweets": 30670,
+	"created_at": "Fri Dec 13 12:17:41 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,276 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Fri Dec 13 12:13:31 +0000 2019",
+		"id": 1205459063498104800,
+		"id_str": "1205460741882597376",
+		"full_text": "[신규 입고]\n임세모님의 캐롤 싱글 앨범 [메리크리스마스다] 명함이 입고되었습니다! 얼마 전 발매된 [메리크리스마스다🎅]도 감상해보세요-:)\n\n https://t.co/UP8XCo4rQw https://t.co/1EkJH5FrSo",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			104
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/UP8XCo4rQw",
+					"expanded_url": "https://vibe.naver.com/album/3664269",
+					"display_url": "vibe.naver.com/album/3664269",
+					"indices": [
+						81,
+						104
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1205459051829530600,
+					"id_str": "1205459051829530624",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/ELqmnu-UEAAH90v.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELqmnu-UEAAH90v.jpg",
+					"url": "https://t.co/1EkJH5FrSo",
+					"display_url": "pic.twitter.com/1EkJH5FrSo",
+					"expanded_url": "https://twitter.com/xindieticket/status/1205459063498104832/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 938,
+							"h": 420,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 304,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 938,
+							"h": 420,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": [
+								{
+									"x": 95,
+									"y": 157,
+									"h": 104,
+									"w": 104
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 95,
+									"y": 157,
+									"h": 104,
+									"w": 104
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 68,
+									"y": 113,
+									"h": 75,
+									"w": 75
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 95,
+									"y": 157,
+									"h": 104,
+									"w": 104
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1205459051829530600,
+					"id_str": "1205459051829530624",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/ELqmnu-UEAAH90v.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELqmnu-UEAAH90v.jpg",
+					"url": "https://t.co/1EkJH5FrSo",
+					"display_url": "pic.twitter.com/1EkJH5FrSo",
+					"expanded_url": "https://twitter.com/xindieticket/status/1205459063498104832/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 938,
+							"h": 420,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 304,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 938,
+							"h": 420,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": [
+								{
+									"x": 95,
+									"y": 157,
+									"h": 104,
+									"w": 104
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 95,
+									"y": 157,
+									"h": 104,
+									"w": 104
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 68,
+									"y": 113,
+									"h": 75,
+									"w": 75
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 95,
+									"y": 157,
+									"h": 104,
+									"w": 104
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1220731080,
+			"id_str": "1220731080",
+			"name": "씬디 티켓라운지",
+			"screen_name": "xindieticket",
+			"location": "서울 마포구 어울마당로 41-1 (홍대주차장길)",
+			"description": "홍대 공연정보를 한눈에 확인할 수 있는 온오프라인 티켓판매처\n\n※ 운영시간: 1-9PM (단, 화요일은 3:30–8:30PM) / 저녁시간: 6-7PM\n☎ 02-322-2218",
+			"url": "https://t.co/dqX6O3D21p",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dqX6O3D21p",
+							"expanded_url": "http://xindieticket.kr",
+							"display_url": "xindieticket.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2833,
+			"friends_count": 2718,
+			"listed_count": 25,
+			"created_at": "Tue Feb 26 07:24:04 +0000 2013",
+			"favourites_count": 4912,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9024,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1200342617373827073/a1vB9pyg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1200342617373827073/a1vB9pyg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1220731080/1575026135",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[신규 입고]\n임세모님의 캐롤 싱글 앨범 [메리크리스마스다] 명함이 입고되었습니다! 얼마 전 발매된 [메리크리스마스다🎅]도 감상해보세요-:)\n\n https://t.co/UP8XCo4rQw https://t.co/1EkJH5FrSo"
+	},
+	{
 		"created_at": "Fri Dec 13 11:13:27 +0000 2019",
 		"id": 1205441802641105000,
 		"id_str": "1205445625522851840",
