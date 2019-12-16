a15c9fc7a0e1e8cd0e9f941b77838b212c2d59a3
솔플파티 <solplparty@gmail.com>
2019-12-16 10:17:44 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30743,
-	"created_at": "Sun Dec 15 16:17:42 +0000 2019",
+	"tweets": 30744,
+	"created_at": "Mon Dec 16 01:17:43 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,401 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Mon Dec 16 00:18:19 +0000 2019",
+		"id": 1206361271475429400,
+		"id_str": "1206367917455106050",
+		"full_text": "TODAY 5:55PM\n#6시5분전 #루시드폴 @lucid_fall_jo https://t.co/sKYFywxG6D",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			40
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "6시5분전",
+					"indices": [
+						13,
+						19
+					]
+				},
+				{
+					"text": "루시드폴",
+					"indices": [
+						20,
+						25
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "lucid_fall_jo",
+					"name": "Lucid Fall",
+					"id": 115856799,
+					"id_str": "115856799",
+					"indices": [
+						26,
+						40
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/sKYFywxG6D",
+					"expanded_url": "https://twitter.com/kr_now/status/1206227518799220737",
+					"display_url": "twitter.com/kr_now/status/…",
+					"indices": [
+						41,
+						64
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 266004552,
+			"id_str": "266004552",
+			"name": "안테나 Antenna",
+			"screen_name": "antennamusic",
+			"location": "대한민국 서울",
+			"description": "Jung Jae Hyung, You Hee Yeol(Toy), Lucid Fall, Peppertones, Park Sae Byul, Kwon Jin Ah, Sam Kim, Jung Seung Hwan, Lee Jin Ah, CHAI, Yun Seokcheol _ Official",
+			"url": "https://t.co/CqfXN7It7U",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/CqfXN7It7U",
+							"expanded_url": "http://www.antenna.co.kr",
+							"display_url": "antenna.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 34876,
+			"friends_count": 2956,
+			"listed_count": 411,
+			"created_at": "Mon Mar 14 13:49:27 +0000 2011",
+			"favourites_count": 57,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2868,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266004552/1576036132",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1206227518799220700,
+		"quoted_status_id_str": "1206227518799220737",
+		"quoted_status_permalink": {
+			"url": "https://t.co/sKYFywxG6D",
+			"expanded": "https://twitter.com/kr_now/status/1206227518799220737",
+			"display": "twitter.com/kr_now/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Dec 15 15:00:25 +0000 2019",
+			"id": 1206227518799220700,
+			"id_str": "1206227518799220737",
+			"full_text": "#NOW온에어 12월 16일(월) 오늘의 타임테이블📌 https://t.co/gisbbPdZhu",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				29
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "NOW온에어",
+						"indices": [
+							0,
+							7
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1206227516353994800,
+						"id_str": "1206227516353994753",
+						"indices": [
+							30,
+							53
+						],
+						"media_url": "http://pbs.twimg.com/media/EL1hiQaU4AERo0C.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EL1hiQaU4AERo0C.jpg",
+						"url": "https://t.co/gisbbPdZhu",
+						"display_url": "pic.twitter.com/gisbbPdZhu",
+						"expanded_url": "https://twitter.com/kr_now/status/1206227518799220737/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1080,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1080,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"large": {
+								"faces": [
+									{
+										"x": 221,
+										"y": 710,
+										"h": 39,
+										"w": 39
+									}
+								]
+							},
+							"orig": {
+								"faces": [
+									{
+										"x": 221,
+										"y": 710,
+										"h": 39,
+										"w": 39
+									}
+								]
+							},
+							"medium": {
+								"faces": [
+									{
+										"x": 221,
+										"y": 710,
+										"h": 39,
+										"w": 39
+									}
+								]
+							},
+							"small": {
+								"faces": [
+									{
+										"x": 139,
+										"y": 447,
+										"h": 24,
+										"w": 24
+									}
+								]
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1206227516353994800,
+						"id_str": "1206227516353994753",
+						"indices": [
+							30,
+							53
+						],
+						"media_url": "http://pbs.twimg.com/media/EL1hiQaU4AERo0C.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EL1hiQaU4AERo0C.jpg",
+						"url": "https://t.co/gisbbPdZhu",
+						"display_url": "pic.twitter.com/gisbbPdZhu",
+						"expanded_url": "https://twitter.com/kr_now/status/1206227518799220737/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1080,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1080,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"large": {
+								"faces": [
+									{
+										"x": 221,
+										"y": 710,
+										"h": 39,
+										"w": 39
+									}
+								]
+							},
+							"orig": {
+								"faces": [
+									{
+										"x": 221,
+										"y": 710,
+										"h": 39,
+										"w": 39
+									}
+								]
+							},
+							"medium": {
+								"faces": [
+									{
+										"x": 221,
+										"y": 710,
+										"h": 39,
+										"w": 39
+									}
+								]
+							},
+							"small": {
+								"faces": [
+									{
+										"x": 139,
+										"y": 447,
+										"h": 24,
+										"w": 24
+									}
+								]
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://www.smartpost.kr/\" rel=\"nofollow\">smartpost.kr</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1159646410414817300,
+				"id_str": "1159646410414817281",
+				"name": "네이버 나우.",
+				"screen_name": "kr_now",
+				"location": "",
+				"description": "어디에도 없었던 새로운 오디오 쇼 #NOW온에어⁣",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 17927,
+				"friends_count": 0,
+				"listed_count": 40,
+				"created_at": "Fri Aug 09 02:03:44 +0000 2019",
+				"favourites_count": 51,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 733,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1164016940198666242/ElqZ-oIg_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1164016940198666242/ElqZ-oIg_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1159646410414817281/1566358241",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 453,
+			"favorite_count": 676,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"retweet_count": 6,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "TODAY 5:55PM\n#6시5분전 #루시드폴 @lucid_fall_jo https://t.co/sKYFywxG6D"
+	},
+	{
 		"created_at": "Sun Dec 15 15:25:01 +0000 2019",
 		"id": 1206227518799220700,
 		"id_str": "1206233706706489352",
@@ -198,7 +593,7 @@ Grailbird.data.tweets_2019_12 =
 			"name": "네이버 나우.",
 			"screen_name": "kr_now",
 			"location": "",
-			"description": "어디에도 없었던 새로운 오디오 쇼 #NOW온에어⁣",
+			"description": "어디에도 없었던 새로운 오디오 쇼 #NOW온에어",
 			"url": null,
 			"entities": {
 				"description": {
