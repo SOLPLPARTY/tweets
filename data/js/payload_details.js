913bee96e7f0fc40c2bb70dbb261d02cff42bc03
솔플파티 <solplparty@gmail.com>
2020-01-01 13:17:33 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31647,
-	"created_at": "Wed Jan 01 03:17:32 +0000 2020",
+	"tweets": 31649,
+	"created_at": "Wed Jan 01 04:17:32 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,585 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Wed Jan 01 03:41:49 +0000 2020",
+		"id": 1212210620679876600,
+		"id_str": "1212217335320322049",
+		"full_text": "2020, 오나라 오나라 아주오나 \n(2020년이여,오시려하는가 오시려하는가 아주 오시려하는가)\n\n2020년1월1일 #예스24스테이지 공연 시간 안내드립니다! https://t.co/HPVO5LA1NV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			88
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "예스24스테이지",
+					"indices": [
+						65,
+						74
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1212210612089962500,
+					"id_str": "1212210612089962496",
+					"indices": [
+						89,
+						112
+					],
+					"media_url": "http://pbs.twimg.com/media/ENKjIQ6UwAAvkNy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ENKjIQ6UwAAvkNy.jpg",
+					"url": "https://t.co/HPVO5LA1NV",
+					"display_url": "pic.twitter.com/HPVO5LA1NV",
+					"expanded_url": "https://twitter.com/yes24stage/status/1212210620679876608/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1212210612089962500,
+					"id_str": "1212210612089962496",
+					"indices": [
+						89,
+						112
+					],
+					"media_url": "http://pbs.twimg.com/media/ENKjIQ6UwAAvkNy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ENKjIQ6UwAAvkNy.jpg",
+					"url": "https://t.co/HPVO5LA1NV",
+					"display_url": "pic.twitter.com/HPVO5LA1NV",
+					"expanded_url": "https://twitter.com/yes24stage/status/1212210620679876608/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1104695434893652000,
+			"id_str": "1104695434893651968",
+			"name": "YES24 STAGE_예스24 스테이지",
+			"screen_name": "yes24stage",
+			"location": "대한민국 서울 종로구 동숭동 대학로12길 21",
+			"description": "예스24(주)가 운영하는 공연장 운영법인인 예스24라이브홀(주)의 #대학로 공연장 #예스24스테이지 ( #YES24STAGE ) 의 공식 트위터입니다.",
+			"url": "https://t.co/O7KKxkPl2c",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/O7KKxkPl2c",
+							"expanded_url": "http://www.yes24stage.com/",
+							"display_url": "yes24stage.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2452,
+			"friends_count": 27,
+			"listed_count": 12,
+			"created_at": "Sun Mar 10 10:48:10 +0000 2019",
+			"favourites_count": 65,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 152,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1104695602405728256/FQ50_eUl_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1104695602405728256/FQ50_eUl_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1104695434893651968/1552216594",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 37,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "2020, 오나라 오나라 아주오나 \n(2020년이여,오시려하는가 오시려하는가 아주 오시려하는가)\n\n2020년1월1일 #예스24스테이지 공연 시간 안내드립니다! https://t.co/HPVO5LA1NV"
+	},
+	{
+		"created_at": "Wed Jan 01 03:41:40 +0000 2020",
+		"id": 1212210092432490500,
+		"id_str": "1212217297131261953",
+		"full_text": "#ROLLINGHALL #OurR(아월) #김영소 #락킷걸 #볼빨간사춘기 #솔루션스 #스무살 #신인류 #쏜애플 #오왠 #오이스터 #이브 #조문근밴드 #지범(ZEEBOMB) #최유리 #크라잉넛 #? #홍이삭 #롤링홀 #롤링25주년기념공연 #롤링25주년 #멜론티켓 https://t.co/zifwrRVb6a",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			145
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ROLLINGHALL",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"text": "OurR",
+					"indices": [
+						13,
+						18
+					]
+				},
+				{
+					"text": "김영소",
+					"indices": [
+						23,
+						27
+					]
+				},
+				{
+					"text": "락킷걸",
+					"indices": [
+						28,
+						32
+					]
+				},
+				{
+					"text": "볼빨간사춘기",
+					"indices": [
+						33,
+						40
+					]
+				},
+				{
+					"text": "솔루션스",
+					"indices": [
+						41,
+						46
+					]
+				},
+				{
+					"text": "스무살",
+					"indices": [
+						47,
+						51
+					]
+				},
+				{
+					"text": "신인류",
+					"indices": [
+						52,
+						56
+					]
+				},
+				{
+					"text": "쏜애플",
+					"indices": [
+						57,
+						61
+					]
+				},
+				{
+					"text": "오왠",
+					"indices": [
+						62,
+						65
+					]
+				},
+				{
+					"text": "오이스터",
+					"indices": [
+						66,
+						71
+					]
+				},
+				{
+					"text": "이브",
+					"indices": [
+						72,
+						75
+					]
+				},
+				{
+					"text": "조문근밴드",
+					"indices": [
+						76,
+						82
+					]
+				},
+				{
+					"text": "지범",
+					"indices": [
+						83,
+						86
+					]
+				},
+				{
+					"text": "최유리",
+					"indices": [
+						96,
+						100
+					]
+				},
+				{
+					"text": "크라잉넛",
+					"indices": [
+						101,
+						106
+					]
+				},
+				{
+					"text": "홍이삭",
+					"indices": [
+						110,
+						114
+					]
+				},
+				{
+					"text": "롤링홀",
+					"indices": [
+						115,
+						119
+					]
+				},
+				{
+					"text": "롤링25주년기념공연",
+					"indices": [
+						120,
+						131
+					]
+				},
+				{
+					"text": "롤링25주년",
+					"indices": [
+						132,
+						139
+					]
+				},
+				{
+					"text": "멜론티켓",
+					"indices": [
+						140,
+						145
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1212209992708657200,
+					"id_str": "1212209992708657152",
+					"indices": [
+						146,
+						169
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1212209992708657152/pu/img/4v3aqcDiyVqU2Dak.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1212209992708657152/pu/img/4v3aqcDiyVqU2Dak.jpg",
+					"url": "https://t.co/zifwrRVb6a",
+					"display_url": "pic.twitter.com/zifwrRVb6a",
+					"expanded_url": "https://twitter.com/rollinghall/status/1212210092432490496/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1212209992708657200,
+					"id_str": "1212209992708657152",
+					"indices": [
+						146,
+						169
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1212209992708657152/pu/img/4v3aqcDiyVqU2Dak.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1212209992708657152/pu/img/4v3aqcDiyVqU2Dak.jpg",
+					"url": "https://t.co/zifwrRVb6a",
+					"display_url": "pic.twitter.com/zifwrRVb6a",
+					"expanded_url": "https://twitter.com/rollinghall/status/1212210092432490496/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 53291,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1212209992708657152/pu/vid/480x270/jqUpL6GEG8CCY_qm.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1212209992708657152/pu/pl/K4hCNU-qgOklbeeb.m3u8?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1212209992708657152/pu/vid/640x360/pvOVYNHgpMh_tH0V.mp4?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1212209992708657152/pu/vid/1280x720/oYH5qXYlA3M9B4DD.mp4?tag=10"
+							}
+						]
+					},
+					"features": {},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1212209829747417000,
+		"in_reply_to_status_id_str": "1212209829747417088",
+		"in_reply_to_user_id": 2185812985,
+		"in_reply_to_user_id_str": "2185812985",
+		"in_reply_to_screen_name": "rollinghall",
+		"user": {
+			"id": 2185812985,
+			"id_str": "2185812985",
+			"name": "롤링홀",
+			"screen_name": "rollinghall",
+			"location": "서울시 마포구 서교동 402-22 B1",
+			"description": "홍대/합정에 위치한 음악 공연장 #롤링홀 입니다.\n👇🏻👇🏻멜론티켓 롤링홀 공연 바로가기.\nhttps://t.co/iP5dSLN5zf",
+			"url": "https://t.co/qVzPUzUTxK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qVzPUzUTxK",
+							"expanded_url": "http://www.rollinghall.co.kr",
+							"display_url": "rollinghall.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/iP5dSLN5zf",
+							"expanded_url": "http://tuney.kr/lNFWyb",
+							"display_url": "tuney.kr/lNFWyb",
+							"indices": [
+								50,
+								73
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3257,
+			"friends_count": 789,
+			"listed_count": 43,
+			"created_at": "Sun Nov 10 07:11:11 +0000 2013",
+			"favourites_count": 51,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 2524,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme11/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme11/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1198865457228345344/vzXWi8vq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1198865457228345344/vzXWi8vq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2185812985/1574666774",
+			"profile_link_color": "D92129",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 12,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#ROLLINGHALL #OurR(아월) #김영소 #락킷걸 #볼빨간사춘기 #솔루션스 #스무살 #신인류 #쏜애플 #오왠 #오이스터 #이브 #조문근밴드 #지범(ZEEBOMB) #최유리 #크라잉넛 #? #홍이삭 #롤링홀 #롤링25주년기념공연 #롤링25주년 #멜론티켓 https://t.co/zifwrRVb6a"
+	},
+	{
 		"created_at": "Wed Jan 01 03:00:09 +0000 2020",
 		"id": 1212206851061883000,
 		"id_str": "1212206851061882883",
