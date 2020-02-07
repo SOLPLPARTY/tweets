3eae5f7d2d4165acdaf65e11bde3d89f8388ff3e
솔플파티 <solplparty@gmail.com>
2020-02-08 01:17:41 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 34083,
-	"created_at": "Fri Feb 07 15:17:38 +0000 2020",
+	"tweets": 34084,
+	"created_at": "Fri Feb 07 16:17:40 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,196 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Fri Feb 07 15:58:21 +0000 2020",
+		"id": 1225802909587165200,
+		"id_str": "1225811043445805057",
+		"full_text": "오늘 약속한 사진!🤳 호스트 #하성운 제2의 집 나우 스튜디오에서 자랑하고 싶은 건... 네 하성운씨의 의견은 나중에 들을게요 일단 저희는 하성운입니다🙋🏻‍♀️🙋🏽‍♀️🙋🏼‍♀️ #심야아이돌 #NOW온에어 #주말잘보내고_월요일에도같이놀자💓 https://t.co/2QMrlcrJKA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			133
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "하성운",
+					"indices": [
+						16,
+						20
+					]
+				},
+				{
+					"text": "심야아이돌",
+					"indices": [
+						100,
+						106
+					]
+				},
+				{
+					"text": "NOW온에어",
+					"indices": [
+						107,
+						114
+					]
+				},
+				{
+					"text": "주말잘보내고_월요일에도같이놀자",
+					"indices": [
+						115,
+						132
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1225802896043692000,
+					"id_str": "1225802896043692034",
+					"indices": [
+						134,
+						157
+					],
+					"media_url": "http://pbs.twimg.com/media/EQLtPUdUUAI_4ip.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQLtPUdUUAI_4ip.jpg",
+					"url": "https://t.co/2QMrlcrJKA",
+					"display_url": "pic.twitter.com/2QMrlcrJKA",
+					"expanded_url": "https://twitter.com/kr_now/status/1225802909587165184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1225802896043692000,
+					"id_str": "1225802896043692034",
+					"indices": [
+						134,
+						157
+					],
+					"media_url": "http://pbs.twimg.com/media/EQLtPUdUUAI_4ip.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQLtPUdUUAI_4ip.jpg",
+					"url": "https://t.co/2QMrlcrJKA",
+					"display_url": "pic.twitter.com/2QMrlcrJKA",
+					"expanded_url": "https://twitter.com/kr_now/status/1225802909587165184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1159646410414817300,
+			"id_str": "1159646410414817281",
+			"name": "네이버 나우.",
+			"screen_name": "kr_now",
+			"location": "",
+			"description": "어디에도 없었던 새로운 오디오 쇼 #NOW온에어⁣",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21306,
+			"friends_count": 0,
+			"listed_count": 46,
+			"created_at": "Fri Aug 09 02:03:44 +0000 2019",
+			"favourites_count": 65,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 1158,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1164016940198666242/ElqZ-oIg_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1164016940198666242/ElqZ-oIg_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1159646410414817281/1566358241",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2050,
+		"favorite_count": 2430,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "오늘 약속한 사진!🤳 호스트 #하성운 제2의 집 나우 스튜디오에서 자랑하고 싶은 건... 네 하성운씨의 의견은 나중에 들을게요 일단 저희는 하성운입니다🙋🏻‍♀️🙋🏽‍♀️🙋🏼‍♀️ #심야아이돌 #NOW온에어 #주말잘보내고_월요일에도같이놀자💓 https://t.co/2QMrlcrJKA"
+	},
+	{
 		"created_at": "Fri Feb 07 15:14:06 +0000 2020",
 		"id": 1225799904968179700,
 		"id_str": "1225799904968179713",
