d5f0d6c93bba63c7038e3aed2e341c2a63778dec
솔플파티 <solplparty@gmail.com>
2020-03-20 00:17:47 +0900

    Added 1 tweets

--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,250 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Thu Mar 19 14:29:24 +0000 2020",
+		"id": 1240635912217694200,
+		"id_str": "1240646561291517957",
+		"full_text": "#20200319\n#나인 #디어클라우드 \n밤11시에 #생방송 으로 진행되는 #굿나인\n인기코너 #배워서나인주나 곧 시작 합니다\n#나인NINE9 #NINE9 #dearcloud\n#Goodnine #후추스튜디오 https://t.co/lhNPHVRGzP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			114
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "나인",
+					"indices": [
+						10,
+						13
+					]
+				},
+				{
+					"text": "디어클라우드",
+					"indices": [
+						14,
+						21
+					]
+				},
+				{
+					"text": "생방송",
+					"indices": [
+						29,
+						33
+					]
+				},
+				{
+					"text": "굿나인",
+					"indices": [
+						42,
+						46
+					]
+				},
+				{
+					"text": "배워서나인주나",
+					"indices": [
+						52,
+						60
+					]
+				},
+				{
+					"text": "나인NINE9",
+					"indices": [
+						70,
+						78
+					]
+				},
+				{
+					"text": "NINE9",
+					"indices": [
+						79,
+						85
+					]
+				},
+				{
+					"text": "dearcloud",
+					"indices": [
+						86,
+						96
+					]
+				},
+				{
+					"text": "Goodnine",
+					"indices": [
+						97,
+						106
+					]
+				},
+				{
+					"text": "후추스튜디오",
+					"indices": [
+						107,
+						114
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1240635888276660200,
+					"id_str": "1240635888276660224",
+					"indices": [
+						115,
+						138
+					],
+					"media_url": "http://pbs.twimg.com/media/ETefxGhU4AACQvF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ETefxGhU4AACQvF.jpg",
+					"url": "https://t.co/lhNPHVRGzP",
+					"display_url": "pic.twitter.com/lhNPHVRGzP",
+					"expanded_url": "https://twitter.com/messenger_yoon/status/1240635912217694209/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1240635888276660200,
+					"id_str": "1240635888276660224",
+					"indices": [
+						115,
+						138
+					],
+					"media_url": "http://pbs.twimg.com/media/ETefxGhU4AACQvF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ETefxGhU4AACQvF.jpg",
+					"url": "https://t.co/lhNPHVRGzP",
+					"display_url": "pic.twitter.com/lhNPHVRGzP",
+					"expanded_url": "https://twitter.com/messenger_yoon/status/1240635912217694209/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 150566482,
+			"id_str": "150566482",
+			"name": "엠와이뮤직(MYmusic Ent)",
+			"screen_name": "messenger_yoon",
+			"location": "Artist Manager (South Korea)",
+			"description": "[Musician] 정준일, 낭만유랑악단, 나인(Nine9), 디어클라우드, 권영찬, 위아영 //mymusicent@naver.com//+82)2.6013.0102",
+			"url": "https://t.co/1IfWMp2Je9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1IfWMp2Je9",
+							"expanded_url": "http://www.mymusicent.co.kr",
+							"display_url": "mymusicent.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2808,
+			"friends_count": 360,
+			"listed_count": 33,
+			"created_at": "Tue Jun 01 07:29:53 +0000 2010",
+			"favourites_count": 15954,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5427,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/540007978649128960/jSz5oRO7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/540007978649128960/jSz5oRO7_normal.jpeg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#20200319\n#나인 #디어클라우드 \n밤11시에 #생방송 으로 진행되는 #굿나인\n인기코너 #배워서나인주나 곧 시작 합니다\n#나인NINE9 #NINE9 #dearcloud\n#Goodnine #후추스튜디오 https://t.co/lhNPHVRGzP"
+	},
+	{
 		"created_at": "Thu Mar 19 13:38:17 +0000 2020",
 		"id": 1240626537616924700,
 		"id_str": "1240633695469371392",
