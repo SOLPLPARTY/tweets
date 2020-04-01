79a24d2707b61d51077dc98603761048e6fb3075
솔플파티 <solplparty@gmail.com>
2020-04-01 10:17:43 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 36796,
-	"created_at": "Tue Mar 31 17:17:56 +0000 2020",
+	"tweets": 36797,
+	"created_at": "Wed Apr 01 01:17:42 +0000 2020",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2020_04.js
@@ -0,0 +1,206 @@
+Grailbird.data.tweets_2020_04 = 
+[
+	{
+		"created_at": "Wed Apr 01 00:29:25 +0000 2020",
+		"id": 1245138934318809000,
+		"id_str": "1245146210987577345",
+		"full_text": "[D-DAY]\n권진아 싱글 ‘뭔가 잘못됐어’\n\n2020. 04. 01. 6PM 발매\n\n#권진아 #뭔가잘못됐어\n#KwonJinAh #SomethingsWrong🌼💫 https://t.co/IPiXxkaUvL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			90
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "권진아",
+					"indices": [
+						48,
+						52
+					]
+				},
+				{
+					"text": "뭔가잘못됐어",
+					"indices": [
+						53,
+						60
+					]
+				},
+				{
+					"text": "KwonJinAh",
+					"indices": [
+						61,
+						71
+					]
+				},
+				{
+					"text": "SomethingsWrong",
+					"indices": [
+						72,
+						88
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1245138897115512800,
+					"id_str": "1245138897115512834",
+					"indices": [
+						91,
+						114
+					],
+					"media_url": "http://pbs.twimg.com/media/EUefOtqXgAILfZ9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EUefOtqXgAILfZ9.jpg",
+					"url": "https://t.co/IPiXxkaUvL",
+					"display_url": "pic.twitter.com/IPiXxkaUvL",
+					"expanded_url": "https://twitter.com/antennamusic/status/1245138934318809088/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1245138897115512800,
+					"id_str": "1245138897115512834",
+					"indices": [
+						91,
+						114
+					],
+					"media_url": "http://pbs.twimg.com/media/EUefOtqXgAILfZ9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EUefOtqXgAILfZ9.jpg",
+					"url": "https://t.co/IPiXxkaUvL",
+					"display_url": "pic.twitter.com/IPiXxkaUvL",
+					"expanded_url": "https://twitter.com/antennamusic/status/1245138934318809088/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 266004552,
+			"id_str": "266004552",
+			"name": "안테나 Antenna",
+			"screen_name": "antennamusic",
+			"location": "대한민국 서울",
+			"description": "Jung Jae Hyung, You Hee Yul(Toy), Lucid Fall, Peppertones, Park Sae Byul, Kwon Jin Ah, Sam Kim, Jung Seung Hwan, Lee Jin Ah, CHAI, Yun Seokcheol _ Official",
+			"url": "https://t.co/CqfXN7It7U",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/CqfXN7It7U",
+							"expanded_url": "http://www.antenna.co.kr",
+							"display_url": "antenna.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 34482,
+			"friends_count": 11,
+			"listed_count": 406,
+			"created_at": "Mon Mar 14 13:49:27 +0000 2011",
+			"favourites_count": 58,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3009,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266004552/1582881097",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 20,
+		"favorite_count": 29,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[D-DAY]\n권진아 싱글 ‘뭔가 잘못됐어’\n\n2020. 04. 01. 6PM 발매\n\n#권진아 #뭔가잘못됐어\n#KwonJinAh #SomethingsWrong🌼💫 https://t.co/IPiXxkaUvL"
+	}
+]
