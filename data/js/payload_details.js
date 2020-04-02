91c3ea1b118b273c28dc3f176f25c123c89701c0
솔플파티 <solplparty@gmail.com>
2020-04-03 01:17:39 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 36930,
-	"created_at": "Thu Apr 02 15:18:03 +0000 2020",
+	"tweets": 36931,
+	"created_at": "Thu Apr 02 16:17:37 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,164 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Thu Apr 02 15:29:25 +0000 2020",
+		"id": 1245727746493280300,
+		"id_str": "1245735091533426690",
+		"full_text": "[#신승훈] Special Album [My Personas] \n\nTeaser 3. 그러자 우리\n\n🔗 https://t.co/kd9BIXEOWM\n🔗 https://t.co/QAJlUpTv6n\n\n#신승훈 #Special_Album #My_Personas \n#그러자_우리 #20200408_6PM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			163
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "신승훈",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "신승훈",
+					"indices": [
+						108,
+						112
+					]
+				},
+				{
+					"text": "Special_Album",
+					"indices": [
+						113,
+						127
+					]
+				},
+				{
+					"text": "My_Personas",
+					"indices": [
+						128,
+						140
+					]
+				},
+				{
+					"text": "그러자_우리",
+					"indices": [
+						142,
+						149
+					]
+				},
+				{
+					"text": "20200408_6PM",
+					"indices": [
+						150,
+						163
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/kd9BIXEOWM",
+					"expanded_url": "https://youtu.be/6La1LsNSnbo",
+					"display_url": "youtu.be/6La1LsNSnbo",
+					"indices": [
+						57,
+						80
+					]
+				},
+				{
+					"url": "https://t.co/QAJlUpTv6n",
+					"expanded_url": "https://www.vlive.tv/video/182218",
+					"display_url": "vlive.tv/video/182218",
+					"indices": [
+						83,
+						106
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1907527430,
+			"id_str": "1907527430",
+			"name": "Dorothy Company",
+			"screen_name": "dorothy_company",
+			"location": "",
+			"description": "도로시컴퍼니 공식 계정/ 신승훈, 로시(Rothy) 소속",
+			"url": "https://t.co/I5K9KkKQb2",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/I5K9KkKQb2",
+							"expanded_url": "http://www.dorothycp.com",
+							"display_url": "dorothycp.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1948,
+			"friends_count": 1,
+			"listed_count": 23,
+			"created_at": "Thu Sep 26 11:04:13 +0000 2013",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 921,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/494382806001139712/8RcnmuiH_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/494382806001139712/8RcnmuiH_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1907527430/1513765441",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 15,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#신승훈] Special Album [My Personas] \n\nTeaser 3. 그러자 우리\n\n🔗 https://t.co/kd9BIXEOWM\n🔗 https://t.co/QAJlUpTv6n\n\n#신승훈 #Special_Album #My_Personas \n#그러자_우리 #20200408_6PM"
+	},
+	{
 		"created_at": "Thu Apr 02 14:43:27 +0000 2020",
 		"id": 1245723525643677700,
 		"id_str": "1245723525643677698",
