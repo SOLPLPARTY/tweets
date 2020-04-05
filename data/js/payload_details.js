e5bbc90acd9b5942d02f2694a44ba8c1631fc08a
솔플파티 <solplparty@gmail.com>
2020-04-06 06:17:39 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 37052,
-	"created_at": "Sun Apr 05 20:17:36 +0000 2020",
+	"tweets": 37053,
+	"created_at": "Sun Apr 05 21:17:37 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Sun Apr 05 20:25:37 +0000 2020",
+		"id": 1246657748030259200,
+		"id_str": "1246896797475209216",
+		"full_text": "[MV] 향니 (hyangni) - 핫소스 / Official Music Video https://t.co/PXx9j8rIi2 via @YouTube\n\n핫소스 뮤직비디오 40만회가 넘었어요!\n뮤직비디오를 감상해주신 분들, 람다람 작가님 정말 감사합니다. 멋지고 재밌는 일이에요!",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			155
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "YouTube",
+					"name": "YouTube #StayHome",
+					"id": 10228272,
+					"id_str": "10228272",
+					"indices": [
+						75,
+						83
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/PXx9j8rIi2",
+					"expanded_url": "https://youtu.be/5X5jE4MjZCs",
+					"display_url": "youtu.be/5X5jE4MjZCs",
+					"indices": [
+						47,
+						70
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 196570704,
+			"id_str": "196570704",
+			"name": "향니 hyangni 이지향",
+			"screen_name": "hyangni",
+			"location": "south korea",
+			"description": "향니 - 핫소스 MV",
+			"url": "https://t.co/6qAo1NdQRV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6qAo1NdQRV",
+							"expanded_url": "https://youtu.be/5X5jE4MjZCs",
+							"display_url": "youtu.be/5X5jE4MjZCs",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 941,
+			"friends_count": 232,
+			"listed_count": 11,
+			"created_at": "Wed Sep 29 12:34:33 +0000 2010",
+			"favourites_count": 404,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4407,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FF6699",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1197856466637443078/ed0B_rlD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1197856466637443078/ed0B_rlD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/196570704/1574426214",
+			"profile_link_color": "B40B43",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E5507E",
+			"profile_text_color": "362720",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 22,
+		"favorite_count": 54,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[MV] 향니 (hyangni) - 핫소스 / Official Music Video https://t.co/PXx9j8rIi2 via @YouTube\n\n핫소스 뮤직비디오 40만회가 넘었어요!\n뮤직비디오를 감상해주신 분들, 람다람 작가님 정말 감사합니다. 멋지고 재밌는 일이에요!"
+	},
+	{
 		"created_at": "Sun Apr 05 19:33:58 +0000 2020",
 		"id": 1246724258455326700,
 		"id_str": "1246883800979664896",
