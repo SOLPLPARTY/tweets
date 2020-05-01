6c9e90c37c29dcfeadfcef6afa1fe4d1dd38c5e2
솔플파티 <solplparty@gmail.com>
2020-05-01 12:17:34 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 38331,
-	"created_at": "Fri May 01 02:17:44 +0000 2020",
+	"tweets": 38333,
+	"created_at": "Fri May 01 03:17:32 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,422 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Fri May 01 03:00:34 +0000 2020",
+		"id": 1256055885614506000,
+		"id_str": "1256055885614505984",
+		"full_text": "#솔플파티 트위터 뉴스레터 (May 1) https://t.co/JpeKqFS0Op",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			46
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "솔플파티",
+					"indices": [
+						0,
+						5
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/JpeKqFS0Op",
+					"expanded_url": "https://nzzl.us/18UcAVF",
+					"display_url": "nzzl.us/18UcAVF",
+					"indices": [
+						23,
+						46
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://nuzzel.com/\" rel=\"nofollow\">Nuzzel</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 968706450779463700,
+			"id_str": "968706450779463682",
+			"name": "솔플파티",
+			"screen_name": "SOLPLPARTY",
+			"location": "문의 contact@solplparty.com",
+			"description": "🌈 문화예술 커뮤니티, 솔플파티\n\n공연일정 / 티켓예매 공지 / 페스티벌 / 인디밴드 / 내한공연 / 콘서트 / 홍대문화 / 영화제 / 전시회 / 북토크\n\n문화예술정보 @ArtManagementKR\n티켓오픈 @TicketOpen\n\n▶ https://t.co/mr16mdiyD3",
+			"url": "https://t.co/GCEfwO8QQD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GCEfwO8QQD",
+							"expanded_url": "http://solplparty.com",
+							"display_url": "solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/mr16mdiyD3",
+							"expanded_url": "http://join.solplparty.com",
+							"display_url": "join.solplparty.com",
+							"indices": [
+								131,
+								154
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 207,
+			"friends_count": 365,
+			"listed_count": 3,
+			"created_at": "Wed Feb 28 04:36:30 +0000 2018",
+			"favourites_count": 260,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 55649,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/968706450779463682/1519792691",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#솔플파티 트위터 뉴스레터 (May 1) https://t.co/JpeKqFS0Op"
+	},
+	{
+		"created_at": "Fri May 01 02:29:29 +0000 2020",
+		"id": 1256044546045079600,
+		"id_str": "1256048063640072192",
+		"full_text": "일렉트릭 뮤즈 머천다이즈 스토어 오픈💙💙💙\n\n공연장에서만 만날 수 있던 일렉트릭 뮤즈 머천다이즈를 온라인에서도 만날 수 있습니다. 티셔츠, 에코백, 버튼, 뱃지, 엽서 등 다양한 머천다이즈를 판매합니다. 오픈 기념 세이수미의 새로운 티셔츠가 첫 선을 보입니다🤩😍\n\nhttps://t.co/o4u7AdK9Wd https://t.co/fJL5KqvMYQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			171
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/o4u7AdK9Wd",
+					"expanded_url": "https://smartstore.naver.com/electricmuse",
+					"display_url": "smartstore.naver.com/electricmuse",
+					"indices": [
+						148,
+						171
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1256044532174475300,
+					"id_str": "1256044532174475264",
+					"indices": [
+						172,
+						195
+					],
+					"media_url": "http://pbs.twimg.com/media/EW5d2OJU0AAOl53.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW5d2OJU0AAOl53.jpg",
+					"url": "https://t.co/fJL5KqvMYQ",
+					"display_url": "pic.twitter.com/fJL5KqvMYQ",
+					"expanded_url": "https://twitter.com/electric_muse/status/1256044546045079552/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1256044532174475300,
+					"id_str": "1256044532174475264",
+					"indices": [
+						172,
+						195
+					],
+					"media_url": "http://pbs.twimg.com/media/EW5d2OJU0AAOl53.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW5d2OJU0AAOl53.jpg",
+					"url": "https://t.co/fJL5KqvMYQ",
+					"display_url": "pic.twitter.com/fJL5KqvMYQ",
+					"expanded_url": "https://twitter.com/electric_muse/status/1256044546045079552/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1256044532170256400,
+					"id_str": "1256044532170256384",
+					"indices": [
+						172,
+						195
+					],
+					"media_url": "http://pbs.twimg.com/media/EW5d2OIUcAAt3Zq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW5d2OIUcAAt3Zq.jpg",
+					"url": "https://t.co/fJL5KqvMYQ",
+					"display_url": "pic.twitter.com/fJL5KqvMYQ",
+					"expanded_url": "https://twitter.com/electric_muse/status/1256044546045079552/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1000,
+							"h": 998,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 998,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 679,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1256044532178669600,
+					"id_str": "1256044532178669568",
+					"indices": [
+						172,
+						195
+					],
+					"media_url": "http://pbs.twimg.com/media/EW5d2OKU0AAeHq-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW5d2OKU0AAeHq-.jpg",
+					"url": "https://t.co/fJL5KqvMYQ",
+					"display_url": "pic.twitter.com/fJL5KqvMYQ",
+					"expanded_url": "https://twitter.com/electric_muse/status/1256044546045079552/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1014,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 671,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1014,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1256044532170231800,
+					"id_str": "1256044532170231809",
+					"indices": [
+						172,
+						195
+					],
+					"media_url": "http://pbs.twimg.com/media/EW5d2OIUEAEvlgU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW5d2OIUEAEvlgU.jpg",
+					"url": "https://t.co/fJL5KqvMYQ",
+					"display_url": "pic.twitter.com/fJL5KqvMYQ",
+					"expanded_url": "https://twitter.com/electric_muse/status/1256044546045079552/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 49219598,
+			"id_str": "49219598",
+			"name": "Electric Muse",
+			"screen_name": "electric_muse",
+			"location": "",
+			"description": "We're indie record label in Seoul since 2006 / Contact electricmuse@naver.com",
+			"url": "https://t.co/Xm46jXAPqj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Xm46jXAPqj",
+							"expanded_url": "http://electricmuse.bandcamp.com",
+							"display_url": "electricmuse.bandcamp.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3436,
+			"friends_count": 1316,
+			"listed_count": 124,
+			"created_at": "Sun Jun 21 03:37:47 +0000 2009",
+			"favourites_count": 4790,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 24653,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/895149144909729792/0y_CTuqS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/895149144909729792/0y_CTuqS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/49219598/1502256266",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "일렉트릭 뮤즈 머천다이즈 스토어 오픈💙💙💙\n\n공연장에서만 만날 수 있던 일렉트릭 뮤즈 머천다이즈를 온라인에서도 만날 수 있습니다. 티셔츠, 에코백, 버튼, 뱃지, 엽서 등 다양한 머천다이즈를 판매합니다. 오픈 기념 세이수미의 새로운 티셔츠가 첫 선을 보입니다🤩😍\n\nhttps://t.co/o4u7AdK9Wd https://t.co/fJL5KqvMYQ"
+	},
+	{
 		"created_at": "Fri May 01 02:00:35 +0000 2020",
 		"id": 1256040790423490600,
 		"id_str": "1256040790423490563",
