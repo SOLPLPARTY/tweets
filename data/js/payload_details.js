79ab2bab3baca67ccbb4646bfc3159f669a91059
솔플파티 <solplparty@gmail.com>
2020-05-02 15:17:34 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 38364,
-	"created_at": "Sat May 02 05:17:33 +0000 2020",
+	"tweets": 38365,
+	"created_at": "Sat May 02 06:17:33 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,216 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sat May 02 05:41:42 +0000 2020",
+		"id": 1256457928829137000,
+		"id_str": "1256458824413782018",
+		"full_text": "[신규입고]\n🌟스트리트H vol.131\n\n홍대앞 동네 문화 잡지, 스트리트H 131호가 입고되었습니다‼️ 이번 호의 표지는 사슴책방 / 일러스트 포스터는 조선왕실 가례에 대한 이야기입니다👑\n\n그외에도 다양한 홍대앞 문화 소식들이 실려있으니\n씬디에 방문하셔서 무가지를 자유롭게 이용해보세요 •ᴗ• https://t.co/bQ7dYiuyeZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			164
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1256457796976996400,
+					"id_str": "1256457796976996353",
+					"indices": [
+						165,
+						188
+					],
+					"media_url": "http://pbs.twimg.com/media/EW_VtZgVcAE5f76.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW_VtZgVcAE5f76.jpg",
+					"url": "https://t.co/bQ7dYiuyeZ",
+					"display_url": "pic.twitter.com/bQ7dYiuyeZ",
+					"expanded_url": "https://twitter.com/xindieticket/status/1256457928829136898/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1256457796976996400,
+					"id_str": "1256457796976996353",
+					"indices": [
+						165,
+						188
+					],
+					"media_url": "http://pbs.twimg.com/media/EW_VtZgVcAE5f76.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW_VtZgVcAE5f76.jpg",
+					"url": "https://t.co/bQ7dYiuyeZ",
+					"display_url": "pic.twitter.com/bQ7dYiuyeZ",
+					"expanded_url": "https://twitter.com/xindieticket/status/1256457928829136898/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1256457825452126200,
+					"id_str": "1256457825452126208",
+					"indices": [
+						165,
+						188
+					],
+					"media_url": "http://pbs.twimg.com/media/EW_VvDlVcAAJcHw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW_VvDlVcAAJcHw.jpg",
+					"url": "https://t.co/bQ7dYiuyeZ",
+					"display_url": "pic.twitter.com/bQ7dYiuyeZ",
+					"expanded_url": "https://twitter.com/xindieticket/status/1256457928829136898/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1220731080,
+			"id_str": "1220731080",
+			"name": "씬디 티켓라운지",
+			"screen_name": "xindieticket",
+			"location": "서울 마포구 어울마당로 41-1 (홍대주차장길)",
+			"description": "홍대 공연정보를 한눈에 확인할 수 있는 온오프라인 티켓판매처\n\n※ 운영시간: 1-9PM / 저녁시간: 6-7PM\n☎ 02-322-2218",
+			"url": "https://t.co/dqX6O3D21p",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dqX6O3D21p",
+							"expanded_url": "http://xindieticket.kr",
+							"display_url": "xindieticket.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2833,
+			"friends_count": 2727,
+			"listed_count": 26,
+			"created_at": "Tue Feb 26 07:24:04 +0000 2013",
+			"favourites_count": 5142,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9240,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1200342617373827073/a1vB9pyg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1200342617373827073/a1vB9pyg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1220731080/1575026135",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[신규입고]\n🌟스트리트H vol.131\n\n홍대앞 동네 문화 잡지, 스트리트H 131호가 입고되었습니다‼️ 이번 호의 표지는 사슴책방 / 일러스트 포스터는 조선왕실 가례에 대한 이야기입니다👑\n\n그외에도 다양한 홍대앞 문화 소식들이 실려있으니\n씬디에 방문하셔서 무가지를 자유롭게 이용해보세요 •ᴗ• https://t.co/bQ7dYiuyeZ"
+	},
+	{
 		"created_at": "Sat May 02 05:16:33 +0000 2020",
 		"id": 1256452495481753600,
 		"id_str": "1256452495481753601",
