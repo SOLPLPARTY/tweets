17027cb84039ac96a7b55712c5438e23c4157301
솔플파티 <solplparty@gmail.com>
2020-05-02 09:17:34 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 38357,
-	"created_at": "Fri May 01 23:17:33 +0000 2020",
+	"tweets": 38358,
+	"created_at": "Sat May 02 00:17:33 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,141 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Fri May 01 23:29:25 +0000 2020",
+		"id": 1256361627995132000,
+		"id_str": "1256365137637801984",
+		"full_text": "from ‘jung jaeil in concert’\n\n#11.\n\nset, dool | 셋, 둘\n\n#정재일 #jungjaeil #푸리 #puri #사물놀이 #samulnori\n\nhttps://t.co/XyGQwJPWaI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			121
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "정재일",
+					"indices": [
+						54,
+						58
+					]
+				},
+				{
+					"text": "jungjaeil",
+					"indices": [
+						59,
+						69
+					]
+				},
+				{
+					"text": "푸리",
+					"indices": [
+						70,
+						73
+					]
+				},
+				{
+					"text": "puri",
+					"indices": [
+						74,
+						79
+					]
+				},
+				{
+					"text": "사물놀이",
+					"indices": [
+						80,
+						85
+					]
+				},
+				{
+					"text": "samulnori",
+					"indices": [
+						86,
+						96
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/XyGQwJPWaI",
+					"expanded_url": "https://youtu.be/Ug2PldM2xDc",
+					"display_url": "youtu.be/Ug2PldM2xDc",
+					"indices": [
+						98,
+						121
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 613235782,
+			"id_str": "613235782",
+			"name": "BLUE BOY",
+			"screen_name": "blueboy_kr",
+			"location": "",
+			"description": "언니네 이발관 / Wouter Hamel / 이자람 판소리 / 한승석&정재일 / [info.blueboy@gmail.com]",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1352,
+			"friends_count": 190,
+			"listed_count": 15,
+			"created_at": "Wed Jun 20 07:48:47 +0000 2012",
+			"favourites_count": 93,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1800,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2341430994/loegdwbpnd5j4vueh8iz_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2341430994/loegdwbpnd5j4vueh8iz_normal.jpeg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "from ‘jung jaeil in concert’\n\n#11.\n\nset, dool | 셋, 둘\n\n#정재일 #jungjaeil #푸리 #puri #사물놀이 #samulnori\n\nhttps://t.co/XyGQwJPWaI"
+	},
+	{
 		"created_at": "Fri May 01 22:48:21 +0000 2020",
 		"id": 1256237171813441500,
 		"id_str": "1256354801991864320",
