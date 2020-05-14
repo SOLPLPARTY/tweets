e43e6075b93b47de7bfdf0a4462204fa36127ac8
솔플파티 <solplparty@gmail.com>
2020-05-15 01:17:41 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 38922,
-	"created_at": "Thu May 14 15:17:38 +0000 2020",
+	"tweets": 38923,
+	"created_at": "Thu May 14 16:17:40 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,269 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Thu May 14 15:29:27 +0000 2020",
+		"id": 1260948044675027000,
+		"id_str": "1260955390969331714",
+		"full_text": "[📽] 류수정 (RYU SU JEONG) 1st Mini Album [Tiger Eyes] Track Video Vol.3\n2020.05.20 6PM RELEASE\n\n🖇YouTube ▶ https://t.co/YGcyZUPVST\n🖇NAVER V ▶ https://t.co/fp6YVWy0RD\n\n#Lovelyz #러블리즈 #RYUSUJEONG #류수정\n#Tiger_Eyes https://t.co/lpQXvVp027",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			207
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Lovelyz",
+					"indices": [
+						164,
+						172
+					]
+				},
+				{
+					"text": "러블리즈",
+					"indices": [
+						173,
+						178
+					]
+				},
+				{
+					"text": "RYUSUJEONG",
+					"indices": [
+						179,
+						190
+					]
+				},
+				{
+					"text": "류수정",
+					"indices": [
+						191,
+						195
+					]
+				},
+				{
+					"text": "Tiger_Eyes",
+					"indices": [
+						196,
+						207
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/YGcyZUPVST",
+					"expanded_url": "https://youtu.be/P3pjTK5885M",
+					"display_url": "youtu.be/P3pjTK5885M",
+					"indices": [
+						104,
+						127
+					]
+				},
+				{
+					"url": "https://t.co/fp6YVWy0RD",
+					"expanded_url": "https://www.vlive.tv/video/191178",
+					"display_url": "vlive.tv/video/191178",
+					"indices": [
+						139,
+						162
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1260928189158748200,
+					"id_str": "1260928189158748160",
+					"indices": [
+						208,
+						231
+					],
+					"media_url": "http://pbs.twimg.com/media/EX-4AwXUcAc28pA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EX-4AwXUcAc28pA.jpg",
+					"url": "https://t.co/lpQXvVp027",
+					"display_url": "pic.twitter.com/lpQXvVp027",
+					"expanded_url": "https://twitter.com/woollim_ent/status/1260948044675026944/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1260928189158748200,
+					"id_str": "1260928189158748160",
+					"indices": [
+						208,
+						231
+					],
+					"media_url": "http://pbs.twimg.com/media/EX-4AwXUcAc28pA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EX-4AwXUcAc28pA.jpg",
+					"url": "https://t.co/lpQXvVp027",
+					"display_url": "pic.twitter.com/lpQXvVp027",
+					"expanded_url": "https://twitter.com/woollim_ent/status/1260948044675026944/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 50918,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1260928189158748160/vid/1280x720/lZVZ88K5NPuuuyjO.mp4?tag=13"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1260928189158748160/vid/640x360/oovUfMaDCvZHgbqj.mp4?tag=13"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1260928189158748160/pl/QbfPrHtERpuYAoR1.m3u8?tag=13"
+							},
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1260928189158748160/vid/480x270/O27bLzYNY9fJQZSX.mp4?tag=13"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 713661673,
+			"id_str": "713661673",
+			"name": "woollim",
+			"screen_name": "woollim_ent",
+			"location": "",
+			"description": "#WoollimEntertainment\n\n#INFINITE \n#Lovelyz\n#Golden_Child\n#Rocket_Punch\n#W_PROJECT",
+			"url": "https://t.co/FVUD6x00cm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/FVUD6x00cm",
+							"expanded_url": "http://www.woolliment.com",
+							"display_url": "woolliment.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 413947,
+			"friends_count": 9,
+			"listed_count": 1789,
+			"created_at": "Tue Jul 24 04:15:30 +0000 2012",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 1846,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1102827421705748480/bJWoBAHC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1102827421705748480/bJWoBAHC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/713661673/1589295900",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 418,
+		"favorite_count": 704,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "[📽] 류수정 (RYU SU JEONG) 1st Mini Album [Tiger Eyes] Track Video Vol.3\n2020.05.20 6PM RELEASE\n\n🖇YouTube ▶ https://t.co/YGcyZUPVST\n🖇NAVER V ▶ https://t.co/fp6YVWy0RD\n\n#Lovelyz #러블리즈 #RYUSUJEONG #류수정\n#Tiger_Eyes https://t.co/lpQXvVp027"
+	},
+	{
 		"created_at": "Thu May 14 14:24:08 +0000 2020",
 		"id": 1260938954125324300,
 		"id_str": "1260938954125324288",
