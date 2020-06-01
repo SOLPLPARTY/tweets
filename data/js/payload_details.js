54b3c692107f4200f9c96fd3b8bf6246f3234818
솔플파티 <solplparty@gmail.com>
2020-06-01 11:17:42 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 39589,
-	"created_at": "Sun May 31 16:17:46 +0000 2020",
+	"tweets": 39590,
+	"created_at": "Mon Jun 01 02:17:41 +0000 2020",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2020_06.js
@@ -0,0 +1,187 @@
+Grailbird.data.tweets_2020_06 = 
+[
+	{
+		"created_at": "Mon Jun 01 01:29:31 +0000 2020",
+		"id": 1267260879407509500,
+		"id_str": "1267266996418359297",
+		"full_text": "모던보이 주간스케줄\n\"2020. 06. 01 ~ 2020. 06. 07\"\n\n스케줄 보러가기 👇\nhttps://t.co/IvPOc1746i https://t.co/BZ46yehdpF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			76
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/IvPOc1746i",
+					"expanded_url": "https://bit.ly/3gDJF0t",
+					"display_url": "bit.ly/3gDJF0t",
+					"indices": [
+						53,
+						76
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1267260865083924500,
+					"id_str": "1267260865083924481",
+					"indices": [
+						77,
+						100
+					],
+					"media_url": "http://pbs.twimg.com/media/EZY3CuUU4AEJ7DX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZY3CuUU4AEJ7DX.jpg",
+					"url": "https://t.co/BZ46yehdpF",
+					"display_url": "pic.twitter.com/BZ46yehdpF",
+					"expanded_url": "https://twitter.com/m_boyrecord/status/1267260879407509504/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 960,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 480,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1267260865083924500,
+					"id_str": "1267260865083924481",
+					"indices": [
+						77,
+						100
+					],
+					"media_url": "http://pbs.twimg.com/media/EZY3CuUU4AEJ7DX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZY3CuUU4AEJ7DX.jpg",
+					"url": "https://t.co/BZ46yehdpF",
+					"display_url": "pic.twitter.com/BZ46yehdpF",
+					"expanded_url": "https://twitter.com/m_boyrecord/status/1267260879407509504/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 960,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 480,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2177785068,
+			"id_str": "2177785068",
+			"name": "모던보이엔터테인먼트",
+			"screen_name": "m_boyrecord",
+			"location": "",
+			"description": "소속아티스트 몽니, 고고보이스, 숨의숲, 윤기타, 빨간의자, 로썸 / 뮤지컬배우 김신의",
+			"url": "https://t.co/wzug4Je0hj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wzug4Je0hj",
+							"expanded_url": "http://modernboyent.com",
+							"display_url": "modernboyent.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1288,
+			"friends_count": 1341,
+			"listed_count": 9,
+			"created_at": "Wed Nov 06 10:06:17 +0000 2013",
+			"favourites_count": 656,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2275,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/838929101633138688/35DKpUK6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/838929101633138688/35DKpUK6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2177785068/1561506274",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "모던보이 주간스케줄\n\"2020. 06. 01 ~ 2020. 06. 07\"\n\n스케줄 보러가기 👇\nhttps://t.co/IvPOc1746i https://t.co/BZ46yehdpF"
+	}
+]
