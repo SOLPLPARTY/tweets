8e58fa72afea5e0ff8ed618017c6eac67aad1098
솔플파티 <solplparty@gmail.com>
2020-06-01 14:17:34 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 39606,
-	"created_at": "Mon Jun 01 04:17:33 +0000 2020",
+	"tweets": 39607,
+	"created_at": "Mon Jun 01 05:17:33 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,137 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Mon Jun 01 05:17:26 +0000 2020",
+		"id": 1267324356465700900,
+		"id_str": "1267324356465700864",
+		"full_text": "#사운드클라우드 #SoundCloud\n아침 by Kwon Wol\nhttps://t.co/0UFJXZukEn",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			59
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "사운드클라우드",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"text": "SoundCloud",
+					"indices": [
+						9,
+						20
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/0UFJXZukEn",
+					"expanded_url": "https://bit.ly/3dqwnCw",
+					"display_url": "bit.ly/3dqwnCw",
+					"indices": [
+						36,
+						59
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 968706450779463700,
+			"id_str": "968706450779463682",
+			"name": "솔플파티",
+			"screen_name": "SOLPLPARTY",
+			"location": "문의: contact@solplparty.com",
+			"description": "🌈 문화예술 커뮤니티, 솔플파티\n\n공연일정 / 티켓예매 공지 / 페스티벌 / 인디밴드 / 내한공연 / 콘서트 / 홍대문화 / 영화제 / 전시회 / 북토크\n\n문화예술정보 @ArtManagementKR\n티켓오픈 @TicketOpen\n\n▶ https://t.co/mr16mdiyD3",
+			"url": "https://t.co/GCEfwO8QQD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GCEfwO8QQD",
+							"expanded_url": "http://solplparty.com",
+							"display_url": "solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/mr16mdiyD3",
+							"expanded_url": "http://join.solplparty.com",
+							"display_url": "join.solplparty.com",
+							"indices": [
+								131,
+								154
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 215,
+			"friends_count": 368,
+			"listed_count": 3,
+			"created_at": "Wed Feb 28 04:36:30 +0000 2018",
+			"favourites_count": 260,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 57210,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/968706450779463682/1519792691",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#사운드클라우드 #SoundCloud\n아침 by Kwon Wol\nhttps://t.co/0UFJXZukEn"
+	},
+	{
 		"created_at": "Mon Jun 01 04:04:26 +0000 2020",
 		"id": 1267305983736459300,
 		"id_str": "1267305983736459270",
@@ -1164,7 +1295,7 @@ Grailbird.data.tweets_2020_06 =
 		"created_at": "Mon Jun 01 03:29:25 +0000 2020",
 		"id": 1267282823846617000,
 		"id_str": "1267297169696047104",
-		"full_text": "[📸 #Tiktok]\n\n#이어달리기_챌린지 의 첫 주자는 러블리즈의 #메보 #케이 #수정 입니다💕\n앞으로도 #이어달리기_With_Woollim 해주세요🏃‍♀️🏃🏃‍♂️\n\n▶ https://t.co/vp3f4v6OLd\n\n#이어달리기 #With_Woollim #Relay_Challenge\n#Lovelyz",
+		"full_text": "[📸 #Tiktok]\n\n#이어달리기_챌린지 의 첫 주자는 러블리즈의 #메보 #케이 #수정 입니다💕\n앞으로도 #이어달리기_With_Woollim 해주세요🏃♀️🏃🏃♂️\n\n▶ https://t.co/vp3f4v6OLd\n\n#이어달리기 #With_Woollim #Relay_Challenge\n#Lovelyz",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -1335,7 +1466,7 @@ Grailbird.data.tweets_2020_06 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "[📸 #Tiktok]\n\n#이어달리기_챌린지 의 첫 주자는 러블리즈의 #메보 #케이 #수정 입니다💕\n앞으로도 #이어달리기_With_Woollim 해주세요🏃‍♀️🏃🏃‍♂️\n\n▶ https://t.co/vp3f4v6OLd\n\n#이어달리기 #With_Woollim #Relay_Challenge\n#Lovelyz"
+		"text": "[📸 #Tiktok]\n\n#이어달리기_챌린지 의 첫 주자는 러블리즈의 #메보 #케이 #수정 입니다💕\n앞으로도 #이어달리기_With_Woollim 해주세요🏃♀️🏃🏃♂️\n\n▶ https://t.co/vp3f4v6OLd\n\n#이어달리기 #With_Woollim #Relay_Challenge\n#Lovelyz"
 	},
 	{
 		"created_at": "Mon Jun 01 03:29:02 +0000 2020",
