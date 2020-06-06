3e1886a3c170a2c35fe651e04b5d57c031fd1287
솔플파티 <solplparty@gmail.com>
2020-06-06 10:17:39 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 39888,
-	"created_at": "Fri Jun 05 19:17:36 +0000 2020",
+	"tweets": 39889,
+	"created_at": "Sat Jun 06 01:17:38 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,141 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Sat Jun 06 00:24:24 +0000 2020",
+		"id": 1268740459129499600,
+		"id_str": "1269062547547684865",
+		"full_text": "SM 측 “샤이니 태민 7월 컴백, 신곡 발표+여러 활동 계획 중”(공식) (출처 : 뉴스엔 | 네이버 TV연예)\n\n👉 https://t.co/HDEPwTlEGd\n\n#TAEMIN #태민 #SHINee #샤이니",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			116
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TAEMIN",
+					"indices": [
+						92,
+						99
+					]
+				},
+				{
+					"text": "태민",
+					"indices": [
+						100,
+						103
+					]
+				},
+				{
+					"text": "SHINee",
+					"indices": [
+						104,
+						111
+					]
+				},
+				{
+					"text": "샤이니",
+					"indices": [
+						112,
+						116
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/HDEPwTlEGd",
+					"expanded_url": "http://naver.me/GEqqk996",
+					"display_url": "naver.me/GEqqk996",
+					"indices": [
+						67,
+						90
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 887973863824306200,
+			"id_str": "887973863824306176",
+			"name": "SHINee",
+			"screen_name": "SHINee",
+			"location": "SHINeeisBACK",
+			"description": "SHINee(샤이니) Official Twitter",
+			"url": "https://t.co/NAMin5AzxD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/NAMin5AzxD",
+							"expanded_url": "https://www.youtube.com/watch?v=_E5JtAcc3cY",
+							"display_url": "youtube.com/watch?v=_E5JtA…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1222812,
+			"friends_count": 2,
+			"listed_count": 2663,
+			"created_at": "Thu Jul 20 09:54:01 +0000 2017",
+			"favourites_count": 14,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 912,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1235407003884765184/YNAEKkPf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1235407003884765184/YNAEKkPf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/887973863824306176/1583378958",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12856,
+		"favorite_count": 28404,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "SM 측 “샤이니 태민 7월 컴백, 신곡 발표+여러 활동 계획 중”(공식) (출처 : 뉴스엔 | 네이버 TV연예)\n\n👉 https://t.co/HDEPwTlEGd\n\n#TAEMIN #태민 #SHINee #샤이니"
+	},
+	{
 		"created_at": "Fri Jun 05 18:51:52 +0000 2020",
 		"id": 1268978866472865800,
 		"id_str": "1268978866472865792",
