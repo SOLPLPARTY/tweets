eb3aab6b75a3d53c58a0f7ad02c4d6fc46758627
솔플파티 <solplparty@gmail.com>
2020-06-09 15:17:40 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 40000,
-	"created_at": "Tue Jun 09 05:17:37 +0000 2020",
+	"tweets": 40001,
+	"created_at": "Tue Jun 09 06:17:38 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Tue Jun 09 05:58:14 +0000 2020",
+		"id": 1270219270786441200,
+		"id_str": "1270233726950223872",
+		"full_text": "평화에 관한 가장 사적인 글 [Peace, blank]\n일곱번째 에세이 &lt;읽기, 박살 에너지, 평화&gt;\n글 | 김복희 (시인)\n전문 ▶ https://t.co/HNDlnqtblL\n*에세이 전문은 공식 홈페이지 CONTENTS 란에서 확인할 수 있습니다. https://t.co/fQTsNgPGJ8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			146
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/HNDlnqtblL",
+					"expanded_url": "https://bit.ly/30liGRM",
+					"display_url": "bit.ly/30liGRM",
+					"indices": [
+						81,
+						104
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1270219239962468400,
+					"id_str": "1270219239962468354",
+					"indices": [
+						147,
+						170
+					],
+					"media_url": "http://pbs.twimg.com/media/EaC5qzjUYAIqLOy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EaC5qzjUYAIqLOy.jpg",
+					"url": "https://t.co/fQTsNgPGJ8",
+					"display_url": "pic.twitter.com/fQTsNgPGJ8",
+					"expanded_url": "https://twitter.com/dmz_peacetrain/status/1270219270786441218/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 800,
+							"h": 600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1270219239962468400,
+					"id_str": "1270219239962468354",
+					"indices": [
+						147,
+						170
+					],
+					"media_url": "http://pbs.twimg.com/media/EaC5qzjUYAIqLOy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EaC5qzjUYAIqLOy.jpg",
+					"url": "https://t.co/fQTsNgPGJ8",
+					"display_url": "pic.twitter.com/fQTsNgPGJ8",
+					"expanded_url": "https://twitter.com/dmz_peacetrain/status/1270219270786441218/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 800,
+							"h": 600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 993804824138473500,
+			"id_str": "993804824138473476",
+			"name": "DMZ Peace Train Music Festival",
+			"screen_name": "dmz_peacetrain",
+			"location": "대한민국 철원군",
+			"description": "지금 우리, 눈부신 평화와 만날 때! 2020.7.18~7.19 Cheorwon, South Korea",
+			"url": "https://t.co/xX5SCptuBk",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xX5SCptuBk",
+							"expanded_url": "http://www.dmzpeacetrain.com",
+							"display_url": "dmzpeacetrain.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 574,
+			"friends_count": 37,
+			"listed_count": 6,
+			"created_at": "Tue May 08 10:48:28 +0000 2018",
+			"favourites_count": 169,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 473,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1215457877323698176/-84nQ6Mx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1215457877323698176/-84nQ6Mx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/993804824138473476/1581320908",
+			"profile_link_color": "007F5A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "평화에 관한 가장 사적인 글 [Peace, blank]\n일곱번째 에세이 &lt;읽기, 박살 에너지, 평화&gt;\n글 | 김복희 (시인)\n전문 ▶ https://t.co/HNDlnqtblL\n*에세이 전문은 공식 홈페이지 CONTENTS 란에서 확인할 수 있습니다. https://t.co/fQTsNgPGJ8"
+	},
+	{
 		"created_at": "Tue Jun 09 05:08:45 +0000 2020",
 		"id": 1270221272186859500,
 		"id_str": "1270221272186859526",
