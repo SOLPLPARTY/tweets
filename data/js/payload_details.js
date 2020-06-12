051bc9cab8c2fb93648c9306b76f70206d77adf8
솔플파티 <solplparty@gmail.com>
2020-06-12 11:17:40 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 40131,
-	"created_at": "Fri Jun 12 00:17:40 +0000 2020",
+	"tweets": 40132,
+	"created_at": "Fri Jun 12 02:17:39 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,289 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Fri Jun 12 01:29:34 +0000 2020",
+		"id": 1271246844639588400,
+		"id_str": "1271253275585773568",
+		"full_text": "📺드디어 이번 주말!\n#DownloadTV 가 라이브네이션 페이스북 페이지를 통해 찾아옵니다!\n단 한번뿐인 스트리밍을 놓치지 마세요🔥\n-\n#내한공연 #라이브네이션코리아 #라네코 #LiveNationKorea #LNK #라이브네이션 #LiveNation #LoveOfLive #LiveFromHome #TogetherApart https://t.co/OmsXOUMpGr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			181
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DownloadTV",
+					"indices": [
+						12,
+						23
+					]
+				},
+				{
+					"text": "내한공연",
+					"indices": [
+						77,
+						82
+					]
+				},
+				{
+					"text": "라이브네이션코리아",
+					"indices": [
+						83,
+						93
+					]
+				},
+				{
+					"text": "라네코",
+					"indices": [
+						94,
+						98
+					]
+				},
+				{
+					"text": "LiveNationKorea",
+					"indices": [
+						99,
+						115
+					]
+				},
+				{
+					"text": "LNK",
+					"indices": [
+						116,
+						120
+					]
+				},
+				{
+					"text": "라이브네이션",
+					"indices": [
+						121,
+						128
+					]
+				},
+				{
+					"text": "LiveNation",
+					"indices": [
+						129,
+						140
+					]
+				},
+				{
+					"text": "LoveOfLive",
+					"indices": [
+						141,
+						152
+					]
+				},
+				{
+					"text": "LiveFromHome",
+					"indices": [
+						153,
+						166
+					]
+				},
+				{
+					"text": "TogetherApart",
+					"indices": [
+						167,
+						181
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1271246817586409500,
+					"id_str": "1271246817586409472",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1271246817586409472/pu/img/7YiEIqQRUYYTn12N.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1271246817586409472/pu/img/7YiEIqQRUYYTn12N.jpg",
+					"url": "https://t.co/OmsXOUMpGr",
+					"display_url": "pic.twitter.com/OmsXOUMpGr",
+					"expanded_url": "https://twitter.com/livenationkr/status/1271246844639588360/video/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1271246817586409500,
+					"id_str": "1271246817586409472",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1271246817586409472/pu/img/7YiEIqQRUYYTn12N.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1271246817586409472/pu/img/7YiEIqQRUYYTn12N.jpg",
+					"url": "https://t.co/OmsXOUMpGr",
+					"display_url": "pic.twitter.com/OmsXOUMpGr",
+					"expanded_url": "https://twitter.com/livenationkr/status/1271246844639588360/video/1",
+					"type": "video",
+					"sizes": {
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 15000,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1271246817586409472/pu/vid/480x480/e_3V9oyJEzDgq8gO.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1271246817586409472/pu/pl/NlWJ6Bm6PwBLbNT6.m3u8?tag=10"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1271246817586409472/pu/vid/320x320/3EkSxTimSRqWeOTF.mp4?tag=10"
+							},
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1271246817586409472/pu/vid/640x640/4GZerAFrJsTZpvTN.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 236888084,
+			"id_str": "236888084",
+			"name": "LIVE NATION KOREA",
+			"screen_name": "livenationkr",
+			"location": "Seoul, South Korea",
+			"description": "라이브네이션코리아 공식 트위터. Paul McCartney, Maroon5, Jason Mraz, Coldplay, MUSE, 등 해외 유명아티스트의 내한공연과 BIGBANG, Blackpink,BTS 등의 대규모 월드투어를 기획한 공연기획사. FOLLOW US!",
+			"url": "http://t.co/WZhcKy3xE8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/WZhcKy3xE8",
+							"expanded_url": "http://www.livenation.kr",
+							"display_url": "livenation.kr",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6808,
+			"friends_count": 232,
+			"listed_count": 28,
+			"created_at": "Tue Jan 11 16:04:41 +0000 2011",
+			"favourites_count": 602,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1954,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/820816720998764545/T7P3FpWQ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/820816720998764545/T7P3FpWQ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/236888084/1475559466",
+			"profile_link_color": "E21836",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "📺드디어 이번 주말!\n#DownloadTV 가 라이브네이션 페이스북 페이지를 통해 찾아옵니다!\n단 한번뿐인 스트리밍을 놓치지 마세요🔥\n-\n#내한공연 #라이브네이션코리아 #라네코 #LiveNationKorea #LNK #라이브네이션 #LiveNation #LoveOfLive #LiveFromHome #TogetherApart https://t.co/OmsXOUMpGr"
+	},
+	{
 		"created_at": "Fri Jun 12 00:05:35 +0000 2020",
 		"id": 1271232140974440400,
 		"id_str": "1271232140974440450",
