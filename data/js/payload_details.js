c7b2163c3d4d555268028bec2c56d17af0cb8077
솔플파티 <solplparty@gmail.com>
2020-06-13 11:17:40 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 40191,
-	"created_at": "Sat Jun 13 01:17:41 +0000 2020",
+	"tweets": 40192,
+	"created_at": "Sat Jun 13 02:17:39 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,141 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Sat Jun 13 01:29:25 +0000 2020",
+		"id": 1271608216435097600,
+		"id_str": "1271615625195073542",
+		"full_text": "[기사] 정승환 ‘언제라도 어디에서라도’ 미리듣기 오픈, 초여름 감성 담았다 \n(출처 : 뉴스엔 | 네이버 TV연예) \nhttps://t.co/6uBsYzfn8H\n\n#정승환 #언제라도어디에서라도\n#JungSeungHwan #WheneverWherever",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			141
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "정승환",
+					"indices": [
+						92,
+						96
+					]
+				},
+				{
+					"text": "언제라도어디에서라도",
+					"indices": [
+						97,
+						108
+					]
+				},
+				{
+					"text": "JungSeungHwan",
+					"indices": [
+						109,
+						123
+					]
+				},
+				{
+					"text": "WheneverWherever",
+					"indices": [
+						124,
+						141
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/6uBsYzfn8H",
+					"expanded_url": "http://naver.me/xT7blRNQ",
+					"display_url": "naver.me/xT7blRNQ",
+					"indices": [
+						67,
+						90
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 266004552,
+			"id_str": "266004552",
+			"name": "안테나 Antenna",
+			"screen_name": "antennamusic",
+			"location": "대한민국 서울",
+			"description": "Jung Jae Hyung, You Hee Yul(Toy), Lucid Fall, Peppertones, Park Sae Byul, Kwon Jin Ah, Sam Kim, Jung Seung Hwan, Lee Jin Ah, CHAI, Yun Seokcheol _ Official",
+			"url": "https://t.co/CqfXN7It7U",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/CqfXN7It7U",
+							"expanded_url": "http://www.antenna.co.kr",
+							"display_url": "antenna.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 34224,
+			"friends_count": 11,
+			"listed_count": 369,
+			"created_at": "Mon Mar 14 13:49:27 +0000 2011",
+			"favourites_count": 12,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3115,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266004552/1591934172",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13,
+		"favorite_count": 25,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[기사] 정승환 ‘언제라도 어디에서라도’ 미리듣기 오픈, 초여름 감성 담았다 \n(출처 : 뉴스엔 | 네이버 TV연예) \nhttps://t.co/6uBsYzfn8H\n\n#정승환 #언제라도어디에서라도\n#JungSeungHwan #WheneverWherever"
+	},
+	{
 		"created_at": "Sat Jun 13 00:29:33 +0000 2020",
 		"id": 1271598981642981400,
 		"id_str": "1271600559905280002",
