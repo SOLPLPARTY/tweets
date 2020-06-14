4e79c91b62da5851ece9b9e2c9b524e86f0a4220
솔플파티 <solplparty@gmail.com>
2020-06-14 20:17:40 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 40225,
-	"created_at": "Sun Jun 14 10:17:39 +0000 2020",
+	"tweets": 40226,
+	"created_at": "Sun Jun 14 11:17:39 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,216 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Sun Jun 14 10:57:36 +0000 2020",
+		"id": 1272090942783144000,
+		"id_str": "1272121003900989440",
+		"full_text": "[NOTICE]\n안녕하세요. 캐스퍼 라디오입니다.\n그동안 저희 라디오를 사랑해 주신 여러분들께 아쉬운 소식을 전하고자 합니다. https://t.co/4zkeBzYMEb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			70
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1272090933362700300,
+					"id_str": "1272090933362700288",
+					"indices": [
+						71,
+						94
+					],
+					"media_url": "http://pbs.twimg.com/media/Eadf9s0UMAAYxl6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eadf9s0UMAAYxl6.jpg",
+					"url": "https://t.co/4zkeBzYMEb",
+					"display_url": "pic.twitter.com/4zkeBzYMEb",
+					"expanded_url": "https://twitter.com/casperradio_/status/1272090942783143937/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1272090933362700300,
+					"id_str": "1272090933362700288",
+					"indices": [
+						71,
+						94
+					],
+					"media_url": "http://pbs.twimg.com/media/Eadf9s0UMAAYxl6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eadf9s0UMAAYxl6.jpg",
+					"url": "https://t.co/4zkeBzYMEb",
+					"display_url": "pic.twitter.com/4zkeBzYMEb",
+					"expanded_url": "https://twitter.com/casperradio_/status/1272090942783143937/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1272090933362692000,
+					"id_str": "1272090933362692098",
+					"indices": [
+						71,
+						94
+					],
+					"media_url": "http://pbs.twimg.com/media/Eadf9s0UEAIQmCp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eadf9s0UEAIQmCp.jpg",
+					"url": "https://t.co/4zkeBzYMEb",
+					"display_url": "pic.twitter.com/4zkeBzYMEb",
+					"expanded_url": "https://twitter.com/casperradio_/status/1272090942783143937/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1022765899126730800,
+			"id_str": "1022765899126730752",
+			"name": "CASPERRADIO",
+			"screen_name": "casperradio_",
+			"location": "",
+			"description": "CASPER RADIO Official Twitter ✨ 키노의 키Sing부스/ 권정열고영배의 십란한밤/ 딕펑스의 비밀상담소/김간지 나잠수의 뮤직코리아/ Best New K-Pop / 𝙈𝙄𝙓𝙏𝘼𝙋𝙀 with special DJ",
+			"url": "https://t.co/nXQtEyzRyE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/nXQtEyzRyE",
+							"expanded_url": "http://www.youtube.com/caspervideomusic",
+							"display_url": "youtube.com/caspervideomus…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3405,
+			"friends_count": 8,
+			"listed_count": 11,
+			"created_at": "Fri Jul 27 08:49:27 +0000 2018",
+			"favourites_count": 923,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 763,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1224696571901566976/Jk6CvjUR_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1224696571901566976/Jk6CvjUR_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1022765899126730752/1576143782",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 632,
+		"favorite_count": 834,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[NOTICE]\n안녕하세요. 캐스퍼 라디오입니다.\n그동안 저희 라디오를 사랑해 주신 여러분들께 아쉬운 소식을 전하고자 합니다. https://t.co/4zkeBzYMEb"
+	},
+	{
 		"created_at": "Sun Jun 14 10:03:42 +0000 2020",
 		"id": 1272107439006126000,
 		"id_str": "1272107439006126080",
