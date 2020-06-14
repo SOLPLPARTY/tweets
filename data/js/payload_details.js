b1376fea4a7158eea799b12e0b0aa2f4ad0e19da
솔플파티 <solplparty@gmail.com>
2020-06-14 13:17:44 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 40219,
-	"created_at": "Sun Jun 14 03:17:39 +0000 2020",
+	"tweets": 40220,
+	"created_at": "Sun Jun 14 04:17:43 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Sun Jun 14 03:58:15 +0000 2020",
+		"id": 1272011953620127700,
+		"id_str": "1272015467725705216",
+		"full_text": "☮ PEACE CONNECTED WITH [STARCRAWLER]\n\n화상 인터뷰 보러가기📺 https://t.co/uXfghoQrJc\n\nLA 사막 한가운데서 자가격리 기간을 보내고 있는 스타크롤러가 피스트레인에 인터뷰로 함께 해주었습니다. 스타크롤러에게 페스티벌 무대, 퍼포먼스, 첫 내한, 그리고 평화는 무슨 의미일까요? https://t.co/8rrpaD0iWe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			180
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/uXfghoQrJc",
+					"expanded_url": "https://youtu.be/ZHc6yvmsMvc",
+					"display_url": "youtu.be/ZHc6yvmsMvc",
+					"indices": [
+						51,
+						74
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1272011946066206700,
+					"id_str": "1272011946066206722",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/media/EacYICNUYAIWZQp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EacYICNUYAIWZQp.jpg",
+					"url": "https://t.co/8rrpaD0iWe",
+					"display_url": "pic.twitter.com/8rrpaD0iWe",
+					"expanded_url": "https://twitter.com/dmz_peacetrain/status/1272011953620127744/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1272011946066206700,
+					"id_str": "1272011946066206722",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/media/EacYICNUYAIWZQp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EacYICNUYAIWZQp.jpg",
+					"url": "https://t.co/8rrpaD0iWe",
+					"display_url": "pic.twitter.com/8rrpaD0iWe",
+					"expanded_url": "https://twitter.com/dmz_peacetrain/status/1272011953620127744/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 993804824138473500,
+			"id_str": "993804824138473476",
+			"name": "DMZ Peace Train Music Festival",
+			"screen_name": "dmz_peacetrain",
+			"location": "대한민국 철원군",
+			"description": "지금 우리, 눈부신 평화와 만날 때! 2020.7.18~7.19 Cheorwon, South Korea",
+			"url": "https://t.co/xX5SCptuBk",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xX5SCptuBk",
+							"expanded_url": "http://www.dmzpeacetrain.com",
+							"display_url": "dmzpeacetrain.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 577,
+			"friends_count": 38,
+			"listed_count": 6,
+			"created_at": "Tue May 08 10:48:28 +0000 2018",
+			"favourites_count": 170,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 491,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1215457877323698176/-84nQ6Mx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1215457877323698176/-84nQ6Mx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/993804824138473476/1581320908",
+			"profile_link_color": "007F5A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "☮ PEACE CONNECTED WITH [STARCRAWLER]\n\n화상 인터뷰 보러가기📺 https://t.co/uXfghoQrJc\n\nLA 사막 한가운데서 자가격리 기간을 보내고 있는 스타크롤러가 피스트레인에 인터뷰로 함께 해주었습니다. 스타크롤러에게 페스티벌 무대, 퍼포먼스, 첫 내한, 그리고 평화는 무슨 의미일까요? https://t.co/8rrpaD0iWe"
+	},
+	{
 		"created_at": "Sun Jun 14 03:11:06 +0000 2020",
 		"id": 1272003604241678300,
 		"id_str": "1272003604241678341",
