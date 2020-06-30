b4317005a29f024068bf7f272e34a341186d000d
솔플파티 <solplparty@gmail.com>
2020-06-30 20:17:52 +0900

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 40870,
-	"created_at": "Tue Jun 30 10:17:49 +0000 2020",
+	"tweets": 40873,
+	"created_at": "Tue Jun 30 11:17:50 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2020_06.js",
 		"year": 2020,
 		"var_name": "tweets_2020_06",
-		"tweet_count": 1281,
+		"tweet_count": 1284,
 		"month": 6
 	},
 	{
