870abcf15f95b2153f0145c28f2c7721110ebf95
솔플파티 <solplparty@gmail.com>
2020-07-24 00:17:45 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 41706,
-	"created_at": "Thu Jul 23 14:17:41 +0000 2020",
+	"tweets": 41707,
+	"created_at": "Thu Jul 23 15:17:43 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Thu Jul 23 14:41:45 +0000 2020",
+		"id": 1286299681652224000,
+		"id_str": "1286310536615952386",
+		"full_text": "Always Like This  @FF \n\n2020/07/24 (Fri)  7pm Start \n\n공연순서! \n긍정편향-차세대-헤이맨-잭킹콩-라쿠나\n\nCover : 25000 won \n(현매로만 진행 70명 한정)\n\n클럽입장을  6pm 에서 6:30pm 으로 수정합니다.\n\n마스크 미치참시 입장불가\nQR 코드 내지  출입 명단 미작성시  입장불가 https://t.co/QGz2nyKOtq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			192
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ff",
+					"name": "ff",
+					"id": 45513,
+					"id_str": "45513",
+					"indices": [
+						18,
+						21
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1286299672688988200,
+					"id_str": "1286299672688988161",
+					"indices": [
+						193,
+						216
+					],
+					"media_url": "http://pbs.twimg.com/media/EdnavLlUEAERhVb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EdnavLlUEAERhVb.jpg",
+					"url": "https://t.co/QGz2nyKOtq",
+					"display_url": "pic.twitter.com/QGz2nyKOtq",
+					"expanded_url": "https://twitter.com/clubFF_/status/1286299681652224001/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1286299672688988200,
+					"id_str": "1286299672688988161",
+					"indices": [
+						193,
+						216
+					],
+					"media_url": "http://pbs.twimg.com/media/EdnavLlUEAERhVb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EdnavLlUEAERhVb.jpg",
+					"url": "https://t.co/QGz2nyKOtq",
+					"display_url": "pic.twitter.com/QGz2nyKOtq",
+					"expanded_url": "https://twitter.com/clubFF_/status/1286299681652224001/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 961,
+							"h": 961,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 455224965,
+			"id_str": "455224965",
+			"name": "클럽에프에프",
+			"screen_name": "clubFF_",
+			"location": "서울 마포구 서교동 407-8 지하1층",
+			"description": "홍대 최고의 NO.1 Indie experience 라이브 & 댄스클럽 에프에프입니다!! 엪엪의 대관 및 오디션 문의(프로필,음원,연락처 필수)는  010-9025-3407 과 ianbrown@naver.com 으로 .",
+			"url": "https://t.co/dmU2vJP8pS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dmU2vJP8pS",
+							"expanded_url": "http://clubff.modoo.at",
+							"display_url": "clubff.modoo.at",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6372,
+			"friends_count": 7,
+			"listed_count": 79,
+			"created_at": "Wed Jan 04 21:41:48 +0000 2012",
+			"favourites_count": 559,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3736,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2263880419/image_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2263880419/image_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "Always Like This  @FF \n\n2020/07/24 (Fri)  7pm Start \n\n공연순서! \n긍정편향-차세대-헤이맨-잭킹콩-라쿠나\n\nCover : 25000 won \n(현매로만 진행 70명 한정)\n\n클럽입장을  6pm 에서 6:30pm 으로 수정합니다.\n\n마스크 미치참시 입장불가\nQR 코드 내지  출입 명단 미작성시  입장불가 https://t.co/QGz2nyKOtq"
+	},
+	{
 		"created_at": "Thu Jul 23 13:26:03 +0000 2020",
 		"id": 1286291488897368000,
 		"id_str": "1286291488897368065",
