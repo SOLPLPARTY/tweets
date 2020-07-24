65b84017809c998f327e00c9ed93947e41ebd281
솔플파티 <solplparty@gmail.com>
2020-07-24 17:17:50 +0900

    Added 1 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2020_07.js",
 		"year": 2020,
 		"var_name": "tweets_2020_07",
-		"tweet_count": 854,
+		"tweet_count": 855,
 		"month": 7
 	},
 	{
