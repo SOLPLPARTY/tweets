9099745a6ded3f9472e350bc32db2f3fcc67ad18
솔플파티 <solplparty@gmail.com>
2020-07-25 20:17:44 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 41768,
-	"created_at": "Sat Jul 25 10:17:46 +0000 2020",
+	"tweets": 41769,
+	"created_at": "Sat Jul 25 11:17:42 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,216 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Sat Jul 25 10:29:25 +0000 2020",
+		"id": 1286958105830412300,
+		"id_str": "1286971811444465664",
+		"full_text": "[#조원선] OCN📺 토일 오리지널 '트레인'의 첫 번째 OST, 조원선이 부른🎤 'I Will Never'가 공개되었습니다🔊 전 음원사이트를 통해 만나보세요🎶\n\n#ocn #드라마 #트레인 #IWillNever https://t.co/SO1fbysQPt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			117
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "조원선",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "ocn",
+					"indices": [
+						91,
+						95
+					]
+				},
+				{
+					"text": "드라마",
+					"indices": [
+						96,
+						100
+					]
+				},
+				{
+					"text": "트레인",
+					"indices": [
+						101,
+						105
+					]
+				},
+				{
+					"text": "IWillNever",
+					"indices": [
+						106,
+						117
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1286958083076313000,
+					"id_str": "1286958083076313089",
+					"indices": [
+						118,
+						141
+					],
+					"media_url": "http://pbs.twimg.com/media/EdwxjtaU0AEtzMX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EdwxjtaU0AEtzMX.jpg",
+					"url": "https://t.co/SO1fbysQPt",
+					"display_url": "pic.twitter.com/SO1fbysQPt",
+					"expanded_url": "https://twitter.com/themysticstory/status/1286958105830412288/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1286958083076313000,
+					"id_str": "1286958083076313089",
+					"indices": [
+						118,
+						141
+					],
+					"media_url": "http://pbs.twimg.com/media/EdwxjtaU0AEtzMX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EdwxjtaU0AEtzMX.jpg",
+					"url": "https://t.co/SO1fbysQPt",
+					"display_url": "pic.twitter.com/SO1fbysQPt",
+					"expanded_url": "https://twitter.com/themysticstory/status/1286958105830412288/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/BKUIHw4jho",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BKUIHw4jho",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 19752,
+			"friends_count": 18,
+			"listed_count": 231,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5256,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1553488269",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#조원선] OCN📺 토일 오리지널 '트레인'의 첫 번째 OST, 조원선이 부른🎤 'I Will Never'가 공개되었습니다🔊 전 음원사이트를 통해 만나보세요🎶\n\n#ocn #드라마 #트레인 #IWillNever https://t.co/SO1fbysQPt"
+	},
+	{
 		"created_at": "Sat Jul 25 09:46:15 +0000 2020",
 		"id": 1286960948813717500,
 		"id_str": "1286960948813717505",
