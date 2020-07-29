f1472366f93091940dcb929d846edcefadaca82d
솔플파티 <solplparty@gmail.com>
2020-07-29 11:17:48 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 41882,
-	"created_at": "Tue Jul 28 17:17:47 +0000 2020",
+	"tweets": 41883,
+	"created_at": "Wed Jul 29 02:17:46 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2020_07.js",
 		"year": 2020,
 		"var_name": "tweets_2020_07",
-		"tweet_count": 1004,
+		"tweet_count": 1005,
 		"month": 7
 	},
 	{
