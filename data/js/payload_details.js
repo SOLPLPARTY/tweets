a61b7c4233568fb0e762fa618e8263aa60f94ecb
솔플파티 <solplparty@gmail.com>
2020-08-03 11:17:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 42074,
-	"created_at": "Sun Aug 02 17:17:34 +0000 2020",
+	"tweets": 42075,
+	"created_at": "Mon Aug 03 02:17:34 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,175 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Mon Aug 03 01:29:34 +0000 2020",
+		"id": 1290090428872528000,
+		"id_str": "1290097444370305024",
+		"full_text": "디어클라우드, 8일 새 싱글 '러버!(LOVER!)' 기습 발매 [공식] (출처 : 스포츠동아 | 네이버 TV연예) \n\n#디어클라우드 #8월8일싱글발매\n제목은 #러버! #LOVER!\n#사랑하는ㅇㅇㅇ에게\n#나인 #이랑 #토근 #dearcloud\n\nhttps://t.co/QdA8eDjd1d",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			160
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "디어클라우드",
+					"indices": [
+						67,
+						74
+					]
+				},
+				{
+					"text": "8월8일싱글발매",
+					"indices": [
+						75,
+						84
+					]
+				},
+				{
+					"text": "러버",
+					"indices": [
+						89,
+						92
+					]
+				},
+				{
+					"text": "LOVER",
+					"indices": [
+						94,
+						100
+					]
+				},
+				{
+					"text": "사랑하는ㅇㅇㅇ에게",
+					"indices": [
+						102,
+						112
+					]
+				},
+				{
+					"text": "나인",
+					"indices": [
+						113,
+						116
+					]
+				},
+				{
+					"text": "이랑",
+					"indices": [
+						117,
+						120
+					]
+				},
+				{
+					"text": "토근",
+					"indices": [
+						121,
+						124
+					]
+				},
+				{
+					"text": "dearcloud",
+					"indices": [
+						125,
+						135
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/QdA8eDjd1d",
+					"expanded_url": "http://naver.me/xKJ1pnnf",
+					"display_url": "naver.me/xKJ1pnnf",
+					"indices": [
+						137,
+						160
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 150566482,
+			"id_str": "150566482",
+			"name": "엠와이뮤직(MYmusic Ent)",
+			"screen_name": "messenger_yoon",
+			"location": "Artist Manager (South Korea)",
+			"description": "[Musician] 정준일, 낭만유랑악단, 나인(Nine9), 디어클라우드, 권영찬, 위아영 //mymusicent@naver.com//+82)2.6013.0102",
+			"url": "https://t.co/1IfWMp2Je9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1IfWMp2Je9",
+							"expanded_url": "http://www.mymusicent.co.kr",
+							"display_url": "mymusicent.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2700,
+			"friends_count": 355,
+			"listed_count": 33,
+			"created_at": "Tue Jun 01 07:29:53 +0000 2010",
+			"favourites_count": 16024,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5487,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/540007978649128960/jSz5oRO7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/540007978649128960/jSz5oRO7_normal.jpeg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "디어클라우드, 8일 새 싱글 '러버!(LOVER!)' 기습 발매 [공식] (출처 : 스포츠동아 | 네이버 TV연예) \n\n#디어클라우드 #8월8일싱글발매\n제목은 #러버! #LOVER!\n#사랑하는ㅇㅇㅇ에게\n#나인 #이랑 #토근 #dearcloud\n\nhttps://t.co/QdA8eDjd1d"
+	},
+	{
 		"created_at": "Sun Aug 02 17:16:13 +0000 2020",
 		"id": 1289973291290001400,
 		"id_str": "1289973291290001409",
