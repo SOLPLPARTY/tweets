ca92d29e31320a2d26365e6920d221a30932496d
솔플파티 <solplparty@gmail.com>
2020-08-16 18:17:40 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 42669,
-	"created_at": "Sun Aug 16 08:17:41 +0000 2020",
+	"tweets": 42670,
+	"created_at": "Sun Aug 16 09:17:39 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,100 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Sun Aug 16 09:05:02 +0000 2020",
+		"id": 1294919158119805000,
+		"id_str": "1294923110647394304",
+		"full_text": "안녕하세요, 데드버튼즈의 공식 트위터 계정입니다. 앞으로 트위터에서도 소식 만나보실 수 있어요. Hi, we’re Deadbuttons. Nice to meet you here!",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			99
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1294915308386361300,
+			"id_str": "1294915308386361345",
+			"name": "Deadbuttons",
+			"screen_name": "deadbuttons",
+			"location": "",
+			"description": "데드버튼즈 Psych-punk band from Seoul, South Korea New album ‘1’ (2020) available on music streaming services",
+			"url": "https://t.co/pDQ72GnhnL",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pDQ72GnhnL",
+							"expanded_url": "http://deadbuttons.bandcamp.com",
+							"display_url": "deadbuttons.bandcamp.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11,
+			"friends_count": 32,
+			"listed_count": 0,
+			"created_at": "Sun Aug 16 08:34:40 +0000 2020",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1294915738784813058/P7cODi1__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1294915738784813058/P7cODi1__normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "안녕하세요, 데드버튼즈의 공식 트위터 계정입니다. 앞으로 트위터에서도 소식 만나보실 수 있어요. Hi, we’re Deadbuttons. Nice to meet you here!"
+	},
+	{
 		"created_at": "Sun Aug 16 08:05:56 +0000 2020",
 		"id": 1294248285566193700,
 		"id_str": "1294908237460828160",
