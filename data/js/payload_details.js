07626c2e7231c86d6681e4958b94f94f78e52312
솔플파티 <solplparty@gmail.com>
2020-09-13 00:17:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 43528,
-	"created_at": "Sat Sep 12 11:17:35 +0000 2020",
+	"tweets": 43529,
+	"created_at": "Sat Sep 12 15:17:35 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,219 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Sat Sep 12 15:06:25 +0000 2020",
+		"id": 1304740154691600400,
+		"id_str": "1304798528766504961",
+		"full_text": "[Official]\n[Colors] LP (Repackage) 일반반 추가 제작 안내\n\n자세한 내용은 아래 링크를 참고해주세요.\nhttps://t.co/VNpxSmACw0\n\n#스텔라장 #Stellajang #Colors #LP발매 https://t.co/0StFzY5Kb8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			128
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "스텔라장",
+					"indices": [
+						97,
+						102
+					]
+				},
+				{
+					"text": "Stellajang",
+					"indices": [
+						103,
+						114
+					]
+				},
+				{
+					"text": "Colors",
+					"indices": [
+						115,
+						122
+					]
+				},
+				{
+					"text": "LP발매",
+					"indices": [
+						123,
+						128
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/VNpxSmACw0",
+					"expanded_url": "https://www.instagram.com/p/CFCL2Z4li-x/",
+					"display_url": "instagram.com/p/CFCL2Z4li-x/",
+					"indices": [
+						72,
+						95
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1304739805008257000,
+					"id_str": "1304739805008257026",
+					"indices": [
+						129,
+						152
+					],
+					"media_url": "http://pbs.twimg.com/media/Ehtd8JnUYAIKc9u.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ehtd8JnUYAIKc9u.jpg",
+					"url": "https://t.co/0StFzY5Kb8",
+					"display_url": "pic.twitter.com/0StFzY5Kb8",
+					"expanded_url": "https://twitter.com/interstellajang/status/1304740154691600384/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1304739805008257000,
+					"id_str": "1304739805008257026",
+					"indices": [
+						129,
+						152
+					],
+					"media_url": "http://pbs.twimg.com/media/Ehtd8JnUYAIKc9u.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ehtd8JnUYAIKc9u.jpg",
+					"url": "https://t.co/0StFzY5Kb8",
+					"display_url": "pic.twitter.com/0StFzY5Kb8",
+					"expanded_url": "https://twitter.com/interstellajang/status/1304740154691600384/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1460678490,
+			"id_str": "1460678490",
+			"name": "스텔라장 Stella Jang",
+			"screen_name": "interstellajang",
+			"location": "대한민국",
+			"description": "Official account💙 Stella Jang's IG : @interstellajang",
+			"url": "https://t.co/hwB1hpI2gc",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hwB1hpI2gc",
+							"expanded_url": "https://www.youtube.com/stellajangtv",
+							"display_url": "youtube.com/stellajangtv",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2409,
+			"friends_count": 0,
+			"listed_count": 23,
+			"created_at": "Sun May 26 21:40:33 +0000 2013",
+			"favourites_count": 1328,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 290,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1247894921274470400/rFYWSz9S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1247894921274470400/rFYWSz9S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1460678490/1586356311",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 20,
+		"favorite_count": 40,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": true,
+		"lang": "ko",
+		"text": "[Official]\n[Colors] LP (Repackage) 일반반 추가 제작 안내\n\n자세한 내용은 아래 링크를 참고해주세요.\nhttps://t.co/VNpxSmACw0\n\n#스텔라장 #Stellajang #Colors #LP발매 https://t.co/0StFzY5Kb8"
+	},
+	{
 		"created_at": "Sat Sep 12 10:26:16 +0000 2020",
 		"id": 1304728026165506000,
 		"id_str": "1304728026165506049",
