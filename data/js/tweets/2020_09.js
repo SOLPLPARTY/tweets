251434a395c739042144ff65f5a00dd5c2803024
솔플파티 <solplparty@gmail.com>
2020-09-14 22:17:36 +0900

    Added 1 tweets

--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Mon Sep 14 13:04:35 +0000 2020",
+		"id": 1305348459772276700,
+		"id_str": "1305492644294848513",
+		"full_text": "박소유 - 우리의 밤은 길지 않으니(Feat.김내현 of 로큰롤라디오) 가 발매되었습니다\n지금 모든 음원사이트에서 들으실 수 있어요\n\n오늘 하루를 견뎌낸 당신에게 드리는 위로가 가 닿기를 바라며_\n\nhttps://t.co/J8V5CqQy12",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			134
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/J8V5CqQy12",
+					"expanded_url": "https://youtu.be/2CFdHsbwwp0",
+					"display_url": "youtu.be/2CFdHsbwwp0",
+					"indices": [
+						111,
+						134
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 187983928,
+			"id_str": "187983928",
+			"name": "박소유",
+			"screen_name": "Blackswans_",
+			"location": "",
+			"description": "https://t.co/UjcZQAtIW3",
+			"url": "https://t.co/vkoh490NKE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vkoh490NKE",
+							"expanded_url": "http://www.parksoyu.com",
+							"display_url": "parksoyu.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UjcZQAtIW3",
+							"expanded_url": "http://www.facebook.com/withparksoyu",
+							"display_url": "facebook.com/withparksoyu",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 689,
+			"friends_count": 126,
+			"listed_count": 19,
+			"created_at": "Tue Sep 07 17:05:43 +0000 2010",
+			"favourites_count": 145,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5146,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/957507339292000256/ZGE5vuTw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/957507339292000256/ZGE5vuTw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/187983928/1431101478",
+			"profile_link_color": "2FC2EF",
+			"profile_sidebar_border_color": "181A1E",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "박소유 - 우리의 밤은 길지 않으니(Feat.김내현 of 로큰롤라디오) 가 발매되었습니다\n지금 모든 음원사이트에서 들으실 수 있어요\n\n오늘 하루를 견뎌낸 당신에게 드리는 위로가 가 닿기를 바라며_\n\nhttps://t.co/J8V5CqQy12"
+	},
+	{
 		"created_at": "Mon Sep 14 11:26:52 +0000 2020",
 		"id": 1305435556164243500,
 		"id_str": "1305468050079059968",
