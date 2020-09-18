b3299e45339da03dfaeb794cce5464c49ace2876
솔플파티 <solplparty@gmail.com>
2020-09-18 15:17:39 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 43642,
-	"created_at": "Fri Sep 18 04:17:37 +0000 2020",
+	"tweets": 43643,
+	"created_at": "Fri Sep 18 06:17:37 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,176 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Fri Sep 18 05:29:25 +0000 2020",
+		"id": 1306820453919457300,
+		"id_str": "1306827647733977094",
+		"full_text": "연약하지만 묵묵히 반짝일 마음들을 모아 \n따뜻함이 필요한 곳에\n우리의 이름으로 작은 온기를 보냈습니다.\n\n고맙고 사랑해요 유애나♥\n\nhttps://t.co/Ez7zQ3VxCo https://t.co/DC0ozfF3rq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Ez7zQ3VxCo",
+					"expanded_url": "https://www.instagram.com/p/CFQ-6xIhfSG/?utm_source=ig_web_copy_link",
+					"display_url": "instagram.com/p/CFQ-6xIhfSG/…",
+					"indices": [
+						74,
+						97
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1306820335644205000,
+					"id_str": "1306820335644205059",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/EiLCK-NUYAMPXzw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EiLCK-NUYAMPXzw.jpg",
+					"url": "https://t.co/DC0ozfF3rq",
+					"display_url": "pic.twitter.com/DC0ozfF3rq",
+					"expanded_url": "https://twitter.com/edam_ent/status/1306820453919457280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 848,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 481,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1448,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1306820335644205000,
+					"id_str": "1306820335644205059",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/EiLCK-NUYAMPXzw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EiLCK-NUYAMPXzw.jpg",
+					"url": "https://t.co/DC0ozfF3rq",
+					"display_url": "pic.twitter.com/DC0ozfF3rq",
+					"expanded_url": "https://twitter.com/edam_ent/status/1306820453919457280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 848,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 481,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1448,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1214016219889553400,
+			"id_str": "1214016219889553409",
+			"name": "EDAM Ent.",
+			"screen_name": "edam_ent",
+			"location": "",
+			"description": "EDAM Entertainment Official",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 30660,
+			"friends_count": 1,
+			"listed_count": 81,
+			"created_at": "Mon Jan 06 02:50:04 +0000 2020",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 58,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1229315447058653184/paC9xrhE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1229315447058653184/paC9xrhE_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1526,
+		"favorite_count": 2525,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "연약하지만 묵묵히 반짝일 마음들을 모아 \n따뜻함이 필요한 곳에\n우리의 이름으로 작은 온기를 보냈습니다.\n\n고맙고 사랑해요 유애나♥\n\nhttps://t.co/Ez7zQ3VxCo https://t.co/DC0ozfF3rq"
+	},
+	{
 		"created_at": "Fri Sep 18 04:11:23 +0000 2020",
 		"id": 1306808007762882600,
 		"id_str": "1306808007762882566",
