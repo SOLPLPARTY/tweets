210304e762a3e6da90baae4c59801a05b080b24b
솔플파티 <solplparty@gmail.com>
2020-09-19 17:17:39 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 43671,
-	"created_at": "Sat Sep 19 07:17:38 +0000 2020",
+	"tweets": 43672,
+	"created_at": "Sat Sep 19 08:17:38 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,163 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Sat Sep 19 07:29:25 +0000 2020",
+		"id": 1307208219933118500,
+		"id_str": "1307220236119146496",
+		"full_text": "#디어클라우드 #페스티벌나다\n오늘 공연은 6시부터 온라인 생중계 됩니다\n좋은 취지의 공연을 지속해주시는\n#네스트나다 식구분들께 박수를!!\n#나인 #이랑 #토근 \n\n1. '페스티벌 나다' 네이버TV\n- 네이버TV : https://t.co/CKHEEhQRFV\n2. '페스티벌 나다' 유튜브\n- 유튜브 : https://t.co/mNv6A6Yqoj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			192
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "디어클라우드",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"text": "페스티벌나다",
+					"indices": [
+						8,
+						15
+					]
+				},
+				{
+					"text": "네스트나다",
+					"indices": [
+						58,
+						64
+					]
+				},
+				{
+					"text": "나인",
+					"indices": [
+						77,
+						80
+					]
+				},
+				{
+					"text": "이랑",
+					"indices": [
+						81,
+						84
+					]
+				},
+				{
+					"text": "토근",
+					"indices": [
+						85,
+						88
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/CKHEEhQRFV",
+					"expanded_url": "https://tv.naver.com/festivalnada",
+					"display_url": "tv.naver.com/festivalnada",
+					"indices": [
+						120,
+						143
+					]
+				},
+				{
+					"url": "https://t.co/mNv6A6Yqoj",
+					"expanded_url": "http://asq.kr/festivalnada",
+					"display_url": "asq.kr/festivalnada",
+					"indices": [
+						169,
+						192
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 150566482,
+			"id_str": "150566482",
+			"name": "엠와이뮤직(MYmusic Ent)",
+			"screen_name": "messenger_yoon",
+			"location": "Artist Manager (South Korea)",
+			"description": "[Musician] 정준일, 낭만유랑악단, 나인(Nine9), 디어클라우드, 권영찬, 위아영 //mymusicent@naver.com//+82)2.6013.0102",
+			"url": "https://t.co/1IfWMp2Je9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1IfWMp2Je9",
+							"expanded_url": "http://www.mymusicent.co.kr",
+							"display_url": "mymusicent.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2684,
+			"friends_count": 342,
+			"listed_count": 33,
+			"created_at": "Tue Jun 01 07:29:53 +0000 2010",
+			"favourites_count": 16591,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5509,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/540007978649128960/jSz5oRO7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/540007978649128960/jSz5oRO7_normal.jpeg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#디어클라우드 #페스티벌나다\n오늘 공연은 6시부터 온라인 생중계 됩니다\n좋은 취지의 공연을 지속해주시는\n#네스트나다 식구분들께 박수를!!\n#나인 #이랑 #토근 \n\n1. '페스티벌 나다' 네이버TV\n- 네이버TV : https://t.co/CKHEEhQRFV\n2. '페스티벌 나다' 유튜브\n- 유튜브 : https://t.co/mNv6A6Yqoj"
+	},
+	{
 		"created_at": "Sat Sep 19 06:23:47 +0000 2020",
 		"id": 1307203718518055000,
 		"id_str": "1307203718518054912",
