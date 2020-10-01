88bbfbaaf37864ad88452d91a6381ae835f953b0
솔플파티 <solplparty@gmail.com>
2020-10-01 14:17:33 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 44094,
-	"created_at": "Thu Oct 01 04:17:32 +0000 2020",
+	"tweets": 44095,
+	"created_at": "Thu Oct 01 05:17:32 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,208 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Thu Oct 01 04:29:25 +0000 2020",
+		"id": 1311513166334103600,
+		"id_str": "1311523589666021376",
+		"full_text": "#연희다방 [BLUE DIAMOND]\n\n\"기존에 연희다방의 노래들은 저와 사랑하는 사람들의 이야기가 바탕이 되었었죠. 이번 앨범은 제가 그동안 생각하고 느꼈던 여러 가지 가치에 관해 이야기하는 기회가 되었어요.\"\n\n✚ https://t.co/H5jVIBR5ZU https://t.co/6HdV9K6gVa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			145
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "연희다방",
+					"indices": [
+						0,
+						5
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/H5jVIBR5ZU",
+					"expanded_url": "https://bit.ly/3mTMrBQ",
+					"display_url": "bit.ly/3mTMrBQ",
+					"indices": [
+						122,
+						145
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1310788579040190500,
+					"id_str": "1310788579040190465",
+					"indices": [
+						146,
+						169
+					],
+					"media_url": "http://pbs.twimg.com/media/EjDbRI1UYAE_3_i.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjDbRI1UYAE_3_i.jpg",
+					"url": "https://t.co/6HdV9K6gVa",
+					"display_url": "pic.twitter.com/6HdV9K6gVa",
+					"expanded_url": "https://twitter.com/mirrorballmusic/status/1311513166334103552/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1310788579040190500,
+					"id_str": "1310788579040190465",
+					"indices": [
+						146,
+						169
+					],
+					"media_url": "http://pbs.twimg.com/media/EjDbRI1UYAE_3_i.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjDbRI1UYAE_3_i.jpg",
+					"url": "https://t.co/6HdV9K6gVa",
+					"display_url": "pic.twitter.com/6HdV9K6gVa",
+					"expanded_url": "https://twitter.com/mirrorballmusic/status/1311513166334103552/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 125049211,
+			"id_str": "125049211",
+			"name": "미러볼뮤직 Mirrorball Music",
+			"screen_name": "mirrorballmusic",
+			"location": "Republic Korea Mapo-gu",
+			"description": "Korean Indie Music Distributor 미러볼뮤직에서 한국 인디 음악의 모든 것을 만나보세요. https://t.co/7KhvfUdI2i",
+			"url": "https://t.co/Ty0n69rcT2",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Ty0n69rcT2",
+							"expanded_url": "http://www.mirrorballmusic.co.kr",
+							"display_url": "mirrorballmusic.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/7KhvfUdI2i",
+							"expanded_url": "http://www.youtube.com/mirrorballmusickorea",
+							"display_url": "youtube.com/mirrorballmusi…",
+							"indices": [
+								62,
+								85
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9508,
+			"friends_count": 2890,
+			"listed_count": 251,
+			"created_at": "Sun Mar 21 14:17:51 +0000 2010",
+			"favourites_count": 954,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 10406,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1285504647734542337/a-q49FRH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1285504647734542337/a-q49FRH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/125049211/1599028531",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#연희다방 [BLUE DIAMOND]\n\n\"기존에 연희다방의 노래들은 저와 사랑하는 사람들의 이야기가 바탕이 되었었죠. 이번 앨범은 제가 그동안 생각하고 느꼈던 여러 가지 가치에 관해 이야기하는 기회가 되었어요.\"\n\n✚ https://t.co/H5jVIBR5ZU https://t.co/6HdV9K6gVa"
+	},
+	{
 		"created_at": "Thu Oct 01 03:51:50 +0000 2020",
 		"id": 1311514132986855400,
 		"id_str": "1311514132986855426",
