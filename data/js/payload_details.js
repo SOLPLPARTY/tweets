d84131ccf0476d0e30f6471da1d14adf19da9b9d
솔플파티 <solplparty@gmail.com>
2020-10-04 14:17:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 44133,
-	"created_at": "Sun Oct 04 03:17:34 +0000 2020",
+	"tweets": 44134,
+	"created_at": "Sun Oct 04 05:17:34 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,207 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Sun Oct 04 05:11:16 +0000 2020",
+		"id": 1312618223095832600,
+		"id_str": "1312621287441928194",
+		"full_text": "라이브클럽데이와 남산골한옥마을이 함께하는 온라인 뮤직 페스티벌. '변신술'. 1시간 뒤부터 시작합니다!\n\n변신술 1부 15:00-17:00\nhttps://t.co/5lPIQEkHzj\n\n변신술 2,3부 18:00-23:00\nhttps://t.co/D2DOU00Ryf\n\n더 많은 정보 보기\nhttps://t.co/PUsaPlucCd https://t.co/wxipakxO7M",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			183
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/5lPIQEkHzj",
+					"expanded_url": "https://tv.naver.com/l/56077",
+					"display_url": "tv.naver.com/l/56077",
+					"indices": [
+						78,
+						101
+					]
+				},
+				{
+					"url": "https://t.co/D2DOU00Ryf",
+					"expanded_url": "https://tv.naver.com/l/56083",
+					"display_url": "tv.naver.com/l/56083",
+					"indices": [
+						124,
+						147
+					]
+				},
+				{
+					"url": "https://t.co/PUsaPlucCd",
+					"expanded_url": "https://url.kr/neTLcf",
+					"display_url": "url.kr/neTLcf",
+					"indices": [
+						160,
+						183
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1312618186903224300,
+					"id_str": "1312618186903224320",
+					"indices": [
+						184,
+						207
+					],
+					"media_url": "http://pbs.twimg.com/media/EjdbSVQVgAAnUPL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjdbSVQVgAAnUPL.jpg",
+					"url": "https://t.co/wxipakxO7M",
+					"display_url": "pic.twitter.com/wxipakxO7M",
+					"expanded_url": "https://twitter.com/cafe_veloso/status/1312618223095832577/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1312618186903224300,
+					"id_str": "1312618186903224320",
+					"indices": [
+						184,
+						207
+					],
+					"media_url": "http://pbs.twimg.com/media/EjdbSVQVgAAnUPL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjdbSVQVgAAnUPL.jpg",
+					"url": "https://t.co/wxipakxO7M",
+					"display_url": "pic.twitter.com/wxipakxO7M",
+					"expanded_url": "https://twitter.com/cafe_veloso/status/1312618223095832577/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 444545059,
+			"id_str": "444545059",
+			"name": "벨로주(veloso)",
+			"screen_name": "cafe_veloso",
+			"location": "홍대",
+			"description": "벨로주 홍대(서교동 372-6) / 벨로주 망원(망원동 422-27)",
+			"url": "https://t.co/IHGjrVSuBz",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/IHGjrVSuBz",
+							"expanded_url": "http://www.veloso.co.kr",
+							"display_url": "veloso.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8745,
+			"friends_count": 145,
+			"listed_count": 136,
+			"created_at": "Fri Dec 23 11:08:35 +0000 2011",
+			"favourites_count": 18,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1376,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1709792493/IMAG0116_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1709792493/IMAG0116_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "라이브클럽데이와 남산골한옥마을이 함께하는 온라인 뮤직 페스티벌. '변신술'. 1시간 뒤부터 시작합니다!\n\n변신술 1부 15:00-17:00\nhttps://t.co/5lPIQEkHzj\n\n변신술 2,3부 18:00-23:00\nhttps://t.co/D2DOU00Ryf\n\n더 많은 정보 보기\nhttps://t.co/PUsaPlucCd https://t.co/wxipakxO7M"
+	},
+	{
 		"created_at": "Sun Oct 04 03:00:52 +0000 2020",
 		"id": 1312588468741181400,
 		"id_str": "1312588468741181441",
