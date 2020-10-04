df3ad096978401c29ef242b8e83c5a0ae4156968
솔플파티 <solplparty@gmail.com>
2020-10-04 16:17:34 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 44136,
-	"created_at": "Sun Oct 04 06:17:33 +0000 2020",
+	"tweets": 44137,
+	"created_at": "Sun Oct 04 07:17:33 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,216 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Sun Oct 04 07:11:26 +0000 2020",
+		"id": 1312643615139610600,
+		"id_str": "1312651526742081536",
+		"full_text": "[신규 입고]\n⠀\n돈패닉서울 38호가 입고 되었습니다😉이번 호에서는 아트 토이 작가와 크루 9팀에 대해 소개한다고 합니다!\n⠀\n원하시는 분들께서는 방문하셔서 직원에게 언제든지 편하게 말씀해주세요. 1인 1부를 기준으로 소진 시까지 무료 배포합니다🥳 https://t.co/WaNlISRHZQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			138
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1312642557705252900,
+					"id_str": "1312642557705252865",
+					"indices": [
+						139,
+						162
+					],
+					"media_url": "http://pbs.twimg.com/media/Ejdxc5kU8AEoPGH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ejdxc5kU8AEoPGH.jpg",
+					"url": "https://t.co/WaNlISRHZQ",
+					"display_url": "pic.twitter.com/WaNlISRHZQ",
+					"expanded_url": "https://twitter.com/xindieticket/status/1312643615139610625/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1312642557705252900,
+					"id_str": "1312642557705252865",
+					"indices": [
+						139,
+						162
+					],
+					"media_url": "http://pbs.twimg.com/media/Ejdxc5kU8AEoPGH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ejdxc5kU8AEoPGH.jpg",
+					"url": "https://t.co/WaNlISRHZQ",
+					"display_url": "pic.twitter.com/WaNlISRHZQ",
+					"expanded_url": "https://twitter.com/xindieticket/status/1312643615139610625/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1312642569478701000,
+					"id_str": "1312642569478701057",
+					"indices": [
+						139,
+						162
+					],
+					"media_url": "http://pbs.twimg.com/media/EjdxdlbVgAEWPyc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjdxdlbVgAEWPyc.jpg",
+					"url": "https://t.co/WaNlISRHZQ",
+					"display_url": "pic.twitter.com/WaNlISRHZQ",
+					"expanded_url": "https://twitter.com/xindieticket/status/1312643615139610625/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1220731080,
+			"id_str": "1220731080",
+			"name": "씬디 티켓라운지",
+			"screen_name": "xindieticket",
+			"location": "서울 마포구 어울마당로 41-1 (홍대주차장길)",
+			"description": "홍대 공연정보를 한눈에 확인할 수 있는 온오프라인 티켓판매처\n(운영시간 : 11-20시, 수요일은 15-20시, 월요일 휴무)\n☎ 02-322-2218",
+			"url": "https://t.co/dqX6O3D21p",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dqX6O3D21p",
+							"expanded_url": "http://xindieticket.kr",
+							"display_url": "xindieticket.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2782,
+			"friends_count": 2696,
+			"listed_count": 23,
+			"created_at": "Tue Feb 26 07:24:04 +0000 2013",
+			"favourites_count": 5292,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9449,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1200342617373827073/a1vB9pyg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1200342617373827073/a1vB9pyg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1220731080/1575026135",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[신규 입고]\n⠀\n돈패닉서울 38호가 입고 되었습니다😉이번 호에서는 아트 토이 작가와 크루 9팀에 대해 소개한다고 합니다!\n⠀\n원하시는 분들께서는 방문하셔서 직원에게 언제든지 편하게 말씀해주세요. 1인 1부를 기준으로 소진 시까지 무료 배포합니다🥳 https://t.co/WaNlISRHZQ"
+	},
+	{
 		"created_at": "Sun Oct 04 06:11:17 +0000 2020",
 		"id": 1312625998932799500,
 		"id_str": "1312636387561213952",
