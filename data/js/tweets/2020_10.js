9cfb672b47b3beeb02328dce97afe5cc38c0f0f3
솔플파티 <solplparty@gmail.com>
2020-10-08 03:17:37 +0900

    Added 1 tweets

--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,221 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Wed Oct 07 17:29:25 +0000 2020",
+		"id": 1313892408556429300,
+		"id_str": "1313894210660118529",
+		"full_text": "2020년 그래미 5관왕에 빛나는 현재 가장 핫한 팝 아이콘, 빌리 아일리시의 “WHERE DO WE GO?” THE LIVE STREAM이 한국 시간 10월 25일(일) 오전 7시에 스트리밍됩니다! \n지금 바로 티켓 예매! 🎫 https://t.co/ll7it80nzb https://t.co/05Ca7PS2dn",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			151
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ll7it80nzb",
+					"expanded_url": "http://livenation.kr/livefromhome",
+					"display_url": "livenation.kr/livefromhome",
+					"indices": [
+						128,
+						151
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1313892325526065200,
+					"id_str": "1313892325526065152",
+					"indices": [
+						152,
+						175
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1313892325526065152/pu/img/gfRkTtodLtClWB6s.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1313892325526065152/pu/img/gfRkTtodLtClWB6s.jpg",
+					"url": "https://t.co/05Ca7PS2dn",
+					"display_url": "pic.twitter.com/05Ca7PS2dn",
+					"expanded_url": "https://twitter.com/livenationkr/status/1313892408556429312/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1313892325526065200,
+					"id_str": "1313892325526065152",
+					"indices": [
+						152,
+						175
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1313892325526065152/pu/img/gfRkTtodLtClWB6s.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1313892325526065152/pu/img/gfRkTtodLtClWB6s.jpg",
+					"url": "https://t.co/05Ca7PS2dn",
+					"display_url": "pic.twitter.com/05Ca7PS2dn",
+					"expanded_url": "https://twitter.com/livenationkr/status/1313892408556429312/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 15015,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1313892325526065152/pu/vid/480x480/ckA_vjxm88H_On7T.mp4?tag=10"
+							},
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1313892325526065152/pu/vid/720x720/fCZIS2vnkJIlq_-s.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1313892325526065152/pu/pl/jFyT7Db7ACeLs73r.m3u8?tag=10"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1313892325526065152/pu/vid/320x320/hDjJhbeVI2v50Wg6.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 236888084,
+			"id_str": "236888084",
+			"name": "LIVE NATION KOREA",
+			"screen_name": "livenationkr",
+			"location": "Seoul, South Korea",
+			"description": "라이브네이션코리아 공식 트위터. Paul McCartney, Maroon5, Jason Mraz, Coldplay, MUSE, 등 해외 유명아티스트의 내한공연과 BIGBANG, Blackpink,BTS 등의 대규모 월드투어를 기획한 공연기획사. FOLLOW US!",
+			"url": "http://t.co/WZhcKy3xE8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/WZhcKy3xE8",
+							"expanded_url": "http://www.livenation.kr",
+							"display_url": "livenation.kr",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6561,
+			"friends_count": 234,
+			"listed_count": 28,
+			"created_at": "Tue Jan 11 16:04:41 +0000 2011",
+			"favourites_count": 601,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2008,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/820816720998764545/T7P3FpWQ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/820816720998764545/T7P3FpWQ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/236888084/1475559466",
+			"profile_link_color": "E21836",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "2020년 그래미 5관왕에 빛나는 현재 가장 핫한 팝 아이콘, 빌리 아일리시의 “WHERE DO WE GO?” THE LIVE STREAM이 한국 시간 10월 25일(일) 오전 7시에 스트리밍됩니다! \n지금 바로 티켓 예매! 🎫 https://t.co/ll7it80nzb https://t.co/05Ca7PS2dn"
+	},
+	{
 		"created_at": "Wed Oct 07 15:00:16 +0000 2020",
 		"id": 1313856676496314400,
 		"id_str": "1313856676496314370",
