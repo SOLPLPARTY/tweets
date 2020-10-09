194c9eccf58386424c17feeee3bf41f4c9028155
솔플파티 <solplparty@gmail.com>
2020-10-10 00:17:37 +0900

    Added 1 tweets

--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,317 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Fri Oct 09 15:11:16 +0000 2020",
+		"id": 1314574245767213000,
+		"id_str": "1314584221923581952",
+		"full_text": "[시공간집 3] 셋째 날의 아름다운 풍경✍✨️✍️🌿\n\n#재미공작소 #시공간집 #시공간집3 #강성은 #김소연 #김행숙 #박시하 #신해욱 #이성미 #이원 #이제니 #정한아 #하재연 \n#서울문화재단 #2020예술공간지원사업 https://t.co/NVRjhphF62",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			121
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "재미공작소",
+					"indices": [
+						30,
+						36
+					]
+				},
+				{
+					"text": "시공간집",
+					"indices": [
+						37,
+						42
+					]
+				},
+				{
+					"text": "시공간집3",
+					"indices": [
+						43,
+						49
+					]
+				},
+				{
+					"text": "강성은",
+					"indices": [
+						50,
+						54
+					]
+				},
+				{
+					"text": "김소연",
+					"indices": [
+						55,
+						59
+					]
+				},
+				{
+					"text": "김행숙",
+					"indices": [
+						60,
+						64
+					]
+				},
+				{
+					"text": "박시하",
+					"indices": [
+						65,
+						69
+					]
+				},
+				{
+					"text": "신해욱",
+					"indices": [
+						70,
+						74
+					]
+				},
+				{
+					"text": "이성미",
+					"indices": [
+						75,
+						79
+					]
+				},
+				{
+					"text": "이원",
+					"indices": [
+						80,
+						83
+					]
+				},
+				{
+					"text": "이제니",
+					"indices": [
+						84,
+						88
+					]
+				},
+				{
+					"text": "정한아",
+					"indices": [
+						89,
+						93
+					]
+				},
+				{
+					"text": "하재연",
+					"indices": [
+						94,
+						98
+					]
+				},
+				{
+					"text": "서울문화재단",
+					"indices": [
+						100,
+						107
+					]
+				},
+				{
+					"text": "2020예술공간지원사업",
+					"indices": [
+						108,
+						121
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1314574021011284000,
+					"id_str": "1314574021011283968",
+					"indices": [
+						122,
+						145
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1314574021011283968/pu/img/q_x9SXM0nF-NXt6Y.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1314574021011283968/pu/img/q_x9SXM0nF-NXt6Y.jpg",
+					"url": "https://t.co/NVRjhphF62",
+					"display_url": "pic.twitter.com/NVRjhphF62",
+					"expanded_url": "https://twitter.com/studio_zemi/status/1314574245767213057/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1314574021011284000,
+					"id_str": "1314574021011283968",
+					"indices": [
+						122,
+						145
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1314574021011283968/pu/img/q_x9SXM0nF-NXt6Y.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1314574021011283968/pu/img/q_x9SXM0nF-NXt6Y.jpg",
+					"url": "https://t.co/NVRjhphF62",
+					"display_url": "pic.twitter.com/NVRjhphF62",
+					"expanded_url": "https://twitter.com/studio_zemi/status/1314574245767213057/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 14097,
+						"variants": [
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1314574021011283968/pu/vid/720x720/vQwzFnwknzNWKZET.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1314574021011283968/pu/pl/_XLdPy6KFQ-Gb3sR.m3u8?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1314574021011283968/pu/vid/480x480/rzX0xKtGQbxt-4NY.mp4?tag=10"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1314574021011283968/pu/vid/320x320/KnySFRWDzc_sxL4-.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1314184159112261600,
+		"in_reply_to_status_id_str": "1314184159112261638",
+		"in_reply_to_user_id": 282449618,
+		"in_reply_to_user_id_str": "282449618",
+		"in_reply_to_screen_name": "studio_zemi",
+		"user": {
+			"id": 282449618,
+			"id_str": "282449618",
+			"name": "재미공작소",
+			"screen_name": "studio_zemi",
+			"location": "서울시 영등포구 문래동 3가 58-84 1층",
+			"description": "재미공작소는 서울 문래동에 위치한 문화예술공간입니다. (Since 2011) 공연, 창작 워크숍, 팝업숍, 전시, 문학 행사, 출판, 캘린더 갤러리 등 '재미있는 일'을 벌이고 있습니다. 재미공작소의 일정 및 예약 관련 정보는 아래 홈블로그 링크에서 바로 확인하실 수 있습니다.",
+			"url": "https://t.co/glbxD8jcYI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/glbxD8jcYI",
+							"expanded_url": "http://blog.naver.com/studiozemi",
+							"display_url": "blog.naver.com/studiozemi",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8137,
+			"friends_count": 583,
+			"listed_count": 83,
+			"created_at": "Fri Apr 15 07:08:52 +0000 2011",
+			"favourites_count": 2252,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 14535,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1314827654/zemi02_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1314827654/zemi02_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/282449618/1398647875",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[시공간집 3] 셋째 날의 아름다운 풍경✍✨️✍️🌿\n\n#재미공작소 #시공간집 #시공간집3 #강성은 #김소연 #김행숙 #박시하 #신해욱 #이성미 #이원 #이제니 #정한아 #하재연 \n#서울문화재단 #2020예술공간지원사업 https://t.co/NVRjhphF62"
+	},
+	{
 		"created_at": "Fri Oct 09 14:11:38 +0000 2020",
 		"id": 1314555412696363000,
 		"id_str": "1314569212128563211",
