271505273a351844931f4ebb5b41700bdf86230e
솔플파티 <solplparty@gmail.com>
2020-10-13 11:17:40 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 44440,
-	"created_at": "Mon Oct 12 17:17:38 +0000 2020",
+	"tweets": 44441,
+	"created_at": "Tue Oct 13 02:17:39 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,230 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Tue Oct 13 01:29:33 +0000 2020",
+		"id": 1315823151704465400,
+		"id_str": "1315826979850067968",
+		"full_text": "The new JTBC drama ‘HUSH’, starring HWANG JUNGMIN &amp; LIM YOONA, to be aired at 11PM KST every FRI, SAT from Dec 11! \nStay tuned for LIM YOONA's acting as an intern journalist named Lee Jisoo! \n\n#LIMYOONA #임윤아 #소녀시대 #GirlsGeneration #허쉬 #HUSH #이지수 https://t.co/0vhHHz73S6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			249
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LIMYOONA",
+					"indices": [
+						197,
+						206
+					]
+				},
+				{
+					"text": "임윤아",
+					"indices": [
+						207,
+						211
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						212,
+						217
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						218,
+						234
+					]
+				},
+				{
+					"text": "허쉬",
+					"indices": [
+						235,
+						238
+					]
+				},
+				{
+					"text": "HUSH",
+					"indices": [
+						239,
+						244
+					]
+				},
+				{
+					"text": "이지수",
+					"indices": [
+						245,
+						249
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1315823141675888600,
+					"id_str": "1315823141675888640",
+					"indices": [
+						250,
+						273
+					],
+					"media_url": "http://pbs.twimg.com/media/EkK-LQLUcAALfx6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EkK-LQLUcAALfx6.jpg",
+					"url": "https://t.co/0vhHHz73S6",
+					"display_url": "pic.twitter.com/0vhHHz73S6",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1315823151704465409/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2000,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1315823141675888600,
+					"id_str": "1315823141675888640",
+					"indices": [
+						250,
+						273
+					],
+					"media_url": "http://pbs.twimg.com/media/EkK-LQLUcAALfx6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EkK-LQLUcAALfx6.jpg",
+					"url": "https://t.co/0vhHHz73S6",
+					"display_url": "pic.twitter.com/0vhHHz73S6",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1315823151704465409/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2000,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 884378714,
+			"id_str": "884378714",
+			"name": "SMTOWN",
+			"screen_name": "SMTOWNGLOBAL",
+			"location": "",
+			"description": "SMEntertainment Group Official Twitter",
+			"url": "https://t.co/vbjfZGuM9Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vbjfZGuM9Y",
+							"expanded_url": "http://www.smtown.com",
+							"display_url": "smtown.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8468930,
+			"friends_count": 52,
+			"listed_count": 8259,
+			"created_at": "Tue Oct 16 12:49:45 +0000 2012",
+			"favourites_count": 1424,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 8757,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "F08BA0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/925281250792824832/dfj3nYUe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/925281250792824832/dfj3nYUe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/884378714/1509443118",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1331,
+		"favorite_count": 5219,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The new JTBC drama ‘HUSH’, starring HWANG JUNGMIN &amp; LIM YOONA, to be aired at 11PM KST every FRI, SAT from Dec 11! \nStay tuned for LIM YOONA's acting as an intern journalist named Lee Jisoo! \n\n#LIMYOONA #임윤아 #소녀시대 #GirlsGeneration #허쉬 #HUSH #이지수 https://t.co/0vhHHz73S6"
+	},
+	{
 		"created_at": "Mon Oct 12 17:17:28 +0000 2020",
 		"id": 1315703144949715000,
 		"id_str": "1315703144949714949",
