3a4f218ff89ed57d569d9c20910b7950f44f908f
솔플파티 <solplparty@gmail.com>
2020-10-19 15:17:43 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 44729,
-	"created_at": "Mon Oct 19 05:17:39 +0000 2020",
+	"tweets": 44730,
+	"created_at": "Mon Oct 19 06:17:42 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Mon Oct 19 05:28:43 +0000 2020",
+		"id": 1318049989038256000,
+		"id_str": "1318061495867953154",
+		"full_text": "Just you wait.\n\n#KDA #ALLOUT #WORLDS2020 https://t.co/dOYQ1FfJaG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			40
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "KDA",
+					"indices": [
+						16,
+						20
+					]
+				},
+				{
+					"text": "ALLOUT",
+					"indices": [
+						21,
+						28
+					]
+				},
+				{
+					"text": "WORLDS2020",
+					"indices": [
+						29,
+						40
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1317898840310120400,
+					"id_str": "1317898840310120448",
+					"indices": [
+						41,
+						64
+					],
+					"media_url": "http://pbs.twimg.com/media/EkoeA0KVgAAO7oq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EkoeA0KVgAAO7oq.jpg",
+					"url": "https://t.co/dOYQ1FfJaG",
+					"display_url": "pic.twitter.com/dOYQ1FfJaG",
+					"expanded_url": "https://twitter.com/KDA_MUSIC/status/1318049989038256130/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1317898840310120400,
+					"id_str": "1317898840310120448",
+					"indices": [
+						41,
+						64
+					],
+					"media_url": "http://pbs.twimg.com/media/EkoeA0KVgAAO7oq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EkoeA0KVgAAO7oq.jpg",
+					"url": "https://t.co/dOYQ1FfJaG",
+					"display_url": "pic.twitter.com/dOYQ1FfJaG",
+					"expanded_url": "https://twitter.com/KDA_MUSIC/status/1318049989038256130/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1289225491233284000,
+			"id_str": "1289225491233284097",
+			"name": "K/DA",
+			"screen_name": "KDA_MUSIC",
+			"location": "",
+			"description": "K/DA OFFICIAL //  EP COMING SOON \n\n#KDAISBACK #COMEBACK #THEBADDEST",
+			"url": "https://t.co/eyi9eF6jzX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/eyi9eF6jzX",
+							"expanded_url": "http://linktr.ee/kda_music",
+							"display_url": "linktr.ee/kda_music",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 294322,
+			"friends_count": 11,
+			"listed_count": 246,
+			"created_at": "Fri Jul 31 15:45:04 +0000 2020",
+			"favourites_count": 195,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 116,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1315638335025696768/pbQnwpwY_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1315638335025696768/pbQnwpwY_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1289225491233284097/1602507599",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4200,
+		"favorite_count": 19307,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Just you wait.\n\n#KDA #ALLOUT #WORLDS2020 https://t.co/dOYQ1FfJaG"
+	},
+	{
 		"created_at": "Mon Oct 19 05:11:16 +0000 2020",
 		"id": 1318050923873292300,
 		"id_str": "1318057105388163073",
