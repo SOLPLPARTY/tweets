11842bee0838500fa481907e83a59322b5c96b0d
솔플파티 <solplparty@gmail.com>
2020-10-21 01:17:44 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 44802,
-	"created_at": "Tue Oct 20 13:17:47 +0000 2020",
+	"tweets": 44803,
+	"created_at": "Tue Oct 20 16:17:43 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,221 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Tue Oct 20 15:29:25 +0000 2020",
+		"id": 1318567799632007200,
+		"id_str": "1318575052628045824",
+		"full_text": "DRIPPIN 1st Mini Album\n[Boyager] Track Prequel | Overdrive\n\n◾2020.10.28 6PM RELEASE\n\nhttps://t.co/YdlT51LnyZ\nhttps://t.co/54PKhzpmSG\n\n#DRIPPIN #드리핀\n#Boyager https://t.co/NKjmvd5YPo",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			156
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DRIPPIN",
+					"indices": [
+						134,
+						142
+					]
+				},
+				{
+					"text": "드리핀",
+					"indices": [
+						143,
+						147
+					]
+				},
+				{
+					"text": "Boyager",
+					"indices": [
+						148,
+						156
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/YdlT51LnyZ",
+					"expanded_url": "http://youtu.be/U0eDjndYssg",
+					"display_url": "youtu.be/U0eDjndYssg",
+					"indices": [
+						85,
+						108
+					]
+				},
+				{
+					"url": "https://t.co/54PKhzpmSG",
+					"expanded_url": "http://vlive.tv/video/218825",
+					"display_url": "vlive.tv/video/218825",
+					"indices": [
+						109,
+						132
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1318561491759505400,
+					"id_str": "1318561491759505408",
+					"indices": [
+						157,
+						180
+					],
+					"media_url": "http://pbs.twimg.com/media/Ekx4sNLVoAA5Bov.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ekx4sNLVoAA5Bov.jpg",
+					"url": "https://t.co/NKjmvd5YPo",
+					"display_url": "pic.twitter.com/NKjmvd5YPo",
+					"expanded_url": "https://twitter.com/woollim_ent/status/1318567799632007168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1318561491759505400,
+					"id_str": "1318561491759505408",
+					"indices": [
+						157,
+						180
+					],
+					"media_url": "http://pbs.twimg.com/media/Ekx4sNLVoAA5Bov.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ekx4sNLVoAA5Bov.jpg",
+					"url": "https://t.co/NKjmvd5YPo",
+					"display_url": "pic.twitter.com/NKjmvd5YPo",
+					"expanded_url": "https://twitter.com/woollim_ent/status/1318567799632007168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 713661673,
+			"id_str": "713661673",
+			"name": "woollim",
+			"screen_name": "woollim_ent",
+			"location": "",
+			"description": "Woollim Entertainment Official Twitter",
+			"url": "https://t.co/FVUD6x00cm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/FVUD6x00cm",
+							"expanded_url": "http://www.woolliment.com",
+							"display_url": "woolliment.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 423804,
+			"friends_count": 10,
+			"listed_count": 1804,
+			"created_at": "Tue Jul 24 04:15:30 +0000 2012",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2048,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1269648322144288769/Ds4vXWM6_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1269648322144288769/Ds4vXWM6_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/713661673/1603033322",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 818,
+		"favorite_count": 1434,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "DRIPPIN 1st Mini Album\n[Boyager] Track Prequel | Overdrive\n\n◾2020.10.28 6PM RELEASE\n\nhttps://t.co/YdlT51LnyZ\nhttps://t.co/54PKhzpmSG\n\n#DRIPPIN #드리핀\n#Boyager https://t.co/NKjmvd5YPo"
+	},
+	{
 		"created_at": "Tue Oct 20 13:13:42 +0000 2020",
 		"id": 1318482602265694200,
 		"id_str": "1318540899836522497",
