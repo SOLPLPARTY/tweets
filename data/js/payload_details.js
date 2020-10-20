708f24944ade0cd38b0dd763cd8b07f7203b9f42
솔플파티 <solplparty@gmail.com>
2020-10-20 22:17:48 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 44801,
-	"created_at": "Tue Oct 20 12:17:43 +0000 2020",
+	"tweets": 44802,
+	"created_at": "Tue Oct 20 13:17:47 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Tue Oct 20 13:13:42 +0000 2020",
+		"id": 1318482602265694200,
+		"id_str": "1318540899836522497",
+		"full_text": "D-DAY ‘Wings’ M/V\nSo!YoON! X Phum Viphurit \nWings\nOUT NOW\n\n[MV] So!YoON! X Phum Viphruit – Wings https://t.co/QSBIRB65o5 https://t.co/nwG7spuQy6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			120
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/QSBIRB65o5",
+					"expanded_url": "https://youtu.be/58Jovcvp5sU",
+					"display_url": "youtu.be/58Jovcvp5sU",
+					"indices": [
+						97,
+						120
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1318482595571589000,
+					"id_str": "1318482595571589121",
+					"indices": [
+						121,
+						144
+					],
+					"media_url": "http://pbs.twimg.com/media/Ekww71-UYAEy-19.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ekww71-UYAEy-19.jpg",
+					"url": "https://t.co/nwG7spuQy6",
+					"display_url": "pic.twitter.com/nwG7spuQy6",
+					"expanded_url": "https://twitter.com/se_so_neon/status/1318482602265694208/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1318482595571589000,
+					"id_str": "1318482595571589121",
+					"indices": [
+						121,
+						144
+					],
+					"media_url": "http://pbs.twimg.com/media/Ekww71-UYAEy-19.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ekww71-UYAEy-19.jpg",
+					"url": "https://t.co/nwG7spuQy6",
+					"display_url": "pic.twitter.com/nwG7spuQy6",
+					"expanded_url": "https://twitter.com/se_so_neon/status/1318482602265694208/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 718392164741472300,
+			"id_str": "718392164741472256",
+			"name": "새소년 SE SO NEON",
+			"screen_name": "se_so_neon",
+			"location": "대한민국 서울",
+			"description": "세계적인 밴드 새소년 🐤 Band SE SO NEON = Soyoon (v/g) / Usu (dr) / Park Hyunjin (b)\n\nnew EP \"비적응 (Nonadaptation)\"\nreleased on 2020. 02. 18. TUE 6PM KST",
+			"url": "https://t.co/SaQppDidSE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SaQppDidSE",
+							"expanded_url": "http://www.instagram.com/se_so_neon",
+							"display_url": "instagram.com/se_so_neon",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9818,
+			"friends_count": 15,
+			"listed_count": 58,
+			"created_at": "Fri Apr 08 10:56:53 +0000 2016",
+			"favourites_count": 305,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1428,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1230785072812314625/frCUDIe2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1230785072812314625/frCUDIe2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/718392164741472256/1581572779",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 335,
+		"favorite_count": 256,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": true,
+		"lang": "en",
+		"text": "D-DAY ‘Wings’ M/V\nSo!YoON! X Phum Viphurit \nWings\nOUT NOW\n\n[MV] So!YoON! X Phum Viphruit – Wings https://t.co/QSBIRB65o5 https://t.co/nwG7spuQy6"
+	},
+	{
 		"created_at": "Tue Oct 20 12:11:17 +0000 2020",
 		"id": 1318515083576705000,
 		"id_str": "1318525191237046275",
