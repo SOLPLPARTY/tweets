55f95043a5d07344392a2cb314b5fddbe4eec0cb
솔플파티 <solplparty@gmail.com>
2020-11-02 16:17:34 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 45559,
-	"created_at": "Mon Nov 02 05:20:23 +0000 2020",
+	"tweets": 45560,
+	"created_at": "Mon Nov 02 07:17:33 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,284 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Mon Nov 02 06:29:24 +0000 2020",
+		"id": 1323142799605162000,
+		"id_str": "1323150198022000640",
+		"full_text": "Raiden X HYO take over the SM STATION playlist!\n\nClick the link below and find your favorite STATION songs!\n\nSpotify: https://t.co/tD2u2ucUtI\n\nApple Music: https://t.co/dqy0KEHBeF\n\n#Raiden #레이든 #HYO #효연 #쿠기 #Coogie #STATION #ThinkAboutMe #GINJO #긴조 #임레이 #IMLAY https://t.co/775v6nXigI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			260
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Raiden",
+					"indices": [
+						181,
+						188
+					]
+				},
+				{
+					"text": "레이든",
+					"indices": [
+						189,
+						193
+					]
+				},
+				{
+					"text": "HYO",
+					"indices": [
+						194,
+						198
+					]
+				},
+				{
+					"text": "효연",
+					"indices": [
+						199,
+						202
+					]
+				},
+				{
+					"text": "쿠기",
+					"indices": [
+						203,
+						206
+					]
+				},
+				{
+					"text": "Coogie",
+					"indices": [
+						207,
+						214
+					]
+				},
+				{
+					"text": "STATION",
+					"indices": [
+						215,
+						223
+					]
+				},
+				{
+					"text": "ThinkAboutMe",
+					"indices": [
+						224,
+						237
+					]
+				},
+				{
+					"text": "GINJO",
+					"indices": [
+						238,
+						244
+					]
+				},
+				{
+					"text": "긴조",
+					"indices": [
+						245,
+						248
+					]
+				},
+				{
+					"text": "임레이",
+					"indices": [
+						249,
+						253
+					]
+				},
+				{
+					"text": "IMLAY",
+					"indices": [
+						254,
+						260
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/tD2u2ucUtI",
+					"expanded_url": "https://smarturl.it/SMSTATION_Spotify",
+					"display_url": "smarturl.it/SMSTATION_Spot…",
+					"indices": [
+						118,
+						141
+					]
+				},
+				{
+					"url": "https://t.co/dqy0KEHBeF",
+					"expanded_url": "https://smarturl.it/SMSTATION_Apple",
+					"display_url": "smarturl.it/SMSTATION_Apple",
+					"indices": [
+						156,
+						179
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1323112232729809000,
+					"id_str": "1323112232729808896",
+					"indices": [
+						261,
+						284
+					],
+					"media_url": "http://pbs.twimg.com/media/ElyjkMXVkAAxSQF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ElyjkMXVkAAxSQF.jpg",
+					"url": "https://t.co/775v6nXigI",
+					"display_url": "pic.twitter.com/775v6nXigI",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1323142799605161984/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2000,
+							"h": 2000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1323112232729809000,
+					"id_str": "1323112232729808896",
+					"indices": [
+						261,
+						284
+					],
+					"media_url": "http://pbs.twimg.com/media/ElyjkMXVkAAxSQF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ElyjkMXVkAAxSQF.jpg",
+					"url": "https://t.co/775v6nXigI",
+					"display_url": "pic.twitter.com/775v6nXigI",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1323142799605161984/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2000,
+							"h": 2000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 884378714,
+			"id_str": "884378714",
+			"name": "SMTOWN",
+			"screen_name": "SMTOWNGLOBAL",
+			"location": "",
+			"description": "SMEntertainment Group Official Twitter",
+			"url": "https://t.co/vbjfZGuM9Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vbjfZGuM9Y",
+							"expanded_url": "http://www.smtown.com",
+							"display_url": "smtown.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8583845,
+			"friends_count": 53,
+			"listed_count": 8358,
+			"created_at": "Tue Oct 16 12:49:45 +0000 2012",
+			"favourites_count": 1486,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 8858,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "F08BA0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/925281250792824832/dfj3nYUe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/925281250792824832/dfj3nYUe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/884378714/1509443118",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 562,
+		"favorite_count": 4608,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Raiden X HYO take over the SM STATION playlist!\n\nClick the link below and find your favorite STATION songs!\n\nSpotify: https://t.co/tD2u2ucUtI\n\nApple Music: https://t.co/dqy0KEHBeF\n\n#Raiden #레이든 #HYO #효연 #쿠기 #Coogie #STATION #ThinkAboutMe #GINJO #긴조 #임레이 #IMLAY https://t.co/775v6nXigI"
+	},
+	{
 		"created_at": "Mon Nov 02 05:18:20 +0000 2020",
 		"id": 1323132310838018000,
 		"id_str": "1323132310838018048",
