83abea67f4b11e8d87a79072d1893568f8189e0b
솔플파티 <solplparty@gmail.com>
2020-11-04 01:17:40 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 45653,
-	"created_at": "Tue Nov 03 14:17:35 +0000 2020",
+	"tweets": 45654,
+	"created_at": "Tue Nov 03 16:17:37 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,10 +1,234 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Tue Nov 03 15:29:24 +0000 2020",
+		"id": 1323641090570858500,
+		"id_str": "1323648481995706368",
+		"full_text": "[LISTEN SPECIAL] 박재정 - 너라는 위로\n2020.11.10 12PM(KST)\n⠀\n#미스틱스토리 #MYSTICSTORY #LISTEN #리슨 #박재정 #parcjaejung #너라는위로 https://t.co/IYUIJB0cpa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			110
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "미스틱스토리",
+					"indices": [
+						53,
+						60
+					]
+				},
+				{
+					"text": "MYSTICSTORY",
+					"indices": [
+						61,
+						73
+					]
+				},
+				{
+					"text": "LISTEN",
+					"indices": [
+						74,
+						81
+					]
+				},
+				{
+					"text": "리슨",
+					"indices": [
+						82,
+						85
+					]
+				},
+				{
+					"text": "박재정",
+					"indices": [
+						86,
+						90
+					]
+				},
+				{
+					"text": "parcjaejung",
+					"indices": [
+						91,
+						103
+					]
+				},
+				{
+					"text": "너라는위로",
+					"indices": [
+						104,
+						110
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1323571777893621800,
+					"id_str": "1323571777893621761",
+					"indices": [
+						111,
+						134
+					],
+					"media_url": "http://pbs.twimg.com/media/El5FhPgU4AEEXSL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/El5FhPgU4AEEXSL.jpg",
+					"url": "https://t.co/IYUIJB0cpa",
+					"display_url": "pic.twitter.com/IYUIJB0cpa",
+					"expanded_url": "https://twitter.com/themysticstory/status/1323641090570858501/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1323571777893621800,
+					"id_str": "1323571777893621761",
+					"indices": [
+						111,
+						134
+					],
+					"media_url": "http://pbs.twimg.com/media/El5FhPgU4AEEXSL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/El5FhPgU4AEEXSL.jpg",
+					"url": "https://t.co/IYUIJB0cpa",
+					"display_url": "pic.twitter.com/IYUIJB0cpa",
+					"expanded_url": "https://twitter.com/themysticstory/status/1323641090570858501/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/BKUIHw4jho",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BKUIHw4jho",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 19771,
+			"friends_count": 18,
+			"listed_count": 220,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5400,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1553488269",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12,
+		"favorite_count": 17,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[LISTEN SPECIAL] 박재정 - 너라는 위로\n2020.11.10 12PM(KST)\n⠀\n#미스틱스토리 #MYSTICSTORY #LISTEN #리슨 #박재정 #parcjaejung #너라는위로 https://t.co/IYUIJB0cpa"
+	},
+	{
 		"created_at": "Tue Nov 03 13:58:30 +0000 2020",
 		"id": 1323610888524296200,
 		"id_str": "1323625602633650176",
-		"full_text": "[KCON:TACT season 2] #TOO\n\nTOO always killing it at #KCONTACT !\n\n▶ KCON official: https://t.co/83zc6intTv\n\nDAY 1-10 NONSTOP STREAMING\n▶ KCON official: https://t.co/XqzsTlO5B7 ​\n▶ Mnet K-POP: https://t.co/J388Ddaq1j ​\n\n#KCON #LetsKCON #KCON2020 https://t.co/sNHpRDxJ5g",
+		"full_text": "[KCON:TACT season 2] #TOO\n\nTOO always killing it at #KCONTACT !\n\n▶ KCON official: https://t.co/83zc6intTv\n\nDAY 1-10 NONSTOP STREAMING\n▶ KCON official: https://t.co/XqzsTlO5B7 \n▶ Mnet K-POP: https://t.co/J388Ddaq1j \n\n#KCON #LetsKCON #KCON2020 https://t.co/sNHpRDxJ5g",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -344,13 +568,13 @@ Grailbird.data.tweets_2020_11 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "[KCON:TACT season 2] #TOO\n\nTOO always killing it at #KCONTACT !\n\n▶ KCON official: https://t.co/83zc6intTv\n\nDAY 1-10 NONSTOP STREAMING\n▶ KCON official: https://t.co/XqzsTlO5B7 ​\n▶ Mnet K-POP: https://t.co/J388Ddaq1j ​\n\n#KCON #LetsKCON #KCON2020 https://t.co/sNHpRDxJ5g"
+		"text": "[KCON:TACT season 2] #TOO\n\nTOO always killing it at #KCONTACT !\n\n▶ KCON official: https://t.co/83zc6intTv\n\nDAY 1-10 NONSTOP STREAMING\n▶ KCON official: https://t.co/XqzsTlO5B7 \n▶ Mnet K-POP: https://t.co/J388Ddaq1j \n\n#KCON #LetsKCON #KCON2020 https://t.co/sNHpRDxJ5g"
 	},
 	{
 		"created_at": "Tue Nov 03 13:58:22 +0000 2020",
 		"id": 1323610886813175800,
 		"id_str": "1323625572677857280",
-		"full_text": "[KCON:TACT season 2] #TOO\n\nTOO always killing it at #KCONTACT !\n\n▶ KCON official: https://t.co/83zc6i5T1X\n\nDAY 1-10 NONSTOP STREAMING\n▶ KCON official: https://t.co/XqzsTm5GZH ​\n▶ Mnet K-POP: https://t.co/J388DcSP9L ​\n\n#KCON #LetsKCON #KCON2020 https://t.co/9bgz5QSmGe",
+		"full_text": "[KCON:TACT season 2] #TOO\n\nTOO always killing it at #KCONTACT !\n\n▶ KCON official: https://t.co/83zc6i5T1X\n\nDAY 1-10 NONSTOP STREAMING\n▶ KCON official: https://t.co/XqzsTm5GZH \n▶ Mnet K-POP: https://t.co/J388DcSP9L \n\n#KCON #LetsKCON #KCON2020 https://t.co/9bgz5QSmGe",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -690,7 +914,7 @@ Grailbird.data.tweets_2020_11 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "[KCON:TACT season 2] #TOO\n\nTOO always killing it at #KCONTACT !\n\n▶ KCON official: https://t.co/83zc6i5T1X\n\nDAY 1-10 NONSTOP STREAMING\n▶ KCON official: https://t.co/XqzsTm5GZH ​\n▶ Mnet K-POP: https://t.co/J388DcSP9L ​\n\n#KCON #LetsKCON #KCON2020 https://t.co/9bgz5QSmGe"
+		"text": "[KCON:TACT season 2] #TOO\n\nTOO always killing it at #KCONTACT !\n\n▶ KCON official: https://t.co/83zc6i5T1X\n\nDAY 1-10 NONSTOP STREAMING\n▶ KCON official: https://t.co/XqzsTm5GZH \n▶ Mnet K-POP: https://t.co/J388DcSP9L \n\n#KCON #LetsKCON #KCON2020 https://t.co/9bgz5QSmGe"
 	},
 	{
 		"created_at": "Tue Nov 03 13:08:59 +0000 2020",
