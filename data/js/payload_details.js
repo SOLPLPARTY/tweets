240367d66e039b04d677fe22f939b834ac18c133
솔플파티 <solplparty@gmail.com>
2020-11-15 15:17:41 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 46194,
-	"created_at": "Sun Nov 15 04:17:44 +0000 2020",
+	"tweets": 46195,
+	"created_at": "Sun Nov 15 06:17:40 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Sun Nov 15 06:11:21 +0000 2020",
+		"id": 1327845781689094100,
+		"id_str": "1327856694865629184",
+		"full_text": "&lt;허클베리핀 16th 옐로우콘서트 + X Stage.V&gt;\n📍 일시 : 11/21 (토) 오후 7시\n📍 장소 : 벨로주 홍대\n📍 티켓 : 예매 50,000원 / 현매 55,000원\n📍 예매 : https://t.co/W1UBO5vhLF\n📍 문의 : 카톡플러스 '튜나레이블'\n* 코로나19 감염 예방을 위해 수용인원의 절반 이하인 60석으로 제한합니다.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			201
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/W1UBO5vhLF",
+					"expanded_url": "https://c11.kr/jc7k",
+					"display_url": "c11.kr/jc7k",
+					"indices": [
+						113,
+						136
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1327845778170151000,
+		"in_reply_to_status_id_str": "1327845778170150913",
+		"in_reply_to_user_id": 1220731080,
+		"in_reply_to_user_id_str": "1220731080",
+		"in_reply_to_screen_name": "xindieticket",
+		"user": {
+			"id": 1220731080,
+			"id_str": "1220731080",
+			"name": "씬디 티켓라운지",
+			"screen_name": "xindieticket",
+			"location": "서울 마포구 어울마당로 41-1 (홍대주차장길)",
+			"description": "홍대 공연정보를 한눈에 확인할 수 있는 온오프라인 티켓판매처\n(운영시간 : 11-20시, 수요일은 15-20시, 월요일 휴무)\n☎ 02-322-2218",
+			"url": "https://t.co/dqX6O3D21p",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dqX6O3D21p",
+							"expanded_url": "http://xindieticket.kr",
+							"display_url": "xindieticket.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2799,
+			"friends_count": 2694,
+			"listed_count": 23,
+			"created_at": "Tue Feb 26 07:24:04 +0000 2013",
+			"favourites_count": 5349,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9525,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1200342617373827073/a1vB9pyg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1200342617373827073/a1vB9pyg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1220731080/1575026135",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "&lt;허클베리핀 16th 옐로우콘서트 + X Stage.V&gt;\n📍 일시 : 11/21 (토) 오후 7시\n📍 장소 : 벨로주 홍대\n📍 티켓 : 예매 50,000원 / 현매 55,000원\n📍 예매 : https://t.co/W1UBO5vhLF\n📍 문의 : 카톡플러스 '튜나레이블'\n* 코로나19 감염 예방을 위해 수용인원의 절반 이하인 60석으로 제한합니다."
+	},
+	{
 		"created_at": "Sun Nov 15 03:53:31 +0000 2020",
 		"id": 1327822010274242600,
 		"id_str": "1327822010274242562",
