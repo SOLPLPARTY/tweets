ba5db178f1389f3e2594b1c6b69d46979ecfea92
솔플파티 <solplparty@gmail.com>
2020-12-04 15:17:35 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 46823,
-	"created_at": "Fri Dec 04 05:17:34 +0000 2020",
+	"tweets": 46824,
+	"created_at": "Fri Dec 04 06:17:34 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,288 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Fri Dec 04 06:11:16 +0000 2020",
+		"id": 1334733689117311000,
+		"id_str": "1334742047031484424",
+		"full_text": "📣오픈 [2021 캘린더 갤러리] \n\n금•토•일 3일 동안 재미공작소에서 안전하게 만나요😌 https://t.co/5RwHet3PTA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			50
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1334733681114509300,
+					"id_str": "1334733681114509312",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/EoXtNhSUcAAfJ2b.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoXtNhSUcAAfJ2b.jpg",
+					"url": "https://t.co/5RwHet3PTA",
+					"display_url": "pic.twitter.com/5RwHet3PTA",
+					"expanded_url": "https://twitter.com/studio_zemi/status/1334733689117310976/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1334733681114509300,
+					"id_str": "1334733681114509312",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/EoXtNhSUcAAfJ2b.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoXtNhSUcAAfJ2b.jpg",
+					"url": "https://t.co/5RwHet3PTA",
+					"display_url": "pic.twitter.com/5RwHet3PTA",
+					"expanded_url": "https://twitter.com/studio_zemi/status/1334733689117310976/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1334733681127116800,
+					"id_str": "1334733681127116800",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/EoXtNhVU0AApW70.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoXtNhVU0AApW70.jpg",
+					"url": "https://t.co/5RwHet3PTA",
+					"display_url": "pic.twitter.com/5RwHet3PTA",
+					"expanded_url": "https://twitter.com/studio_zemi/status/1334733689117310976/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1334733681118781400,
+					"id_str": "1334733681118781440",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/EoXtNhTVoAAryMy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoXtNhTVoAAryMy.jpg",
+					"url": "https://t.co/5RwHet3PTA",
+					"display_url": "pic.twitter.com/5RwHet3PTA",
+					"expanded_url": "https://twitter.com/studio_zemi/status/1334733689117310976/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1334733681114529800,
+					"id_str": "1334733681114529797",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/EoXtNhSUwAUX7_q.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoXtNhSUwAUX7_q.jpg",
+					"url": "https://t.co/5RwHet3PTA",
+					"display_url": "pic.twitter.com/5RwHet3PTA",
+					"expanded_url": "https://twitter.com/studio_zemi/status/1334733689117310976/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1334109365884702700,
+		"in_reply_to_status_id_str": "1334109365884702720",
+		"in_reply_to_user_id": 282449618,
+		"in_reply_to_user_id_str": "282449618",
+		"in_reply_to_screen_name": "studio_zemi",
+		"user": {
+			"id": 282449618,
+			"id_str": "282449618",
+			"name": "재미공작소",
+			"screen_name": "studio_zemi",
+			"location": "서울시 영등포구 문래동 3가 58-84 1층",
+			"description": "재미공작소는 서울 문래동에 위치한 문화예술공간입니다. (Since 2011) 공연, 창작 워크숍, 팝업숍, 전시, 문학 행사, 출판, 캘린더 갤러리 등 '재미있는 일'을 벌이고 있습니다. 재미공작소의 일정 및 예약 관련 정보는 아래 홈블로그 링크에서 바로 확인하실 수 있습니다.",
+			"url": "https://t.co/glbxD8jcYI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/glbxD8jcYI",
+							"expanded_url": "http://blog.naver.com/studiozemi",
+							"display_url": "blog.naver.com/studiozemi",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8125,
+			"friends_count": 576,
+			"listed_count": 80,
+			"created_at": "Fri Apr 15 07:08:52 +0000 2011",
+			"favourites_count": 2258,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 14602,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1314827654/zemi02_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1314827654/zemi02_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/282449618/1398647875",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "📣오픈 [2021 캘린더 갤러리] \n\n금•토•일 3일 동안 재미공작소에서 안전하게 만나요😌 https://t.co/5RwHet3PTA"
+	},
+	{
 		"created_at": "Fri Dec 04 05:11:17 +0000 2020",
 		"id": 1334717542640668700,
 		"id_str": "1334726948312985600",
