934f2207968cf09395a9f638307a670fe55176d8
솔플파티 <solplparty@gmail.com>
2020-12-10 22:17:37 +0900

    Added 1 tweets

--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,235 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Thu Dec 10 12:29:25 +0000 2020",
+		"id": 1337004288761991200,
+		"id_str": "1337011536292933634",
+		"full_text": "김성규(Kim Sung Kyu) 3rd Mini Album\n[INSIDE ME]｜Live Clip Preview\n\nhttps://t.co/dUwuDcNmvg\nhttps://t.co/Ql5HRgdutl\n\n#INFINITE #인피니트\n#Kim_Sung_Kyu #김성규\n#INSIDE_ME https://t.co/xOEE1QcTsS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			158
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "INFINITE",
+					"indices": [
+						113,
+						122
+					]
+				},
+				{
+					"text": "인피니트",
+					"indices": [
+						123,
+						128
+					]
+				},
+				{
+					"text": "Kim_Sung_Kyu",
+					"indices": [
+						129,
+						142
+					]
+				},
+				{
+					"text": "김성규",
+					"indices": [
+						143,
+						147
+					]
+				},
+				{
+					"text": "INSIDE_ME",
+					"indices": [
+						148,
+						158
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/dUwuDcNmvg",
+					"expanded_url": "http://youtu.be/8po4os3aQj0",
+					"display_url": "youtu.be/8po4os3aQj0",
+					"indices": [
+						64,
+						87
+					]
+				},
+				{
+					"url": "https://t.co/Ql5HRgdutl",
+					"expanded_url": "http://vlive.tv/video/227591",
+					"display_url": "vlive.tv/video/227591",
+					"indices": [
+						88,
+						111
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1336999420726919200,
+					"id_str": "1336999420726919168",
+					"indices": [
+						159,
+						182
+					],
+					"media_url": "http://pbs.twimg.com/media/Eo3547FUwAAqxDK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eo3547FUwAAqxDK.jpg",
+					"url": "https://t.co/xOEE1QcTsS",
+					"display_url": "pic.twitter.com/xOEE1QcTsS",
+					"expanded_url": "https://twitter.com/woollim_ent/status/1337004288761991170/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1336999420726919200,
+					"id_str": "1336999420726919168",
+					"indices": [
+						159,
+						182
+					],
+					"media_url": "http://pbs.twimg.com/media/Eo3547FUwAAqxDK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eo3547FUwAAqxDK.jpg",
+					"url": "https://t.co/xOEE1QcTsS",
+					"display_url": "pic.twitter.com/xOEE1QcTsS",
+					"expanded_url": "https://twitter.com/woollim_ent/status/1337004288761991170/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 713661673,
+			"id_str": "713661673",
+			"name": "woollim",
+			"screen_name": "woollim_ent",
+			"location": "",
+			"description": "Woollim Entertainment Official Twitter",
+			"url": "https://t.co/FVUD6x00cm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/FVUD6x00cm",
+							"expanded_url": "http://www.woolliment.com",
+							"display_url": "woolliment.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 424477,
+			"friends_count": 10,
+			"listed_count": 1813,
+			"created_at": "Tue Jul 24 04:15:30 +0000 2012",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2065,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1269648322144288769/Ds4vXWM6_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1269648322144288769/Ds4vXWM6_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/713661673/1607169766",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 715,
+		"favorite_count": 1299,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "김성규(Kim Sung Kyu) 3rd Mini Album\n[INSIDE ME]｜Live Clip Preview\n\nhttps://t.co/dUwuDcNmvg\nhttps://t.co/Ql5HRgdutl\n\n#INFINITE #인피니트\n#Kim_Sung_Kyu #김성규\n#INSIDE_ME https://t.co/xOEE1QcTsS"
+	},
+	{
 		"created_at": "Thu Dec 10 12:07:01 +0000 2020",
 		"id": 1337005901325201400,
 		"id_str": "1337005901325201409",
