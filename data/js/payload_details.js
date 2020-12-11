b14a1a73f6137da831990af05cfffc0515609410
솔플파티 <solplparty@gmail.com>
2020-12-11 23:17:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 47045,
-	"created_at": "Fri Dec 11 13:17:37 +0000 2020",
+	"tweets": 47046,
+	"created_at": "Fri Dec 11 14:17:37 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,148 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Fri Dec 11 13:29:25 +0000 2020",
+		"id": 1337382186228437000,
+		"id_str": "1337389023069949953",
+		"full_text": "정승환 Winter Single ‘어김없이 이 거리에’\n(Jung Seung Hwan ‘Winter Again’)\n\nLIVE FILM with 멜로망스 정동환\n\n❄️ https://t.co/IYlwfnVmx4 \n\n#정승환 #어김없이_이_거리에 #정동환\n#JungSeungHwan #Winter_Again",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			169
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "정승환",
+					"indices": [
+						119,
+						123
+					]
+				},
+				{
+					"text": "어김없이_이_거리에",
+					"indices": [
+						124,
+						135
+					]
+				},
+				{
+					"text": "정동환",
+					"indices": [
+						136,
+						140
+					]
+				},
+				{
+					"text": "JungSeungHwan",
+					"indices": [
+						141,
+						155
+					]
+				},
+				{
+					"text": "Winter_Again",
+					"indices": [
+						156,
+						169
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/IYlwfnVmx4",
+					"expanded_url": "https://youtu.be/T7ScKrpkBZw",
+					"display_url": "youtu.be/T7ScKrpkBZw",
+					"indices": [
+						93,
+						116
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 266004552,
+			"id_str": "266004552",
+			"name": "안테나 Antenna",
+			"screen_name": "antennamusic",
+			"location": "대한민국 서울",
+			"description": "Jung Jae Hyung, You Hee Yul(Toy), Lucid Fall, Peppertones, Park Sae Byul, Kwon Jin Ah, Sam Kim, Jung Seunghwan, Lee Jin Ah, CHAI, Yun Seokcheol, Jukjae Official",
+			"url": "https://t.co/CqfXN7It7U",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/CqfXN7It7U",
+							"expanded_url": "http://www.antenna.co.kr",
+							"display_url": "antenna.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 35068,
+			"friends_count": 10,
+			"listed_count": 368,
+			"created_at": "Mon Mar 14 13:49:27 +0000 2011",
+			"favourites_count": 13,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3358,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266004552/1606994307",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 42,
+		"favorite_count": 67,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "정승환 Winter Single ‘어김없이 이 거리에’\n(Jung Seung Hwan ‘Winter Again’)\n\nLIVE FILM with 멜로망스 정동환\n\n❄️ https://t.co/IYlwfnVmx4 \n\n#정승환 #어김없이_이_거리에 #정동환\n#JungSeungHwan #Winter_Again"
+	},
+	{
 		"created_at": "Fri Dec 11 12:29:25 +0000 2020",
 		"id": 1337366695116034000,
 		"id_str": "1337373923625799682",
