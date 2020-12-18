4c618c7b5ab08e04b8ae96bd0b54138416f5dbae
솔플파티 <solplparty@gmail.com>
2020-12-19 00:17:42 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 47245,
-	"created_at": "Fri Dec 18 13:17:38 +0000 2020",
+	"tweets": 47246,
+	"created_at": "Fri Dec 18 15:17:41 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Fri Dec 18 14:29:25 +0000 2020",
+		"id": 1339929842557493200,
+		"id_str": "1339940837971648512",
+		"full_text": "'어김없이 이 거리에' 그리고 \n‘겨울의 우리들' 발매 기념\n\n정승환의 V LIVE가 찾아옵니다 🎄💛\n\n그리고 스페셜 게스트!\n\n글라스데코에 관심이 조금은 있을 것 같은 ,,\n트럼펫을 ,, 잘 불 것 같은 ,, 🎺⁉️\n\n- 일시 : 2020년 12월 20일 (일) 4:30PM\n- 채널 : 정승환 V LIVE 채널\n\n일요일에 만나요 💛 https://t.co/GbsgHxZvr0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			187
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1339929832872792000,
+					"id_str": "1339929832872792065",
+					"indices": [
+						188,
+						211
+					],
+					"media_url": "http://pbs.twimg.com/media/EphjFXBU0AEDKia.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EphjFXBU0AEDKia.jpg",
+					"url": "https://t.co/GbsgHxZvr0",
+					"display_url": "pic.twitter.com/GbsgHxZvr0",
+					"expanded_url": "https://twitter.com/antennamusic/status/1339929842557493248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1339929832872792000,
+					"id_str": "1339929832872792065",
+					"indices": [
+						188,
+						211
+					],
+					"media_url": "http://pbs.twimg.com/media/EphjFXBU0AEDKia.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EphjFXBU0AEDKia.jpg",
+					"url": "https://t.co/GbsgHxZvr0",
+					"display_url": "pic.twitter.com/GbsgHxZvr0",
+					"expanded_url": "https://twitter.com/antennamusic/status/1339929842557493248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 266004552,
+			"id_str": "266004552",
+			"name": "안테나 Antenna",
+			"screen_name": "antennamusic",
+			"location": "대한민국 서울",
+			"description": "정재형, 유희열(Toy), 루시드폴, 페퍼톤스, 박새별, 권진아, Sam Kim, 정승환, 이진아, CHAI, 윤석철, 적재, 서동환 Official",
+			"url": "https://t.co/CqfXN7It7U",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/CqfXN7It7U",
+							"expanded_url": "http://www.antenna.co.kr",
+							"display_url": "antenna.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 35137,
+			"friends_count": 10,
+			"listed_count": 369,
+			"created_at": "Mon Mar 14 13:49:27 +0000 2011",
+			"favourites_count": 13,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3378,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266004552/1606994307",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 72,
+		"favorite_count": 97,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "'어김없이 이 거리에' 그리고 \n‘겨울의 우리들' 발매 기념\n\n정승환의 V LIVE가 찾아옵니다 🎄💛\n\n그리고 스페셜 게스트!\n\n글라스데코에 관심이 조금은 있을 것 같은 ,,\n트럼펫을 ,, 잘 불 것 같은 ,, 🎺⁉️\n\n- 일시 : 2020년 12월 20일 (일) 4:30PM\n- 채널 : 정승환 V LIVE 채널\n\n일요일에 만나요 💛 https://t.co/GbsgHxZvr0"
+	},
+	{
 		"created_at": "Fri Dec 18 13:11:28 +0000 2020",
 		"id": 1339916425574531000,
 		"id_str": "1339921221962612736",
