f0b3a2d8dd89600af3dfc76327e53d98201132e5
솔플파티 <solplparty@gmail.com>
2020-12-20 15:17:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 47267,
-	"created_at": "Sun Dec 20 05:17:39 +0000 2020",
+	"tweets": 47268,
+	"created_at": "Sun Dec 20 06:17:37 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,162 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Sun Dec 20 05:29:24 +0000 2020",
+		"id": 1340519709322563600,
+		"id_str": "1340529716856537088",
+		"full_text": "[#고민시] '스위트홈' 고민시 - “300%의 애정으로 몰입했다”\n\n▶️ https://t.co/4IEv4PvDbb\n\n#스위트홈 #씨네21 #인터뷰 #이은유 #넷플릭스 #Netflix",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			103
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "고민시",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "스위트홈",
+					"indices": [
+						67,
+						72
+					]
+				},
+				{
+					"text": "씨네21",
+					"indices": [
+						73,
+						78
+					]
+				},
+				{
+					"text": "인터뷰",
+					"indices": [
+						79,
+						83
+					]
+				},
+				{
+					"text": "이은유",
+					"indices": [
+						84,
+						88
+					]
+				},
+				{
+					"text": "넷플릭스",
+					"indices": [
+						89,
+						94
+					]
+				},
+				{
+					"text": "Netflix",
+					"indices": [
+						95,
+						103
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/4IEv4PvDbb",
+					"expanded_url": "https://bit.ly/3arOKIA",
+					"display_url": "bit.ly/3arOKIA",
+					"indices": [
+						42,
+						65
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/BKUIHw4jho",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BKUIHw4jho",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 19893,
+			"friends_count": 18,
+			"listed_count": 227,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5475,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1553488269",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#고민시] '스위트홈' 고민시 - “300%의 애정으로 몰입했다”\n\n▶️ https://t.co/4IEv4PvDbb\n\n#스위트홈 #씨네21 #인터뷰 #이은유 #넷플릭스 #Netflix"
+	},
+	{
 		"created_at": "Sun Dec 20 05:11:17 +0000 2020",
 		"id": 1340522460047929300,
 		"id_str": "1340525154657595395",
