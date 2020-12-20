89b49e86c6135836954611258cf2ccdefb58ddee
솔플파티 <solplparty@gmail.com>
2020-12-20 21:17:40 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 47277,
-	"created_at": "Sun Dec 20 11:17:40 +0000 2020",
+	"tweets": 47278,
+	"created_at": "Sun Dec 20 12:17:39 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,223 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Sun Dec 20 11:29:25 +0000 2020",
+		"id": 1340613366876758000,
+		"id_str": "1340620315106414594",
+		"full_text": "[#서장훈] '2020 SBS 연예대상' 특별상 명예사원상 수상🎉\n\n미스틱스토리 서장훈의 수상을 다시 한 번 축하합니다✨\n\n#SBS연예대상 #특별상 #명예사원상 #SBS #연예대상 https://t.co/uxu1y6DGKO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			100
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "서장훈",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "SBS연예대상",
+					"indices": [
+						69,
+						77
+					]
+				},
+				{
+					"text": "특별상",
+					"indices": [
+						78,
+						82
+					]
+				},
+				{
+					"text": "명예사원상",
+					"indices": [
+						83,
+						89
+					]
+				},
+				{
+					"text": "SBS",
+					"indices": [
+						90,
+						94
+					]
+				},
+				{
+					"text": "연예대상",
+					"indices": [
+						95,
+						100
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1340613364171436000,
+					"id_str": "1340613364171436032",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/EprQwHiUcAAERSS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EprQwHiUcAAERSS.jpg",
+					"url": "https://t.co/uxu1y6DGKO",
+					"display_url": "pic.twitter.com/uxu1y6DGKO",
+					"expanded_url": "https://twitter.com/themysticstory/status/1340613366876758017/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1340613364171436000,
+					"id_str": "1340613364171436032",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/EprQwHiUcAAERSS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EprQwHiUcAAERSS.jpg",
+					"url": "https://t.co/uxu1y6DGKO",
+					"display_url": "pic.twitter.com/uxu1y6DGKO",
+					"expanded_url": "https://twitter.com/themysticstory/status/1340613366876758017/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/BKUIHw4jho",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BKUIHw4jho",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 19901,
+			"friends_count": 18,
+			"listed_count": 227,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5477,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1553488269",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#서장훈] '2020 SBS 연예대상' 특별상 명예사원상 수상🎉\n\n미스틱스토리 서장훈의 수상을 다시 한 번 축하합니다✨\n\n#SBS연예대상 #특별상 #명예사원상 #SBS #연예대상 https://t.co/uxu1y6DGKO"
+	},
+	{
 		"created_at": "Sun Dec 20 11:11:27 +0000 2020",
 		"id": 1340605751195123700,
 		"id_str": "1340615795404132353",
