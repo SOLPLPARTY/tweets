3d20df3f8d17ebf35c2b8a53e6117179a9beaea4
솔플파티 <solplparty@gmail.com>
2021-01-03 14:17:34 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 47774,
-	"created_at": "Sun Jan 03 03:17:33 +0000 2021",
+	"tweets": 47775,
+	"created_at": "Sun Jan 03 05:17:33 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Sun Jan 03 04:29:25 +0000 2021",
+		"id": 1345573404213039000,
+		"id_str": "1345588048680153088",
+		"full_text": "[Release]\n\nSHINDRUM [Who I Am]\nOverthink (Feat. Sam Kim)\n\nLyrics by SHINDRUM HAEUN 샘김 (Sam Kim)\nComposed by SHINDRUM HAEUN 샘김 (Sam Kim) Jordan Rakei\nArranged by SHINDRUM HAEUN\n\n샘김이 작사, 작곡, 보컬로 참여한 ‘Overthink’는\n전 온라인 음원 사이트에서 감상하실 수 있습니다 💚 https://t.co/TIzX6jCj58",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			238
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1345573394973040600,
+					"id_str": "1345573394973040640",
+					"indices": [
+						239,
+						262
+					],
+					"media_url": "http://pbs.twimg.com/media/Eqxv36KVoAAA93z.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eqxv36KVoAAA93z.jpg",
+					"url": "https://t.co/TIzX6jCj58",
+					"display_url": "pic.twitter.com/TIzX6jCj58",
+					"expanded_url": "https://twitter.com/antennamusic/status/1345573404213039104/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1345573394973040600,
+					"id_str": "1345573394973040640",
+					"indices": [
+						239,
+						262
+					],
+					"media_url": "http://pbs.twimg.com/media/Eqxv36KVoAAA93z.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eqxv36KVoAAA93z.jpg",
+					"url": "https://t.co/TIzX6jCj58",
+					"display_url": "pic.twitter.com/TIzX6jCj58",
+					"expanded_url": "https://twitter.com/antennamusic/status/1345573404213039104/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 266004552,
+			"id_str": "266004552",
+			"name": "안테나 Antenna",
+			"screen_name": "antennamusic",
+			"location": "대한민국 서울",
+			"description": "정재형, 유희열(Toy), 루시드폴, 페퍼톤스, 박새별, 권진아, Sam Kim, 정승환, 이진아, CHAI, 윤석철, 적재, 서동환 Official / 2020 안테나 크리스마스 캐럴 ‘겨울의 우리들’ Release!",
+			"url": "https://t.co/CqfXN7It7U",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/CqfXN7It7U",
+							"expanded_url": "http://www.antenna.co.kr",
+							"display_url": "antenna.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 35274,
+			"friends_count": 10,
+			"listed_count": 366,
+			"created_at": "Mon Mar 14 13:49:27 +0000 2011",
+			"favourites_count": 13,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3404,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266004552/1608569605",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 32,
+		"favorite_count": 75,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[Release]\n\nSHINDRUM [Who I Am]\nOverthink (Feat. Sam Kim)\n\nLyrics by SHINDRUM HAEUN 샘김 (Sam Kim)\nComposed by SHINDRUM HAEUN 샘김 (Sam Kim) Jordan Rakei\nArranged by SHINDRUM HAEUN\n\n샘김이 작사, 작곡, 보컬로 참여한 ‘Overthink’는\n전 온라인 음원 사이트에서 감상하실 수 있습니다 💚 https://t.co/TIzX6jCj58"
+	},
+	{
 		"created_at": "Sun Jan 03 03:01:18 +0000 2021",
 		"id": 1345565872606371800,
 		"id_str": "1345565872606371841",
