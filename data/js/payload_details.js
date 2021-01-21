80179a1415a38d463b43b7640af43158b7cacd35
솔플파티 <solplparty@gmail.com>
2021-01-21 11:17:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 48143,
-	"created_at": "Wed Jan 20 15:17:41 +0000 2021",
+	"tweets": 48144,
+	"created_at": "Thu Jan 21 02:17:37 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2021_01.js",
 		"year": 2021,
 		"var_name": "tweets_2021_01",
-		"tweet_count": 415,
+		"tweet_count": 416,
 		"month": 1
 	},
 	{
