7d802a57b7c105dd3aad9433737f85373f3c99dc
솔플파티 <solplparty@gmail.com>
2021-01-25 01:17:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 48226,
-	"created_at": "Sun Jan 24 15:17:40 +0000 2021",
+	"tweets": 48227,
+	"created_at": "Sun Jan 24 16:17:37 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,148 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Sun Jan 24 15:29:30 +0000 2021",
+		"id": 1353361805603598300,
+		"id_str": "1353364309615763456",
+		"full_text": "[#이찬형] '경이로운 소문' 이찬형 \"수호로 지낸 시간, 영광스럽고 행복한 순간이었다\"(인터뷰①)\n\n▶️ https://t.co/j2NTaeQiYD\n\n#경이로운소문 #권수호 #융 #수호",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			104
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이찬형",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "경이로운소문",
+					"indices": [
+						85,
+						92
+					]
+				},
+				{
+					"text": "권수호",
+					"indices": [
+						93,
+						97
+					]
+				},
+				{
+					"text": "융",
+					"indices": [
+						98,
+						100
+					]
+				},
+				{
+					"text": "수호",
+					"indices": [
+						101,
+						104
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/j2NTaeQiYD",
+					"expanded_url": "https://bit.ly/3pg9TKf",
+					"display_url": "bit.ly/3pg9TKf",
+					"indices": [
+						60,
+						83
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/BKUIHw4jho",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BKUIHw4jho",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20232,
+			"friends_count": 18,
+			"listed_count": 241,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5565,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1553488269",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#이찬형] '경이로운 소문' 이찬형 \"수호로 지낸 시간, 영광스럽고 행복한 순간이었다\"(인터뷰①)\n\n▶️ https://t.co/j2NTaeQiYD\n\n#경이로운소문 #권수호 #융 #수호"
+	},
+	{
 		"created_at": "Sun Jan 24 15:01:14 +0000 2021",
 		"id": 1353357196034265000,
 		"id_str": "1353357196034265089",
