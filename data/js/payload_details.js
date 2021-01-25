ca8d5b54982d9a64467fff34aba1e3ce865134d8
솔플파티 <solplparty@gmail.com>
2021-01-25 19:17:41 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 48249,
-	"created_at": "Mon Jan 25 09:17:38 +0000 2021",
+	"tweets": 48250,
+	"created_at": "Mon Jan 25 10:17:40 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,235 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Mon Jan 25 09:29:25 +0000 2021",
+		"id": 1353628775196422100,
+		"id_str": "1353636078658457601",
+		"full_text": "골든차일드(Golden Child) '안아줄게 (Burn It)' MV\n\n2021.01.25 6PM RELEASE\n\nhttps://t.co/cAQNIqLcq0\nhttps://t.co/TpTLWG29lX\n\n#GoldenChild #골든차일드 #YES\n#안아줄게 #Burn_It https://t.co/VrMhMKK1tK",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			153
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GoldenChild",
+					"indices": [
+						114,
+						126
+					]
+				},
+				{
+					"text": "골든차일드",
+					"indices": [
+						127,
+						133
+					]
+				},
+				{
+					"text": "YES",
+					"indices": [
+						134,
+						138
+					]
+				},
+				{
+					"text": "안아줄게",
+					"indices": [
+						139,
+						144
+					]
+				},
+				{
+					"text": "Burn_It",
+					"indices": [
+						145,
+						153
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/cAQNIqLcq0",
+					"expanded_url": "http://youtu.be/lDWM2QnPPAY",
+					"display_url": "youtu.be/lDWM2QnPPAY",
+					"indices": [
+						65,
+						88
+					]
+				},
+				{
+					"url": "https://t.co/TpTLWG29lX",
+					"expanded_url": "http://vlive.tv/video/234051",
+					"display_url": "vlive.tv/video/234051",
+					"indices": [
+						89,
+						112
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1353626172110725000,
+					"id_str": "1353626172110725120",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/EskL1OtU4AALycL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EskL1OtU4AALycL.jpg",
+					"url": "https://t.co/VrMhMKK1tK",
+					"display_url": "pic.twitter.com/VrMhMKK1tK",
+					"expanded_url": "https://twitter.com/woollim_ent/status/1353628775196422144/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1353626172110725000,
+					"id_str": "1353626172110725120",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/EskL1OtU4AALycL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EskL1OtU4AALycL.jpg",
+					"url": "https://t.co/VrMhMKK1tK",
+					"display_url": "pic.twitter.com/VrMhMKK1tK",
+					"expanded_url": "https://twitter.com/woollim_ent/status/1353628775196422144/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 713661673,
+			"id_str": "713661673",
+			"name": "woollim",
+			"screen_name": "woollim_ent",
+			"location": "",
+			"description": "Woollim Entertainment Official Twitter",
+			"url": "https://t.co/FVUD6x00cm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/FVUD6x00cm",
+							"expanded_url": "http://www.woolliment.com",
+							"display_url": "woolliment.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 425251,
+			"friends_count": 10,
+			"listed_count": 1816,
+			"created_at": "Tue Jul 24 04:15:30 +0000 2012",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2105,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1269648322144288769/Ds4vXWM6_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1269648322144288769/Ds4vXWM6_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/713661673/1610618486",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1288,
+		"favorite_count": 2161,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "골든차일드(Golden Child) '안아줄게 (Burn It)' MV\n\n2021.01.25 6PM RELEASE\n\nhttps://t.co/cAQNIqLcq0\nhttps://t.co/TpTLWG29lX\n\n#GoldenChild #골든차일드 #YES\n#안아줄게 #Burn_It https://t.co/VrMhMKK1tK"
+	},
+	{
 		"created_at": "Mon Jan 25 09:06:34 +0000 2021",
 		"id": 1353630330830336000,
 		"id_str": "1353630330830336003",
