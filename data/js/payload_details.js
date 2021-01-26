5a2c07af0815cad99d6bf4448b8b8b3841a7a8d6
솔플파티 <solplparty@gmail.com>
2021-01-26 20:17:40 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 48281,
-	"created_at": "Tue Jan 26 10:17:40 +0000 2021",
+	"tweets": 48282,
+	"created_at": "Tue Jan 26 11:17:39 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,236 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Tue Jan 26 10:29:25 +0000 2021",
+		"id": 1354003460241297400,
+		"id_str": "1354013566291107840",
+		"full_text": "#임이랑(#디어클라우드) #식물수다\n잠시동안의 방학 기간을 끝내고\n오늘 밤12시부터 새로 시작 됩니다\n#임이랑의식물수다 #EBS\n#EBS라디오 #팟캐스트 #팟빵 https://t.co/57UtMzIUMw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			89
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "임이랑",
+					"indices": [
+						0,
+						4
+					]
+				},
+				{
+					"text": "디어클라우드",
+					"indices": [
+						5,
+						12
+					]
+				},
+				{
+					"text": "식물수다",
+					"indices": [
+						14,
+						19
+					]
+				},
+				{
+					"text": "임이랑의식물수다",
+					"indices": [
+						57,
+						66
+					]
+				},
+				{
+					"text": "EBS",
+					"indices": [
+						67,
+						71
+					]
+				},
+				{
+					"text": "EBS라디오",
+					"indices": [
+						72,
+						79
+					]
+				},
+				{
+					"text": "팟캐스트",
+					"indices": [
+						80,
+						85
+					]
+				},
+				{
+					"text": "팟빵",
+					"indices": [
+						86,
+						89
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1354003456638427100,
+					"id_str": "1354003456638427143",
+					"indices": [
+						90,
+						113
+					],
+					"media_url": "http://pbs.twimg.com/media/Espi-FGVoAcwLHE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Espi-FGVoAcwLHE.jpg",
+					"url": "https://t.co/57UtMzIUMw",
+					"display_url": "pic.twitter.com/57UtMzIUMw",
+					"expanded_url": "https://twitter.com/messenger_yoon/status/1354003460241297409/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1354003456638427100,
+					"id_str": "1354003456638427143",
+					"indices": [
+						90,
+						113
+					],
+					"media_url": "http://pbs.twimg.com/media/Espi-FGVoAcwLHE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Espi-FGVoAcwLHE.jpg",
+					"url": "https://t.co/57UtMzIUMw",
+					"display_url": "pic.twitter.com/57UtMzIUMw",
+					"expanded_url": "https://twitter.com/messenger_yoon/status/1354003460241297409/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 150566482,
+			"id_str": "150566482",
+			"name": "엠와이뮤직(MYmusic Ent)",
+			"screen_name": "messenger_yoon",
+			"location": "Artist Manager (South Korea)",
+			"description": "[Musician] 정준일, 낭만유랑악단, 나인(Nine9), 디어클라우드, 권영찬, 위아영 //mymusicent@naver.com//+82)2.6013.0102",
+			"url": "https://t.co/1IfWMp2Je9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1IfWMp2Je9",
+							"expanded_url": "http://www.mymusicent.co.kr",
+							"display_url": "mymusicent.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2677,
+			"friends_count": 341,
+			"listed_count": 32,
+			"created_at": "Tue Jun 01 07:29:53 +0000 2010",
+			"favourites_count": 18144,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5601,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/540007978649128960/jSz5oRO7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/540007978649128960/jSz5oRO7_normal.jpeg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#임이랑(#디어클라우드) #식물수다\n잠시동안의 방학 기간을 끝내고\n오늘 밤12시부터 새로 시작 됩니다\n#임이랑의식물수다 #EBS\n#EBS라디오 #팟캐스트 #팟빵 https://t.co/57UtMzIUMw"
+	},
+	{
 		"created_at": "Tue Jan 26 09:29:42 +0000 2021",
 		"id": 1353996470366531600,
 		"id_str": "1353998539437780992",
