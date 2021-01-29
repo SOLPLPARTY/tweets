558e056454ab178b8eb3baaa61e83b3f2db5c183
솔플파티 <solplparty@gmail.com>
2021-01-29 14:17:43 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 48353,
-	"created_at": "Fri Jan 29 04:17:41 +0000 2021",
+	"tweets": 48355,
+	"created_at": "Fri Jan 29 05:17:42 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,297 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Fri Jan 29 05:14:38 +0000 2021",
+		"id": 1355021511510405000,
+		"id_str": "1355021511510405120",
+		"full_text": "#문화가있는날 #블로그\n집콕의 지루함을 떨치자! OTT 채널별 최애 콘텐츠\nhttps://t.co/x7WmnjYrwm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			65
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "문화가있는날",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"text": "블로그",
+					"indices": [
+						8,
+						12
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/x7WmnjYrwm",
+					"expanded_url": "https://bit.ly/3ciuVEA",
+					"display_url": "bit.ly/3ciuVEA",
+					"indices": [
+						42,
+						65
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.powerapps.com\" rel=\"nofollow\">Microsoft Power Platform</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 968706450779463700,
+			"id_str": "968706450779463682",
+			"name": "솔플파티",
+			"screen_name": "SOLPLPARTY",
+			"location": "문의: contact@solplparty.com",
+			"description": "🌈 문화예술 커뮤니티, 솔플파티\n\n#문화예술정보 @ArtManagementKR\n#뮤직매거진 @Magazine_KR\n#티켓오픈 @TicketOpen\n#음악방송 @KPOP_Broadcast\n\n\n▶ 참가하기\nhttps://t.co/VTy0e7c0kj\n\n❓ ABOUT\nhttps://t.co/eNOwJHvWcy",
+			"url": "https://t.co/L6Ap35US8u",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/L6Ap35US8u",
+							"expanded_url": "https://solplparty.com",
+							"display_url": "solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VTy0e7c0kj",
+							"expanded_url": "http://join.solpl.party",
+							"display_url": "join.solpl.party",
+							"indices": [
+								113,
+								136
+							]
+						},
+						{
+							"url": "https://t.co/eNOwJHvWcy",
+							"expanded_url": "http://about.solpl.party",
+							"display_url": "about.solpl.party",
+							"indices": [
+								146,
+								169
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 255,
+			"friends_count": 440,
+			"listed_count": 2,
+			"created_at": "Wed Feb 28 04:36:30 +0000 2018",
+			"favourites_count": 247,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 67790,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/968706450779463682/1519792691",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#문화가있는날 #블로그\n집콕의 지루함을 떨치자! OTT 채널별 최애 콘텐츠\nhttps://t.co/x7WmnjYrwm"
+	},
+	{
+		"created_at": "Fri Jan 29 04:29:28 +0000 2021",
+		"id": 1355005102063382500,
+		"id_str": "1355010146485858305",
+		"full_text": "멜론 https://t.co/tNfhlztz0T\n지니 https://t.co/2iyDNrq1CZ\n플로 https://t.co/egM75l3Owl\n벅스 https://t.co/ny4mvPVxwH\n바이브 https://t.co/i7dgUkk5xY\n유튜브 https://t.co/1RWfps9lyz\n\nAvailable on all digital outlets worldwide on Feb 10 and on Bandcamp now.🙌🙌🙌",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			241
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/tNfhlztz0T",
+					"expanded_url": "https://bit.ly/3aaSnRz",
+					"display_url": "bit.ly/3aaSnRz",
+					"indices": [
+						3,
+						26
+					]
+				},
+				{
+					"url": "https://t.co/2iyDNrq1CZ",
+					"expanded_url": "https://bit.ly/2MDiZSQ",
+					"display_url": "bit.ly/2MDiZSQ",
+					"indices": [
+						30,
+						53
+					]
+				},
+				{
+					"url": "https://t.co/egM75l3Owl",
+					"expanded_url": "https://bit.ly/2NLrtYV",
+					"display_url": "bit.ly/2NLrtYV",
+					"indices": [
+						57,
+						80
+					]
+				},
+				{
+					"url": "https://t.co/ny4mvPVxwH",
+					"expanded_url": "https://bit.ly/36kpUaA",
+					"display_url": "bit.ly/36kpUaA",
+					"indices": [
+						84,
+						107
+					]
+				},
+				{
+					"url": "https://t.co/i7dgUkk5xY",
+					"expanded_url": "https://bit.ly/36oa4fb",
+					"display_url": "bit.ly/36oa4fb",
+					"indices": [
+						112,
+						135
+					]
+				},
+				{
+					"url": "https://t.co/1RWfps9lyz",
+					"expanded_url": "https://youtu.be/hoFSLaXse_U",
+					"display_url": "youtu.be/hoFSLaXse_U",
+					"indices": [
+						140,
+						163
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1355005098007482400,
+		"in_reply_to_status_id_str": "1355005098007482369",
+		"in_reply_to_user_id": 49219598,
+		"in_reply_to_user_id_str": "49219598",
+		"in_reply_to_screen_name": "electric_muse",
+		"user": {
+			"id": 49219598,
+			"id_str": "49219598",
+			"name": "Electric Muse",
+			"screen_name": "electric_muse",
+			"location": "",
+			"description": "We're indie record label in Seoul since 2006 / Contact electricmuse@naver.com",
+			"url": "https://t.co/Xm46jXAPqj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Xm46jXAPqj",
+							"expanded_url": "http://electricmuse.bandcamp.com",
+							"display_url": "electricmuse.bandcamp.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3396,
+			"friends_count": 1302,
+			"listed_count": 103,
+			"created_at": "Sun Jun 21 03:37:47 +0000 2009",
+			"favourites_count": 5233,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 25042,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1345631035782909952/HMJD1AoS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1345631035782909952/HMJD1AoS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/49219598/1502256266",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "멜론 https://t.co/tNfhlztz0T\n지니 https://t.co/2iyDNrq1CZ\n플로 https://t.co/egM75l3Owl\n벅스 https://t.co/ny4mvPVxwH\n바이브 https://t.co/i7dgUkk5xY\n유튜브 https://t.co/1RWfps9lyz\n\nAvailable on all digital outlets worldwide on Feb 10 and on Bandcamp now.🙌🙌🙌"
+	},
+	{
 		"created_at": "Fri Jan 29 03:43:53 +0000 2021",
 		"id": 1354998673462222800,
 		"id_str": "1354998673462222849",
