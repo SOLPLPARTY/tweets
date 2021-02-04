e985059ace6065fad5b9c8a406054b4ccd4d4f58
솔플파티 <solplparty@gmail.com>
2021-02-04 19:17:35 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 48513,
-	"created_at": "Thu Feb 04 09:17:33 +0000 2021",
+	"tweets": 48514,
+	"created_at": "Thu Feb 04 10:17:34 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,148 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Thu Feb 04 09:29:25 +0000 2021",
+		"id": 1357255259479965700,
+		"id_str": "1357259957247311872",
+		"full_text": "[Release]\n\n개코, 권진아 - ‘마음이 그래’\n\n✨ LIVE CLIP ✨\n✔️ https://t.co/bUg5ICM75B\n\nLyrics by 권진아\nComposed by 권진아\nArranged by philtre\n\n#권진아 #KwonJinAh \n#개코 #Gaeko #마음이그래",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			158
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "권진아",
+					"indices": [
+						124,
+						128
+					]
+				},
+				{
+					"text": "KwonJinAh",
+					"indices": [
+						129,
+						139
+					]
+				},
+				{
+					"text": "개코",
+					"indices": [
+						141,
+						144
+					]
+				},
+				{
+					"text": "Gaeko",
+					"indices": [
+						145,
+						151
+					]
+				},
+				{
+					"text": "마음이그래",
+					"indices": [
+						152,
+						158
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/bUg5ICM75B",
+					"expanded_url": "https://youtu.be/51RkNGIM5oE",
+					"display_url": "youtu.be/51RkNGIM5oE",
+					"indices": [
+						48,
+						71
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 266004552,
+			"id_str": "266004552",
+			"name": "안테나 Antenna",
+			"screen_name": "antennamusic",
+			"location": "대한민국 서울",
+			"description": "정재형, 유희열(Toy), 루시드폴, 페퍼톤스, 박새별, 권진아, Sam Kim, 정승환, 이진아, CHAI, 윤석철, 적재, 서동환 Official / 2020 안테나 크리스마스 캐럴 ‘겨울의 우리들’ Release!",
+			"url": "https://t.co/CqfXN7It7U",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/CqfXN7It7U",
+							"expanded_url": "http://www.antenna.co.kr",
+							"display_url": "antenna.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 35439,
+			"friends_count": 10,
+			"listed_count": 359,
+			"created_at": "Mon Mar 14 13:49:27 +0000 2011",
+			"favourites_count": 13,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3426,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266004552/1608569605",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 36,
+		"favorite_count": 40,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[Release]\n\n개코, 권진아 - ‘마음이 그래’\n\n✨ LIVE CLIP ✨\n✔️ https://t.co/bUg5ICM75B\n\nLyrics by 권진아\nComposed by 권진아\nArranged by philtre\n\n#권진아 #KwonJinAh \n#개코 #Gaeko #마음이그래"
+	},
+	{
 		"created_at": "Thu Feb 04 09:15:02 +0000 2021",
 		"id": 1357256339408507000,
 		"id_str": "1357256339408506880",
