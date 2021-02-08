436d05edbca589739d082b685933a81edd3daf8d
솔플파티 <solplparty@gmail.com>
2021-02-09 00:17:43 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 48604,
-	"created_at": "Mon Feb 08 14:17:35 +0000 2021",
+	"tweets": 48605,
+	"created_at": "Mon Feb 08 15:17:42 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,216 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Mon Feb 08 14:29:25 +0000 2021",
+		"id": 1358778266966888400,
+		"id_str": "1358785006638948352",
+		"full_text": "4th annual All Nations Reggae Symposium. \n  \"Reggae: From Jamaica to a worldwide movement of cultural resurgence and resistance\" https://t.co/EGSBuG1RfH",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			128
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1358778258150416400,
+					"id_str": "1358778258150416388",
+					"indices": [
+						129,
+						152
+					],
+					"media_url": "http://pbs.twimg.com/media/EttZoG2UYAQy5bw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EttZoG2UYAQy5bw.jpg",
+					"url": "https://t.co/EGSBuG1RfH",
+					"display_url": "pic.twitter.com/EGSBuG1RfH",
+					"expanded_url": "https://twitter.com/E_S_Sounds/status/1358778266966888449/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 509,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 999,
+							"h": 748,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 999,
+							"h": 748,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1358778258150416400,
+					"id_str": "1358778258150416388",
+					"indices": [
+						129,
+						152
+					],
+					"media_url": "http://pbs.twimg.com/media/EttZoG2UYAQy5bw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EttZoG2UYAQy5bw.jpg",
+					"url": "https://t.co/EGSBuG1RfH",
+					"display_url": "pic.twitter.com/EGSBuG1RfH",
+					"expanded_url": "https://twitter.com/E_S_Sounds/status/1358778266966888449/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 509,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 999,
+							"h": 748,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 999,
+							"h": 748,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1358778258439819300,
+					"id_str": "1358778258439819266",
+					"indices": [
+						129,
+						152
+					],
+					"media_url": "http://pbs.twimg.com/media/EttZoH7UUAIoyAb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EttZoH7UUAIoyAb.jpg",
+					"url": "https://t.co/EGSBuG1RfH",
+					"display_url": "pic.twitter.com/EGSBuG1RfH",
+					"expanded_url": "https://twitter.com/E_S_Sounds/status/1358778266966888449/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1125,
+							"h": 2000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2855837208,
+			"id_str": "2855837208",
+			"name": "동양 표준 음향사/ Eastern Standard Sounds",
+			"screen_name": "E_S_Sounds",
+			"location": "Seoul, Korea",
+			"description": "All Roots and Culture/ Label/ Agency/ Distribute/ Produce/",
+			"url": "https://t.co/oI8deHx50b",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/oI8deHx50b",
+							"expanded_url": "http://www.easternstandardsounds.com",
+							"display_url": "easternstandardsounds.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 620,
+			"friends_count": 1787,
+			"listed_count": 10,
+			"created_at": "Tue Oct 14 19:11:59 +0000 2014",
+			"favourites_count": 234,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1501,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/669566643438116865/faHYzrAM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/669566643438116865/faHYzrAM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2855837208/1609456279",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "4th annual All Nations Reggae Symposium. \n  \"Reggae: From Jamaica to a worldwide movement of cultural resurgence and resistance\" https://t.co/EGSBuG1RfH"
+	},
+	{
 		"created_at": "Mon Feb 08 13:29:28 +0000 2021",
 		"id": 1358760069924495400,
 		"id_str": "1358769920293634048",
