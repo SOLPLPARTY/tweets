33b9385c75446300f3219411dae23632cf25e350
솔플파티 <solplparty@gmail.com>
2021-02-12 12:17:36 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 48686,
-	"created_at": "Thu Feb 11 21:17:35 +0000 2021",
+	"tweets": 48688,
+	"created_at": "Fri Feb 12 03:17:35 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2021_02.js",
 		"year": 2021,
 		"var_name": "tweets_2021_02",
-		"tweet_count": 277,
+		"tweet_count": 279,
 		"month": 2
 	},
 	{
