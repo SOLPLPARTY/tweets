6802d23b05dd17aaa3cd1e9f4f348084b3981989
솔플파티 <solplparty@gmail.com>
2021-02-18 15:17:39 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 48847,
-	"created_at": "Thu Feb 18 05:17:37 +0000 2021",
+	"tweets": 48848,
+	"created_at": "Thu Feb 18 06:17:38 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,209 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Thu Feb 18 05:29:29 +0000 2021",
+		"id": 1362265589008371700,
+		"id_str": "1362273007041978368",
+		"full_text": "[#정진운] 영화 ‘오! 마이 고스트’ 캐스팅✔\n\n귀신 보는 능력을 지닌 스튜디오 FD, 주인공 태민 역으로 활약 예고🔥\n\n좌충우돌 코믹 호러 '오! 마이 고스트' COMING SOON🔜\n\n#영화 #오마이고스트 #태민 https://t.co/kGbWRzIeEk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			121
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "정진운",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "영화",
+					"indices": [
+						106,
+						109
+					]
+				},
+				{
+					"text": "오마이고스트",
+					"indices": [
+						110,
+						117
+					]
+				},
+				{
+					"text": "태민",
+					"indices": [
+						118,
+						121
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1362225622022451200,
+					"id_str": "1362225622022451202",
+					"indices": [
+						122,
+						145
+					],
+					"media_url": "http://pbs.twimg.com/media/EueY_GCVcAIHZT6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EueY_GCVcAIHZT6.jpg",
+					"url": "https://t.co/kGbWRzIeEk",
+					"display_url": "pic.twitter.com/kGbWRzIeEk",
+					"expanded_url": "https://twitter.com/themysticstory/status/1362265589008371714/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1362225622022451200,
+					"id_str": "1362225622022451202",
+					"indices": [
+						122,
+						145
+					],
+					"media_url": "http://pbs.twimg.com/media/EueY_GCVcAIHZT6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EueY_GCVcAIHZT6.jpg",
+					"url": "https://t.co/kGbWRzIeEk",
+					"display_url": "pic.twitter.com/kGbWRzIeEk",
+					"expanded_url": "https://twitter.com/themysticstory/status/1362265589008371714/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/BKUIHw4jho",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BKUIHw4jho",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20505,
+			"friends_count": 18,
+			"listed_count": 239,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5633,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1553488269",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 11,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#정진운] 영화 ‘오! 마이 고스트’ 캐스팅✔\n\n귀신 보는 능력을 지닌 스튜디오 FD, 주인공 태민 역으로 활약 예고🔥\n\n좌충우돌 코믹 호러 '오! 마이 고스트' COMING SOON🔜\n\n#영화 #오마이고스트 #태민 https://t.co/kGbWRzIeEk"
+	},
+	{
 		"created_at": "Thu Feb 18 05:11:17 +0000 2021",
 		"id": 1362263965133246500,
 		"id_str": "1362268426526425088",
