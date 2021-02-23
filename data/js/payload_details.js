ece1a75f0f63493a3fc0a6815cdac66a9c875a33
솔플파티 <solplparty@gmail.com>
2021-02-23 16:17:39 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 48972,
-	"created_at": "Tue Feb 23 06:17:39 +0000 2021",
+	"tweets": 48973,
+	"created_at": "Tue Feb 23 07:17:38 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,162 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Tue Feb 23 06:29:25 +0000 2021",
+		"id": 1364090849554927600,
+		"id_str": "1364100029355597828",
+		"full_text": "[#NEWS] ‘오월의 청춘’ 고민시, “극에 대한 고민多, 디테일한 부분까지 연기하려 노력” [스타@스타일]\n\n▶️ https://t.co/1J4z535hWl\n\n#고민시 #좋아하면울리는2 #오월의청춘 #지리산 #앳스타일 #화보",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			127
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "고민시",
+					"indices": [
+						91,
+						95
+					]
+				},
+				{
+					"text": "좋아하면울리는2",
+					"indices": [
+						96,
+						105
+					]
+				},
+				{
+					"text": "오월의청춘",
+					"indices": [
+						106,
+						112
+					]
+				},
+				{
+					"text": "지리산",
+					"indices": [
+						113,
+						117
+					]
+				},
+				{
+					"text": "앳스타일",
+					"indices": [
+						118,
+						123
+					]
+				},
+				{
+					"text": "화보",
+					"indices": [
+						124,
+						127
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/1J4z535hWl",
+					"expanded_url": "http://bit.ly/3btHaMs",
+					"display_url": "bit.ly/3btHaMs",
+					"indices": [
+						66,
+						89
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/BKUIHw4jho",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BKUIHw4jho",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20565,
+			"friends_count": 18,
+			"listed_count": 236,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5648,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1553488269",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] ‘오월의 청춘’ 고민시, “극에 대한 고민多, 디테일한 부분까지 연기하려 노력” [스타@스타일]\n\n▶️ https://t.co/1J4z535hWl\n\n#고민시 #좋아하면울리는2 #오월의청춘 #지리산 #앳스타일 #화보"
+	},
+	{
 		"created_at": "Tue Feb 23 05:29:31 +0000 2021",
 		"id": 1364081221907619800,
 		"id_str": "1364084956469755906",
