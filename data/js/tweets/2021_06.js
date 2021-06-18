f726adeca71f13e4c490ca9e173d482df3b2a2b7
솔플파티 <solplparty@gmail.com>
2021-06-18 19:17:34 +0900

    Added 1 tweets

--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,10 +1,151 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Fri Jun 18 09:44:54 +0000 2021",
+		"id": 1405823827876233200,
+		"id_str": "1405823827876233216",
+		"full_text": "#민트페이퍼 #MINTPAPER\n수능기출에 도전한 N년차 직장인...※과도한 경쟁심 주의※ (수능특집, 스피드퀴즈, 할리갈리, 무의식) | [암쏘큐] Ep.10 | 민트페이퍼\nhttps://t.co/JrVRAWRtMK",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			121
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "민트페이퍼",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "MINTPAPER",
+					"indices": [
+						7,
+						17
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/JrVRAWRtMK",
+					"expanded_url": "https://youtu.be/0oD4cO9O2nI",
+					"display_url": "youtu.be/0oD4cO9O2nI",
+					"indices": [
+						98,
+						121
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.powerapps.com\" rel=\"nofollow\">Microsoft Power Platform</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 968706450779463700,
+			"id_str": "968706450779463682",
+			"name": "솔플파티",
+			"screen_name": "SOLPLPARTY",
+			"location": "문의: contact@solplparty.com",
+			"description": "🌈 문화예술 커뮤니티, 솔플파티\n\n#문화예술정보 @ArtManagementKR\n#뮤직매거진 @Magazine_KR\n#티켓오픈 @TicketOpen\n#음악방송 @KPOP_Broadcast\n\n\n▶ 참가하기\nhttps://t.co/VTy0e7c0kj\n\n❓ ABOUT\nhttps://t.co/eNOwJHvWcy",
+			"url": "https://t.co/L6Ap35US8u",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/L6Ap35US8u",
+							"expanded_url": "https://solplparty.com",
+							"display_url": "solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VTy0e7c0kj",
+							"expanded_url": "http://join.solpl.party",
+							"display_url": "join.solpl.party",
+							"indices": [
+								113,
+								136
+							]
+						},
+						{
+							"url": "https://t.co/eNOwJHvWcy",
+							"expanded_url": "http://about.solpl.party",
+							"display_url": "about.solpl.party",
+							"indices": [
+								146,
+								169
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 261,
+			"friends_count": 463,
+			"listed_count": 3,
+			"created_at": "Wed Feb 28 04:36:30 +0000 2018",
+			"favourites_count": 246,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 69496,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/968706450779463682/1519792691",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#민트페이퍼 #MINTPAPER\n수능기출에 도전한 N년차 직장인...※과도한 경쟁심 주의※ (수능특집, 스피드퀴즈, 할리갈리, 무의식) | [암쏘큐] Ep.10 | 민트페이퍼\nhttps://t.co/JrVRAWRtMK"
+	},
+	{
 		"created_at": "Fri Jun 18 09:12:50 +0000 2021",
 		"id": 1405815759247491000,
 		"id_str": "1405815759247491075",
-		"full_text": "#유니버설뮤직코리아 #Universal_Music_Korea\n야! 나랑 사랑하자!❤️‍🔥 | 제시 제이(Jessie J) - I Want Love (한글 자막)\nhttps://t.co/fRIJWtiVxV",
+		"full_text": "#유니버설뮤직코리아 #Universal_Music_Korea\n야! 나랑 사랑하자!❤️🔥 | 제시 제이(Jessie J) - I Want Love (한글 자막)\nhttps://t.co/fRIJWtiVxV",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -139,7 +280,7 @@ Grailbird.data.tweets_2021_06 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "#유니버설뮤직코리아 #Universal_Music_Korea\n야! 나랑 사랑하자!❤️‍🔥 | 제시 제이(Jessie J) - I Want Love (한글 자막)\nhttps://t.co/fRIJWtiVxV"
+		"text": "#유니버설뮤직코리아 #Universal_Music_Korea\n야! 나랑 사랑하자!❤️🔥 | 제시 제이(Jessie J) - I Want Love (한글 자막)\nhttps://t.co/fRIJWtiVxV"
 	},
 	{
 		"created_at": "Fri Jun 18 09:05:16 +0000 2021",
