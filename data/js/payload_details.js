e14f9ec14bcda496fd717f696e3a776adb4d1afa
솔플파티 <solplparty@gmail.com>
2021-06-29 00:17:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 50312,
-	"created_at": "Mon Jun 28 13:17:38 +0000 2021",
+	"tweets": 50313,
+	"created_at": "Mon Jun 28 15:17:37 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,149 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Mon Jun 28 14:29:24 +0000 2021",
+		"id": 1409515406792953900,
+		"id_str": "1409519306665234441",
+		"full_text": "[Video] 적재\n\n산악회 아저씨들이 부르는\nSilk Sonic - Leave the door open 🚪\n(통키타연주-적재)\n\n✔️ https://t.co/jFJA4pSzs5\n\n#적재 #Jukjae #한사랑산악회\n#SilkSonic #LeaveTheDoorOpen",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			149
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "적재",
+					"indices": [
+						101,
+						104
+					]
+				},
+				{
+					"text": "Jukjae",
+					"indices": [
+						105,
+						112
+					]
+				},
+				{
+					"text": "한사랑산악회",
+					"indices": [
+						113,
+						120
+					]
+				},
+				{
+					"text": "SilkSonic",
+					"indices": [
+						121,
+						131
+					]
+				},
+				{
+					"text": "LeaveTheDoorOpen",
+					"indices": [
+						132,
+						149
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/jFJA4pSzs5",
+					"expanded_url": "https://youtu.be/kCNmE4HVCjI",
+					"display_url": "youtu.be/kCNmE4HVCjI",
+					"indices": [
+						76,
+						99
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 266004552,
+			"id_str": "266004552",
+			"name": "안테나 Antenna",
+			"screen_name": "antennamusic",
+			"location": "대한민국 서울",
+			"description": "정재형, 유희열(Toy), 루시드폴, 페퍼톤스, 박새별, 권진아, Sam Kim, 정승환, 이진아, 윤석철, 적재, 서동환 Official / Sam Kim (샘김) Single ‘The Juice’ 2021. 06. 23. 6PM Release!",
+			"url": "https://t.co/XSWWwEx210",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XSWWwEx210",
+							"expanded_url": "http://www.antenna.co.kr",
+							"display_url": "antenna.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 36307,
+			"friends_count": 14,
+			"listed_count": 373,
+			"created_at": "Mon Mar 14 13:49:27 +0000 2011",
+			"favourites_count": 13,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3671,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266004552/1623754978",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 32,
+		"favorite_count": 46,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[Video] 적재\n\n산악회 아저씨들이 부르는\nSilk Sonic - Leave the door open 🚪\n(통키타연주-적재)\n\n✔️ https://t.co/jFJA4pSzs5\n\n#적재 #Jukjae #한사랑산악회\n#SilkSonic #LeaveTheDoorOpen"
+	},
+	{
 		"created_at": "Mon Jun 28 13:11:16 +0000 2021",
 		"id": 1409488059813404700,
 		"id_str": "1409499643281608710",
