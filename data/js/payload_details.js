e3349075ed4a5403e1c0f223995b7de9c29a623a
솔플파티 <solplparty@gmail.com>
2021-06-29 12:17:39 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 50315,
-	"created_at": "Tue Jun 29 02:17:39 +0000 2021",
+	"tweets": 50316,
+	"created_at": "Tue Jun 29 03:17:38 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,156 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Tue Jun 29 02:29:25 +0000 2021",
+		"id": 1409695579937017900,
+		"id_str": "1409700501130645505",
+		"full_text": "[#정인] '비긴어게인 오픈마이크'🎤\n정인과 카더가든이 함께 부른 Thinking Out Loud🎵 \n\n🔗https://t.co/f2N3Uu5DHc\n\n#JTBC #비긴어게인 #오픈마이크 #EdSheeran #ThinkingOutLoud",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			131
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "정인",
+					"indices": [
+						1,
+						4
+					]
+				},
+				{
+					"text": "JTBC",
+					"indices": [
+						84,
+						89
+					]
+				},
+				{
+					"text": "비긴어게인",
+					"indices": [
+						90,
+						96
+					]
+				},
+				{
+					"text": "오픈마이크",
+					"indices": [
+						97,
+						103
+					]
+				},
+				{
+					"text": "EdSheeran",
+					"indices": [
+						104,
+						114
+					]
+				},
+				{
+					"text": "ThinkingOutLoud",
+					"indices": [
+						115,
+						131
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/f2N3Uu5DHc",
+					"expanded_url": "http://youtu.be/UmZufXyK-kw",
+					"display_url": "youtu.be/UmZufXyK-kw",
+					"indices": [
+						59,
+						82
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/BKUIHw4jho",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BKUIHw4jho",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21199,
+			"friends_count": 17,
+			"listed_count": 238,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6059,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1553488269",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#정인] '비긴어게인 오픈마이크'🎤\n정인과 카더가든이 함께 부른 Thinking Out Loud🎵 \n\n🔗https://t.co/f2N3Uu5DHc\n\n#JTBC #비긴어게인 #오픈마이크 #EdSheeran #ThinkingOutLoud"
+	},
+	{
 		"created_at": "Tue Jun 29 01:47:53 +0000 2021",
 		"id": 1409690052108558300,
 		"id_str": "1409690052108558342",
