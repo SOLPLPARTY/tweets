953830bb4a3640cafb7441bfb5db1c98ad88d769
솔플파티 <solplparty@gmail.com>
2021-06-30 22:17:42 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 50416,
-	"created_at": "Wed Jun 30 12:17:41 +0000 2021",
+	"tweets": 50417,
+	"created_at": "Wed Jun 30 13:17:41 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,124 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Wed Jun 30 12:29:25 +0000 2021",
+		"id": 1410210584411312000,
+		"id_str": "1410213883999145985",
+		"full_text": "@danpyunsun 수정이 필요할것 같아 댓글 답니다~ 김일두 새계절의 프로듀서와 연주로 참여한 종민님은 말씀하신 이종민님이 아니라 두루흥업의 김종민 입니다. https://t.co/NlbLqHKF0l",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			113
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "danpyunsun",
+					"name": "단편선 Danpyunsun 2021",
+					"id": 64089669,
+					"id_str": "64089669",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/NlbLqHKF0l",
+					"expanded_url": "https://drmicp.com/15313967",
+					"display_url": "drmicp.com/15313967",
+					"indices": [
+						90,
+						113
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1409877415153881000,
+		"in_reply_to_status_id_str": "1409877415153881094",
+		"in_reply_to_user_id": 64089669,
+		"in_reply_to_user_id_str": "64089669",
+		"in_reply_to_screen_name": "danpyunsun",
+		"user": {
+			"id": 1302973592448036900,
+			"id_str": "1302973592448036869",
+			"name": "두루미 흥업",
+			"screen_name": "dooroomi_c",
+			"location": "",
+			"description": "",
+			"url": "https://t.co/w0g5ZZkoX6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/w0g5ZZkoX6",
+							"expanded_url": "http://drmicp.com",
+							"display_url": "drmicp.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 128,
+			"friends_count": 176,
+			"listed_count": 1,
+			"created_at": "Mon Sep 07 14:14:53 +0000 2020",
+			"favourites_count": 614,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 371,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1349789670326951938/3GJe96y5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1349789670326951938/3GJe96y5_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1302973592448036869/1614584101",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "@danpyunsun 수정이 필요할것 같아 댓글 답니다~ 김일두 새계절의 프로듀서와 연주로 참여한 종민님은 말씀하신 이종민님이 아니라 두루흥업의 김종민 입니다. https://t.co/NlbLqHKF0l"
+	},
+	{
 		"created_at": "Wed Jun 30 11:58:37 +0000 2021",
 		"id": 1410191576005976000,
 		"id_str": "1410206135479586821",
