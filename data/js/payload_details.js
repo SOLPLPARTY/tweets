0bc88a6f1bcfa4917975ad7f846b5004ea04520c
솔플파티 <solplparty@gmail.com>
2021-07-01 12:17:32 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 50423,
-	"created_at": "Thu Jul 01 02:17:36 +0000 2021",
+	"tweets": 50425,
+	"created_at": "Thu Jul 01 03:17:32 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,451 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Thu Jul 01 03:11:16 +0000 2021",
+		"id": 1410432975078973400,
+		"id_str": "1410435811988979713",
+		"full_text": "&lt;웨이브투어스 단독 콘서트 : life always goes on and on,&gt;\n\n· 일시 : 2021년 07월 30일 (금) 오후 8시, 07월 31일 (토) 오후 5시, 08월 01일 (일) 오후 7시\n· 장소 : 롤링홀\n· 출연 : 웨이브투어스\n· 예매 : 멜론티켓 2021년 07월 08일 (목) 오후 8시 예매오픈\n\n#웨이브투어스 #롤링홀 #멜론티켓 https://t.co/zp96SJHVwy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			207
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "웨이브투어스",
+					"indices": [
+						189,
+						196
+					]
+				},
+				{
+					"text": "롤링홀",
+					"indices": [
+						197,
+						201
+					]
+				},
+				{
+					"text": "멜론티켓",
+					"indices": [
+						202,
+						207
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1409720685929271300,
+					"id_str": "1409720685929271296",
+					"indices": [
+						208,
+						231
+					],
+					"media_url": "http://pbs.twimg.com/media/E5BVfq1VkAAyvZh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5BVfq1VkAAyvZh.jpg",
+					"url": "https://t.co/zp96SJHVwy",
+					"display_url": "pic.twitter.com/zp96SJHVwy",
+					"expanded_url": "https://twitter.com/rollinghall1995/status/1410432975078973443/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1409720685929271300,
+					"id_str": "1409720685929271296",
+					"indices": [
+						208,
+						231
+					],
+					"media_url": "http://pbs.twimg.com/media/E5BVfq1VkAAyvZh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5BVfq1VkAAyvZh.jpg",
+					"url": "https://t.co/zp96SJHVwy",
+					"display_url": "pic.twitter.com/zp96SJHVwy",
+					"expanded_url": "https://twitter.com/rollinghall1995/status/1410432975078973443/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1291327050578456600,
+			"id_str": "1291327050578456578",
+			"name": "롤링홀",
+			"screen_name": "rollinghall1995",
+			"location": "서울시 마포구 어울마당로 35 신보빌딩 지하1층",
+			"description": "홍대/합정에 위치한 음악 공연장 #롤링홀 입니다٩( ᐛ )و\nConcert live hall in South Korea\n-\n👇🏻👇🏻멜론티켓 롤링홀 기획공연 바로가기.\nhttps://t.co/TADR20wMLG",
+			"url": "https://t.co/C9YRGggByu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/C9YRGggByu",
+							"expanded_url": "http://www.rollinghall.co.kr/default/",
+							"display_url": "rollinghall.co.kr/default/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/TADR20wMLG",
+							"expanded_url": "https://bit.ly/2ManUuA",
+							"display_url": "bit.ly/2ManUuA",
+							"indices": [
+								93,
+								116
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 180,
+			"friends_count": 3,
+			"listed_count": 2,
+			"created_at": "Thu Aug 06 10:56:17 +0000 2020",
+			"favourites_count": 102,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 237,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1291327708689862662/9hOLVWB2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1291327708689862662/9hOLVWB2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1291327050578456578/1596712102",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "&lt;웨이브투어스 단독 콘서트 : life always goes on and on,&gt;\n\n· 일시 : 2021년 07월 30일 (금) 오후 8시, 07월 31일 (토) 오후 5시, 08월 01일 (일) 오후 7시\n· 장소 : 롤링홀\n· 출연 : 웨이브투어스\n· 예매 : 멜론티켓 2021년 07월 08일 (목) 오후 8시 예매오픈\n\n#웨이브투어스 #롤링홀 #멜론티켓 https://t.co/zp96SJHVwy"
+	},
+	{
+		"created_at": "Thu Jul 01 02:58:14 +0000 2021",
+		"id": 1410417876431827000,
+		"id_str": "1410432532424835072",
+		"full_text": "#ASTRO's Breaking Live REPLAY OPEN 💝\nFULL Ver. ▶ https://t.co/FIfySrDfnk\n\n#KCONTACT #KCONTACT4U https://t.co/KRtbJ80yqp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			95
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ASTRO",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "KCONTACT",
+					"indices": [
+						74,
+						83
+					]
+				},
+				{
+					"text": "KCONTACT4U",
+					"indices": [
+						84,
+						95
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/FIfySrDfnk",
+					"expanded_url": "https://youtu.be/AZR3sXGPQ9E",
+					"display_url": "youtu.be/AZR3sXGPQ9E",
+					"indices": [
+						49,
+						72
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1410194189367660500,
+					"id_str": "1410194189367660547",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1410194189367660547/pu/img/6Q92-fi0S5vytyL9.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1410194189367660547/pu/img/6Q92-fi0S5vytyL9.jpg",
+					"url": "https://t.co/KRtbJ80yqp",
+					"display_url": "pic.twitter.com/KRtbJ80yqp",
+					"expanded_url": "https://twitter.com/KCON_official/status/1410417876431826944/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1410194189367660500,
+					"id_str": "1410194189367660547",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1410194189367660547/pu/img/6Q92-fi0S5vytyL9.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1410194189367660547/pu/img/6Q92-fi0S5vytyL9.jpg",
+					"url": "https://t.co/KRtbJ80yqp",
+					"display_url": "pic.twitter.com/KRtbJ80yqp",
+					"expanded_url": "https://twitter.com/KCON_official/status/1410417876431826944/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 30030,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1410194189367660547/pu/pl/o_WQ1yxmVTkouygt.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1410194189367660547/pu/vid/640x360/xNtiRF4_zJLiYh62.mp4?tag=12"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1410194189367660547/pu/vid/1280x720/TvYBDKZLw9uklOCm.mp4?tag=12"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1410194189367660547/pu/vid/480x270/YaIv54M4EYM6eRTy.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 135447868,
+			"id_str": "135447868",
+			"name": "KCON official",
+			"screen_name": "KCON_official",
+			"location": "Mnet, Seoul, Republic of KOREA",
+			"description": "KCON/KCON:TACT official twitter / JUNE 19(SAT)~27(SUN), 2021",
+			"url": "https://t.co/IDnJhWG279",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/IDnJhWG279",
+							"expanded_url": "http://kconofficial.com",
+							"display_url": "kconofficial.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 375435,
+			"friends_count": 75,
+			"listed_count": 994,
+			"created_at": "Wed Apr 21 09:56:48 +0000 2010",
+			"favourites_count": 128,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 10372,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1391536361627291651/aEkp5dth_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1391536361627291651/aEkp5dth_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/135447868/1620603098",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 445,
+		"favorite_count": 997,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#ASTRO's Breaking Live REPLAY OPEN 💝\nFULL Ver. ▶ https://t.co/FIfySrDfnk\n\n#KCONTACT #KCONTACT4U https://t.co/KRtbJ80yqp"
+	},
+	{
 		"created_at": "Thu Jul 01 02:02:33 +0000 2021",
 		"id": 1410418516315066400,
 		"id_str": "1410418516315066368",
