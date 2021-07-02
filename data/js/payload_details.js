00794a76282660ffa0df9aa2c511c9b5b2289a53
솔플파티 <solplparty@gmail.com>
2021-07-02 20:17:34 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 50505,
-	"created_at": "Fri Jul 02 10:17:33 +0000 2021",
+	"tweets": 50506,
+	"created_at": "Fri Jul 02 11:17:33 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,163 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Fri Jul 02 10:29:27 +0000 2021",
+		"id": 1410901125809676300,
+		"id_str": "1410908468916129795",
+		"full_text": "[#권혁수] [#삼대장]\n\n오늘의 키워드\n[모기, 파리, 바퀴벌레 중 무엇이 가장 극혐인가]\n\n⚠WARNING⚠ \n헛소리+억지로 이루어져 있습니다\n\n과연 이 🐶소리로 가득한 토론회의 승자는? \n다음주 #권혁수감성 에서 공개됩니다💥\n\n👉https://t.co/N9MwF11p0n\n\n#KwunHyukSoo #토론회 #강유미 #이세영",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			183
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "권혁수",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "삼대장",
+					"indices": [
+						8,
+						12
+					]
+				},
+				{
+					"text": "권혁수감성",
+					"indices": [
+						112,
+						118
+					]
+				},
+				{
+					"text": "KwunHyukSoo",
+					"indices": [
+						156,
+						168
+					]
+				},
+				{
+					"text": "토론회",
+					"indices": [
+						169,
+						173
+					]
+				},
+				{
+					"text": "강유미",
+					"indices": [
+						174,
+						178
+					]
+				},
+				{
+					"text": "이세영",
+					"indices": [
+						179,
+						183
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/N9MwF11p0n",
+					"expanded_url": "https://youtu.be/ijbvBK4DIDw",
+					"display_url": "youtu.be/ijbvBK4DIDw",
+					"indices": [
+						131,
+						154
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1219506842935513000,
+			"id_str": "1219506842935513088",
+			"name": "휴맵컨텐츠(HUMAP)",
+			"screen_name": "humap_official",
+			"location": "",
+			"description": "휴맵컨텐츠(HUMAP CONTENTS) Official Twitter",
+			"url": "https://t.co/fBZZK3SmX6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fBZZK3SmX6",
+							"expanded_url": "http://www.humapcontents.com/",
+							"display_url": "humapcontents.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 323,
+			"friends_count": 1,
+			"listed_count": 2,
+			"created_at": "Tue Jan 21 06:28:27 +0000 2020",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 368,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1219506842935513088/1579755898",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#권혁수] [#삼대장]\n\n오늘의 키워드\n[모기, 파리, 바퀴벌레 중 무엇이 가장 극혐인가]\n\n⚠WARNING⚠ \n헛소리+억지로 이루어져 있습니다\n\n과연 이 🐶소리로 가득한 토론회의 승자는? \n다음주 #권혁수감성 에서 공개됩니다💥\n\n👉https://t.co/N9MwF11p0n\n\n#KwunHyukSoo #토론회 #강유미 #이세영"
+	},
+	{
 		"created_at": "Fri Jul 02 09:29:49 +0000 2021",
 		"id": 1410891266280202200,
 		"id_str": "1410893463864938502",
