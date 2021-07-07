86d6d7c577109b5088ebda98d95adc5d2ad536cc
솔플파티 <solplparty@gmail.com>
2021-07-07 21:17:35 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 50621,
-	"created_at": "Wed Jul 07 10:17:34 +0000 2021",
+	"tweets": 50622,
+	"created_at": "Wed Jul 07 12:17:34 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,163 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Wed Jul 07 11:29:24 +0000 2021",
+		"id": 1412726943565316000,
+		"id_str": "1412735498876076036",
+		"full_text": "[NEWS] #권혁수 ‘#SNL코리아’ 합류…“즐거움을 드리기 위해 회의 거듭하고 있다” [공식]\n\n👉 https://t.co/K1sC2LXiKO\n\n#휴맵컨텐츠 #HUMAPCONTENTS #KwunHyukSoo #SNLKOREA #쿠팡플레이",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			134
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "권혁수",
+					"indices": [
+						7,
+						11
+					]
+				},
+				{
+					"text": "SNL코리아",
+					"indices": [
+						13,
+						20
+					]
+				},
+				{
+					"text": "휴맵컨텐츠",
+					"indices": [
+						83,
+						89
+					]
+				},
+				{
+					"text": "HUMAPCONTENTS",
+					"indices": [
+						90,
+						104
+					]
+				},
+				{
+					"text": "KwunHyukSoo",
+					"indices": [
+						105,
+						117
+					]
+				},
+				{
+					"text": "SNLKOREA",
+					"indices": [
+						118,
+						127
+					]
+				},
+				{
+					"text": "쿠팡플레이",
+					"indices": [
+						128,
+						134
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/K1sC2LXiKO",
+					"expanded_url": "http://naver.me/GEAhJWLS",
+					"display_url": "naver.me/GEAhJWLS",
+					"indices": [
+						58,
+						81
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1219506842935513000,
+			"id_str": "1219506842935513088",
+			"name": "휴맵컨텐츠(HUMAP)",
+			"screen_name": "humap_official",
+			"location": "",
+			"description": "휴맵컨텐츠(HUMAP CONTENTS) Official Twitter",
+			"url": "https://t.co/fBZZK3SmX6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fBZZK3SmX6",
+							"expanded_url": "http://www.humapcontents.com/",
+							"display_url": "humapcontents.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 322,
+			"friends_count": 1,
+			"listed_count": 2,
+			"created_at": "Tue Jan 21 06:28:27 +0000 2020",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 374,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1219506842935513088/1579755898",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[NEWS] #권혁수 ‘#SNL코리아’ 합류…“즐거움을 드리기 위해 회의 거듭하고 있다” [공식]\n\n👉 https://t.co/K1sC2LXiKO\n\n#휴맵컨텐츠 #HUMAPCONTENTS #KwunHyukSoo #SNLKOREA #쿠팡플레이"
+	},
+	{
 		"created_at": "Wed Jul 07 10:14:20 +0000 2021",
 		"id": 1412716607202369500,
 		"id_str": "1412716607202369537",
