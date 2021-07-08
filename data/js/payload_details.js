6ea638615038c946edc716c19db0d83970d0a1d1
솔플파티 <solplparty@gmail.com>
2021-07-08 22:17:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 50668,
-	"created_at": "Thu Jul 08 12:17:35 +0000 2021",
+	"tweets": 50669,
+	"created_at": "Thu Jul 08 13:17:35 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,156 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Thu Jul 08 12:29:24 +0000 2021",
+		"id": 1413098302678077400,
+		"id_str": "1413112986328309763",
+		"full_text": "[POST] [#인순이]\n\n코로나 블루, '인순이'로 극복~! \n\n포스트가 공개되었습니다! \n#휴맵컨텐츠 네이버 포스트에서 확인해주세요🥰\n\n👉https://t.co/X6VLhm8jaJ\n\n#HUMAPCONTENTS #INSOONI #베란다음악회 #성동문화재단",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			143
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "인순이",
+					"indices": [
+						8,
+						12
+					]
+				},
+				{
+					"text": "휴맵컨텐츠",
+					"indices": [
+						52,
+						58
+					]
+				},
+				{
+					"text": "HUMAPCONTENTS",
+					"indices": [
+						104,
+						118
+					]
+				},
+				{
+					"text": "INSOONI",
+					"indices": [
+						119,
+						127
+					]
+				},
+				{
+					"text": "베란다음악회",
+					"indices": [
+						128,
+						135
+					]
+				},
+				{
+					"text": "성동문화재단",
+					"indices": [
+						136,
+						143
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/X6VLhm8jaJ",
+					"expanded_url": "http://naver.me/GvktLQs4",
+					"display_url": "naver.me/GvktLQs4",
+					"indices": [
+						79,
+						102
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1219506842935513000,
+			"id_str": "1219506842935513088",
+			"name": "휴맵컨텐츠(HUMAP)",
+			"screen_name": "humap_official",
+			"location": "",
+			"description": "휴맵컨텐츠(HUMAP CONTENTS) Official Twitter",
+			"url": "https://t.co/fBZZK3SmX6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fBZZK3SmX6",
+							"expanded_url": "http://www.humapcontents.com/",
+							"display_url": "humapcontents.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 321,
+			"friends_count": 1,
+			"listed_count": 2,
+			"created_at": "Tue Jan 21 06:28:27 +0000 2020",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 377,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1219506842935513088/1579755898",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[POST] [#인순이]\n\n코로나 블루, '인순이'로 극복~! \n\n포스트가 공개되었습니다! \n#휴맵컨텐츠 네이버 포스트에서 확인해주세요🥰\n\n👉https://t.co/X6VLhm8jaJ\n\n#HUMAPCONTENTS #INSOONI #베란다음악회 #성동문화재단"
+	},
+	{
 		"created_at": "Thu Jul 08 11:58:14 +0000 2021",
 		"id": 1413099701239357400,
 		"id_str": "1413105142560329728",
