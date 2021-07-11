06b8520b04b9d275b2acef687eacb18dac34f25f
솔플파티 <solplparty@gmail.com>
2021-07-11 15:17:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 50748,
-	"created_at": "Sun Jul 11 04:17:35 +0000 2021",
+	"tweets": 50749,
+	"created_at": "Sun Jul 11 06:17:35 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,241 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Sun Jul 11 05:29:24 +0000 2021",
+		"id": 1414084109819732000,
+		"id_str": "1414094453514588162",
+		"full_text": "[유라]\n유라(youra) New Single [Pacemaker]\n1. 유라(youra) - 놀이공원 (Roller Coaster)\n2. 유라(youra) - 서울 사이보그 (Seoul Cyborg)\n\n2021.07.12 6PM KST\n\n#유라 #music #음악 #노래 https://t.co/5Mrin18OLV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			152
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "유라",
+					"indices": [
+						134,
+						137
+					]
+				},
+				{
+					"text": "music",
+					"indices": [
+						138,
+						144
+					]
+				},
+				{
+					"text": "음악",
+					"indices": [
+						145,
+						148
+					]
+				},
+				{
+					"text": "노래",
+					"indices": [
+						149,
+						152
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1414084033483460600,
+					"id_str": "1414084033483460609",
+					"indices": [
+						153,
+						176
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1414084033483460609/pu/img/6BWTk6ZgEC_04nQy.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1414084033483460609/pu/img/6BWTk6ZgEC_04nQy.jpg",
+					"url": "https://t.co/5Mrin18OLV",
+					"display_url": "pic.twitter.com/5Mrin18OLV",
+					"expanded_url": "https://twitter.com/munhwainkr/status/1414084109819731971/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 601,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 954,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 954,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1414084033483460600,
+					"id_str": "1414084033483460609",
+					"indices": [
+						153,
+						176
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1414084033483460609/pu/img/6BWTk6ZgEC_04nQy.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1414084033483460609/pu/img/6BWTk6ZgEC_04nQy.jpg",
+					"url": "https://t.co/5Mrin18OLV",
+					"display_url": "pic.twitter.com/5Mrin18OLV",
+					"expanded_url": "https://twitter.com/munhwainkr/status/1414084109819731971/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 601,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 954,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 954,
+							"h": 1080,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							53,
+							60
+						],
+						"duration_millis": 32183,
+						"variants": [
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1414084033483460609/pu/vid/320x362/Q8P26b-pyVJkcy_4.mp4?tag=12"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1414084033483460609/pu/vid/720x814/Fp4ucelawEsWWl4n.mp4?tag=12"
+							},
+							{
+								"bitrate": 950000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1414084033483460609/pu/vid/480x542/1HDoFvQtlIpDPUmM.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1414084033483460609/pu/pl/IpBQ2ns6OxvPue-u.m3u8?tag=12&container=fmp4"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 743014855653752800,
+			"id_str": "743014855653752833",
+			"name": "문화인",
+			"screen_name": "munhwainkr",
+			"location": "",
+			"description": "🎧뮤직레이블 문화인의 공식트위터입니다. 윤현상/유라/로지/김우주/나히/에버루아/아일",
+			"url": "https://t.co/EWOG41vu9R",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/EWOG41vu9R",
+							"expanded_url": "http://www.munhwain.kr",
+							"display_url": "munhwain.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 790,
+			"friends_count": 8,
+			"listed_count": 17,
+			"created_at": "Wed Jun 15 09:38:40 +0000 2016",
+			"favourites_count": 59,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 925,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/743017072494448640/pk13sRIF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/743017072494448640/pk13sRIF_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/743014855653752833/1513566567",
+			"profile_link_color": "199B2B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[유라]\n유라(youra) New Single [Pacemaker]\n1. 유라(youra) - 놀이공원 (Roller Coaster)\n2. 유라(youra) - 서울 사이보그 (Seoul Cyborg)\n\n2021.07.12 6PM KST\n\n#유라 #music #음악 #노래 https://t.co/5Mrin18OLV"
+	},
+	{
 		"created_at": "Sun Jul 11 03:29:32 +0000 2021",
 		"id": 1414057915661516800,
 		"id_str": "1414064284481597442",
