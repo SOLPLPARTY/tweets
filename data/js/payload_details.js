01fc99a0fa7159fe2552ce2729df2a9cbd8f17a5
솔플파티 <solplparty@gmail.com>
2021-07-12 23:17:37 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 50800,
-	"created_at": "Mon Jul 12 13:17:35 +0000 2021",
+	"tweets": 50801,
+	"created_at": "Mon Jul 12 14:17:36 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,177 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Mon Jul 12 13:29:25 +0000 2021",
+		"id": 1414566487419879400,
+		"id_str": "1414577638212087810",
+		"full_text": "[VIDEO] [#권혁수] #싱스테이3\n\n[EP.2] 본업을 되찾은 MC재환과 고음폭발 길구봉구🎼 최고의 개배우에서 가수까지 노리는 권혁수🔥의 티키타카 [싱스테이3]\n\n👉 https://t.co/PE2sh4cdtn\n\n#KwunHyukSoo #휴맵컨텐츠 #HUMAPCONTENTS #Lifetime #seezn #길구봉구 #김재환",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			183
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "권혁수",
+					"indices": [
+						9,
+						13
+					]
+				},
+				{
+					"text": "싱스테이3",
+					"indices": [
+						15,
+						21
+					]
+				},
+				{
+					"text": "KwunHyukSoo",
+					"indices": [
+						121,
+						133
+					]
+				},
+				{
+					"text": "휴맵컨텐츠",
+					"indices": [
+						134,
+						140
+					]
+				},
+				{
+					"text": "HUMAPCONTENTS",
+					"indices": [
+						141,
+						155
+					]
+				},
+				{
+					"text": "Lifetime",
+					"indices": [
+						156,
+						165
+					]
+				},
+				{
+					"text": "seezn",
+					"indices": [
+						166,
+						172
+					]
+				},
+				{
+					"text": "길구봉구",
+					"indices": [
+						173,
+						178
+					]
+				},
+				{
+					"text": "김재환",
+					"indices": [
+						179,
+						183
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/PE2sh4cdtn",
+					"expanded_url": "https://youtu.be/yWxl2deEsY4",
+					"display_url": "youtu.be/yWxl2deEsY4",
+					"indices": [
+						96,
+						119
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1219506842935513000,
+			"id_str": "1219506842935513088",
+			"name": "휴맵컨텐츠(HUMAP)",
+			"screen_name": "humap_official",
+			"location": "",
+			"description": "휴맵컨텐츠(HUMAP CONTENTS) Official Twitter",
+			"url": "https://t.co/fBZZK3SmX6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fBZZK3SmX6",
+							"expanded_url": "http://www.humapcontents.com/",
+							"display_url": "humapcontents.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 320,
+			"friends_count": 1,
+			"listed_count": 2,
+			"created_at": "Tue Jan 21 06:28:27 +0000 2020",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 380,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1219506842935513088/1579755898",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[VIDEO] [#권혁수] #싱스테이3\n\n[EP.2] 본업을 되찾은 MC재환과 고음폭발 길구봉구🎼 최고의 개배우에서 가수까지 노리는 권혁수🔥의 티키타카 [싱스테이3]\n\n👉 https://t.co/PE2sh4cdtn\n\n#KwunHyukSoo #휴맵컨텐츠 #HUMAPCONTENTS #Lifetime #seezn #길구봉구 #김재환"
+	},
+	{
 		"created_at": "Mon Jul 12 12:29:24 +0000 2021",
 		"id": 1414548682586812400,
 		"id_str": "1414562537916469249",
