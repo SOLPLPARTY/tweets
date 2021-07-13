21bf33ca368ae82b373e185aa3d4fab763ca94ee
솔플파티 <solplparty@gmail.com>
2021-07-13 10:17:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 50801,
-	"created_at": "Mon Jul 12 14:17:36 +0000 2021",
+	"tweets": 50802,
+	"created_at": "Tue Jul 13 01:17:37 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,156 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Tue Jul 13 00:29:25 +0000 2021",
+		"id": 1414738899541258200,
+		"id_str": "1414743732700782595",
+		"full_text": "[#정인] 장마 (Feat.영준 of Brown Eyed Soul) Live Clip Preview🎤\n\n 🔗https://t.co/VJx7Ot83gm\n\n2021.07.13 6PM(KST)\n\n#JUNGIN #장마 #브라운아이드소울 #영준 #방구석캐스팅",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			139
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "정인",
+					"indices": [
+						1,
+						4
+					]
+				},
+				{
+					"text": "JUNGIN",
+					"indices": [
+						106,
+						113
+					]
+				},
+				{
+					"text": "장마",
+					"indices": [
+						114,
+						117
+					]
+				},
+				{
+					"text": "브라운아이드소울",
+					"indices": [
+						118,
+						127
+					]
+				},
+				{
+					"text": "영준",
+					"indices": [
+						128,
+						131
+					]
+				},
+				{
+					"text": "방구석캐스팅",
+					"indices": [
+						132,
+						139
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/VJx7Ot83gm",
+					"expanded_url": "http://youtu.be/W3TrDaFrzXM",
+					"display_url": "youtu.be/W3TrDaFrzXM",
+					"indices": [
+						60,
+						83
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/BKUIHw4jho",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BKUIHw4jho",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21253,
+			"friends_count": 17,
+			"listed_count": 235,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6091,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1553488269",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "[#정인] 장마 (Feat.영준 of Brown Eyed Soul) Live Clip Preview🎤\n\n 🔗https://t.co/VJx7Ot83gm\n\n2021.07.13 6PM(KST)\n\n#JUNGIN #장마 #브라운아이드소울 #영준 #방구석캐스팅"
+	},
+	{
 		"created_at": "Mon Jul 12 13:29:25 +0000 2021",
 		"id": 1414566487419879400,
 		"id_str": "1414577638212087810",
