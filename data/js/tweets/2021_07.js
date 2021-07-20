a13307cbc21c4cc92d18ceaed690d01bbd59a48b
솔플파티 <solplparty@gmail.com>
2021-07-20 21:17:40 +0900

    Added 1 tweets

--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,163 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Tue Jul 20 11:29:25 +0000 2021",
+		"id": 1417443684514091000,
+		"id_str": "1417446541669847040",
+		"full_text": "['KNOCK' Spoiler_Talk] #권진아\n\n권진아 Summer Single 'KNOCK (With 박문치)'\nKwon Jin Ah Summer Single 'KNOCK (With PARKMOONCHI)'\n\n💖 'KNOCK' Spoiler_Talk\nhttps://t.co/IPrlZEmpDs⠀\n\n2021. 07. 27. 6PM Release\n\n#권진아 #KwonJinAh #KNOCK\n#박문치 #PARKMOONCHI\n#20210727_6PM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			250
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "권진아",
+					"indices": [
+						23,
+						27
+					]
+				},
+				{
+					"text": "권진아",
+					"indices": [
+						196,
+						200
+					]
+				},
+				{
+					"text": "KwonJinAh",
+					"indices": [
+						201,
+						211
+					]
+				},
+				{
+					"text": "KNOCK",
+					"indices": [
+						212,
+						218
+					]
+				},
+				{
+					"text": "박문치",
+					"indices": [
+						219,
+						223
+					]
+				},
+				{
+					"text": "PARKMOONCHI",
+					"indices": [
+						224,
+						236
+					]
+				},
+				{
+					"text": "20210727_6PM",
+					"indices": [
+						237,
+						250
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/IPrlZEmpDs",
+					"expanded_url": "https://youtu.be/qtcC3ZXHuR8",
+					"display_url": "youtu.be/qtcC3ZXHuR8",
+					"indices": [
+						143,
+						166
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 266004552,
+			"id_str": "266004552",
+			"name": "안테나 Antenna",
+			"screen_name": "antennamusic",
+			"location": "대한민국 서울",
+			"description": "정재형, 유희열(Toy), 유재석, 루시드폴, 페퍼톤스, 박새별, 권진아, Sam Kim, 정승환, 이진아, 윤석철, 적재, 서동환 Official / 정재형 Single 'Dance of Phrase (Feat. 김아일)' 2021. 07. 21. 6PM Release!",
+			"url": "https://t.co/XSWWwEx210",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XSWWwEx210",
+							"expanded_url": "http://www.antenna.co.kr",
+							"display_url": "antenna.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 36685,
+			"friends_count": 10,
+			"listed_count": 374,
+			"created_at": "Mon Mar 14 13:49:27 +0000 2011",
+			"favourites_count": 13,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3728,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266004552/1623754978",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 25,
+		"favorite_count": 39,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "['KNOCK' Spoiler_Talk] #권진아\n\n권진아 Summer Single 'KNOCK (With 박문치)'\nKwon Jin Ah Summer Single 'KNOCK (With PARKMOONCHI)'\n\n💖 'KNOCK' Spoiler_Talk\nhttps://t.co/IPrlZEmpDs⠀\n\n2021. 07. 27. 6PM Release\n\n#권진아 #KwonJinAh #KNOCK\n#박문치 #PARKMOONCHI\n#20210727_6PM"
+	},
+	{
 		"created_at": "Tue Jul 20 10:49:30 +0000 2021",
 		"id": 1417436498413211600,
 		"id_str": "1417436498413211657",
