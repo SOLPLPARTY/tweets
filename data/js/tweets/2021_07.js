60b9b681de4c1b990e70626990aec3731b9cb2a3
솔플파티 <solplparty@gmail.com>
2021-07-23 22:17:42 +0900

    Added 1 tweets

--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,248 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Fri Jul 23 12:29:24 +0000 2021",
+		"id": 1418544909028446200,
+		"id_str": "1418548804425551878",
+		"full_text": "[Video] #안테나엔젤스\n\n냉동 창고에서 펼쳐진 안테나 엔젤스와 펭수의 ‘펭긴 어게인’, 빛나는 여름 플레이리스트들로 준비했어요. 지금 ‘자이언트 펭TV’ 유튜브 채널에서 만나보세요 🐧💙\n\n 🐧 https://t.co/Nlx5IfV3V7\n\n#이진아 #정승환 #권진아 #샘김\n#EBS #자이언트펭TV #펭수 https://t.co/64pY6Oezjs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			172
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "안테나엔젤스",
+					"indices": [
+						8,
+						15
+					]
+				},
+				{
+					"text": "이진아",
+					"indices": [
+						136,
+						140
+					]
+				},
+				{
+					"text": "정승환",
+					"indices": [
+						141,
+						145
+					]
+				},
+				{
+					"text": "권진아",
+					"indices": [
+						146,
+						150
+					]
+				},
+				{
+					"text": "샘김",
+					"indices": [
+						151,
+						154
+					]
+				},
+				{
+					"text": "EBS",
+					"indices": [
+						155,
+						159
+					]
+				},
+				{
+					"text": "자이언트펭TV",
+					"indices": [
+						160,
+						168
+					]
+				},
+				{
+					"text": "펭수",
+					"indices": [
+						169,
+						172
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Nlx5IfV3V7",
+					"expanded_url": "http://youtu.be/VJloo9poYGo",
+					"display_url": "youtu.be/VJloo9poYGo",
+					"indices": [
+						111,
+						134
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1418544901390704600,
+					"id_str": "1418544901390704642",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/media/E6-vEnFVoAI7Qmm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E6-vEnFVoAI7Qmm.jpg",
+					"url": "https://t.co/64pY6Oezjs",
+					"display_url": "pic.twitter.com/64pY6Oezjs",
+					"expanded_url": "https://twitter.com/antennamusic/status/1418544909028446211/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1418544901390704600,
+					"id_str": "1418544901390704642",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/media/E6-vEnFVoAI7Qmm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E6-vEnFVoAI7Qmm.jpg",
+					"url": "https://t.co/64pY6Oezjs",
+					"display_url": "pic.twitter.com/64pY6Oezjs",
+					"expanded_url": "https://twitter.com/antennamusic/status/1418544909028446211/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 266004552,
+			"id_str": "266004552",
+			"name": "안테나 Antenna",
+			"screen_name": "antennamusic",
+			"location": "대한민국 서울",
+			"description": "정재형, 유희열(Toy), 유재석, 루시드폴, 페퍼톤스, 박새별, 권진아, Sam Kim, 정승환, 이진아, 윤석철, 적재, 서동환 Official / 정재형 Single 'Dance of Phrase (Feat. 김아일)' 2021. 07. 21. 6PM Release!",
+			"url": "https://t.co/XSWWwEx210",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XSWWwEx210",
+							"expanded_url": "http://www.antenna.co.kr",
+							"display_url": "antenna.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 36737,
+			"friends_count": 10,
+			"listed_count": 372,
+			"created_at": "Mon Mar 14 13:49:27 +0000 2011",
+			"favourites_count": 13,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3741,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266004552/1626862648",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 62,
+		"favorite_count": 88,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[Video] #안테나엔젤스\n\n냉동 창고에서 펼쳐진 안테나 엔젤스와 펭수의 ‘펭긴 어게인’, 빛나는 여름 플레이리스트들로 준비했어요. 지금 ‘자이언트 펭TV’ 유튜브 채널에서 만나보세요 🐧💙\n\n 🐧 https://t.co/Nlx5IfV3V7\n\n#이진아 #정승환 #권진아 #샘김\n#EBS #자이언트펭TV #펭수 https://t.co/64pY6Oezjs"
+	},
+	{
 		"created_at": "Fri Jul 23 11:29:30 +0000 2021",
 		"id": 1418526308364169200,
 		"id_str": "1418533726418575362",
