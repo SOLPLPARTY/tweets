67653bd70e93f516189293b1f0d807ba6dc0a762
솔플파티 <solplparty@gmail.com>
2021-08-02 17:17:33 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 51431,
-	"created_at": "Mon Aug 02 05:17:34 +0000 2021",
+	"tweets": 51432,
+	"created_at": "Mon Aug 02 08:17:32 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,156 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Mon Aug 02 07:29:33 +0000 2021",
+		"id": 1422095283123409000,
+		"id_str": "1422097219478491140",
+		"full_text": "[NEWS] 강원문화재단, 강원국제트리엔날레 2021 홍보대사 '가수 #인순이 위촉'\n\n👉 https://t.co/XJ55QHrfVq\n\n#휴맵컨텐츠 #HUMAPCONTENTS #INSOONI #강원국제트리엔날레 #홍보대사",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			123
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "인순이",
+					"indices": [
+						39,
+						43
+					]
+				},
+				{
+					"text": "휴맵컨텐츠",
+					"indices": [
+						76,
+						82
+					]
+				},
+				{
+					"text": "HUMAPCONTENTS",
+					"indices": [
+						83,
+						97
+					]
+				},
+				{
+					"text": "INSOONI",
+					"indices": [
+						98,
+						106
+					]
+				},
+				{
+					"text": "강원국제트리엔날레",
+					"indices": [
+						107,
+						117
+					]
+				},
+				{
+					"text": "홍보대사",
+					"indices": [
+						118,
+						123
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/XJ55QHrfVq",
+					"expanded_url": "http://www.weeklytoday.com/news/articleView.html?idxno=391305",
+					"display_url": "weeklytoday.com/news/articleVi…",
+					"indices": [
+						51,
+						74
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1219506842935513000,
+			"id_str": "1219506842935513088",
+			"name": "휴맵컨텐츠(HUMAP)",
+			"screen_name": "humap_official",
+			"location": "",
+			"description": "휴맵컨텐츠(HUMAP CONTENTS) Official Twitter",
+			"url": "https://t.co/fBZZK3SmX6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fBZZK3SmX6",
+							"expanded_url": "http://www.humapcontents.com/",
+							"display_url": "humapcontents.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 356,
+			"friends_count": 1,
+			"listed_count": 2,
+			"created_at": "Tue Jan 21 06:28:27 +0000 2020",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 277,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1219506842935513088/1579755898",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[NEWS] 강원문화재단, 강원국제트리엔날레 2021 홍보대사 '가수 #인순이 위촉'\n\n👉 https://t.co/XJ55QHrfVq\n\n#휴맵컨텐츠 #HUMAPCONTENTS #INSOONI #강원국제트리엔날레 #홍보대사"
+	},
+	{
 		"created_at": "Mon Aug 02 05:11:17 +0000 2021",
 		"id": 1422047694323810300,
 		"id_str": "1422062424639868931",
