97d63a5a9a46aa582776bcaafee377c1abe534c7
솔플파티 <solplparty@gmail.com>
2021-08-06 23:17:35 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 51568,
-	"created_at": "Fri Aug 06 13:17:34 +0000 2021",
+	"tweets": 51569,
+	"created_at": "Fri Aug 06 14:17:34 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,263 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Fri Aug 06 13:29:25 +0000 2021",
+		"id": 1423622382342836200,
+		"id_str": "1423637335263043586",
+		"full_text": "[#김성은] 언빌리버블 미모의 김성은 광고 촬영 현장 비하인드📸\n\n▶https://t.co/alaqHZjMur\n\n# 광고 #촬영 #비하인드 #네이버포스트 #미스틱스토리 https://t.co/VA7piQmiA9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			93
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "김성은",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "촬영",
+					"indices": [
+						68,
+						71
+					]
+				},
+				{
+					"text": "비하인드",
+					"indices": [
+						72,
+						77
+					]
+				},
+				{
+					"text": "네이버포스트",
+					"indices": [
+						78,
+						85
+					]
+				},
+				{
+					"text": "미스틱스토리",
+					"indices": [
+						86,
+						93
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/alaqHZjMur",
+					"expanded_url": "http://naver.me/xPIzInFQ",
+					"display_url": "naver.me/xPIzInFQ",
+					"indices": [
+						38,
+						61
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1423568652687712300,
+					"id_str": "1423568652687712261",
+					"indices": [
+						94,
+						117
+					],
+					"media_url": "http://pbs.twimg.com/media/E8GIJbDVIAU5BqU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8GIJbDVIAU5BqU.jpg",
+					"url": "https://t.co/VA7piQmiA9",
+					"display_url": "pic.twitter.com/VA7piQmiA9",
+					"expanded_url": "https://twitter.com/themysticstory/status/1423622382342836229/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1423568652687712300,
+					"id_str": "1423568652687712261",
+					"indices": [
+						94,
+						117
+					],
+					"media_url": "http://pbs.twimg.com/media/E8GIJbDVIAU5BqU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8GIJbDVIAU5BqU.jpg",
+					"url": "https://t.co/VA7piQmiA9",
+					"display_url": "pic.twitter.com/VA7piQmiA9",
+					"expanded_url": "https://twitter.com/themysticstory/status/1423622382342836229/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1423568670324772900,
+					"id_str": "1423568670324772866",
+					"indices": [
+						94,
+						117
+					],
+					"media_url": "http://pbs.twimg.com/media/E8GIKcwVUAIwXM6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8GIKcwVUAIwXM6.jpg",
+					"url": "https://t.co/VA7piQmiA9",
+					"display_url": "pic.twitter.com/VA7piQmiA9",
+					"expanded_url": "https://twitter.com/themysticstory/status/1423622382342836229/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/BKUIHw4jho",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BKUIHw4jho",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21319,
+			"friends_count": 14,
+			"listed_count": 237,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6151,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1553488269",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#김성은] 언빌리버블 미모의 김성은 광고 촬영 현장 비하인드📸\n\n▶https://t.co/alaqHZjMur\n\n# 광고 #촬영 #비하인드 #네이버포스트 #미스틱스토리 https://t.co/VA7piQmiA9"
+	},
+	{
 		"created_at": "Fri Aug 06 12:29:25 +0000 2021",
 		"id": 1423615014343037000,
 		"id_str": "1423622234657206273",
