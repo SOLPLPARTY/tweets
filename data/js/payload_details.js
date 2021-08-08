81d8d7d24b5d1a528eb743378fe15084fc455a84
솔플파티 <solplparty@gmail.com>
2021-08-08 23:17:34 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 51607,
-	"created_at": "Sun Aug 08 12:17:36 +0000 2021",
+	"tweets": 51608,
+	"created_at": "Sun Aug 08 14:17:33 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Sun Aug 08 14:11:16 +0000 2021",
+		"id": 1424367308768641000,
+		"id_str": "1424372645265645570",
+		"full_text": "이번주 토요일에 있을 예정이었던 “차세대 100분쇼” 를  코로나  상황이 조금은 나아진 환경에서 공연하는게  낫다라는 판단아래  전격 연기를 결정했습니다.  \n\n많이 기대해주셨던 공연팬 분들께  진심으로 사과의 말씀을 올리며   조금만 참고 기다려주시면  다시 공연 재개할날이 올거라 믿습니다. https://t.co/7geYPAXmUa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			165
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1424367298748448800,
+					"id_str": "1424367298748448772",
+					"indices": [
+						166,
+						189
+					],
+					"media_url": "http://pbs.twimg.com/media/E8RegveVgAQ4CPM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8RegveVgAQ4CPM.jpg",
+					"url": "https://t.co/7geYPAXmUa",
+					"display_url": "pic.twitter.com/7geYPAXmUa",
+					"expanded_url": "https://twitter.com/clubFF_/status/1424367308768641026/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1424367298748448800,
+					"id_str": "1424367298748448772",
+					"indices": [
+						166,
+						189
+					],
+					"media_url": "http://pbs.twimg.com/media/E8RegveVgAQ4CPM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8RegveVgAQ4CPM.jpg",
+					"url": "https://t.co/7geYPAXmUa",
+					"display_url": "pic.twitter.com/7geYPAXmUa",
+					"expanded_url": "https://twitter.com/clubFF_/status/1424367308768641026/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 455224965,
+			"id_str": "455224965",
+			"name": "클럽에프에프",
+			"screen_name": "clubFF_",
+			"location": "서울 마포구 서교동 407-8 지하1층",
+			"description": "홍대 최고의 NO.1 Indie experience 라이브 & 댄스클럽 에프에프입니다!! 엪엪의 대관 및 오디션 문의(프로필,음원,연락처 필수)는  010-9025-3407 과 ianbrown@naver.com 으로 .",
+			"url": "https://t.co/dmU2vJP8pS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dmU2vJP8pS",
+							"expanded_url": "http://clubff.modoo.at",
+							"display_url": "clubff.modoo.at",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6161,
+			"friends_count": 8,
+			"listed_count": 74,
+			"created_at": "Wed Jan 04 21:41:48 +0000 2012",
+			"favourites_count": 560,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3787,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2263880419/image_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2263880419/image_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "이번주 토요일에 있을 예정이었던 “차세대 100분쇼” 를  코로나  상황이 조금은 나아진 환경에서 공연하는게  낫다라는 판단아래  전격 연기를 결정했습니다.  \n\n많이 기대해주셨던 공연팬 분들께  진심으로 사과의 말씀을 올리며   조금만 참고 기다려주시면  다시 공연 재개할날이 올거라 믿습니다. https://t.co/7geYPAXmUa"
+	},
+	{
 		"created_at": "Sun Aug 08 11:29:24 +0000 2021",
 		"id": 1424329854510538800,
 		"id_str": "1424331910617210883",
