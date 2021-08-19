c6bf1aa8c5253e9e48638d6dfbf4699dfb106bde
솔플파티 <solplparty@gmail.com>
2021-08-19 22:17:39 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 51948,
-	"created_at": "Thu Aug 19 12:20:28 +0000 2021",
+	"tweets": 51949,
+	"created_at": "Thu Aug 19 13:17:38 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,356 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Thu Aug 19 13:11:17 +0000 2021",
+		"id": 1428340981519298600,
+		"id_str": "1428343814264066048",
+		"full_text": "📢 8/20 공연 현장 안내\n\n&lt;나상현씨밴드 단독 콘서트 : SPACE TOUR&gt;\n· 티켓수령 : 7시 (현장구매 가능)\n· 입장 : 7시 30분\n· 공연시작 : 8시\n· 출연 : 나상현씨밴드\n\n#나상현씨밴드 #단독콘서트\n#롤링홀 #ROLLINGHALL\n#홍대 #공연 #인디 #멜론티켓 https://t.co/9CVf4bGZLV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			165
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "나상현씨밴드",
+					"indices": [
+						115,
+						122
+					]
+				},
+				{
+					"text": "단독콘서트",
+					"indices": [
+						123,
+						129
+					]
+				},
+				{
+					"text": "롤링홀",
+					"indices": [
+						130,
+						134
+					]
+				},
+				{
+					"text": "ROLLINGHALL",
+					"indices": [
+						135,
+						147
+					]
+				},
+				{
+					"text": "홍대",
+					"indices": [
+						148,
+						151
+					]
+				},
+				{
+					"text": "공연",
+					"indices": [
+						152,
+						155
+					]
+				},
+				{
+					"text": "인디",
+					"indices": [
+						156,
+						159
+					]
+				},
+				{
+					"text": "멜론티켓",
+					"indices": [
+						160,
+						165
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1428326353305145300,
+					"id_str": "1428326353305145348",
+					"indices": [
+						166,
+						189
+					],
+					"media_url": "http://pbs.twimg.com/media/E9JvQDAVEAQAQnO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9JvQDAVEAQAQnO.jpg",
+					"url": "https://t.co/9CVf4bGZLV",
+					"display_url": "pic.twitter.com/9CVf4bGZLV",
+					"expanded_url": "https://twitter.com/rollinghall1995/status/1428340981519298572/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 849,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 481,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 849,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1428326353305145300,
+					"id_str": "1428326353305145348",
+					"indices": [
+						166,
+						189
+					],
+					"media_url": "http://pbs.twimg.com/media/E9JvQDAVEAQAQnO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9JvQDAVEAQAQnO.jpg",
+					"url": "https://t.co/9CVf4bGZLV",
+					"display_url": "pic.twitter.com/9CVf4bGZLV",
+					"expanded_url": "https://twitter.com/rollinghall1995/status/1428340981519298572/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 849,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 481,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 849,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1428326284229185500,
+					"id_str": "1428326284229185543",
+					"indices": [
+						166,
+						189
+					],
+					"media_url": "http://pbs.twimg.com/media/E9JvMBrVkActo4w.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9JvMBrVkActo4w.jpg",
+					"url": "https://t.co/9CVf4bGZLV",
+					"display_url": "pic.twitter.com/9CVf4bGZLV",
+					"expanded_url": "https://twitter.com/rollinghall1995/status/1428340981519298572/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1753,
+							"h": 1240,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 481,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 849,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1428326323361968000,
+					"id_str": "1428326323361968138",
+					"indices": [
+						166,
+						189
+					],
+					"media_url": "http://pbs.twimg.com/media/E9JvOTdUcAoavxI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9JvOTdUcAoavxI.jpg",
+					"url": "https://t.co/9CVf4bGZLV",
+					"display_url": "pic.twitter.com/9CVf4bGZLV",
+					"expanded_url": "https://twitter.com/rollinghall1995/status/1428340981519298572/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 481,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 849,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1753,
+							"h": 1240,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1428326323714281500,
+					"id_str": "1428326323714281474",
+					"indices": [
+						166,
+						189
+					],
+					"media_url": "http://pbs.twimg.com/media/E9JvOUxUUAIe-E4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9JvOUxUUAIe-E4.jpg",
+					"url": "https://t.co/9CVf4bGZLV",
+					"display_url": "pic.twitter.com/9CVf4bGZLV",
+					"expanded_url": "https://twitter.com/rollinghall1995/status/1428340981519298572/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 481,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1753,
+							"h": 1240,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 849,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1291327050578456600,
+			"id_str": "1291327050578456578",
+			"name": "롤링홀",
+			"screen_name": "rollinghall1995",
+			"location": "서울시 마포구 어울마당로 35 신보빌딩 지하1층",
+			"description": "홍대/합정에 위치한 음악 공연장 #롤링홀 입니다٩( ᐛ )و\nConcert live hall in South Korea\n-\n👇🏻👇🏻멜론티켓 롤링홀 기획공연 바로가기.\nhttps://t.co/K14VaPTNnq",
+			"url": "https://t.co/C9YRGggByu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/C9YRGggByu",
+							"expanded_url": "http://www.rollinghall.co.kr/default/",
+							"display_url": "rollinghall.co.kr/default/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/K14VaPTNnq",
+							"expanded_url": "http://bit.ly/3C4QrHt",
+							"display_url": "bit.ly/3C4QrHt",
+							"indices": [
+								93,
+								116
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 198,
+			"friends_count": 3,
+			"listed_count": 0,
+			"created_at": "Thu Aug 06 10:56:17 +0000 2020",
+			"favourites_count": 102,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 256,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1291327708689862662/9hOLVWB2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1291327708689862662/9hOLVWB2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1291327050578456578/1596712102",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "📢 8/20 공연 현장 안내\n\n&lt;나상현씨밴드 단독 콘서트 : SPACE TOUR&gt;\n· 티켓수령 : 7시 (현장구매 가능)\n· 입장 : 7시 30분\n· 공연시작 : 8시\n· 출연 : 나상현씨밴드\n\n#나상현씨밴드 #단독콘서트\n#롤링홀 #ROLLINGHALL\n#홍대 #공연 #인디 #멜론티켓 https://t.co/9CVf4bGZLV"
+	},
+	{
 		"created_at": "Thu Aug 19 11:29:32 +0000 2021",
 		"id": 1428309235146715100,
 		"id_str": "1428318207446818819",
