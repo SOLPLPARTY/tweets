c3f4bfb3457f6279c2af7a17ab42e0f6c7bbf914
솔플파티 <solplparty@gmail.com>
2021-08-20 22:17:41 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 51984,
-	"created_at": "Fri Aug 20 11:17:38 +0000 2021",
+	"tweets": 51985,
+	"created_at": "Fri Aug 20 13:17:40 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,253 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Fri Aug 20 12:29:25 +0000 2021",
+		"id": 1428686807084847000,
+		"id_str": "1428695664943316997",
+		"full_text": "[Video] #이진아\n\n이진아 'Destination Moon' 커버곡 영상이 공개 되었습니다🧸❣️ 오늘같은 금요일 저녁에 잘 어울리는 이진아의 커버곡을 유튜브 '이진아JinA Lee' 채널에서 지금 바로 만나보세요🌙✨\n\n🔗 https://t.co/8PYt3CBAPb\n\n#LeeJinAh #DestinationMoon #커버곡 #Cover https://t.co/j10mxIaFKz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			190
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이진아",
+					"indices": [
+						8,
+						12
+					]
+				},
+				{
+					"text": "LeeJinAh",
+					"indices": [
+						152,
+						161
+					]
+				},
+				{
+					"text": "DestinationMoon",
+					"indices": [
+						162,
+						178
+					]
+				},
+				{
+					"text": "커버곡",
+					"indices": [
+						179,
+						183
+					]
+				},
+				{
+					"text": "Cover",
+					"indices": [
+						184,
+						190
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/8PYt3CBAPb",
+					"expanded_url": "https://youtu.be/_dAU2wfewCU",
+					"display_url": "youtu.be/_dAU2wfewCU",
+					"indices": [
+						127,
+						150
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1428686538796245000,
+					"id_str": "1428686538796244992",
+					"indices": [
+						191,
+						214
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1428686538796244992/pu/img/8pEDQXvqmKbFumQo.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1428686538796244992/pu/img/8pEDQXvqmKbFumQo.jpg",
+					"url": "https://t.co/j10mxIaFKz",
+					"display_url": "pic.twitter.com/j10mxIaFKz",
+					"expanded_url": "https://twitter.com/antennamusic/status/1428686807084847111/video/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 480,
+							"h": 600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 480,
+							"h": 600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 480,
+							"h": 600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1428686538796245000,
+					"id_str": "1428686538796244992",
+					"indices": [
+						191,
+						214
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1428686538796244992/pu/img/8pEDQXvqmKbFumQo.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1428686538796244992/pu/img/8pEDQXvqmKbFumQo.jpg",
+					"url": "https://t.co/j10mxIaFKz",
+					"display_url": "pic.twitter.com/j10mxIaFKz",
+					"expanded_url": "https://twitter.com/antennamusic/status/1428686807084847111/video/1",
+					"type": "video",
+					"sizes": {
+						"large": {
+							"w": 480,
+							"h": 600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 480,
+							"h": 600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 480,
+							"h": 600,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							4,
+							5
+						],
+						"duration_millis": 60000,
+						"variants": [
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1428686538796244992/pu/vid/320x400/5IabHFWABTvOF4AZ.mp4?tag=12"
+							},
+							{
+								"bitrate": 950000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1428686538796244992/pu/vid/480x600/DYhGytk7VgLYrkZ3.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1428686538796244992/pu/pl/yzy-FiNft5IWGUOI.m3u8?tag=12&container=fmp4"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 266004552,
+			"id_str": "266004552",
+			"name": "안테나 Antenna",
+			"screen_name": "antennamusic",
+			"location": "대한민국 서울",
+			"description": "정재형, 유희열(Toy), 유재석, 루시드폴, 페퍼톤스, 박새별, 권진아, Sam Kim, 정승환, 이진아, 윤석철, 적재, 서동환 Official / 권진아 Summer Single 'KNOCK (With 박문치)' 2021. 07. 27. 6PM Release!",
+			"url": "https://t.co/XSWWwEx210",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XSWWwEx210",
+							"expanded_url": "http://www.antenna.co.kr",
+							"display_url": "antenna.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 36911,
+			"friends_count": 10,
+			"listed_count": 372,
+			"created_at": "Mon Mar 14 13:49:27 +0000 2011",
+			"favourites_count": 13,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3860,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266004552/1626862648",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 18,
+		"favorite_count": 39,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[Video] #이진아\n\n이진아 'Destination Moon' 커버곡 영상이 공개 되었습니다🧸❣️ 오늘같은 금요일 저녁에 잘 어울리는 이진아의 커버곡을 유튜브 '이진아JinA Lee' 채널에서 지금 바로 만나보세요🌙✨\n\n🔗 https://t.co/8PYt3CBAPb\n\n#LeeJinAh #DestinationMoon #커버곡 #Cover https://t.co/j10mxIaFKz"
+	},
+	{
 		"created_at": "Fri Aug 20 10:29:40 +0000 2021",
 		"id": 1428662265012572200,
 		"id_str": "1428665530240716804",
