5651370f52cde76b334a0405a723fcf5f221ce67
솔플파티 <solplparty@gmail.com>
2021-08-25 22:17:44 +0900

    Added 1 tweets

--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Wed Aug 25 13:11:17 +0000 2021",
+		"id": 1430516983146164200,
+		"id_str": "1430518141239562244",
+		"full_text": "2021년 8월 26일 목요일~9월 5일 일요일\n스틸페이스 바 영업시간:\n목/금/토/일요일 오후 4시~9시\n\n26 AUG - 5 SEP 2021\nCLUB STEEL FACE BAR - Opening Hours:\nThursday, Friday, Saturday &amp; Sunday 4PM~9PM\n\n#clubsteelface #steelface #musiclovers #seoul https://t.co/OljUBuOpN9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			236
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "clubsteelface",
+					"indices": [
+						167,
+						181
+					]
+				},
+				{
+					"text": "steelface",
+					"indices": [
+						182,
+						192
+					]
+				},
+				{
+					"text": "musiclovers",
+					"indices": [
+						193,
+						205
+					]
+				},
+				{
+					"text": "seoul",
+					"indices": [
+						206,
+						212
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/OljUBuOpN9",
+					"expanded_url": "https://www.instagram.com/p/CS_5q2AFYno/?utm_medium=twitter",
+					"display_url": "instagram.com/p/CS_5q2AFYno/…",
+					"indices": [
+						213,
+						236
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://instagram.com\" rel=\"nofollow\">Instagram</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4144667354,
+			"id_str": "4144667354",
+			"name": "CLUB STEEL FACE",
+			"screen_name": "clubsteelface",
+			"location": "10, Jandari-ro, Mapo-gu, Seoul",
+			"description": "Since 2015, Live Music Venue & Bar in Seoul, Korea(south)",
+			"url": "https://t.co/qNZdtjMBbN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qNZdtjMBbN",
+							"expanded_url": "http://steelface.com",
+							"display_url": "steelface.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 221,
+			"friends_count": 1,
+			"listed_count": 7,
+			"created_at": "Fri Nov 06 09:37:46 +0000 2015",
+			"favourites_count": 482,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1235,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/662565134255063044/GlG4R5IZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/662565134255063044/GlG4R5IZ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4144667354/1453393563",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "2021년 8월 26일 목요일~9월 5일 일요일\n스틸페이스 바 영업시간:\n목/금/토/일요일 오후 4시~9시\n\n26 AUG - 5 SEP 2021\nCLUB STEEL FACE BAR - Opening Hours:\nThursday, Friday, Saturday &amp; Sunday 4PM~9PM\n\n#clubsteelface #steelface #musiclovers #seoul https://t.co/OljUBuOpN9"
+	},
+	{
 		"created_at": "Wed Aug 25 11:29:25 +0000 2021",
 		"id": 1430492358290776000,
 		"id_str": "1430492504680448000",
