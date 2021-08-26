d242621e7bb3dcf425d170c802dcfdf91ee83f0c
솔플파티 <solplparty@gmail.com>
2021-08-26 23:17:42 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 52165,
-	"created_at": "Thu Aug 26 13:17:41 +0000 2021",
+	"tweets": 52166,
+	"created_at": "Thu Aug 26 14:17:41 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,313 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Thu Aug 26 13:29:25 +0000 2021",
+		"id": 1430870139545731000,
+		"id_str": "1430885091895648262",
+		"full_text": "[#김성은] 눈부셔서 선글라스 필참😎 김성은 맥앤지나 현장 비하인드📸\n\n▶https://t.co/2qeaWWn8fi\n\n#화보 #맥앤지나 #촬영 #비하인드 #네이버포스트 #미스틱스토리 https://t.co/cKTdwpTlTJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			101
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "김성은",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "화보",
+					"indices": [
+						66,
+						69
+					]
+				},
+				{
+					"text": "맥앤지나",
+					"indices": [
+						70,
+						75
+					]
+				},
+				{
+					"text": "촬영",
+					"indices": [
+						76,
+						79
+					]
+				},
+				{
+					"text": "비하인드",
+					"indices": [
+						80,
+						85
+					]
+				},
+				{
+					"text": "네이버포스트",
+					"indices": [
+						86,
+						93
+					]
+				},
+				{
+					"text": "미스틱스토리",
+					"indices": [
+						94,
+						101
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2qeaWWn8fi",
+					"expanded_url": "http://naver.me/GHE4BDfA",
+					"display_url": "naver.me/GHE4BDfA",
+					"indices": [
+						41,
+						64
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1430847372607688700,
+					"id_str": "1430847372607688709",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/E9tkGrwVEAU1wuw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9tkGrwVEAU1wuw.jpg",
+					"url": "https://t.co/cKTdwpTlTJ",
+					"display_url": "pic.twitter.com/cKTdwpTlTJ",
+					"expanded_url": "https://twitter.com/themysticstory/status/1430870139545731082/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1365,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1430847372607688700,
+					"id_str": "1430847372607688709",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/E9tkGrwVEAU1wuw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9tkGrwVEAU1wuw.jpg",
+					"url": "https://t.co/cKTdwpTlTJ",
+					"display_url": "pic.twitter.com/cKTdwpTlTJ",
+					"expanded_url": "https://twitter.com/themysticstory/status/1430870139545731082/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1365,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1430847372624466000,
+					"id_str": "1430847372624465924",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/E9tkGr0VEAQHQdR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9tkGr0VEAQHQdR.jpg",
+					"url": "https://t.co/cKTdwpTlTJ",
+					"display_url": "pic.twitter.com/cKTdwpTlTJ",
+					"expanded_url": "https://twitter.com/themysticstory/status/1430870139545731082/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1365,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1430847372637053000,
+					"id_str": "1430847372637052931",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/E9tkGr3VIAMMYk8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9tkGr3VIAMMYk8.jpg",
+					"url": "https://t.co/cKTdwpTlTJ",
+					"display_url": "pic.twitter.com/cKTdwpTlTJ",
+					"expanded_url": "https://twitter.com/themysticstory/status/1430870139545731082/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1365,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/BKUIHw4jho",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BKUIHw4jho",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21457,
+			"friends_count": 13,
+			"listed_count": 235,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6185,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1553488269",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#김성은] 눈부셔서 선글라스 필참😎 김성은 맥앤지나 현장 비하인드📸\n\n▶https://t.co/2qeaWWn8fi\n\n#화보 #맥앤지나 #촬영 #비하인드 #네이버포스트 #미스틱스토리 https://t.co/cKTdwpTlTJ"
+	},
+	{
 		"created_at": "Thu Aug 26 12:29:30 +0000 2021",
 		"id": 1430859222590230500,
 		"id_str": "1430870013712470023",
