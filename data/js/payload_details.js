d550713880f38bc201249564dcd07b63424d6a4f
솔플파티 <solplparty@gmail.com>
2021-08-28 15:17:40 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 52210,
-	"created_at": "Sat Aug 28 04:17:41 +0000 2021",
+	"tweets": 52211,
+	"created_at": "Sat Aug 28 06:17:40 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,246 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Sat Aug 28 05:29:24 +0000 2021",
+		"id": 1431481669077274600,
+		"id_str": "1431489070937649155",
+		"full_text": "[#손태진] KBS2 '불후의 명곡' 출연✔\n\n'포르테 디 콰트로' 손태진의 멋진 무대를 볼 수 있는 오늘🎤\n저녁 6시 5분 KBS2 본방사수⭕\n\n#KBS2 #불후의명곡 #포르테디콰트로 https://t.co/bstomZKaPg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			103
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "손태진",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "KBS2",
+					"indices": [
+						82,
+						87
+					]
+				},
+				{
+					"text": "불후의명곡",
+					"indices": [
+						88,
+						94
+					]
+				},
+				{
+					"text": "포르테디콰트로",
+					"indices": [
+						95,
+						103
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1431456892841201700,
+					"id_str": "1431456892841201666",
+					"indices": [
+						104,
+						127
+					],
+					"media_url": "http://pbs.twimg.com/media/E92OdbkVkAINj24.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E92OdbkVkAINj24.jpg",
+					"url": "https://t.co/bstomZKaPg",
+					"display_url": "pic.twitter.com/bstomZKaPg",
+					"expanded_url": "https://twitter.com/themysticstory/status/1431481669077274625/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1431456892841201700,
+					"id_str": "1431456892841201666",
+					"indices": [
+						104,
+						127
+					],
+					"media_url": "http://pbs.twimg.com/media/E92OdbkVkAINj24.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E92OdbkVkAINj24.jpg",
+					"url": "https://t.co/bstomZKaPg",
+					"display_url": "pic.twitter.com/bstomZKaPg",
+					"expanded_url": "https://twitter.com/themysticstory/status/1431481669077274625/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1431456893109616600,
+					"id_str": "1431456893109616645",
+					"indices": [
+						104,
+						127
+					],
+					"media_url": "http://pbs.twimg.com/media/E92OdckVQAUWvxO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E92OdckVQAUWvxO.jpg",
+					"url": "https://t.co/bstomZKaPg",
+					"display_url": "pic.twitter.com/bstomZKaPg",
+					"expanded_url": "https://twitter.com/themysticstory/status/1431481669077274625/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/BKUIHw4jho",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BKUIHw4jho",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21477,
+			"friends_count": 13,
+			"listed_count": 235,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6189,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1553488269",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 32,
+		"favorite_count": 31,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#손태진] KBS2 '불후의 명곡' 출연✔\n\n'포르테 디 콰트로' 손태진의 멋진 무대를 볼 수 있는 오늘🎤\n저녁 6시 5분 KBS2 본방사수⭕\n\n#KBS2 #불후의명곡 #포르테디콰트로 https://t.co/bstomZKaPg"
+	},
+	{
 		"created_at": "Sat Aug 28 03:29:27 +0000 2021",
 		"id": 1431452248911990800,
 		"id_str": "1431458884502310914",
