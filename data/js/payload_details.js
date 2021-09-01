3016c3b1f5ba703ca8a86b6bf4d2176e1f9b4e64
솔플파티 <solplparty@gmail.com>
2021-09-01 23:17:33 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 52359,
-	"created_at": "Wed Sep 01 12:17:32 +0000 2021",
+	"tweets": 52360,
+	"created_at": "Wed Sep 01 14:17:32 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,170 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Wed Sep 01 13:29:34 +0000 2021",
+		"id": 1433058363516993500,
+		"id_str": "1433059456913453064",
+		"full_text": "[VIDEO] [#권혁수] #SNL코리아 \n\n쿠팡플레이 첫 오리지널 SNL코리아 공개일\n\n👉 https://t.co/tCqla0YxnN\n\n#KwunHyukSoo #휴맵컨텐츠 #HUMAPCONTENTS #SNLKOREA #쿠팡플레이 #CoupangPlay",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			141
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "권혁수",
+					"indices": [
+						9,
+						13
+					]
+				},
+				{
+					"text": "SNL코리아",
+					"indices": [
+						15,
+						22
+					]
+				},
+				{
+					"text": "KwunHyukSoo",
+					"indices": [
+						77,
+						89
+					]
+				},
+				{
+					"text": "휴맵컨텐츠",
+					"indices": [
+						90,
+						96
+					]
+				},
+				{
+					"text": "HUMAPCONTENTS",
+					"indices": [
+						97,
+						111
+					]
+				},
+				{
+					"text": "SNLKOREA",
+					"indices": [
+						112,
+						121
+					]
+				},
+				{
+					"text": "쿠팡플레이",
+					"indices": [
+						122,
+						128
+					]
+				},
+				{
+					"text": "CoupangPlay",
+					"indices": [
+						129,
+						141
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/tCqla0YxnN",
+					"expanded_url": "https://youtu.be/5rWDifmf3L0",
+					"display_url": "youtu.be/5rWDifmf3L0",
+					"indices": [
+						52,
+						75
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1219506842935513000,
+			"id_str": "1219506842935513088",
+			"name": "휴맵컨텐츠(HUMAP)",
+			"screen_name": "humap_official",
+			"location": "",
+			"description": "휴맵컨텐츠(HUMAP CONTENTS) Official Twitter",
+			"url": "https://t.co/fBZZK3SmX6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fBZZK3SmX6",
+							"expanded_url": "http://www.humapcontents.com/",
+							"display_url": "humapcontents.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 359,
+			"friends_count": 1,
+			"listed_count": 2,
+			"created_at": "Tue Jan 21 06:28:27 +0000 2020",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 306,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1219506842935513088/1579755898",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[VIDEO] [#권혁수] #SNL코리아 \n\n쿠팡플레이 첫 오리지널 SNL코리아 공개일\n\n👉 https://t.co/tCqla0YxnN\n\n#KwunHyukSoo #휴맵컨텐츠 #HUMAPCONTENTS #SNLKOREA #쿠팡플레이 #CoupangPlay"
+	},
+	{
 		"created_at": "Wed Sep 01 11:29:24 +0000 2021",
 		"id": 1433019161177854000,
 		"id_str": "1433029218913370115",
