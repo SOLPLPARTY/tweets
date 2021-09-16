611c174fb6db83697cd87bd36b09b9e879cca1d7
솔플파티 <solplparty@gmail.com>
2021-09-16 22:17:41 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 52927,
-	"created_at": "Thu Sep 16 12:17:41 +0000 2021",
+	"tweets": 52928,
+	"created_at": "Thu Sep 16 13:17:40 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,149 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Thu Sep 16 12:29:25 +0000 2021",
+		"id": 1438472740181213200,
+		"id_str": "1438480137180352527",
+		"full_text": "최유리의 '동그라미' 배민라이브가 공개되었습니다. 짙은 감성의 싱어송라이터 최유리의 배민라이브를 지금 바로 확인해보세요!\n⠀\n▷ https://t.co/5mWEGLheUE\n\n#최유리 #동그라미 #배민라이브\n#ChoiYuRee #Shape",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			132
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "최유리",
+					"indices": [
+						97,
+						101
+					]
+				},
+				{
+					"text": "동그라미",
+					"indices": [
+						102,
+						107
+					]
+				},
+				{
+					"text": "배민라이브",
+					"indices": [
+						108,
+						114
+					]
+				},
+				{
+					"text": "ChoiYuRee",
+					"indices": [
+						115,
+						125
+					]
+				},
+				{
+					"text": "Shape",
+					"indices": [
+						126,
+						132
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/5mWEGLheUE",
+					"expanded_url": "https://youtu.be/opH7dIA9-iU",
+					"display_url": "youtu.be/opH7dIA9-iU",
+					"indices": [
+						72,
+						95
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 265792818,
+			"id_str": "265792818",
+			"name": "쇼파르엔터테인먼트",
+			"screen_name": "shofar_ent",
+			"location": "",
+			"description": "SHOFAR ENTERTAINMENT Official Twitter",
+			"url": "https://t.co/VtrzF3mRza",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/VtrzF3mRza",
+							"expanded_url": "http://shofar-ent.com",
+							"display_url": "shofar-ent.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7727,
+			"friends_count": 4,
+			"listed_count": 66,
+			"created_at": "Mon Mar 14 04:31:00 +0000 2011",
+			"favourites_count": 53,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4483,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1380084076573417474/i-NclciR_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1380084076573417474/i-NclciR_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/265792818/1631001017",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "최유리의 '동그라미' 배민라이브가 공개되었습니다. 짙은 감성의 싱어송라이터 최유리의 배민라이브를 지금 바로 확인해보세요!\n⠀\n▷ https://t.co/5mWEGLheUE\n\n#최유리 #동그라미 #배민라이브\n#ChoiYuRee #Shape"
+	},
+	{
 		"created_at": "Thu Sep 16 12:05:13 +0000 2021",
 		"id": 1438474047910813700,
 		"id_str": "1438474047910813696",
