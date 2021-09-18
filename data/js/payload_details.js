bf58641dbcd62724173fbea7aa156e25a407a500
솔플파티 <solplparty@gmail.com>
2021-09-18 13:17:41 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 53015,
-	"created_at": "Sat Sep 18 03:17:40 +0000 2021",
+	"tweets": 53016,
+	"created_at": "Sat Sep 18 04:17:40 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,149 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Sat Sep 18 03:29:27 +0000 2021",
+		"id": 1439063267280703500,
+		"id_str": "1439069026572021761",
+		"full_text": "[#김사월] 리퍼반 페스티벌 Korea Spotlight 쇼케이스 비하인드 스토리\n\n김사월의 리퍼반 페스티벌 온라인 쇼케이스 비하인드 영상이 공개되었습니다.\n\nFull ver. on KOCCA MUSIC Youtube\n▶️ https://t.co/blwnI2EInt\n\n#리퍼반 #리퍼반페스티벌 #온라인쇼케이스 #비하인드",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			178
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "김사월",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "리퍼반",
+					"indices": [
+						150,
+						154
+					]
+				},
+				{
+					"text": "리퍼반페스티벌",
+					"indices": [
+						155,
+						163
+					]
+				},
+				{
+					"text": "온라인쇼케이스",
+					"indices": [
+						164,
+						172
+					]
+				},
+				{
+					"text": "비하인드",
+					"indices": [
+						173,
+						178
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/blwnI2EInt",
+					"expanded_url": "https://bit.ly/3tNdLpe",
+					"display_url": "bit.ly/3tNdLpe",
+					"indices": [
+						125,
+						148
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 404720294,
+			"id_str": "404720294",
+			"name": "유어썸머 Your Summer",
+			"screen_name": "YoursummerKR",
+			"location": "한국",
+			"description": "유어썸머(Your Summer)는 cott(콧), 계피, 김사월, 김사월X김해원, 너드커넥션, 랄라스윗, 못(Mot), 박소은, 성진환, 슬릭, 아마도이자람밴드, 오지은, 우주왕복선싸이드미러, 우주히피, 이랑, 이하윤, 전기뱀장어와 함께합니다.",
+			"url": "https://t.co/M1QKGb4pGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/M1QKGb4pGi",
+							"expanded_url": "https://linktr.ee/yoursummer",
+							"display_url": "linktr.ee/yoursummer",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2138,
+			"friends_count": 23,
+			"listed_count": 11,
+			"created_at": "Fri Nov 04 09:50:32 +0000 2011",
+			"favourites_count": 549,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5450,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1368882268060540929/XY97X-lQ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1368882268060540929/XY97X-lQ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/404720294/1615202264",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#김사월] 리퍼반 페스티벌 Korea Spotlight 쇼케이스 비하인드 스토리\n\n김사월의 리퍼반 페스티벌 온라인 쇼케이스 비하인드 영상이 공개되었습니다.\n\nFull ver. on KOCCA MUSIC Youtube\n▶️ https://t.co/blwnI2EInt\n\n#리퍼반 #리퍼반페스티벌 #온라인쇼케이스 #비하인드"
+	},
+	{
 		"created_at": "Sat Sep 18 03:05:50 +0000 2021",
 		"id": 1439063084593725400,
 		"id_str": "1439063084593725443",
