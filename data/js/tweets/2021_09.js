9c7fbbb061d46324a47ddb8adb5429bd9fe82628
솔플파티 <solplparty@gmail.com>
2021-09-19 20:17:41 +0900

    Added 1 tweets

--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,149 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Sun Sep 19 10:29:25 +0000 2021",
+		"id": 1439522485955752000,
+		"id_str": "1439537101880664065",
+		"full_text": "[Teaser #4]\n⠀\n보라미유, ASTER, NEO - 'On &amp; On'\n2021.09.20. 6PM Release\n⠀\n▷ https://t.co/dgkJqlKwH3\n\n#보라미유 #아스터 #네오 \n#이니셜뮤직 #쇼파르엔터테인먼트",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			133
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "보라미유",
+					"indices": [
+						100,
+						105
+					]
+				},
+				{
+					"text": "아스터",
+					"indices": [
+						106,
+						110
+					]
+				},
+				{
+					"text": "네오",
+					"indices": [
+						111,
+						114
+					]
+				},
+				{
+					"text": "이니셜뮤직",
+					"indices": [
+						116,
+						122
+					]
+				},
+				{
+					"text": "쇼파르엔터테인먼트",
+					"indices": [
+						123,
+						133
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/dgkJqlKwH3",
+					"expanded_url": "https://youtu.be/x3S7kpNjzg4",
+					"display_url": "youtu.be/x3S7kpNjzg4",
+					"indices": [
+						75,
+						98
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 265792818,
+			"id_str": "265792818",
+			"name": "쇼파르엔터테인먼트",
+			"screen_name": "shofar_ent",
+			"location": "",
+			"description": "SHOFAR ENTERTAINMENT Official Twitter",
+			"url": "https://t.co/VtrzF3mRza",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/VtrzF3mRza",
+							"expanded_url": "http://shofar-ent.com",
+							"display_url": "shofar-ent.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7723,
+			"friends_count": 4,
+			"listed_count": 0,
+			"created_at": "Mon Mar 14 04:31:00 +0000 2011",
+			"favourites_count": 53,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4487,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1380084076573417474/i-NclciR_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1380084076573417474/i-NclciR_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/265792818/1631001017",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[Teaser #4]\n⠀\n보라미유, ASTER, NEO - 'On &amp; On'\n2021.09.20. 6PM Release\n⠀\n▷ https://t.co/dgkJqlKwH3\n\n#보라미유 #아스터 #네오 \n#이니셜뮤직 #쇼파르엔터테인먼트"
+	},
+	{
 		"created_at": "Sun Sep 19 10:11:25 +0000 2021",
 		"id": 1439530013947089000,
 		"id_str": "1439532574234357764",
