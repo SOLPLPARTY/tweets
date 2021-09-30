7b932ac0643999036226c91dcaab8f65f360a961
솔플파티 <solplparty@gmail.com>
2021-09-30 14:17:41 +0900

    Added 2 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2021_09.js",
 		"year": 2021,
 		"var_name": "tweets_2021_09",
-		"tweet_count": 1279,
+		"tweet_count": 1281,
 		"month": 9
 	},
 	{
