ac828240e2bd015b80acf38fe21fa1679e7ffd6f
솔플파티 <solplparty@gmail.com>
2021-10-01 15:17:33 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 53631,
-	"created_at": "Fri Oct 01 05:17:32 +0000 2021",
+	"tweets": 53632,
+	"created_at": "Fri Oct 01 06:17:32 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,297 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Fri Oct 01 05:29:31 +0000 2021",
+		"id": 1443805849726509000,
+		"id_str": "1443810285534523392",
+		"full_text": "[#너드커넥션] &lt;Grand Mint Festival 2021&gt; 취소공지\n\n10월 24일 너드커넥션이 함께 할 예정이었던 &lt;Grand Mint Festival 2021&gt;이 \n코로나19 사회적 거리두기로 인해 취소되었음을 알려드립니다. \n\n자세한 사항은 첨부된 사진 속 공지 또는 grandmintfestival 공지를 확인해주세요. https://t.co/PyugidPpkc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			197
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "너드커넥션",
+					"indices": [
+						1,
+						7
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1443805843317616600,
+					"id_str": "1443805843317616640",
+					"indices": [
+						198,
+						221
+					],
+					"media_url": "http://pbs.twimg.com/media/FAltw8-UcAAAD_3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FAltw8-UcAAAD_3.jpg",
+					"url": "https://t.co/PyugidPpkc",
+					"display_url": "pic.twitter.com/PyugidPpkc",
+					"expanded_url": "https://twitter.com/YoursummerKR/status/1443805849726509060/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1443805843317616600,
+					"id_str": "1443805843317616640",
+					"indices": [
+						198,
+						221
+					],
+					"media_url": "http://pbs.twimg.com/media/FAltw8-UcAAAD_3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FAltw8-UcAAAD_3.jpg",
+					"url": "https://t.co/PyugidPpkc",
+					"display_url": "pic.twitter.com/PyugidPpkc",
+					"expanded_url": "https://twitter.com/YoursummerKR/status/1443805849726509060/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1443805843313488000,
+					"id_str": "1443805843313487878",
+					"indices": [
+						198,
+						221
+					],
+					"media_url": "http://pbs.twimg.com/media/FAltw89VcAY0l6I.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FAltw89VcAY0l6I.jpg",
+					"url": "https://t.co/PyugidPpkc",
+					"display_url": "pic.twitter.com/PyugidPpkc",
+					"expanded_url": "https://twitter.com/YoursummerKR/status/1443805849726509060/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1443805843317661700,
+					"id_str": "1443805843317661696",
+					"indices": [
+						198,
+						221
+					],
+					"media_url": "http://pbs.twimg.com/media/FAltw8-VIAAxOVC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FAltw8-VIAAxOVC.jpg",
+					"url": "https://t.co/PyugidPpkc",
+					"display_url": "pic.twitter.com/PyugidPpkc",
+					"expanded_url": "https://twitter.com/YoursummerKR/status/1443805849726509060/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1443805843485433900,
+					"id_str": "1443805843485433859",
+					"indices": [
+						198,
+						221
+					],
+					"media_url": "http://pbs.twimg.com/media/FAltw9mVIAMvHke.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FAltw9mVIAMvHke.jpg",
+					"url": "https://t.co/PyugidPpkc",
+					"display_url": "pic.twitter.com/PyugidPpkc",
+					"expanded_url": "https://twitter.com/YoursummerKR/status/1443805849726509060/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 404720294,
+			"id_str": "404720294",
+			"name": "유어썸머 Your Summer",
+			"screen_name": "YoursummerKR",
+			"location": "한국",
+			"description": "유어썸머(Your Summer)는 cott(콧), 계피, 김사월, 김사월X김해원, 너드커넥션, 랄라스윗, 못(Mot), 박소은, 성진환, 슬릭, 아마도이자람밴드, 오지은, 우주왕복선싸이드미러, 우주히피, 이랑, 이하윤, 전기뱀장어와 함께합니다.",
+			"url": "https://t.co/M1QKGb4pGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/M1QKGb4pGi",
+							"expanded_url": "https://linktr.ee/yoursummer",
+							"display_url": "linktr.ee/yoursummer",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2133,
+			"friends_count": 23,
+			"listed_count": 11,
+			"created_at": "Fri Nov 04 09:50:32 +0000 2011",
+			"favourites_count": 549,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5492,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1368882268060540929/XY97X-lQ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1368882268060540929/XY97X-lQ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/404720294/1615202264",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#너드커넥션] &lt;Grand Mint Festival 2021&gt; 취소공지\n\n10월 24일 너드커넥션이 함께 할 예정이었던 &lt;Grand Mint Festival 2021&gt;이 \n코로나19 사회적 거리두기로 인해 취소되었음을 알려드립니다. \n\n자세한 사항은 첨부된 사진 속 공지 또는 grandmintfestival 공지를 확인해주세요. https://t.co/PyugidPpkc"
+	},
+	{
 		"created_at": "Fri Oct 01 04:29:24 +0000 2021",
 		"id": 1443782472404197400,
 		"id_str": "1443795158869463041",
