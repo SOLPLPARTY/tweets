d6b3bf0ad7b6f7d95378ace78baa526f08f69ad0
솔플파티 <solplparty@gmail.com>
2021-10-03 23:17:33 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 53718,
-	"created_at": "Sun Oct 03 12:17:32 +0000 2021",
+	"tweets": 53719,
+	"created_at": "Sun Oct 03 14:17:32 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,292 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Sun Oct 03 14:11:17 +0000 2021",
+		"id": 1444663945084616700,
+		"id_str": "1444666366661955587",
+		"full_text": "[ 공지 _ 프리즘홀 10월 스케쥴 ]\n\n10월 프리즘홀 스케쥴을 확인하세요!\n멋있는 공연들이 진행되고 있습니다!\n\n모든 공연 및 상시 대관 문의 : 070-8150-2979\n\n #프리즘홀 #prismhall #10월공연 #10월스케쥴 #홍대공연 #인디공연 #기획공연 #단독공연 #인디문화 #무대를지켜주세요 #슬기로운인디생활 https://t.co/ldoJoJDQZT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			181
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "프리즘홀",
+					"indices": [
+						100,
+						105
+					]
+				},
+				{
+					"text": "prismhall",
+					"indices": [
+						106,
+						116
+					]
+				},
+				{
+					"text": "10월공연",
+					"indices": [
+						117,
+						123
+					]
+				},
+				{
+					"text": "10월스케쥴",
+					"indices": [
+						124,
+						131
+					]
+				},
+				{
+					"text": "홍대공연",
+					"indices": [
+						132,
+						137
+					]
+				},
+				{
+					"text": "인디공연",
+					"indices": [
+						138,
+						143
+					]
+				},
+				{
+					"text": "기획공연",
+					"indices": [
+						144,
+						149
+					]
+				},
+				{
+					"text": "단독공연",
+					"indices": [
+						150,
+						155
+					]
+				},
+				{
+					"text": "인디문화",
+					"indices": [
+						156,
+						161
+					]
+				},
+				{
+					"text": "무대를지켜주세요",
+					"indices": [
+						162,
+						171
+					]
+				},
+				{
+					"text": "슬기로운인디생활",
+					"indices": [
+						172,
+						181
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1444663937115439000,
+					"id_str": "1444663937115439117",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/media/FAx6MleVQA0aI0o.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FAx6MleVQA0aI0o.jpg",
+					"url": "https://t.co/ldoJoJDQZT",
+					"display_url": "pic.twitter.com/ldoJoJDQZT",
+					"expanded_url": "https://twitter.com/prismhall/status/1444663945084616709/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1444663937115439000,
+					"id_str": "1444663937115439117",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/media/FAx6MleVQA0aI0o.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FAx6MleVQA0aI0o.jpg",
+					"url": "https://t.co/ldoJoJDQZT",
+					"display_url": "pic.twitter.com/ldoJoJDQZT",
+					"expanded_url": "https://twitter.com/prismhall/status/1444663945084616709/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 700,
+							"h": 700,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 491231175,
+			"id_str": "491231175",
+			"name": "PrismHall",
+			"screen_name": "prismhall",
+			"location": "Mapo Seokyo-Dong",
+			"description": "최상급 무대장비와 시설. 엄청난 높이의 천고와 광활한 객석. 각종 파티,힙합,댄스,밴드 등 all_round concert, 실내페스티벌 등 모든 게 가능한 공연장입니다!",
+			"url": "https://t.co/VTKFWeLxvh",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/VTKFWeLxvh",
+							"expanded_url": "http://www.prismhall.com",
+							"display_url": "prismhall.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2049,
+			"friends_count": 1126,
+			"listed_count": 28,
+			"created_at": "Mon Feb 13 12:03:08 +0000 2012",
+			"favourites_count": 71,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1963,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/942698829089869824/bkVw2sYC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/942698829089869824/bkVw2sYC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/491231175/1513591891",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "1104338421d4e001",
+			"url": "https://api.twitter.com/1.1/geo/id/1104338421d4e001.json",
+			"place_type": "poi",
+			"name": "프리즘홀",
+			"full_name": "프리즘홀",
+			"country_code": "KR",
+			"country": "대한민국",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							126.917463,
+							37.551686
+						],
+						[
+							126.917463,
+							37.551686
+						],
+						[
+							126.917463,
+							37.551686
+						],
+						[
+							126.917463,
+							37.551686
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[ 공지 _ 프리즘홀 10월 스케쥴 ]\n\n10월 프리즘홀 스케쥴을 확인하세요!\n멋있는 공연들이 진행되고 있습니다!\n\n모든 공연 및 상시 대관 문의 : 070-8150-2979\n\n #프리즘홀 #prismhall #10월공연 #10월스케쥴 #홍대공연 #인디공연 #기획공연 #단독공연 #인디문화 #무대를지켜주세요 #슬기로운인디생활 https://t.co/ldoJoJDQZT"
+	},
+	{
 		"created_at": "Sun Oct 03 12:11:17 +0000 2021",
 		"id": 1444631317824487400,
 		"id_str": "1444636168516210690",
