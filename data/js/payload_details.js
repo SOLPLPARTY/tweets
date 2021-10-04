5264ca90b20caa89eb53239cd43f41074ab75254
솔플파티 <solplparty@gmail.com>
2021-10-04 23:17:35 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 53752,
-	"created_at": "Mon Oct 04 12:17:34 +0000 2021",
+	"tweets": 53753,
+	"created_at": "Mon Oct 04 14:17:34 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,278 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Mon Oct 04 13:29:30 +0000 2021",
+		"id": 1445018129701113900,
+		"id_str": "1445018241554915330",
+		"full_text": "[#cott] New Single \n하필(HOW YOU FEEL?)\n2021.10.07. 12PM \n\n#cott #콧 #싱글 #하필 #single #howyoufeel #10월7일 #정오 #발매 https://t.co/5h91CaQe03",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			108
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "cott",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "cott",
+					"indices": [
+						57,
+						62
+					]
+				},
+				{
+					"text": "콧",
+					"indices": [
+						63,
+						65
+					]
+				},
+				{
+					"text": "싱글",
+					"indices": [
+						66,
+						69
+					]
+				},
+				{
+					"text": "하필",
+					"indices": [
+						70,
+						73
+					]
+				},
+				{
+					"text": "single",
+					"indices": [
+						74,
+						81
+					]
+				},
+				{
+					"text": "howyoufeel",
+					"indices": [
+						82,
+						93
+					]
+				},
+				{
+					"text": "10월7일",
+					"indices": [
+						94,
+						100
+					]
+				},
+				{
+					"text": "정오",
+					"indices": [
+						101,
+						104
+					]
+				},
+				{
+					"text": "발매",
+					"indices": [
+						105,
+						108
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1445018072805347300,
+					"id_str": "1445018072805347334",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1445018072805347334/pu/img/nmyTJoCN206pmtGK.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1445018072805347334/pu/img/nmyTJoCN206pmtGK.jpg",
+					"url": "https://t.co/5h91CaQe03",
+					"display_url": "pic.twitter.com/5h91CaQe03",
+					"expanded_url": "https://twitter.com/YoursummerKR/status/1445018129701113857/video/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1445018072805347300,
+					"id_str": "1445018072805347334",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1445018072805347334/pu/img/nmyTJoCN206pmtGK.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1445018072805347334/pu/img/nmyTJoCN206pmtGK.jpg",
+					"url": "https://t.co/5h91CaQe03",
+					"display_url": "pic.twitter.com/5h91CaQe03",
+					"expanded_url": "https://twitter.com/YoursummerKR/status/1445018129701113857/video/1",
+					"type": "video",
+					"sizes": {
+						"small": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 480,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 16209,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1445018072805347334/pu/vid/480x480/Nt-fh8WnmbO2sJM1.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1445018072805347334/pu/pl/Yzz3GZ0vtOASXJOr.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1445018072805347334/pu/vid/320x320/Z0o5c0GMQErDbWS8.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 404720294,
+			"id_str": "404720294",
+			"name": "유어썸머 Your Summer",
+			"screen_name": "YoursummerKR",
+			"location": "한국",
+			"description": "유어썸머(Your Summer)는 cott(콧), 계피, 김사월, 김사월X김해원, 너드커넥션, 랄라스윗, 못(Mot), 박소은, 성진환, 슬릭, 아마도이자람밴드, 오지은, 우주왕복선싸이드미러, 우주히피, 이랑, 이하윤, 전기뱀장어와 함께합니다.",
+			"url": "https://t.co/M1QKGb4pGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/M1QKGb4pGi",
+							"expanded_url": "https://linktr.ee/yoursummer",
+							"display_url": "linktr.ee/yoursummer",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2135,
+			"friends_count": 23,
+			"listed_count": 11,
+			"created_at": "Fri Nov 04 09:50:32 +0000 2011",
+			"favourites_count": 549,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5508,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1368882268060540929/XY97X-lQ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1368882268060540929/XY97X-lQ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/404720294/1615202264",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "[#cott] New Single \n하필(HOW YOU FEEL?)\n2021.10.07. 12PM \n\n#cott #콧 #싱글 #하필 #single #howyoufeel #10월7일 #정오 #발매 https://t.co/5h91CaQe03"
+	},
+	{
 		"created_at": "Mon Oct 04 12:11:17 +0000 2021",
 		"id": 1444997369041326000,
 		"id_str": "1444998555727503362",
