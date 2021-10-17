79259a06b3b2a56e2f1da929e10448b7f400f090
솔플파티 <solplparty@gmail.com>
2021-10-17 21:17:40 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 54258,
-	"created_at": "Sun Oct 17 10:17:41 +0000 2021",
+	"tweets": 54259,
+	"created_at": "Sun Oct 17 12:17:39 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,301 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Sun Oct 17 12:11:16 +0000 2021",
+		"id": 1449703084762931200,
+		"id_str": "1449709596315111428",
+		"full_text": "새로운 한주 클럽빵 공연일정입니다 관객입장도 가능하니 더러 발걸음 해주시길~ 유튜브라이브는 계속~ #클럽빵 #전다인 #베이비슈가 #김태인 #건훈씨 #이장혁 #하늘과바람과날개의끝 #조슈아 #곽주나 #셀린셀리셀리느 #본 #글라스 #이디어츠 #스모킹구스 #마치 #피라 #앨리스밴드 https://t.co/mMdTd8Mvdl",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			153
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "클럽빵",
+					"indices": [
+						55,
+						59
+					]
+				},
+				{
+					"text": "전다인",
+					"indices": [
+						60,
+						64
+					]
+				},
+				{
+					"text": "베이비슈가",
+					"indices": [
+						65,
+						71
+					]
+				},
+				{
+					"text": "김태인",
+					"indices": [
+						72,
+						76
+					]
+				},
+				{
+					"text": "건훈씨",
+					"indices": [
+						77,
+						81
+					]
+				},
+				{
+					"text": "이장혁",
+					"indices": [
+						82,
+						86
+					]
+				},
+				{
+					"text": "하늘과바람과날개의끝",
+					"indices": [
+						87,
+						98
+					]
+				},
+				{
+					"text": "조슈아",
+					"indices": [
+						99,
+						103
+					]
+				},
+				{
+					"text": "곽주나",
+					"indices": [
+						104,
+						108
+					]
+				},
+				{
+					"text": "셀린셀리셀리느",
+					"indices": [
+						109,
+						117
+					]
+				},
+				{
+					"text": "본",
+					"indices": [
+						118,
+						120
+					]
+				},
+				{
+					"text": "글라스",
+					"indices": [
+						121,
+						125
+					]
+				},
+				{
+					"text": "이디어츠",
+					"indices": [
+						126,
+						131
+					]
+				},
+				{
+					"text": "스모킹구스",
+					"indices": [
+						132,
+						138
+					]
+				},
+				{
+					"text": "마치",
+					"indices": [
+						139,
+						142
+					]
+				},
+				{
+					"text": "피라",
+					"indices": [
+						143,
+						146
+					]
+				},
+				{
+					"text": "앨리스밴드",
+					"indices": [
+						147,
+						153
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1449703078576328700,
+					"id_str": "1449703078576328711",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/FB5hRORVEAcMQQh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FB5hRORVEAcMQQh.jpg",
+					"url": "https://t.co/mMdTd8Mvdl",
+					"display_url": "pic.twitter.com/mMdTd8Mvdl",
+					"expanded_url": "https://twitter.com/club_bbang/status/1449703084762931201/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1449703078576328700,
+					"id_str": "1449703078576328711",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/FB5hRORVEAcMQQh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FB5hRORVEAcMQQh.jpg",
+					"url": "https://t.co/mMdTd8Mvdl",
+					"display_url": "pic.twitter.com/mMdTd8Mvdl",
+					"expanded_url": "https://twitter.com/club_bbang/status/1449703084762931201/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1387140522,
+			"id_str": "1387140522",
+			"name": "클럽 빵",
+			"screen_name": "club_bbang",
+			"location": "서울 마포 서교동 327-18 지하",
+			"description": "모던록 라이브클럽 빵, 공연일정을 알려드려요! 문의 010-8910-1089, cafebbang@hanmail.net",
+			"url": "https://t.co/ZROqsiLHaK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ZROqsiLHaK",
+							"expanded_url": "http://cafe.daum.net/cafebbang",
+							"display_url": "cafe.daum.net/cafebbang",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2486,
+			"friends_count": 877,
+			"listed_count": 36,
+			"created_at": "Sun Apr 28 14:57:43 +0000 2013",
+			"favourites_count": 74,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1814,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3617429436/49aa1ea87a3bdf4e12ef8ab826e7b229_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3617429436/49aa1ea87a3bdf4e12ef8ab826e7b229_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1387140522/1367815826",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "새로운 한주 클럽빵 공연일정입니다 관객입장도 가능하니 더러 발걸음 해주시길~ 유튜브라이브는 계속~ #클럽빵 #전다인 #베이비슈가 #김태인 #건훈씨 #이장혁 #하늘과바람과날개의끝 #조슈아 #곽주나 #셀린셀리셀리느 #본 #글라스 #이디어츠 #스모킹구스 #마치 #피라 #앨리스밴드 https://t.co/mMdTd8Mvdl"
+	},
+	{
 		"created_at": "Sun Oct 17 09:29:24 +0000 2021",
 		"id": 1449661470820626400,
 		"id_str": "1449668862010593287",
