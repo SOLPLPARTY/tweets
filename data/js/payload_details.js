d354f9af631a8b3794a1d7c9c69a32650b886db3
솔플파티 <solplparty@gmail.com>
2021-10-18 20:17:44 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 54291,
-	"created_at": "Mon Oct 18 10:17:41 +0000 2021",
+	"tweets": 54293,
+	"created_at": "Mon Oct 18 11:17:43 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,412 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Mon Oct 18 10:29:31 +0000 2021",
+		"id": 1450042707129237500,
+		"id_str": "1450046376017006594",
+		"full_text": "[#권혁수] [#백돌이대탈출]\n\n그냥 과녁수와🎯\n밑도 끝도 없는 부캐컨셉 밀고 나가는 백돌이 대환장 파티..\n\n#백돌이대탈출_부캐골프시대 에서 확인하고 가세요~!\n\n👉 https://t.co/ess34mMdTj\n\n#KwunHyukSoo #휴맵컨텐츠 #HUMAPCONTENTS #골프 #이지훈 #훈 #김시우 https://t.co/dBfY4YlwRU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			170
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "권혁수",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "백돌이대탈출",
+					"indices": [
+						8,
+						15
+					]
+				},
+				{
+					"text": "백돌이대탈출_부캐골프시대",
+					"indices": [
+						62,
+						76
+					]
+				},
+				{
+					"text": "KwunHyukSoo",
+					"indices": [
+						119,
+						131
+					]
+				},
+				{
+					"text": "휴맵컨텐츠",
+					"indices": [
+						132,
+						138
+					]
+				},
+				{
+					"text": "HUMAPCONTENTS",
+					"indices": [
+						139,
+						153
+					]
+				},
+				{
+					"text": "골프",
+					"indices": [
+						154,
+						157
+					]
+				},
+				{
+					"text": "이지훈",
+					"indices": [
+						158,
+						162
+					]
+				},
+				{
+					"text": "훈",
+					"indices": [
+						163,
+						165
+					]
+				},
+				{
+					"text": "김시우",
+					"indices": [
+						166,
+						170
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ess34mMdTj",
+					"expanded_url": "https://youtu.be/zURRGQw8Raw",
+					"display_url": "youtu.be/zURRGQw8Raw",
+					"indices": [
+						94,
+						117
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1450042695741698000,
+					"id_str": "1450042695741698053",
+					"indices": [
+						171,
+						194
+					],
+					"media_url": "http://pbs.twimg.com/media/FB-WJizVcAU_cnr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FB-WJizVcAU_cnr.jpg",
+					"url": "https://t.co/dBfY4YlwRU",
+					"display_url": "pic.twitter.com/dBfY4YlwRU",
+					"expanded_url": "https://twitter.com/humap_official/status/1450042707129237508/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1450042695741698000,
+					"id_str": "1450042695741698053",
+					"indices": [
+						171,
+						194
+					],
+					"media_url": "http://pbs.twimg.com/media/FB-WJizVcAU_cnr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FB-WJizVcAU_cnr.jpg",
+					"url": "https://t.co/dBfY4YlwRU",
+					"display_url": "pic.twitter.com/dBfY4YlwRU",
+					"expanded_url": "https://twitter.com/humap_official/status/1450042707129237508/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1219506842935513000,
+			"id_str": "1219506842935513088",
+			"name": "휴맵컨텐츠(HUMAP)",
+			"screen_name": "humap_official",
+			"location": "",
+			"description": "휴맵컨텐츠(HUMAP CONTENTS) Official Twitter",
+			"url": "https://t.co/fBZZK3SmX6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fBZZK3SmX6",
+							"expanded_url": "http://www.humapcontents.com/",
+							"display_url": "humapcontents.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 375,
+			"friends_count": 1,
+			"listed_count": 2,
+			"created_at": "Tue Jan 21 06:28:27 +0000 2020",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 372,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1219506842935513088/1579755898",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#권혁수] [#백돌이대탈출]\n\n그냥 과녁수와🎯\n밑도 끝도 없는 부캐컨셉 밀고 나가는 백돌이 대환장 파티..\n\n#백돌이대탈출_부캐골프시대 에서 확인하고 가세요~!\n\n👉 https://t.co/ess34mMdTj\n\n#KwunHyukSoo #휴맵컨텐츠 #HUMAPCONTENTS #골프 #이지훈 #훈 #김시우 https://t.co/dBfY4YlwRU"
+	},
+	{
+		"created_at": "Mon Oct 18 10:29:28 +0000 2021",
+		"id": 1450035664565653500,
+		"id_str": "1450046362968596480",
+		"full_text": "[VIDEO] [#권혁수] #벤자민권의_상상은현실이된다\n\n[벤자민터뷰_반말인터뷰 ①권혁수 편] 아직도 벤자민권 안보나?\n\n👉 https://t.co/driMeQDsen\n\n#KwunHyukSoo #휴맵컨텐츠 #HUMAPCONTENTS #가치삽시다TV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			138
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "권혁수",
+					"indices": [
+						9,
+						13
+					]
+				},
+				{
+					"text": "벤자민권의_상상은현실이된다",
+					"indices": [
+						15,
+						30
+					]
+				},
+				{
+					"text": "KwunHyukSoo",
+					"indices": [
+						95,
+						107
+					]
+				},
+				{
+					"text": "휴맵컨텐츠",
+					"indices": [
+						108,
+						114
+					]
+				},
+				{
+					"text": "HUMAPCONTENTS",
+					"indices": [
+						115,
+						129
+					]
+				},
+				{
+					"text": "가치삽시다TV",
+					"indices": [
+						130,
+						138
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/driMeQDsen",
+					"expanded_url": "https://youtu.be/QTTvyJHZQq8",
+					"display_url": "youtu.be/QTTvyJHZQq8",
+					"indices": [
+						70,
+						93
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1219506842935513000,
+			"id_str": "1219506842935513088",
+			"name": "휴맵컨텐츠(HUMAP)",
+			"screen_name": "humap_official",
+			"location": "",
+			"description": "휴맵컨텐츠(HUMAP CONTENTS) Official Twitter",
+			"url": "https://t.co/fBZZK3SmX6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fBZZK3SmX6",
+							"expanded_url": "http://www.humapcontents.com/",
+							"display_url": "humapcontents.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 375,
+			"friends_count": 1,
+			"listed_count": 2,
+			"created_at": "Tue Jan 21 06:28:27 +0000 2020",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 372,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1219506842935513088/1579755898",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[VIDEO] [#권혁수] #벤자민권의_상상은현실이된다\n\n[벤자민터뷰_반말인터뷰 ①권혁수 편] 아직도 벤자민권 안보나?\n\n👉 https://t.co/driMeQDsen\n\n#KwunHyukSoo #휴맵컨텐츠 #HUMAPCONTENTS #가치삽시다TV"
+	},
+	{
 		"created_at": "Mon Oct 18 10:07:51 +0000 2021",
 		"id": 1450040925615542300,
 		"id_str": "1450040925615542278",
@@ -286,7 +692,7 @@ Grailbird.data.tweets_2021_10 =
 		"created_at": "Mon Oct 18 09:29:44 +0000 2021",
 		"id": 1450030978399805400,
 		"id_str": "1450031332273369090",
-		"full_text": "이진혁 ‘Ctrl+V’ Released!!\n❤️‍🔥 https://t.co/25x7N3F33X https://t.co/cHPkbuTxe7",
+		"full_text": "이진혁 ‘Ctrl+V’ Released!!\n❤️🔥 https://t.co/25x7N3F33X https://t.co/cHPkbuTxe7",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -465,7 +871,7 @@ Grailbird.data.tweets_2021_10 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "이진혁 ‘Ctrl+V’ Released!!\n❤️‍🔥 https://t.co/25x7N3F33X https://t.co/cHPkbuTxe7"
+		"text": "이진혁 ‘Ctrl+V’ Released!!\n❤️🔥 https://t.co/25x7N3F33X https://t.co/cHPkbuTxe7"
 	},
 	{
 		"created_at": "Mon Oct 18 09:29:40 +0000 2021",
