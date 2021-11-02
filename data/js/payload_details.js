56104b1f1c326f2f3b4a0a019d2a120e719ff4ab
솔플파티 <solplparty@gmail.com>
2021-11-02 09:17:33 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 54804,
-	"created_at": "Mon Nov 01 16:17:32 +0000 2021",
+	"tweets": 54805,
+	"created_at": "Tue Nov 02 00:17:32 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,352 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Mon Nov 01 23:29:25 +0000 2021",
+		"id": 1455315886823252000,
+		"id_str": "1455316073348227077",
+		"full_text": "[#MIRYO] 2021.11.02 MIRYO DAY🎉\n\nHappy Birthday to MIRYO🎂\n미스틱스토리 아티스트 미료의 생일을 축하합니다🎁❤\n\n#211102 #happybirthday #birthday #생일 https://t.co/YTPGNuMl7C",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			122
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MIRYO",
+					"indices": [
+						1,
+						7
+					]
+				},
+				{
+					"text": "happybirthday",
+					"indices": [
+						94,
+						108
+					]
+				},
+				{
+					"text": "birthday",
+					"indices": [
+						109,
+						118
+					]
+				},
+				{
+					"text": "생일",
+					"indices": [
+						119,
+						122
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/YTPGNuMl7C",
+					"expanded_url": "https://twitter.com/officialBEG/status/1455313024495984643",
+					"display_url": "twitter.com/officialBEG/st…",
+					"indices": [
+						123,
+						146
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/BKUIHw4jho",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BKUIHw4jho",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 26412,
+			"friends_count": 15,
+			"listed_count": 248,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6336,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1553488269",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1455313024495984600,
+		"quoted_status_id_str": "1455313024495984643",
+		"quoted_status_permalink": {
+			"url": "https://t.co/YTPGNuMl7C",
+			"expanded": "https://twitter.com/officialBEG/status/1455313024495984643",
+			"display": "twitter.com/officialBEG/st…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Nov 01 23:17:18 +0000 2021",
+			"id": 1455313024495984600,
+			"id_str": "1455313024495984643",
+			"full_text": "[MIRYO] HAPPY BIRTHDAY, MIRYO!\n\n#20211102 #MIRYO #미료 #브라운아이드걸스 #BrownEyedGirls https://t.co/Imdl8CpzQO",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				78
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "MIRYO",
+						"indices": [
+							42,
+							48
+						]
+					},
+					{
+						"text": "미료",
+						"indices": [
+							49,
+							52
+						]
+					},
+					{
+						"text": "브라운아이드걸스",
+						"indices": [
+							53,
+							62
+						]
+					},
+					{
+						"text": "BrownEyedGirls",
+						"indices": [
+							63,
+							78
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1455312319094075400,
+						"id_str": "1455312319094075392",
+						"indices": [
+							79,
+							102
+						],
+						"media_url": "http://pbs.twimg.com/media/FDJO1_jaMAAtp0e.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FDJO1_jaMAAtp0e.jpg",
+						"url": "https://t.co/Imdl8CpzQO",
+						"display_url": "pic.twitter.com/Imdl8CpzQO",
+						"expanded_url": "https://twitter.com/officialBEG/status/1455313024495984643/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1920,
+								"h": 1920,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1455312319094075400,
+						"id_str": "1455312319094075392",
+						"indices": [
+							79,
+							102
+						],
+						"media_url": "http://pbs.twimg.com/media/FDJO1_jaMAAtp0e.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FDJO1_jaMAAtp0e.jpg",
+						"url": "https://t.co/Imdl8CpzQO",
+						"display_url": "pic.twitter.com/Imdl8CpzQO",
+						"expanded_url": "https://twitter.com/officialBEG/status/1455313024495984643/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1920,
+								"h": 1920,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 181069412,
+				"id_str": "181069412",
+				"name": "officialBEG",
+				"screen_name": "officialBEG",
+				"location": "",
+				"description": "",
+				"url": "https://t.co/AYJbDV6mB4",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/AYJbDV6mB4",
+								"expanded_url": "https://www.youtube.com/officialBEG",
+								"display_url": "youtube.com/officialBEG",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 50541,
+				"friends_count": 6,
+				"listed_count": 675,
+				"created_at": "Sat Aug 21 05:28:27 +0000 2010",
+				"favourites_count": 87,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 2499,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "131516",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1265883421395435520/jdsJNbaC_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1265883421395435520/jdsJNbaC_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/181069412/1571417049",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 68,
+			"favorite_count": 193,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 5,
+		"favorite_count": 20,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#MIRYO] 2021.11.02 MIRYO DAY🎉\n\nHappy Birthday to MIRYO🎂\n미스틱스토리 아티스트 미료의 생일을 축하합니다🎁❤\n\n#211102 #happybirthday #birthday #생일 https://t.co/YTPGNuMl7C"
+	},
+	{
 		"created_at": "Mon Nov 01 15:29:26 +0000 2021",
 		"id": 1455184480289759200,
 		"id_str": "1455195285723000838",
