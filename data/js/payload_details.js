59b26a2992fd0cd87641dfdfc344dbb88cfaaf37
솔플파티 <solplparty@gmail.com>
2021-11-02 21:17:34 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 54832,
-	"created_at": "Tue Nov 02 11:17:33 +0000 2021",
+	"tweets": 54833,
+	"created_at": "Tue Nov 02 12:17:33 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,10 +1,301 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Tue Nov 02 12:11:16 +0000 2021",
+		"id": 1455487128993812500,
+		"id_str": "1455507803251032074",
+		"full_text": "✔️상상마당 11월 프로그램🗣\n11월에도 야심차게 준비한 프로그램들😉\nKT&amp;G 상상마당 홍대 춘천 대치 등\n공간별 프로그램들을 확인하시고 즐겨보세요🤟🏻\n#상상마당_주요프로그램 https://t.co/r2Cy9LAAcp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			101
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "상상마당_주요프로그램",
+					"indices": [
+						89,
+						101
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1455487119875805200,
+					"id_str": "1455487119875805184",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/FDLt0vNakAAE8uw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDLt0vNakAAE8uw.jpg",
+					"url": "https://t.co/r2Cy9LAAcp",
+					"display_url": "pic.twitter.com/r2Cy9LAAcp",
+					"expanded_url": "https://twitter.com/SSmadang/status/1455487128993812483/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1199,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1543,
+							"h": 1542,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1455487119875805200,
+					"id_str": "1455487119875805184",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/FDLt0vNakAAE8uw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDLt0vNakAAE8uw.jpg",
+					"url": "https://t.co/r2Cy9LAAcp",
+					"display_url": "pic.twitter.com/r2Cy9LAAcp",
+					"expanded_url": "https://twitter.com/SSmadang/status/1455487128993812483/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1199,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1543,
+							"h": 1542,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1455487119875797000,
+					"id_str": "1455487119875796992",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/FDLt0vNacAAAauo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDLt0vNacAAAauo.jpg",
+					"url": "https://t.co/r2Cy9LAAcp",
+					"display_url": "pic.twitter.com/r2Cy9LAAcp",
+					"expanded_url": "https://twitter.com/SSmadang/status/1455487128993812483/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1543,
+							"h": 1542,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1199,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1455487119875797000,
+					"id_str": "1455487119875796994",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/FDLt0vNacAIUZat.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDLt0vNacAIUZat.jpg",
+					"url": "https://t.co/r2Cy9LAAcp",
+					"display_url": "pic.twitter.com/r2Cy9LAAcp",
+					"expanded_url": "https://twitter.com/SSmadang/status/1455487128993812483/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1199,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1543,
+							"h": 1542,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1455487119875801000,
+					"id_str": "1455487119875801089",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/FDLt0vNagAEYgQP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDLt0vNagAEYgQP.jpg",
+					"url": "https://t.co/r2Cy9LAAcp",
+					"display_url": "pic.twitter.com/r2Cy9LAAcp",
+					"expanded_url": "https://twitter.com/SSmadang/status/1455487128993812483/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1199,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1543,
+							"h": 1542,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 110358280,
+			"id_str": "110358280",
+			"name": "KT&G 상상마당",
+			"screen_name": "SSmadang",
+			"location": "서울 홍대 (Hongdae, Seoul)",
+			"description": "[복합문화공간] #상상마당 영화ㅣ공연ㅣ디자인ㅣ전시ㅣ교육ㅣ카페",
+			"url": "https://t.co/zkpyp6uMPi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zkpyp6uMPi",
+							"expanded_url": "http://sangsangmadang.com",
+							"display_url": "sangsangmadang.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 17755,
+			"friends_count": 1878,
+			"listed_count": 289,
+			"created_at": "Mon Feb 01 07:45:21 +0000 2010",
+			"favourites_count": 560,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9481,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1364071617765142529/2yKhRO_m_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1364071617765142529/2yKhRO_m_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/110358280/1614764527",
+			"profile_link_color": "242D8C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "595959",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "✔️상상마당 11월 프로그램🗣\n11월에도 야심차게 준비한 프로그램들😉\nKT&amp;G 상상마당 홍대 춘천 대치 등\n공간별 프로그램들을 확인하시고 즐겨보세요🤟🏻\n#상상마당_주요프로그램 https://t.co/r2Cy9LAAcp"
+	},
+	{
 		"created_at": "Tue Nov 02 10:29:29 +0000 2021",
 		"id": 1455469642315485200,
 		"id_str": "1455482184861634563",
-		"full_text": "#Wilcox\n\n❤️‍🩹\n𝖶𝗂𝗅𝖼𝗈𝗑 [𝟓𝟎/𝟓𝟎]\n2021.11.03.pm6(kst)\n\n#윌콕스 #5050 #fiftyfifty https://t.co/gXI7GEgII9",
+		"full_text": "#Wilcox\n\n❤️🩹\n𝖶𝗂𝗅𝖼𝗈𝗑 [𝟓𝟎/𝟓𝟎]\n2021.11.03.pm6(kst)\n\n#윌콕스 #5050 #fiftyfifty https://t.co/gXI7GEgII9",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -225,7 +516,7 @@ Grailbird.data.tweets_2021_11 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "#Wilcox\n\n❤️‍🩹\n𝖶𝗂𝗅𝖼𝗈𝗑 [𝟓𝟎/𝟓𝟎]\n2021.11.03.pm6(kst)\n\n#윌콕스 #5050 #fiftyfifty https://t.co/gXI7GEgII9"
+		"text": "#Wilcox\n\n❤️🩹\n𝖶𝗂𝗅𝖼𝗈𝗑 [𝟓𝟎/𝟓𝟎]\n2021.11.03.pm6(kst)\n\n#윌콕스 #5050 #fiftyfifty https://t.co/gXI7GEgII9"
 	},
 	{
 		"created_at": "Tue Nov 02 09:29:56 +0000 2021",
