4ef9b5bde2de8fc40630006f650073e1916263af
솔플파티 <solplparty@gmail.com>
2021-11-03 21:20:24 +0900

    Added 1 tweets

--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,147 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Wed Nov 03 12:08:39 +0000 2021",
+		"id": 1455869531855933400,
+		"id_str": "1455869531855933450",
+		"full_text": "#워너뮤직코리아 #Warner_Music_Korea\n[TEASER] Milena 밀레나 - Night Train\nhttps://t.co/npLQrpWKWF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			86
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "워너뮤직코리아",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"text": "Warner_Music_Korea",
+					"indices": [
+						9,
+						28
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/npLQrpWKWF",
+					"expanded_url": "https://youtu.be/5mpZZYPjuKw",
+					"display_url": "youtu.be/5mpZZYPjuKw",
+					"indices": [
+						63,
+						86
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.powerapps.com\" rel=\"nofollow\">Microsoft Power Platform</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 968706450779463700,
+			"id_str": "968706450779463682",
+			"name": "솔플파티",
+			"screen_name": "SOLPLPARTY",
+			"location": "문의: contact@solplparty.com",
+			"description": "혼자서도 즐겁게! \n문화예술 커뮤니티, 솔플파티 🤘 \n\n▶ 참가하기 \nhttps://t.co/Uq1kQ79SRI \n\n❓ ABOUT\nhttps://t.co/OtYqQtyXgi ",
+			"url": "https://t.co/L6Ap35US8u",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/L6Ap35US8u",
+							"expanded_url": "https://solplparty.com",
+							"display_url": "solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/Uq1kQ79SRI",
+							"expanded_url": "http://join.solpl.party",
+							"display_url": "join.solpl.party",
+							"indices": [
+								39,
+								62
+							]
+						},
+						{
+							"url": "https://t.co/OtYqQtyXgi",
+							"expanded_url": "http://about.solpl.party",
+							"display_url": "about.solpl.party",
+							"indices": [
+								73,
+								96
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 255,
+			"friends_count": 467,
+			"listed_count": 3,
+			"created_at": "Wed Feb 28 04:36:30 +0000 2018",
+			"favourites_count": 246,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 75545,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/968706450779463682/1519792691",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#워너뮤직코리아 #Warner_Music_Korea\n[TEASER] Milena 밀레나 - Night Train\nhttps://t.co/npLQrpWKWF"
+	},
+	{
 		"created_at": "Wed Nov 03 11:11:26 +0000 2021",
 		"id": 1455844349388877800,
 		"id_str": "1455855130180562956",
@@ -670,7 +811,7 @@ Grailbird.data.tweets_2021_11 =
 		"created_at": "Wed Nov 03 10:29:36 +0000 2021",
 		"id": 1455840671835705300,
 		"id_str": "1455844603215720450",
-		"full_text": "#Wilcox\n\n❤️‍🩹\n윌콕스 (Wilcox) - 50/50 MV \nhttps://t.co/eHuJSgB5mV \n\n#윌콕스 #5050 #fiftyfifty https://t.co/WWZZkldbtQ",
+		"full_text": "#Wilcox\n\n❤️🩹\n윌콕스 (Wilcox) - 50/50 MV \nhttps://t.co/eHuJSgB5mV \n\n#윌콕스 #5050 #fiftyfifty https://t.co/WWZZkldbtQ",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -901,7 +1042,7 @@ Grailbird.data.tweets_2021_11 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "#Wilcox\n\n❤️‍🩹\n윌콕스 (Wilcox) - 50/50 MV \nhttps://t.co/eHuJSgB5mV \n\n#윌콕스 #5050 #fiftyfifty https://t.co/WWZZkldbtQ"
+		"text": "#Wilcox\n\n❤️🩹\n윌콕스 (Wilcox) - 50/50 MV \nhttps://t.co/eHuJSgB5mV \n\n#윌콕스 #5050 #fiftyfifty https://t.co/WWZZkldbtQ"
 	},
 	{
 		"created_at": "Wed Nov 03 10:29:32 +0000 2021",
