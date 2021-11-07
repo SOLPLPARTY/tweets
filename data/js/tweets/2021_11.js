2118e73648d6012cad5d9a42db108ea9b048fcc2
솔플파티 <solplparty@gmail.com>
2021-11-08 01:17:36 +0900

    Added 1 tweets

--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Sun Nov 07 15:29:25 +0000 2021",
+		"id": 1457362283063955500,
+		"id_str": "1457369607593938945",
+		"full_text": "🎼 하루 끝 감성 라이브, &lt;LISTEN STAGE&gt; 11월의 라인업을 공개합니다.\n\n11월 25일(THU) 손태진\n\n*미스틱스토리 공식 유튜브 채널에서 저녁 7시 30분부터 실시간 스트리밍으로 보실 수 있습니다.\n\n#LISTENSTAGE #리슨스테이지 #미스틱커넥트 #손태진 https://t.co/79wd3PyQ9I",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			160
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LISTENSTAGE",
+					"indices": [
+						127,
+						139
+					]
+				},
+				{
+					"text": "리슨스테이지",
+					"indices": [
+						140,
+						147
+					]
+				},
+				{
+					"text": "미스틱커넥트",
+					"indices": [
+						148,
+						155
+					]
+				},
+				{
+					"text": "손태진",
+					"indices": [
+						156,
+						160
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1457362275179036700,
+					"id_str": "1457362275179036677",
+					"indices": [
+						161,
+						184
+					],
+					"media_url": "http://pbs.twimg.com/media/FDmXRJEaAAUxmir.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDmXRJEaAAUxmir.jpg",
+					"url": "https://t.co/79wd3PyQ9I",
+					"display_url": "pic.twitter.com/79wd3PyQ9I",
+					"expanded_url": "https://twitter.com/themysticstory/status/1457362283063955466/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1457362275179036700,
+					"id_str": "1457362275179036677",
+					"indices": [
+						161,
+						184
+					],
+					"media_url": "http://pbs.twimg.com/media/FDmXRJEaAAUxmir.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDmXRJEaAAUxmir.jpg",
+					"url": "https://t.co/79wd3PyQ9I",
+					"display_url": "pic.twitter.com/79wd3PyQ9I",
+					"expanded_url": "https://twitter.com/themysticstory/status/1457362283063955466/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/BKUIHw4jho",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BKUIHw4jho",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 26715,
+			"friends_count": 15,
+			"listed_count": 253,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6348,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1553488269",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 20,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "🎼 하루 끝 감성 라이브, &lt;LISTEN STAGE&gt; 11월의 라인업을 공개합니다.\n\n11월 25일(THU) 손태진\n\n*미스틱스토리 공식 유튜브 채널에서 저녁 7시 30분부터 실시간 스트리밍으로 보실 수 있습니다.\n\n#LISTENSTAGE #리슨스테이지 #미스틱커넥트 #손태진 https://t.co/79wd3PyQ9I"
+	},
+	{
 		"created_at": "Sun Nov 07 09:29:37 +0000 2021",
 		"id": 1457272856262242300,
 		"id_str": "1457279059176607746",
