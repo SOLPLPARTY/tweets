7512c5a0a5507e4a87cbdf3c5e5a69177d4276cc
솔플파티 <solplparty@gmail.com>
2021-11-16 15:17:39 +0900

    Added 1 tweets

--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,156 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Tue Nov 16 05:29:24 +0000 2021",
+		"id": 1460466212790366200,
+		"id_str": "1460480099845816325",
+		"full_text": "[NEWS] KYCC 온라인 자선 콘서트 개최…이기홍·#인순이 등 출연\n\n👉 https://t.co/lS8yOkNJCY\n\n#INSOONI #휴맵컨텐츠 #HUMAPCONTENTS #KYCC #온라인 #자선콘서트",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			116
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "INSOONI",
+					"indices": [
+						68,
+						76
+					]
+				},
+				{
+					"text": "휴맵컨텐츠",
+					"indices": [
+						77,
+						83
+					]
+				},
+				{
+					"text": "HUMAPCONTENTS",
+					"indices": [
+						84,
+						98
+					]
+				},
+				{
+					"text": "KYCC",
+					"indices": [
+						99,
+						104
+					]
+				},
+				{
+					"text": "온라인",
+					"indices": [
+						105,
+						109
+					]
+				},
+				{
+					"text": "자선콘서트",
+					"indices": [
+						110,
+						116
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/lS8yOkNJCY",
+					"expanded_url": "https://news.koreadaily.com/2021/11/15/society/community/20211115180925527.html",
+					"display_url": "news.koreadaily.com/2021/11/15/soc…",
+					"indices": [
+						43,
+						66
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1219506842935513000,
+			"id_str": "1219506842935513088",
+			"name": "휴맵컨텐츠(HUMAP)",
+			"screen_name": "humap_official",
+			"location": "",
+			"description": "휴맵컨텐츠(HUMAP CONTENTS) Official Twitter",
+			"url": "https://t.co/fBZZK3SmX6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fBZZK3SmX6",
+							"expanded_url": "http://www.humapcontents.com/",
+							"display_url": "humapcontents.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 393,
+			"friends_count": 2,
+			"listed_count": 2,
+			"created_at": "Tue Jan 21 06:28:27 +0000 2020",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 464,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1219506842935513088/1579755898",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[NEWS] KYCC 온라인 자선 콘서트 개최…이기홍·#인순이 등 출연\n\n👉 https://t.co/lS8yOkNJCY\n\n#INSOONI #휴맵컨텐츠 #HUMAPCONTENTS #KYCC #온라인 #자선콘서트"
+	},
+	{
 		"created_at": "Tue Nov 16 03:30:00 +0000 2021",
 		"id": 1460443816775729200,
 		"id_str": "1460450050589474817",
