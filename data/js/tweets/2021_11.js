c25dc079206a90e7ef42565022cca5d8928878ae
솔플파티 <solplparty@gmail.com>
2021-11-16 11:17:39 +0900

    Added 1 tweets

--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,170 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Tue Nov 16 01:29:31 +0000 2021",
+		"id": 1460417125097431000,
+		"id_str": "1460419727763853320",
+		"full_text": "[#News] #적재\n\n적재, ‘서로의 서로’ 발매…데뷔 첫 전국투어까지\n\n(출처: 스포츠동아 | 네이버 TV연예)\n\n✔️ https://t.co/lCPqew5v1w\n\n#Jukjae #서로의서로 #OneAnother\n#꿈 #SkyDream\n#20211116_6PM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			147
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "News",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "적재",
+					"indices": [
+						8,
+						11
+					]
+				},
+				{
+					"text": "Jukjae",
+					"indices": [
+						94,
+						101
+					]
+				},
+				{
+					"text": "서로의서로",
+					"indices": [
+						102,
+						108
+					]
+				},
+				{
+					"text": "OneAnother",
+					"indices": [
+						109,
+						120
+					]
+				},
+				{
+					"text": "꿈",
+					"indices": [
+						121,
+						123
+					]
+				},
+				{
+					"text": "SkyDream",
+					"indices": [
+						124,
+						133
+					]
+				},
+				{
+					"text": "20211116_6PM",
+					"indices": [
+						134,
+						147
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/lCPqew5v1w",
+					"expanded_url": "http://naver.me/FwNOaSDD",
+					"display_url": "naver.me/FwNOaSDD",
+					"indices": [
+						69,
+						92
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 266004552,
+			"id_str": "266004552",
+			"name": "안테나 Antenna",
+			"screen_name": "antennamusic",
+			"location": "대한민국 서울",
+			"description": "정재형, 유희열(Toy), 유재석, 루시드폴, 페퍼톤스, 박새별, 권진아, Sam Kim, 정승환, 이진아, 윤석철, 적재, 서동환 Official / 2021 적재 전국투어 '서로의 서로' - 서울       2021. 11. 19. ~ 2021. 11. 21. @ YES24 라이브홀",
+			"url": "https://t.co/XSWWwEPbf8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XSWWwEPbf8",
+							"expanded_url": "http://www.antenna.co.kr",
+							"display_url": "antenna.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 37520,
+			"friends_count": 10,
+			"listed_count": 375,
+			"created_at": "Mon Mar 14 13:49:27 +0000 2011",
+			"favourites_count": 14,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 4157,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266004552/1636948067",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13,
+		"favorite_count": 18,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#News] #적재\n\n적재, ‘서로의 서로’ 발매…데뷔 첫 전국투어까지\n\n(출처: 스포츠동아 | 네이버 TV연예)\n\n✔️ https://t.co/lCPqew5v1w\n\n#Jukjae #서로의서로 #OneAnother\n#꿈 #SkyDream\n#20211116_6PM"
+	},
+	{
 		"created_at": "Tue Nov 16 01:11:16 +0000 2021",
 		"id": 1460408018147823600,
 		"id_str": "1460415138910068736",
