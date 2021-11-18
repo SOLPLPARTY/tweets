91366abd9e1b451f0d71beb981780d8784c2cd4f
솔플파티 <solplparty@gmail.com>
2021-11-18 10:17:40 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 55370,
-	"created_at": "Wed Nov 17 15:17:39 +0000 2021",
+	"tweets": 55371,
+	"created_at": "Thu Nov 18 01:17:40 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2021_11.js",
 		"year": 2021,
 		"var_name": "tweets_2021_11",
-		"tweet_count": 604,
+		"tweet_count": 605,
 		"month": 11
 	},
 	{
