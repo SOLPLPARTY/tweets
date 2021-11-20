9bb386d4395f25afeb9b90815eef548d7663c748
솔플파티 <solplparty@gmail.com>
2021-11-21 01:17:41 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 55481,
-	"created_at": "Sat Nov 20 14:17:39 +0000 2021",
+	"tweets": 55482,
+	"created_at": "Sat Nov 20 16:17:40 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,196 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Sat Nov 20 15:29:25 +0000 2021",
+		"id": 1462073255812948000,
+		"id_str": "1462080646877261831",
+		"full_text": "[Congrats]\n⠀\nHappy 3rd Debut Anniversary\n⠀\n보라미유의 데뷔 3주년을 축하합니다🎉\n항상 응원해주신 팬분들께 감사드리며\n앞으로도 많은 사랑 부탁드려요💜\n\n#Happy_Boramiyu_Day\n#보라미유_데뷔3주년_축하해요 https://t.co/r9yCvCiH2B",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			139
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Happy_Boramiyu_Day",
+					"indices": [
+						103,
+						122
+					]
+				},
+				{
+					"text": "보라미유_데뷔3주년_축하해요",
+					"indices": [
+						123,
+						139
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1461940457672302600,
+					"id_str": "1461940457672302593",
+					"indices": [
+						140,
+						163
+					],
+					"media_url": "http://pbs.twimg.com/media/FEnbGb5UYAEkQ4P.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FEnbGb5UYAEkQ4P.jpg",
+					"url": "https://t.co/r9yCvCiH2B",
+					"display_url": "pic.twitter.com/r9yCvCiH2B",
+					"expanded_url": "https://twitter.com/shofar_ent/status/1462073255812947969/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1461940457672302600,
+					"id_str": "1461940457672302593",
+					"indices": [
+						140,
+						163
+					],
+					"media_url": "http://pbs.twimg.com/media/FEnbGb5UYAEkQ4P.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FEnbGb5UYAEkQ4P.jpg",
+					"url": "https://t.co/r9yCvCiH2B",
+					"display_url": "pic.twitter.com/r9yCvCiH2B",
+					"expanded_url": "https://twitter.com/shofar_ent/status/1462073255812947969/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 265792818,
+			"id_str": "265792818",
+			"name": "쇼파르엔터테인먼트",
+			"screen_name": "shofar_ent",
+			"location": "",
+			"description": "SHOFAR ENTERTAINMENT Official Twitter",
+			"url": "https://t.co/zzQk2iA0pQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zzQk2iA0pQ",
+							"expanded_url": "http://shofar-ent.com/",
+							"display_url": "shofar-ent.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7765,
+			"friends_count": 4,
+			"listed_count": 65,
+			"created_at": "Mon Mar 14 04:31:00 +0000 2011",
+			"favourites_count": 53,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4570,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1380084076573417474/i-NclciR_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1380084076573417474/i-NclciR_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/265792818/1636423956",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 15,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[Congrats]\n⠀\nHappy 3rd Debut Anniversary\n⠀\n보라미유의 데뷔 3주년을 축하합니다🎉\n항상 응원해주신 팬분들께 감사드리며\n앞으로도 많은 사랑 부탁드려요💜\n\n#Happy_Boramiyu_Day\n#보라미유_데뷔3주년_축하해요 https://t.co/r9yCvCiH2B"
+	},
+	{
 		"created_at": "Sat Nov 20 13:58:39 +0000 2021",
 		"id": 1462052918597673000,
 		"id_str": "1462057805184131078",
