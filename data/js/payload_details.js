5858648d704785c301037ef162a8adb62cf9c0e4
솔플파티 <solplparty@gmail.com>
2021-12-06 17:20:26 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 56065,
-	"created_at": "Mon Dec 06 07:20:16 +0000 2021",
+	"tweets": 56066,
+	"created_at": "Mon Dec 06 08:20:25 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,253 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Mon Dec 06 07:29:28 +0000 2021",
+		"id": 1467753373223256000,
+		"id_str": "1467758070239481866",
+		"full_text": "카더가든이 &lt;2021 슈퍼 스피드 뮤직 페스티벌&gt;에 출연합니다. \n\n📌 티켓 오픈 : 2021.12.09(목) 오후 3시 \n📌 예매처 : 인터파크 티켓(온라인/오프라인), 예스24(오프라인)\n\n자세한 사항은 각 예매처 페이지를 확인해주세요. https://t.co/8VLdX3fyQ5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			140
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1467753340704419800,
+					"id_str": "1467753340704419840",
+					"indices": [
+						141,
+						164
+					],
+					"media_url": "http://pbs.twimg.com/media/FF6B4wqUcAAQvu-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FF6B4wqUcAAQvu-.jpg",
+					"url": "https://t.co/8VLdX3fyQ5",
+					"display_url": "pic.twitter.com/8VLdX3fyQ5",
+					"expanded_url": "https://twitter.com/DRDRacompany/status/1467753373223256066/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1467753340704419800,
+					"id_str": "1467753340704419840",
+					"indices": [
+						141,
+						164
+					],
+					"media_url": "http://pbs.twimg.com/media/FF6B4wqUcAAQvu-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FF6B4wqUcAAQvu-.jpg",
+					"url": "https://t.co/8VLdX3fyQ5",
+					"display_url": "pic.twitter.com/8VLdX3fyQ5",
+					"expanded_url": "https://twitter.com/DRDRacompany/status/1467753373223256066/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1467753347897708500,
+					"id_str": "1467753347897708545",
+					"indices": [
+						141,
+						164
+					],
+					"media_url": "http://pbs.twimg.com/media/FF6B5LdVUAEZnjc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FF6B5LdVUAEZnjc.jpg",
+					"url": "https://t.co/8VLdX3fyQ5",
+					"display_url": "pic.twitter.com/8VLdX3fyQ5",
+					"expanded_url": "https://twitter.com/DRDRacompany/status/1467753373223256066/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1467753364796493800,
+					"id_str": "1467753364796493826",
+					"indices": [
+						141,
+						164
+					],
+					"media_url": "http://pbs.twimg.com/media/FF6B6KaUUAIqhOr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FF6B6KaUUAIqhOr.jpg",
+					"url": "https://t.co/8VLdX3fyQ5",
+					"display_url": "pic.twitter.com/8VLdX3fyQ5",
+					"expanded_url": "https://twitter.com/DRDRacompany/status/1467753373223256066/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 240145508,
+			"id_str": "240145508",
+			"name": "DooRooDooRoo Artist Company",
+			"screen_name": "DRDRacompany",
+			"location": "대한민국 서울",
+			"description": "두루두루 아티스트 컴퍼니",
+			"url": "https://t.co/y5yKgWQtSS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y5yKgWQtSS",
+							"expanded_url": "http://www.drdr.ac",
+							"display_url": "drdr.ac",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3971,
+			"friends_count": 9,
+			"listed_count": 0,
+			"created_at": "Wed Jan 19 07:40:00 +0000 2011",
+			"favourites_count": 82,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2881,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/958993158259404800/hnwLXo7-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/958993158259404800/hnwLXo7-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/240145508/1517471917",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "카더가든이 &lt;2021 슈퍼 스피드 뮤직 페스티벌&gt;에 출연합니다. \n\n📌 티켓 오픈 : 2021.12.09(목) 오후 3시 \n📌 예매처 : 인터파크 티켓(온라인/오프라인), 예스24(오프라인)\n\n자세한 사항은 각 예매처 페이지를 확인해주세요. https://t.co/8VLdX3fyQ5"
+	},
+	{
 		"created_at": "Mon Dec 06 06:30:10 +0000 2021",
 		"id": 1467732599787655200,
 		"id_str": "1467743146016653316",
