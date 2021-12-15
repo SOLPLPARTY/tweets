1620af18ba4d460c93c5506b919ffc6369d3ce11
솔플파티 <solplparty@gmail.com>
2021-12-16 00:17:41 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 56448,
-	"created_at": "Wed Dec 15 14:17:41 +0000 2021",
+	"tweets": 56449,
+	"created_at": "Wed Dec 15 15:17:40 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,265 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Wed Dec 15 14:29:25 +0000 2021",
+		"id": 1471117856943140900,
+		"id_str": "1471125244219990019",
+		"full_text": "YouTube Originals \"Re:MASTERPIECE\" Official Trailer\n\n👉 https://t.co/57Pit5S8sA\n\n2021.12.23 11PM KST\nSMTOWN YouTube channel\n\n#aespa #BoA #DreamsComeTrue\n#리마스터피스 #ReMASTERPIECE #YouTubeOriginals https://t.co/cDl02adl72",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			192
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "aespa",
+					"indices": [
+						124,
+						130
+					]
+				},
+				{
+					"text": "BoA",
+					"indices": [
+						131,
+						135
+					]
+				},
+				{
+					"text": "DreamsComeTrue",
+					"indices": [
+						136,
+						151
+					]
+				},
+				{
+					"text": "리마스터피스",
+					"indices": [
+						152,
+						159
+					]
+				},
+				{
+					"text": "ReMASTERPIECE",
+					"indices": [
+						160,
+						174
+					]
+				},
+				{
+					"text": "YouTubeOriginals",
+					"indices": [
+						175,
+						192
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/57Pit5S8sA",
+					"expanded_url": "http://youtu.be/xyQYSXYjbzo",
+					"display_url": "youtu.be/xyQYSXYjbzo",
+					"indices": [
+						55,
+						78
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1471051025331069000,
+					"id_str": "1471051025331068930",
+					"indices": [
+						193,
+						216
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1471051025331068930/pu/img/md6QLLRuqN_jSAQk.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1471051025331068930/pu/img/md6QLLRuqN_jSAQk.jpg",
+					"url": "https://t.co/cDl02adl72",
+					"display_url": "pic.twitter.com/cDl02adl72",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1471117856943140866/video/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1471051025331069000,
+					"id_str": "1471051025331068930",
+					"indices": [
+						193,
+						216
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1471051025331068930/pu/img/md6QLLRuqN_jSAQk.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1471051025331068930/pu/img/md6QLLRuqN_jSAQk.jpg",
+					"url": "https://t.co/cDl02adl72",
+					"display_url": "pic.twitter.com/cDl02adl72",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1471117856943140866/video/1",
+					"type": "video",
+					"sizes": {
+						"large": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							4,
+							5
+						],
+						"duration_millis": 49066,
+						"variants": [
+							{
+								"bitrate": 950000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1471051025331068930/pu/vid/480x600/uEqelcCY_MhVQfGz.mp4?tag=12"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1471051025331068930/pu/vid/320x400/zquUxOTkEvRK47NH.mp4?tag=12"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1471051025331068930/pu/vid/720x900/3Ouujr0wDJaJOivv.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1471051025331068930/pu/pl/hSFjcJfXACyqtdbB.m3u8?tag=12&container=fmp4&v=39d"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 884378714,
+			"id_str": "884378714",
+			"name": "SMTOWN",
+			"screen_name": "SMTOWNGLOBAL",
+			"location": "",
+			"description": "SMEntertainment Group Official Twitter",
+			"url": "https://t.co/zjb7YWlJE5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zjb7YWlJE5",
+							"expanded_url": "http://www.smtown.com",
+							"display_url": "smtown.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10302429,
+			"friends_count": 55,
+			"listed_count": 11289,
+			"created_at": "Tue Oct 16 12:49:45 +0000 2012",
+			"favourites_count": 2026,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 11366,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "F08BA0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1468958715566985226/eed5B2Wp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1468958715566985226/eed5B2Wp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/884378714/1639062025",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3064,
+		"favorite_count": 8615,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "YouTube Originals \"Re:MASTERPIECE\" Official Trailer\n\n👉 https://t.co/57Pit5S8sA\n\n2021.12.23 11PM KST\nSMTOWN YouTube channel\n\n#aespa #BoA #DreamsComeTrue\n#리마스터피스 #ReMASTERPIECE #YouTubeOriginals https://t.co/cDl02adl72"
+	},
+	{
 		"created_at": "Wed Dec 15 14:14:57 +0000 2021",
 		"id": 1471121607192203300,
 		"id_str": "1471121607192203265",
