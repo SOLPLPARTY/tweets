79a450f4e74624b749cf067eefae82e5a397560d
솔플파티 <solplparty@gmail.com>
2022-01-04 21:17:34 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 57185,
-	"created_at": "Tue Jan 04 11:17:33 +0000 2022",
+	"tweets": 57186,
+	"created_at": "Tue Jan 04 12:17:33 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,189 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Tue Jan 04 11:29:25 +0000 2022",
+		"id": 1478324482398126000,
+		"id_str": "1478327703354888194",
+		"full_text": "당기시오 단독 공연 “To Be Another Day”]\n당기시오 휴지기 전 마지막 단독공연 \"To Be Another Day\" in 대구, 서울\n\n▫일시\n- 대구: 2022년 01월 15일 (토) 밤 7시\n- 서울: 2022년 01월 16일 (일) 밤 7시\n\n상세 정보는 Instagram 또는 Facebook 페이지를 통해 확인 하실 수 있습니다.\n#당기시오 https://t.co/agk8bwgqcR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			202
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "당기시오",
+					"indices": [
+						197,
+						202
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1478324032147984400,
+					"id_str": "1478324032147984385",
+					"indices": [
+						203,
+						226
+					],
+					"media_url": "http://pbs.twimg.com/media/FIQP3-8akAECuKw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FIQP3-8akAECuKw.jpg",
+					"url": "https://t.co/agk8bwgqcR",
+					"display_url": "pic.twitter.com/agk8bwgqcR",
+					"expanded_url": "https://twitter.com/sugar_records/status/1478324482398126080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1478324032147984400,
+					"id_str": "1478324032147984385",
+					"indices": [
+						203,
+						226
+					],
+					"media_url": "http://pbs.twimg.com/media/FIQP3-8akAECuKw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FIQP3-8akAECuKw.jpg",
+					"url": "https://t.co/agk8bwgqcR",
+					"display_url": "pic.twitter.com/agk8bwgqcR",
+					"expanded_url": "https://twitter.com/sugar_records/status/1478324482398126080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 94343878,
+			"id_str": "94343878",
+			"name": "슈가레코드",
+			"screen_name": "sugar_records",
+			"location": "Seoul, KR",
+			"description": "#달에닿아 #뷰티핸섬 #리썬 #모브닝 #엔분의일 #당기시오 #우리같은사람들 슈가레코드",
+			"url": "https://t.co/MaZwhWbQNT",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/MaZwhWbQNT",
+							"expanded_url": "http://sugar-records.com",
+							"display_url": "sugar-records.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 576,
+			"friends_count": 38,
+			"listed_count": 9,
+			"created_at": "Thu Dec 03 14:51:49 +0000 2009",
+			"favourites_count": 444,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7566,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1087640124270075904/QfkBjdmp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1087640124270075904/QfkBjdmp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/94343878/1548148600",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "DFDFDF",
+			"profile_sidebar_fill_color": "F3F3F3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "당기시오 단독 공연 “To Be Another Day”]\n당기시오 휴지기 전 마지막 단독공연 \"To Be Another Day\" in 대구, 서울\n\n▫일시\n- 대구: 2022년 01월 15일 (토) 밤 7시\n- 서울: 2022년 01월 16일 (일) 밤 7시\n\n상세 정보는 Instagram 또는 Facebook 페이지를 통해 확인 하실 수 있습니다.\n#당기시오 https://t.co/agk8bwgqcR"
+	},
+	{
 		"created_at": "Tue Jan 04 11:11:29 +0000 2022",
 		"id": 1478315687148155000,
 		"id_str": "1478323190791348225",
