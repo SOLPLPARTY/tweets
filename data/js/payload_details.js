49020b5569a865e181742059f5dab490ef87ecc5
솔플파티 <solplparty@gmail.com>
2022-01-11 23:17:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 57383,
-	"created_at": "Tue Jan 11 12:17:36 +0000 2022",
+	"tweets": 57384,
+	"created_at": "Tue Jan 11 14:17:35 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Tue Jan 11 13:29:25 +0000 2022",
+		"id": 1480887222920482800,
+		"id_str": "1480894617600114691",
+		"full_text": "🎼 하루 끝 감성 라이브, 2022년 &lt;LISTEN STAGE&gt; 1월의 라인업을 공개합니다\n\n1월 27일(THU) 홍자\n\n*미스틱스토리 공식 유튜브 채널에서 저녁 7시 30분부터 실시간 스트리밍으로 보실 수 있습니다.\n\n#LISTENSTAGE #리슨스테이지 #미스틱커넥트 #홍자 #HONGJA https://t.co/k8Yhc4sfX2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			169
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LISTENSTAGE",
+					"indices": [
+						129,
+						141
+					]
+				},
+				{
+					"text": "리슨스테이지",
+					"indices": [
+						142,
+						149
+					]
+				},
+				{
+					"text": "미스틱커넥트",
+					"indices": [
+						150,
+						157
+					]
+				},
+				{
+					"text": "홍자",
+					"indices": [
+						158,
+						161
+					]
+				},
+				{
+					"text": "HONGJA",
+					"indices": [
+						162,
+						169
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1480817180031983600,
+					"id_str": "1480817180031983624",
+					"indices": [
+						170,
+						193
+					],
+					"media_url": "http://pbs.twimg.com/media/FIzrYSkaIAgG6s_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FIzrYSkaIAgG6s_.jpg",
+					"url": "https://t.co/k8Yhc4sfX2",
+					"display_url": "pic.twitter.com/k8Yhc4sfX2",
+					"expanded_url": "https://twitter.com/themysticstory/status/1480887222920482820/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1920,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1480817180031983600,
+					"id_str": "1480817180031983624",
+					"indices": [
+						170,
+						193
+					],
+					"media_url": "http://pbs.twimg.com/media/FIzrYSkaIAgG6s_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FIzrYSkaIAgG6s_.jpg",
+					"url": "https://t.co/k8Yhc4sfX2",
+					"display_url": "pic.twitter.com/k8Yhc4sfX2",
+					"expanded_url": "https://twitter.com/themysticstory/status/1480887222920482820/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1920,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/pgQyM4g8RF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pgQyM4g8RF",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 32733,
+			"friends_count": 15,
+			"listed_count": 269,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6588,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1641552189",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 23,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "🎼 하루 끝 감성 라이브, 2022년 &lt;LISTEN STAGE&gt; 1월의 라인업을 공개합니다\n\n1월 27일(THU) 홍자\n\n*미스틱스토리 공식 유튜브 채널에서 저녁 7시 30분부터 실시간 스트리밍으로 보실 수 있습니다.\n\n#LISTENSTAGE #리슨스테이지 #미스틱커넥트 #홍자 #HONGJA https://t.co/k8Yhc4sfX2"
+	},
+	{
 		"created_at": "Tue Jan 11 11:29:30 +0000 2022",
 		"id": 1480857020257357800,
 		"id_str": "1480864440471932929",
