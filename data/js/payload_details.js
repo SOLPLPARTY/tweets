60fb72fdb5afe1d35c91d55bd27d8752ad3582db
솔플파티 <solplparty@gmail.com>
2022-01-13 01:17:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 57412,
-	"created_at": "Wed Jan 12 15:17:34 +0000 2022",
+	"tweets": 57413,
+	"created_at": "Wed Jan 12 16:17:35 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Wed Jan 12 15:29:24 +0000 2022",
+		"id": 1481279836341432300,
+		"id_str": "1481287203099938818",
+		"full_text": "[#하람] 2022.01.13 HARAM DAY🎉\n\nHappy Birthday to HARAM🎂\n빌리 하람의 생일을 축하합니다🎁❤\n\n#220113 #happybirthday #birthday #생일 https://t.co/bYwqv7vyBR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			110
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "하람",
+					"indices": [
+						1,
+						4
+					]
+				},
+				{
+					"text": "happybirthday",
+					"indices": [
+						82,
+						96
+					]
+				},
+				{
+					"text": "birthday",
+					"indices": [
+						97,
+						106
+					]
+				},
+				{
+					"text": "생일",
+					"indices": [
+						107,
+						110
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1481279809091096600,
+					"id_str": "1481279809091096578",
+					"indices": [
+						111,
+						134
+					],
+					"media_url": "http://pbs.twimg.com/media/FI6QI2HVUAIdljk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FI6QI2HVUAIdljk.jpg",
+					"url": "https://t.co/bYwqv7vyBR",
+					"display_url": "pic.twitter.com/bYwqv7vyBR",
+					"expanded_url": "https://twitter.com/themysticstory/status/1481279836341432322/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1920,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1481279809091096600,
+					"id_str": "1481279809091096578",
+					"indices": [
+						111,
+						134
+					],
+					"media_url": "http://pbs.twimg.com/media/FI6QI2HVUAIdljk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FI6QI2HVUAIdljk.jpg",
+					"url": "https://t.co/bYwqv7vyBR",
+					"display_url": "pic.twitter.com/bYwqv7vyBR",
+					"expanded_url": "https://twitter.com/themysticstory/status/1481279836341432322/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1920,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/pgQyM4g8RF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pgQyM4g8RF",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 32766,
+			"friends_count": 15,
+			"listed_count": 275,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6592,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1641552189",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1689,
+		"favorite_count": 3476,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#하람] 2022.01.13 HARAM DAY🎉\n\nHappy Birthday to HARAM🎂\n빌리 하람의 생일을 축하합니다🎁❤\n\n#220113 #happybirthday #birthday #생일 https://t.co/bYwqv7vyBR"
+	},
+	{
 		"created_at": "Wed Jan 12 15:16:06 +0000 2022",
 		"id": 1481283853201424400,
 		"id_str": "1481283853201424398",
