47933c5aa4abee6cbad99362d8b6e01cdb07aa1f
솔플파티 <solplparty@gmail.com>
2022-01-15 15:17:37 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 57476,
-	"created_at": "Sat Jan 15 04:17:36 +0000 2022",
+	"tweets": 57477,
+	"created_at": "Sat Jan 15 06:17:37 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,10 +1,151 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Sat Jan 15 06:01:37 +0000 2022",
+		"id": 1482231477597315000,
+		"id_str": "1482231477597315077",
+		"full_text": "#유니버설뮤직코리아 #Universal_Music_Korea\n반응 난리난 가장 완벽하고 황홀한 듀엣 OST💛| Ansel Elgort, Rachel Zegler - Balcony Scene (Tonight) (가사/한글/해석)\nhttps://t.co/BlHC9fhSn5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			150
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "유니버설뮤직코리아",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"text": "Universal_Music_Korea",
+					"indices": [
+						11,
+						33
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/BlHC9fhSn5",
+					"expanded_url": "https://youtu.be/SIsINCA50Fo",
+					"display_url": "youtu.be/SIsINCA50Fo",
+					"indices": [
+						127,
+						150
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.powerapps.com\" rel=\"nofollow\">Microsoft Power Platform</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 968706450779463700,
+			"id_str": "968706450779463682",
+			"name": "솔플파티",
+			"screen_name": "SOLPLPARTY",
+			"location": "문의: contact@solplparty.com",
+			"description": "혼자서도 즐겁게! \n문화예술 커뮤니티, 솔플파티 🤘 \n\n▶ 참가하기 \nhttps://t.co/Uq1kQ79SRI \n\n❓ ABOUT\nhttps://t.co/OtYqQtyXgi ",
+			"url": "https://t.co/L6Ap35US8u",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/L6Ap35US8u",
+							"expanded_url": "https://solplparty.com",
+							"display_url": "solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/Uq1kQ79SRI",
+							"expanded_url": "http://join.solpl.party",
+							"display_url": "join.solpl.party",
+							"indices": [
+								39,
+								62
+							]
+						},
+						{
+							"url": "https://t.co/OtYqQtyXgi",
+							"expanded_url": "http://about.solpl.party",
+							"display_url": "about.solpl.party",
+							"indices": [
+								73,
+								96
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 253,
+			"friends_count": 466,
+			"listed_count": 0,
+			"created_at": "Wed Feb 28 04:36:30 +0000 2018",
+			"favourites_count": 240,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 78739,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/968706450779463682/1519792691",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#유니버설뮤직코리아 #Universal_Music_Korea\n반응 난리난 가장 완벽하고 황홀한 듀엣 OST💛| Ansel Elgort, Rachel Zegler - Balcony Scene (Tonight) (가사/한글/해석)\nhttps://t.co/BlHC9fhSn5"
+	},
+	{
 		"created_at": "Sat Jan 15 04:11:17 +0000 2022",
 		"id": 1482193322785529900,
 		"id_str": "1482203711137062912",
-		"full_text": "1월, 당신의 일상에 작은 쉼표를 남길 두 가지 🧚‍♀️\n\n슬로우 디자인 신규입고 소식입니다!\n\n🔸 키토부 \n- 종이책갈피\n\n🔹 폴카랩 \n- 2022 아줄레주 캘린더\n- 아줄레주 타일 엽서\n- 아줄레주 카드 들깨꽃\n\n📌 인스타그램에서 자세히 보기\nhttps://t.co/jbbmAzqX93 https://t.co/Cdzb9YYtWN",
+		"full_text": "1월, 당신의 일상에 작은 쉼표를 남길 두 가지 🧚♀️\n\n슬로우 디자인 신규입고 소식입니다!\n\n🔸 키토부 \n- 종이책갈피\n\n🔹 폴카랩 \n- 2022 아줄레주 캘린더\n- 아줄레주 타일 엽서\n- 아줄레주 카드 들깨꽃\n\n📌 인스타그램에서 자세히 보기\nhttps://t.co/jbbmAzqX93 https://t.co/Cdzb9YYtWN",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -291,7 +432,7 @@ Grailbird.data.tweets_2022_01 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "1월, 당신의 일상에 작은 쉼표를 남길 두 가지 🧚‍♀️\n\n슬로우 디자인 신규입고 소식입니다!\n\n🔸 키토부 \n- 종이책갈피\n\n🔹 폴카랩 \n- 2022 아줄레주 캘린더\n- 아줄레주 타일 엽서\n- 아줄레주 카드 들깨꽃\n\n📌 인스타그램에서 자세히 보기\nhttps://t.co/jbbmAzqX93 https://t.co/Cdzb9YYtWN"
+		"text": "1월, 당신의 일상에 작은 쉼표를 남길 두 가지 🧚♀️\n\n슬로우 디자인 신규입고 소식입니다!\n\n🔸 키토부 \n- 종이책갈피\n\n🔹 폴카랩 \n- 2022 아줄레주 캘린더\n- 아줄레주 타일 엽서\n- 아줄레주 카드 들깨꽃\n\n📌 인스타그램에서 자세히 보기\nhttps://t.co/jbbmAzqX93 https://t.co/Cdzb9YYtWN"
 	},
 	{
 		"created_at": "Sat Jan 15 03:11:33 +0000 2022",
