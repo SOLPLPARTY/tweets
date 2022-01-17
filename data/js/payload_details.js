cec01164bd4f84960b18d826cc9dd431768e6ce0
솔플파티 <solplparty@gmail.com>
2022-01-17 22:17:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 57536,
-	"created_at": "Mon Jan 17 12:17:36 +0000 2022",
+	"tweets": 57537,
+	"created_at": "Mon Jan 17 13:17:37 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Mon Jan 17 12:29:24 +0000 2022",
+		"id": 1483046170830119000,
+		"id_str": "1483053844422615042",
+		"full_text": "&lt;모브닝 단독 콘서트 '사랑을 위하여' : 롤링 27주년 기념 공연&gt;\n· 일시 : \n2022년 02월 25일 (금) 오후 8시\n2022년 02월 26일 (토) 오후 5시\n· 장소 : 롤링홀\n· 출연 : 모브닝(MOVNING)\n· 예매 : 멜론티켓 2022년 01월 24일 (월) 오후 8시 예매오픈\n· 가격 : 예매 77,000원 / 당일 현장구매 88,000원 https://t.co/8eI3Y9q8Fz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			208
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1483045731455754200,
+					"id_str": "1483045731455754242",
+					"indices": [
+						209,
+						232
+					],
+					"media_url": "http://pbs.twimg.com/media/FJTWPDkUYAISDMs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJTWPDkUYAISDMs.jpg",
+					"url": "https://t.co/8eI3Y9q8Fz",
+					"display_url": "pic.twitter.com/8eI3Y9q8Fz",
+					"expanded_url": "https://twitter.com/sugar_records/status/1483046170830118917/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 679,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1079,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1079,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1483045731455754200,
+					"id_str": "1483045731455754242",
+					"indices": [
+						209,
+						232
+					],
+					"media_url": "http://pbs.twimg.com/media/FJTWPDkUYAISDMs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJTWPDkUYAISDMs.jpg",
+					"url": "https://t.co/8eI3Y9q8Fz",
+					"display_url": "pic.twitter.com/8eI3Y9q8Fz",
+					"expanded_url": "https://twitter.com/sugar_records/status/1483046170830118917/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 679,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1079,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1079,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 94343878,
+			"id_str": "94343878",
+			"name": "슈가레코드",
+			"screen_name": "sugar_records",
+			"location": "Seoul, KR",
+			"description": "#달에닿아 #뷰티핸섬 #리썬 #모브닝 #엔분의일 #당기시오 #우리같은사람들 슈가레코드",
+			"url": "https://t.co/MaZwhWbQNT",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/MaZwhWbQNT",
+							"expanded_url": "http://sugar-records.com",
+							"display_url": "sugar-records.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 577,
+			"friends_count": 38,
+			"listed_count": 10,
+			"created_at": "Thu Dec 03 14:51:49 +0000 2009",
+			"favourites_count": 446,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7570,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1087640124270075904/QfkBjdmp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1087640124270075904/QfkBjdmp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/94343878/1548148600",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "DFDFDF",
+			"profile_sidebar_fill_color": "F3F3F3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "&lt;모브닝 단독 콘서트 '사랑을 위하여' : 롤링 27주년 기념 공연&gt;\n· 일시 : \n2022년 02월 25일 (금) 오후 8시\n2022년 02월 26일 (토) 오후 5시\n· 장소 : 롤링홀\n· 출연 : 모브닝(MOVNING)\n· 예매 : 멜론티켓 2022년 01월 24일 (월) 오후 8시 예매오픈\n· 가격 : 예매 77,000원 / 당일 현장구매 88,000원 https://t.co/8eI3Y9q8Fz"
+	},
+	{
 		"created_at": "Mon Jan 17 11:29:25 +0000 2022",
 		"id": 1483031369840468000,
 		"id_str": "1483038745561608196",
