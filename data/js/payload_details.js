71d97672603fbb9eac4480487a193adbd57764b5
솔플파티 <solplparty@gmail.com>
2022-01-20 17:17:40 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 57617,
-	"created_at": "Thu Jan 20 07:17:38 +0000 2022",
+	"tweets": 57618,
+	"created_at": "Thu Jan 20 08:17:39 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,277 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Thu Jan 20 07:29:32 +0000 2022",
+		"id": 1484057264382410800,
+		"id_str": "1484065540217921537",
+		"full_text": "휘인(Whee In)의 인사 영상이 도착했습니다😎\n2nd Mini Album [WHEE] 를 지금 바로 음원 사이트에서 만나보세요!\n\n✨https://t.co/VymJaSukBQ \n⠀\n#warnerkpop #warnermusic #워너뮤직케이팝 #휘인 #WheeIn #WHEE #오묘해 #더라이브레이블 @THEL1VE_LABEL https://t.co/441PKoaxH7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			183
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "warnerkpop",
+					"indices": [
+						103,
+						114
+					]
+				},
+				{
+					"text": "warnermusic",
+					"indices": [
+						115,
+						127
+					]
+				},
+				{
+					"text": "워너뮤직케이팝",
+					"indices": [
+						128,
+						136
+					]
+				},
+				{
+					"text": "휘인",
+					"indices": [
+						137,
+						140
+					]
+				},
+				{
+					"text": "WheeIn",
+					"indices": [
+						141,
+						148
+					]
+				},
+				{
+					"text": "WHEE",
+					"indices": [
+						149,
+						154
+					]
+				},
+				{
+					"text": "오묘해",
+					"indices": [
+						155,
+						159
+					]
+				},
+				{
+					"text": "더라이브레이블",
+					"indices": [
+						160,
+						168
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "THEL1VE_LABEL",
+					"name": "THEL1VE_OFFICIAL",
+					"id": 1413012874654457900,
+					"id_str": "1413012874654457858",
+					"indices": [
+						169,
+						183
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/VymJaSukBQ",
+					"expanded_url": "http://Wheein.lnk.to/Wheein",
+					"display_url": "Wheein.lnk.to/Wheein",
+					"indices": [
+						76,
+						99
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1484057008915775500,
+					"id_str": "1484057008915775491",
+					"indices": [
+						184,
+						207
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1484057008915775491/pu/img/AjJ3Z3034WLepStF.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1484057008915775491/pu/img/AjJ3Z3034WLepStF.jpg",
+					"url": "https://t.co/441PKoaxH7",
+					"display_url": "pic.twitter.com/441PKoaxH7",
+					"expanded_url": "https://twitter.com/Warner_Music_Kr/status/1484057264382410752/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1484057008915775500,
+					"id_str": "1484057008915775491",
+					"indices": [
+						184,
+						207
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1484057008915775491/pu/img/AjJ3Z3034WLepStF.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1484057008915775491/pu/img/AjJ3Z3034WLepStF.jpg",
+					"url": "https://t.co/441PKoaxH7",
+					"display_url": "pic.twitter.com/441PKoaxH7",
+					"expanded_url": "https://twitter.com/Warner_Music_Kr/status/1484057264382410752/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 42208,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1484057008915775491/pu/vid/540x540/MGbpV4umr0dz8T7P.mp4?tag=12"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1484057008915775491/pu/vid/320x320/qZ2kLkMH-9ZfU2ee.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1484057008915775491/pu/pl/1GvEbK0H6zRKKH5S.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1484057008915775491/pu/vid/720x720/muIioOIIU0bTBVnD.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 206117108,
+			"id_str": "206117108",
+			"name": "Warner Music Korea",
+			"screen_name": "Warner_Music_Kr",
+			"location": "Seoul, Korea",
+			"description": "워너뮤직코리아 공식 트위터",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 53886,
+			"friends_count": 494,
+			"listed_count": 198,
+			"created_at": "Fri Oct 22 09:32:47 +0000 2010",
+			"favourites_count": 1621,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 11404,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1267377694565490688/FD-WdFum_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1267377694565490688/FD-WdFum_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/206117108/1636703401",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "023BE5",
+			"profile_text_color": "2566FC",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 929,
+		"favorite_count": 1543,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "휘인(Whee In)의 인사 영상이 도착했습니다😎\n2nd Mini Album [WHEE] 를 지금 바로 음원 사이트에서 만나보세요!\n\n✨https://t.co/VymJaSukBQ \n⠀\n#warnerkpop #warnermusic #워너뮤직케이팝 #휘인 #WheeIn #WHEE #오묘해 #더라이브레이블 @THEL1VE_LABEL https://t.co/441PKoaxH7"
+	},
+	{
 		"created_at": "Thu Jan 20 07:15:30 +0000 2022",
 		"id": 1484062009469677600,
 		"id_str": "1484062009469677572",
