af50669ea49aaf9260874b79fe8b65426e608503
솔플파티 <solplparty@gmail.com>
2022-01-25 20:20:30 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 57752,
-	"created_at": "Tue Jan 25 10:17:39 +0000 2022",
+	"tweets": 57753,
+	"created_at": "Tue Jan 25 11:20:29 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,300 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Tue Jan 25 10:29:33 +0000 2022",
+		"id": 1485915354526371800,
+		"id_str": "1485922784136114185",
+		"full_text": "[#Teaser] #이진아\n\n이진아 싱글 ‘람팜팜(Rum Pum Pum)’\nLee Jin Ah Single ‘Rum Pum Pum’\n\n💗 Highlight Medley\nhttps://t.co/JGjKTSv9vL\n\n2022. 01. 28. 6PM Release\n\n#LeeJinAh #람팜팜 #RumPumPum\n#먹고싶은것도많아 #RhythmofHope #choco\n#DPRCREAM #SOLE\n#20220128_6PM https://t.co/DdEeO8iG6u",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			232
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Teaser",
+					"indices": [
+						1,
+						8
+					]
+				},
+				{
+					"text": "이진아",
+					"indices": [
+						10,
+						14
+					]
+				},
+				{
+					"text": "LeeJinAh",
+					"indices": [
+						146,
+						155
+					]
+				},
+				{
+					"text": "람팜팜",
+					"indices": [
+						156,
+						160
+					]
+				},
+				{
+					"text": "RumPumPum",
+					"indices": [
+						161,
+						171
+					]
+				},
+				{
+					"text": "먹고싶은것도많아",
+					"indices": [
+						172,
+						181
+					]
+				},
+				{
+					"text": "RhythmofHope",
+					"indices": [
+						182,
+						195
+					]
+				},
+				{
+					"text": "choco",
+					"indices": [
+						196,
+						202
+					]
+				},
+				{
+					"text": "DPRCREAM",
+					"indices": [
+						203,
+						212
+					]
+				},
+				{
+					"text": "SOLE",
+					"indices": [
+						213,
+						218
+					]
+				},
+				{
+					"text": "20220128_6PM",
+					"indices": [
+						219,
+						232
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/JGjKTSv9vL",
+					"expanded_url": "https://youtu.be/WtB2YvFbkrg",
+					"display_url": "youtu.be/WtB2YvFbkrg",
+					"indices": [
+						94,
+						117
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1485888551472726000,
+					"id_str": "1485888551472726024",
+					"indices": [
+						233,
+						256
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1485888551472726024/pu/img/n_JwcfBTeETfWRyN.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1485888551472726024/pu/img/n_JwcfBTeETfWRyN.jpg",
+					"url": "https://t.co/DdEeO8iG6u",
+					"display_url": "pic.twitter.com/DdEeO8iG6u",
+					"expanded_url": "https://twitter.com/antennamusic/status/1485915354526371844/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1485888551472726000,
+					"id_str": "1485888551472726024",
+					"indices": [
+						233,
+						256
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1485888551472726024/pu/img/n_JwcfBTeETfWRyN.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1485888551472726024/pu/img/n_JwcfBTeETfWRyN.jpg",
+					"url": "https://t.co/DdEeO8iG6u",
+					"display_url": "pic.twitter.com/DdEeO8iG6u",
+					"expanded_url": "https://twitter.com/antennamusic/status/1485915354526371844/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 70000,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1485888551472726024/pu/vid/640x360/B6N-gO2FPUvxJhWP.mp4?tag=12"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1485888551472726024/pu/vid/480x270/OnHdDYbXYvYbdSf4.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1485888551472726024/pu/pl/G-qp4RsU9HZWsw0u.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1485888551472726024/pu/vid/1280x720/FZNTK43tmwb6vNEp.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 266004552,
+			"id_str": "266004552",
+			"name": "안테나 Antenna",
+			"screen_name": "antennamusic",
+			"location": "대한민국 서울",
+			"description": "유희열(Toy), 유재석, 정재형, 루시드폴, 페퍼톤스(신재평&이장원), 박새별, 샘김, 이진아, 권진아, 정승환, 윤석철, 적재, 이미주, 서동환, 황현조 Official / 이진아 싱글 ‘람팜팜(Rum Pum Pum)’ 2022. 01. 28. 6PM Release💗",
+			"url": "https://t.co/XSWWwEx210",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XSWWwEx210",
+							"expanded_url": "http://www.antenna.co.kr",
+							"display_url": "antenna.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 40272,
+			"friends_count": 10,
+			"listed_count": 385,
+			"created_at": "Mon Mar 14 13:49:27 +0000 2011",
+			"favourites_count": 14,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 4567,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266004552/1643018512",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 45,
+		"favorite_count": 60,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#Teaser] #이진아\n\n이진아 싱글 ‘람팜팜(Rum Pum Pum)’\nLee Jin Ah Single ‘Rum Pum Pum’\n\n💗 Highlight Medley\nhttps://t.co/JGjKTSv9vL\n\n2022. 01. 28. 6PM Release\n\n#LeeJinAh #람팜팜 #RumPumPum\n#먹고싶은것도많아 #RhythmofHope #choco\n#DPRCREAM #SOLE\n#20220128_6PM https://t.co/DdEeO8iG6u"
+	},
+	{
 		"created_at": "Tue Jan 25 09:29:40 +0000 2022",
 		"id": 1485900426126192600,
 		"id_str": "1485907714610020355",
