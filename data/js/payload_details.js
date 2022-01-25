c596b8525fe9d0402307aae5cd0c4001439db149
솔플파티 <solplparty@gmail.com>
2022-01-25 23:17:40 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 57753,
-	"created_at": "Tue Jan 25 11:20:29 +0000 2022",
+	"tweets": 57754,
+	"created_at": "Tue Jan 25 14:17:39 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Tue Jan 25 13:29:24 +0000 2022",
+		"id": 1485964229223927800,
+		"id_str": "1485968046757928965",
+		"full_text": "부산에 계신 팬 여러분 많이 기다리셨죠? \n모브닝 부산 공연 \"사랑을 위하여\"\n\n공연 날짜 : 2월 19일 (토요일)\n공연 장소 : 금사락 (부산광역시 금정구 개좌로 162)\n공연 시간 : 오후 7시\n티켓 오픈 : 1월 28일 오후 8시 (멜론 티켓)\n예매 페이지 오픈 : 1월 27일 오후 8시\n\n공연 문의 | 010-2232-6612 https://t.co/zHArH3EHk7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			188
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1485963925988380700,
+					"id_str": "1485963925988380678",
+					"indices": [
+						189,
+						212
+					],
+					"media_url": "http://pbs.twimg.com/media/FJ80UVWVEAYjXFu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJ80UVWVEAYjXFu.jpg",
+					"url": "https://t.co/zHArH3EHk7",
+					"display_url": "pic.twitter.com/zHArH3EHk7",
+					"expanded_url": "https://twitter.com/sugar_records/status/1485964229223927810/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1485963925988380700,
+					"id_str": "1485963925988380678",
+					"indices": [
+						189,
+						212
+					],
+					"media_url": "http://pbs.twimg.com/media/FJ80UVWVEAYjXFu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJ80UVWVEAYjXFu.jpg",
+					"url": "https://t.co/zHArH3EHk7",
+					"display_url": "pic.twitter.com/zHArH3EHk7",
+					"expanded_url": "https://twitter.com/sugar_records/status/1485964229223927810/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 94343878,
+			"id_str": "94343878",
+			"name": "슈가레코드",
+			"screen_name": "sugar_records",
+			"location": "Seoul, KR",
+			"description": "#달에닿아 #뷰티핸섬 #리썬 #모브닝 #엔분의일 #당기시오 #우리같은사람들 슈가레코드",
+			"url": "https://t.co/MaZwhWbQNT",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/MaZwhWbQNT",
+							"expanded_url": "http://sugar-records.com",
+							"display_url": "sugar-records.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 581,
+			"friends_count": 38,
+			"listed_count": 9,
+			"created_at": "Thu Dec 03 14:51:49 +0000 2009",
+			"favourites_count": 446,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7572,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1087640124270075904/QfkBjdmp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1087640124270075904/QfkBjdmp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/94343878/1548148600",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "DFDFDF",
+			"profile_sidebar_fill_color": "F3F3F3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "부산에 계신 팬 여러분 많이 기다리셨죠? \n모브닝 부산 공연 \"사랑을 위하여\"\n\n공연 날짜 : 2월 19일 (토요일)\n공연 장소 : 금사락 (부산광역시 금정구 개좌로 162)\n공연 시간 : 오후 7시\n티켓 오픈 : 1월 28일 오후 8시 (멜론 티켓)\n예매 페이지 오픈 : 1월 27일 오후 8시\n\n공연 문의 | 010-2232-6612 https://t.co/zHArH3EHk7"
+	},
+	{
 		"created_at": "Tue Jan 25 10:29:33 +0000 2022",
 		"id": 1485915354526371800,
 		"id_str": "1485922784136114185",
