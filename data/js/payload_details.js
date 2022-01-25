3a1bc7141af57b2d159020d0ed81504887788f3f
솔플파티 <solplparty@gmail.com>
2022-01-26 01:17:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 57754,
-	"created_at": "Tue Jan 25 14:17:39 +0000 2022",
+	"tweets": 57755,
+	"created_at": "Tue Jan 25 16:17:37 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,224 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Tue Jan 25 15:29:24 +0000 2022",
+		"id": 1485990851071332400,
+		"id_str": "1485998245826469890",
+		"full_text": "[#HBD🎂] #적재\n\n2022년 1월 26일\n적재의 생일을 축하합니다🎉\n\n#Jukjae\n#2022_0126 #HappyJukjaeDay\n#천재만재_정적재_적말_생일축하해\n#ShiningMyJukjae https://t.co/5JEoqhx5pR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			112
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HBD",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "적재",
+					"indices": [
+						8,
+						11
+					]
+				},
+				{
+					"text": "Jukjae",
+					"indices": [
+						42,
+						49
+					]
+				},
+				{
+					"text": "HappyJukjaeDay",
+					"indices": [
+						61,
+						76
+					]
+				},
+				{
+					"text": "천재만재_정적재_적말_생일축하해",
+					"indices": [
+						77,
+						95
+					]
+				},
+				{
+					"text": "ShiningMyJukjae",
+					"indices": [
+						96,
+						112
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1485915330966786000,
+					"id_str": "1485915330966786051",
+					"indices": [
+						113,
+						136
+					],
+					"media_url": "http://pbs.twimg.com/media/FJ8IHuyVEAMi_h_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJ8IHuyVEAMi_h_.jpg",
+					"url": "https://t.co/5JEoqhx5pR",
+					"display_url": "pic.twitter.com/5JEoqhx5pR",
+					"expanded_url": "https://twitter.com/antennamusic/status/1485990851071332355/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1199,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2047,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1485915330966786000,
+					"id_str": "1485915330966786051",
+					"indices": [
+						113,
+						136
+					],
+					"media_url": "http://pbs.twimg.com/media/FJ8IHuyVEAMi_h_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJ8IHuyVEAMi_h_.jpg",
+					"url": "https://t.co/5JEoqhx5pR",
+					"display_url": "pic.twitter.com/5JEoqhx5pR",
+					"expanded_url": "https://twitter.com/antennamusic/status/1485990851071332355/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1199,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2047,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 266004552,
+			"id_str": "266004552",
+			"name": "안테나 Antenna",
+			"screen_name": "antennamusic",
+			"location": "대한민국 서울",
+			"description": "유희열(Toy), 유재석, 정재형, 루시드폴, 페퍼톤스(신재평&이장원), 박새별, 샘김, 이진아, 권진아, 정승환, 윤석철, 적재, 이미주, 서동환, 황현조 Official / 이진아 싱글 ‘람팜팜(Rum Pum Pum)’ 2022. 01. 28. 6PM Release💗",
+			"url": "https://t.co/XSWWwEx210",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XSWWwEx210",
+							"expanded_url": "http://www.antenna.co.kr",
+							"display_url": "antenna.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 40276,
+			"friends_count": 10,
+			"listed_count": 386,
+			"created_at": "Mon Mar 14 13:49:27 +0000 2011",
+			"favourites_count": 14,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 4568,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266004552/1643018512",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 160,
+		"favorite_count": 231,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#HBD🎂] #적재\n\n2022년 1월 26일\n적재의 생일을 축하합니다🎉\n\n#Jukjae\n#2022_0126 #HappyJukjaeDay\n#천재만재_정적재_적말_생일축하해\n#ShiningMyJukjae https://t.co/5JEoqhx5pR"
+	},
+	{
 		"created_at": "Tue Jan 25 13:29:24 +0000 2022",
 		"id": 1485964229223927800,
 		"id_str": "1485968046757928965",
