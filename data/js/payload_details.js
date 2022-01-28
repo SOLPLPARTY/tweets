3e6ca3850ce051a0be3bb8291db4b2a214138277
솔플파티 <solplparty@gmail.com>
2022-01-29 02:17:41 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 57830,
-	"created_at": "Fri Jan 28 14:17:41 +0000 2022",
+	"tweets": 57831,
+	"created_at": "Fri Jan 28 17:17:40 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,270 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Fri Jan 28 16:29:25 +0000 2022",
+		"id": 1487099844783382500,
+		"id_str": "1487100509198692361",
+		"full_text": "[#Photo] #이진아\n\n오늘 발매된 이진아 ‘람팜팜’의 첫 라이브를 들려드린 유희열의 스케치북💗 이진아가 부르는 희망의 주문 “람팜팜 팜팜 팜팜팜🎵”과 함께 행복한 연휴의 시작 보내시길 바라요!🧸❣️\n\n🔗 https://t.co/g2aptShYkV\n\n#LeeJinAh #람팜팜 #RumPumPum\n#유희열의스케치북 https://t.co/MGVKjuCVIT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			177
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Photo",
+					"indices": [
+						1,
+						7
+					]
+				},
+				{
+					"text": "이진아",
+					"indices": [
+						9,
+						13
+					]
+				},
+				{
+					"text": "LeeJinAh",
+					"indices": [
+						142,
+						151
+					]
+				},
+				{
+					"text": "람팜팜",
+					"indices": [
+						152,
+						156
+					]
+				},
+				{
+					"text": "RumPumPum",
+					"indices": [
+						157,
+						167
+					]
+				},
+				{
+					"text": "유희열의스케치북",
+					"indices": [
+						168,
+						177
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/g2aptShYkV",
+					"expanded_url": "http://naver.me/xXPBV4PL",
+					"display_url": "naver.me/xXPBV4PL",
+					"indices": [
+						117,
+						140
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1487099837384622000,
+					"id_str": "1487099837384622080",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/media/FKM9bFDaAAAOLTn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKM9bFDaAAAOLTn.jpg",
+					"url": "https://t.co/MGVKjuCVIT",
+					"display_url": "pic.twitter.com/MGVKjuCVIT",
+					"expanded_url": "https://twitter.com/antennamusic/status/1487099844783382528/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1487099837384622000,
+					"id_str": "1487099837384622080",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/media/FKM9bFDaAAAOLTn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKM9bFDaAAAOLTn.jpg",
+					"url": "https://t.co/MGVKjuCVIT",
+					"display_url": "pic.twitter.com/MGVKjuCVIT",
+					"expanded_url": "https://twitter.com/antennamusic/status/1487099844783382528/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1487099837585985500,
+					"id_str": "1487099837585985540",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/media/FKM9bFzakAQb26l.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKM9bFzakAQb26l.jpg",
+					"url": "https://t.co/MGVKjuCVIT",
+					"display_url": "pic.twitter.com/MGVKjuCVIT",
+					"expanded_url": "https://twitter.com/antennamusic/status/1487099844783382528/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 266004552,
+			"id_str": "266004552",
+			"name": "안테나 Antenna",
+			"screen_name": "antennamusic",
+			"location": "대한민국 서울",
+			"description": "유희열(Toy), 유재석, 정재형, 루시드폴, 페퍼톤스(신재평&이장원), 박새별, 샘김, 이진아, 권진아, 정승환, 윤석철, 적재, 이미주, 서동환, 황현조 Official / 이진아 싱글 ‘람팜팜(Rum Pum Pum)’ 2022. 01. 28. 6PM Release💗",
+			"url": "https://t.co/XSWWwEx210",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XSWWwEx210",
+							"expanded_url": "http://www.antenna.co.kr",
+							"display_url": "antenna.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 40293,
+			"friends_count": 10,
+			"listed_count": 385,
+			"created_at": "Mon Mar 14 13:49:27 +0000 2011",
+			"favourites_count": 14,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 4585,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266004552/1643018512",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14,
+		"favorite_count": 33,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#Photo] #이진아\n\n오늘 발매된 이진아 ‘람팜팜’의 첫 라이브를 들려드린 유희열의 스케치북💗 이진아가 부르는 희망의 주문 “람팜팜 팜팜 팜팜팜🎵”과 함께 행복한 연휴의 시작 보내시길 바라요!🧸❣️\n\n🔗 https://t.co/g2aptShYkV\n\n#LeeJinAh #람팜팜 #RumPumPum\n#유희열의스케치북 https://t.co/MGVKjuCVIT"
+	},
+	{
 		"created_at": "Fri Jan 28 14:11:16 +0000 2022",
 		"id": 1487047813443772400,
 		"id_str": "1487065745397792775",
