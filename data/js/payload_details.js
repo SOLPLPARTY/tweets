05617db1c206c34b0789ef35e0db52bd56f46536
솔플파티 <solplparty@gmail.com>
2022-01-29 19:17:40 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 57840,
-	"created_at": "Sat Jan 29 09:17:39 +0000 2022",
+	"tweets": 57841,
+	"created_at": "Sat Jan 29 10:17:39 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,280 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Sat Jan 29 09:29:24 +0000 2022",
+		"id": 1487349803264749600,
+		"id_str": "1487357200318222340",
+		"full_text": "[Teaser]\nATEEZ (에이티즈) - 'Don't Stop'  Official Music Video TEASER\n⠀\n▶https://t.co/nYRVMtk7c8\n2022.01.31 6PM (KST)\n⠀\n#UNIVERSE #ATEEZ #DontStop\n#유니버스 #에이티즈 #ATEEZ_DontStop\n#UNIVERSE_MUSIC\n@ATEEZofficial @into__universe https://t.co/v8Z3YXusvb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			217
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "UNIVERSE",
+					"indices": [
+						116,
+						125
+					]
+				},
+				{
+					"text": "ATEEZ",
+					"indices": [
+						126,
+						132
+					]
+				},
+				{
+					"text": "DontStop",
+					"indices": [
+						133,
+						142
+					]
+				},
+				{
+					"text": "유니버스",
+					"indices": [
+						143,
+						148
+					]
+				},
+				{
+					"text": "에이티즈",
+					"indices": [
+						149,
+						154
+					]
+				},
+				{
+					"text": "ATEEZ_DontStop",
+					"indices": [
+						155,
+						170
+					]
+				},
+				{
+					"text": "UNIVERSE_MUSIC",
+					"indices": [
+						171,
+						186
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ATEEZofficial",
+					"name": "ATEEZ(에이티즈)",
+					"id": 923415570522914800,
+					"id_str": "923415570522914819",
+					"indices": [
+						187,
+						201
+					]
+				},
+				{
+					"screen_name": "into__universe",
+					"name": "UNIVERSE_OFFICIAL",
+					"id": 1301330441786462200,
+					"id_str": "1301330441786462209",
+					"indices": [
+						202,
+						217
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/nYRVMtk7c8",
+					"expanded_url": "https://youtu.be/eiHcsbXf9bk",
+					"display_url": "youtu.be/eiHcsbXf9bk",
+					"indices": [
+						69,
+						92
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1486960100464291800,
+					"id_str": "1486960100464291843",
+					"indices": [
+						218,
+						241
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1486960100464291843/pu/img/n3_nOtdTqPJkjBbg.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1486960100464291843/pu/img/n3_nOtdTqPJkjBbg.jpg",
+					"url": "https://t.co/v8Z3YXusvb",
+					"display_url": "pic.twitter.com/v8Z3YXusvb",
+					"expanded_url": "https://twitter.com/Warner_Music_Kr/status/1487349803264749568/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1486960100464291800,
+					"id_str": "1486960100464291843",
+					"indices": [
+						218,
+						241
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1486960100464291843/pu/img/n3_nOtdTqPJkjBbg.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1486960100464291843/pu/img/n3_nOtdTqPJkjBbg.jpg",
+					"url": "https://t.co/v8Z3YXusvb",
+					"display_url": "pic.twitter.com/v8Z3YXusvb",
+					"expanded_url": "https://twitter.com/Warner_Music_Kr/status/1487349803264749568/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 14264,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1486960100464291843/pu/vid/540x540/6u-vF_aeuQHgkXwt.mp4?tag=12"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1486960100464291843/pu/vid/320x320/KOcFktZVIoGIqCju.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1486960100464291843/pu/pl/ZF5FfdonQCPH3QJ7.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1486960100464291843/pu/vid/720x720/Ix4FT8TX0zHT8fbl.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 206117108,
+			"id_str": "206117108",
+			"name": "Warner Music Korea",
+			"screen_name": "Warner_Music_Kr",
+			"location": "Seoul, Korea",
+			"description": "워너뮤직코리아 공식 트위터",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 54147,
+			"friends_count": 495,
+			"listed_count": 199,
+			"created_at": "Fri Oct 22 09:32:47 +0000 2010",
+			"favourites_count": 1621,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 11425,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1267377694565490688/FD-WdFum_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1267377694565490688/FD-WdFum_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/206117108/1636703401",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "023BE5",
+			"profile_text_color": "2566FC",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1109,
+		"favorite_count": 1667,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "[Teaser]\nATEEZ (에이티즈) - 'Don't Stop'  Official Music Video TEASER\n⠀\n▶https://t.co/nYRVMtk7c8\n2022.01.31 6PM (KST)\n⠀\n#UNIVERSE #ATEEZ #DontStop\n#유니버스 #에이티즈 #ATEEZ_DontStop\n#UNIVERSE_MUSIC\n@ATEEZofficial @into__universe https://t.co/v8Z3YXusvb"
+	},
+	{
 		"created_at": "Sat Jan 29 09:11:17 +0000 2022",
 		"id": 1487337610129203200,
 		"id_str": "1487352638815584256",
