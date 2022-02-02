230c2ae8af2aa34ddf36eaa8552547127ce8bec1
솔플파티 <solplparty@gmail.com>
2022-02-02 20:17:33 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 57895,
-	"created_at": "Wed Feb 02 10:17:32 +0000 2022",
+	"tweets": 57896,
+	"created_at": "Wed Feb 02 11:17:32 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,262 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Wed Feb 02 10:29:24 +0000 2022",
+		"id": 1488815420589043700,
+		"id_str": "1488821850645929986",
+		"full_text": "[#권혁수] [#백돌이대탈출]\n\n골프 실력과 아무 상관없는 골프윷놀이게임 \n걸잡이 과녁수와 조격대는\n이 게임의 승자가 될 수 있을 것인지?\n\n#백돌이대탈출_부캐골프시대 에서 확인🥰\n\n 👉 https://t.co/jO0N6Z5RHG\n\n#KwunHyukSoo #휴맵컨텐츠 #HUMAPCONTENTS #골프 #조현영 #최수임 #김시우 https://t.co/5cfw3Cvj5D",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			183
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "권혁수",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "백돌이대탈출",
+					"indices": [
+						8,
+						15
+					]
+				},
+				{
+					"text": "백돌이대탈출_부캐골프시대",
+					"indices": [
+						79,
+						93
+					]
+				},
+				{
+					"text": "KwunHyukSoo",
+					"indices": [
+						130,
+						142
+					]
+				},
+				{
+					"text": "휴맵컨텐츠",
+					"indices": [
+						143,
+						149
+					]
+				},
+				{
+					"text": "HUMAPCONTENTS",
+					"indices": [
+						150,
+						164
+					]
+				},
+				{
+					"text": "골프",
+					"indices": [
+						165,
+						168
+					]
+				},
+				{
+					"text": "조현영",
+					"indices": [
+						169,
+						173
+					]
+				},
+				{
+					"text": "최수임",
+					"indices": [
+						174,
+						178
+					]
+				},
+				{
+					"text": "김시우",
+					"indices": [
+						179,
+						183
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/jO0N6Z5RHG",
+					"expanded_url": "https://youtu.be/r37MispFyoo",
+					"display_url": "youtu.be/r37MispFyoo",
+					"indices": [
+						105,
+						128
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1488815414516011000,
+					"id_str": "1488815414516011010",
+					"indices": [
+						184,
+						207
+					],
+					"media_url": "http://pbs.twimg.com/media/FKlVuz5aUAIOM_V.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKlVuz5aUAIOM_V.jpg",
+					"url": "https://t.co/5cfw3Cvj5D",
+					"display_url": "pic.twitter.com/5cfw3Cvj5D",
+					"expanded_url": "https://twitter.com/humap_official/status/1488815420589043714/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1440,
+							"h": 810,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1488815414516011000,
+					"id_str": "1488815414516011010",
+					"indices": [
+						184,
+						207
+					],
+					"media_url": "http://pbs.twimg.com/media/FKlVuz5aUAIOM_V.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKlVuz5aUAIOM_V.jpg",
+					"url": "https://t.co/5cfw3Cvj5D",
+					"display_url": "pic.twitter.com/5cfw3Cvj5D",
+					"expanded_url": "https://twitter.com/humap_official/status/1488815420589043714/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1440,
+							"h": 810,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1219506842935513000,
+			"id_str": "1219506842935513088",
+			"name": "휴맵컨텐츠(HUMAP)",
+			"screen_name": "humap_official",
+			"location": "",
+			"description": "휴맵컨텐츠(HUMAP CONTENTS) Official Twitter",
+			"url": "https://t.co/fBZZK3SmX6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fBZZK3SmX6",
+							"expanded_url": "http://www.humapcontents.com/",
+							"display_url": "humapcontents.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 395,
+			"friends_count": 2,
+			"listed_count": 2,
+			"created_at": "Tue Jan 21 06:28:27 +0000 2020",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 588,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1219506842935513088/1579755898",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#권혁수] [#백돌이대탈출]\n\n골프 실력과 아무 상관없는 골프윷놀이게임 \n걸잡이 과녁수와 조격대는\n이 게임의 승자가 될 수 있을 것인지?\n\n#백돌이대탈출_부캐골프시대 에서 확인🥰\n\n 👉 https://t.co/jO0N6Z5RHG\n\n#KwunHyukSoo #휴맵컨텐츠 #HUMAPCONTENTS #골프 #조현영 #최수임 #김시우 https://t.co/5cfw3Cvj5D"
+	},
+	{
 		"created_at": "Wed Feb 02 09:29:35 +0000 2022",
 		"id": 1488799556565278700,
 		"id_str": "1488806795447607300",
