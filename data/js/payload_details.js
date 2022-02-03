f963026321477f6fd6cada2ac6c9a7721d353708
솔플파티 <solplparty@gmail.com>
2022-02-03 12:17:33 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 57902,
-	"created_at": "Thu Feb 03 02:17:32 +0000 2022",
+	"tweets": 57903,
+	"created_at": "Thu Feb 03 03:17:32 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Thu Feb 03 03:11:16 +0000 2022",
+		"id": 1489071150864998400,
+		"id_str": "1489073978908020737",
+		"full_text": "&lt;너드커넥션 단독 콘서트 : 롤링 27주년 기념 공연&gt;\n⠀\n· 일시 : 2022년 03월 04일 (금) 오후 8시, 03월 05일 (토) 오후 5시\n· 장소 : 롤링홀\n· 출연 : 너드커넥션\n· 예매 : 멜론티켓 2022년 02월 07일 (월) 오후 8시 예매오픈\n⠀\nDesign by 오이웍스\n⠀\n#롤링홀 #너드커넥션 \n#롤링27주년 #멜론티켓 https://t.co/2VWLCvertm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			199
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "롤링홀",
+					"indices": [
+						173,
+						177
+					]
+				},
+				{
+					"text": "너드커넥션",
+					"indices": [
+						178,
+						184
+					]
+				},
+				{
+					"text": "롤링27주년",
+					"indices": [
+						186,
+						193
+					]
+				},
+				{
+					"text": "멜론티켓",
+					"indices": [
+						194,
+						199
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1483747882528546800,
+					"id_str": "1483747882528546823",
+					"indices": [
+						200,
+						223
+					],
+					"media_url": "http://pbs.twimg.com/media/FJdU1oJaIActcZ-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJdU1oJaIActcZ-.jpg",
+					"url": "https://t.co/2VWLCvertm",
+					"display_url": "pic.twitter.com/2VWLCvertm",
+					"expanded_url": "https://twitter.com/rollinghall1995/status/1489071150864998403/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1081,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1081,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 679,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1483747882528546800,
+					"id_str": "1483747882528546823",
+					"indices": [
+						200,
+						223
+					],
+					"media_url": "http://pbs.twimg.com/media/FJdU1oJaIActcZ-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJdU1oJaIActcZ-.jpg",
+					"url": "https://t.co/2VWLCvertm",
+					"display_url": "pic.twitter.com/2VWLCvertm",
+					"expanded_url": "https://twitter.com/rollinghall1995/status/1489071150864998403/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1081,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1081,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 679,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1291327050578456600,
+			"id_str": "1291327050578456578",
+			"name": "롤링홀",
+			"screen_name": "rollinghall1995",
+			"location": "서울시 마포구 어울마당로 35 신보빌딩 지하1층",
+			"description": "홍대/합정에 위치한 음악 공연장 #롤링홀 입니다٩( ᐛ )و\nConcert live hall in South Korea\n-\n👇🏻👇🏻멜론티켓 롤링홀 기획공연 바로가기.\nhttps://t.co/K14VaPTNnq",
+			"url": "https://t.co/C9YRGggByu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/C9YRGggByu",
+							"expanded_url": "http://www.rollinghall.co.kr/default/",
+							"display_url": "rollinghall.co.kr/default/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/K14VaPTNnq",
+							"expanded_url": "http://bit.ly/3C4QrHt",
+							"display_url": "bit.ly/3C4QrHt",
+							"indices": [
+								93,
+								116
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 332,
+			"friends_count": 3,
+			"listed_count": 3,
+			"created_at": "Thu Aug 06 10:56:17 +0000 2020",
+			"favourites_count": 133,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 402,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1291327708689862662/9hOLVWB2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1291327708689862662/9hOLVWB2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1291327050578456578/1596712102",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "&lt;너드커넥션 단독 콘서트 : 롤링 27주년 기념 공연&gt;\n⠀\n· 일시 : 2022년 03월 04일 (금) 오후 8시, 03월 05일 (토) 오후 5시\n· 장소 : 롤링홀\n· 출연 : 너드커넥션\n· 예매 : 멜론티켓 2022년 02월 07일 (월) 오후 8시 예매오픈\n⠀\nDesign by 오이웍스\n⠀\n#롤링홀 #너드커넥션 \n#롤링27주년 #멜론티켓 https://t.co/2VWLCvertm"
+	},
+	{
 		"created_at": "Thu Feb 03 02:07:51 +0000 2022",
 		"id": 1489058016171339800,
 		"id_str": "1489058016171339778",
