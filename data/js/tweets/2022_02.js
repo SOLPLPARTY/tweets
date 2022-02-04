e90d22dff1d67520f04954fec6096bab29607c70
솔플파티 <solplparty@gmail.com>
2022-02-04 15:17:33 +0900

    Added 1 tweets

--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,242 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Fri Feb 04 05:29:25 +0000 2022",
+		"id": 1489470196851294200,
+		"id_str": "1489471130092163077",
+		"full_text": "[#Notice] #윤석철\n\n2022 윤석철트리오 단독공연 ‘익숙하고 일정한’\n\n✔️일시\n2022. 2. 26. (토) 6PM\n2022. 2. 27. (일) 5PM\n\n✔️장소\n서강대 메리홀 대극장\n\n✔️티켓 오픈\n2021. 2. 8. (화) 8PM @ 멜론 티켓\n\n@SCJAZZY\n#YunSeokCheol #YunSeokCheolTrio\n#윤석철트리오 #익숙하고일정한 #단독공연 https://t.co/XcnFGy10mr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			211
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Notice",
+					"indices": [
+						1,
+						8
+					]
+				},
+				{
+					"text": "윤석철",
+					"indices": [
+						10,
+						14
+					]
+				},
+				{
+					"text": "YunSeokCheol",
+					"indices": [
+						157,
+						170
+					]
+				},
+				{
+					"text": "YunSeokCheolTrio",
+					"indices": [
+						171,
+						188
+					]
+				},
+				{
+					"text": "윤석철트리오",
+					"indices": [
+						189,
+						196
+					]
+				},
+				{
+					"text": "익숙하고일정한",
+					"indices": [
+						197,
+						205
+					]
+				},
+				{
+					"text": "단독공연",
+					"indices": [
+						206,
+						211
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SCJAZZY",
+					"name": "윤석철",
+					"id": 129854311,
+					"id_str": "129854311",
+					"indices": [
+						148,
+						156
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1489470016898879500,
+					"id_str": "1489470016898879497",
+					"indices": [
+						212,
+						235
+					],
+					"media_url": "http://pbs.twimg.com/media/FKupFrzaUAkebw3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKupFrzaUAkebw3.jpg",
+					"url": "https://t.co/XcnFGy10mr",
+					"display_url": "pic.twitter.com/XcnFGy10mr",
+					"expanded_url": "https://twitter.com/antennamusic/status/1489470196851294208/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1448,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 848,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 481,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1489470016898879500,
+					"id_str": "1489470016898879497",
+					"indices": [
+						212,
+						235
+					],
+					"media_url": "http://pbs.twimg.com/media/FKupFrzaUAkebw3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKupFrzaUAkebw3.jpg",
+					"url": "https://t.co/XcnFGy10mr",
+					"display_url": "pic.twitter.com/XcnFGy10mr",
+					"expanded_url": "https://twitter.com/antennamusic/status/1489470196851294208/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1448,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 848,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 481,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 266004552,
+			"id_str": "266004552",
+			"name": "안테나 Antenna",
+			"screen_name": "antennamusic",
+			"location": "대한민국 서울",
+			"description": "유희열(Toy), 유재석, 정재형, 루시드폴, 페퍼톤스(신재평&이장원), 박새별, 샘김, 이진아, 권진아, 정승환, 윤석철, 적재, 이미주, 서동환, 황현조 Official / 이진아 싱글 ‘람팜팜(Rum Pum Pum)’ 2022. 01. 28. 6PM Release💗",
+			"url": "https://t.co/XSWWwEx210",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XSWWwEx210",
+							"expanded_url": "http://www.antenna.co.kr",
+							"display_url": "antenna.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 40333,
+			"friends_count": 11,
+			"listed_count": 386,
+			"created_at": "Mon Mar 14 13:49:27 +0000 2011",
+			"favourites_count": 14,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 4589,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266004552/1643018512",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 14,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#Notice] #윤석철\n\n2022 윤석철트리오 단독공연 ‘익숙하고 일정한’\n\n✔️일시\n2022. 2. 26. (토) 6PM\n2022. 2. 27. (일) 5PM\n\n✔️장소\n서강대 메리홀 대극장\n\n✔️티켓 오픈\n2021. 2. 8. (화) 8PM @ 멜론 티켓\n\n@SCJAZZY\n#YunSeokCheol #YunSeokCheolTrio\n#윤석철트리오 #익숙하고일정한 #단독공연 https://t.co/XcnFGy10mr"
+	},
+	{
 		"created_at": "Fri Feb 04 04:55:20 +0000 2022",
 		"id": 1489462554204287000,
 		"id_str": "1489462554204286979",
