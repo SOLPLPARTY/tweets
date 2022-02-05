920776e65506d9976cb8e9027fe866bd804c6a64
솔플파티 <solplparty@gmail.com>
2022-02-05 21:17:34 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 57951,
-	"created_at": "Sat Feb 05 10:17:33 +0000 2022",
+	"tweets": 57952,
+	"created_at": "Sat Feb 05 12:17:33 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,227 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Sat Feb 05 11:58:23 +0000 2022",
+		"id": 1489929798734565400,
+		"id_str": "1489931404427333634",
+		"full_text": "2002년으로 가서 챔피언 들으면서 광장에서 월드컵 응원하기\nvs\n2012년으로 가서 강남스타일 UCC 만들기\n\n\"2\" 특집 슬케파 여러분의 선택은?\n2️⃣https://t.co/T43gV7PpKB https://t.co/25ji4INnmD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			110
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/T43gV7PpKB",
+					"expanded_url": "https://now.naver.com/player/10255",
+					"display_url": "now.naver.com/player/10255",
+					"indices": [
+						87,
+						110
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1489926495665410000,
+					"id_str": "1489926495665410048",
+					"indices": [
+						111,
+						134
+					],
+					"media_url": "http://pbs.twimg.com/media/FK1IQPuUUAA0H9V.png",
+					"media_url_https": "https://pbs.twimg.com/media/FK1IQPuUUAA0H9V.png",
+					"url": "https://t.co/25ji4INnmD",
+					"display_url": "pic.twitter.com/25ji4INnmD",
+					"expanded_url": "https://twitter.com/seulpeumkpop/status/1489929798734565380/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 550,
+							"h": 412,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 550,
+							"h": 412,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 550,
+							"h": 412,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1489926495665410000,
+					"id_str": "1489926495665410048",
+					"indices": [
+						111,
+						134
+					],
+					"media_url": "http://pbs.twimg.com/media/FK1IQPuUUAA0H9V.png",
+					"media_url_https": "https://pbs.twimg.com/media/FK1IQPuUUAA0H9V.png",
+					"url": "https://t.co/25ji4INnmD",
+					"display_url": "pic.twitter.com/25ji4INnmD",
+					"expanded_url": "https://twitter.com/seulpeumkpop/status/1489929798734565380/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 550,
+							"h": 412,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 550,
+							"h": 412,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 550,
+							"h": 412,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				},
+				{
+					"id": 1489926536006230000,
+					"id_str": "1489926536006230017",
+					"indices": [
+						111,
+						134
+					],
+					"media_url": "http://pbs.twimg.com/media/FK1ISmAUYAEL78Y.png",
+					"media_url_https": "https://pbs.twimg.com/media/FK1ISmAUYAEL78Y.png",
+					"url": "https://t.co/25ji4INnmD",
+					"display_url": "pic.twitter.com/25ji4INnmD",
+					"expanded_url": "https://twitter.com/seulpeumkpop/status/1489929798734565380/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 550,
+							"h": 310,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 550,
+							"h": 310,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 550,
+							"h": 310,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 729495493080809500,
+			"id_str": "729495493080809473",
+			"name": "슬케파",
+			"screen_name": "seulpeumkpop",
+			"location": "",
+			"description": "본방송 매주 토요일 저녁 9시 NAVER NOW / 재방송 화요일~토요일 NAVER NOW 24H / 지난회차 다시보기 VIBE",
+			"url": "https://t.co/kho8Mn53lC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kho8Mn53lC",
+							"expanded_url": "https://now.naver.com/player/10255",
+							"display_url": "now.naver.com/player/10255",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7664,
+			"friends_count": 41,
+			"listed_count": 21,
+			"created_at": "Mon May 09 02:17:33 +0000 2016",
+			"favourites_count": 833,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1896,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1434539405981536256/7d7DZF6f_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1434539405981536256/7d7DZF6f_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/729495493080809473/1621333964",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "2002년으로 가서 챔피언 들으면서 광장에서 월드컵 응원하기\nvs\n2012년으로 가서 강남스타일 UCC 만들기\n\n\"2\" 특집 슬케파 여러분의 선택은?\n2️⃣https://t.co/T43gV7PpKB https://t.co/25ji4INnmD"
+	},
+	{
 		"created_at": "Sat Feb 05 09:29:24 +0000 2022",
 		"id": 1489881444956934100,
 		"id_str": "1489893915377418241",
