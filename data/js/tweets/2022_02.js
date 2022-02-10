33eb2c55741427cf1c92055ceb1d4859e93ae294
솔플파티 <solplparty@gmail.com>
2022-02-10 17:17:35 +0900

    Added 1 tweets

--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,147 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Thu Feb 10 07:30:01 +0000 2022",
+		"id": 1491675809274380300,
+		"id_str": "1491675809274380289",
+		"full_text": "#ONSTAGE #온스테이지\n실용음악 시대의 안티테제, The 1234-dah!\nhttps://t.co/sVkXLFuCFl",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			68
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ONSTAGE",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"text": "온스테이지",
+					"indices": [
+						9,
+						15
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/sVkXLFuCFl",
+					"expanded_url": "https://bit.ly/3uJ2poL",
+					"display_url": "bit.ly/3uJ2poL",
+					"indices": [
+						45,
+						68
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.powerapps.com\" rel=\"nofollow\">Microsoft Power Platform</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 968706450779463700,
+			"id_str": "968706450779463682",
+			"name": "솔플파티",
+			"screen_name": "SOLPLPARTY",
+			"location": "문의: contact@solplparty.com",
+			"description": "혼자서도 즐겁게! \n문화예술 커뮤니티, 솔플파티 🤘 \n\n▶ 참가하기 \nhttps://t.co/Uq1kQ79SRI \n\n❓ ABOUT\nhttps://t.co/OtYqQtyXgi ",
+			"url": "https://t.co/L6Ap35US8u",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/L6Ap35US8u",
+							"expanded_url": "https://solplparty.com",
+							"display_url": "solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/Uq1kQ79SRI",
+							"expanded_url": "http://join.solpl.party",
+							"display_url": "join.solpl.party",
+							"indices": [
+								39,
+								62
+							]
+						},
+						{
+							"url": "https://t.co/OtYqQtyXgi",
+							"expanded_url": "http://about.solpl.party",
+							"display_url": "about.solpl.party",
+							"indices": [
+								73,
+								96
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 253,
+			"friends_count": 466,
+			"listed_count": 3,
+			"created_at": "Wed Feb 28 04:36:30 +0000 2018",
+			"favourites_count": 240,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 79469,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/968706450779463682/1519792691",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#ONSTAGE #온스테이지\n실용음악 시대의 안티테제, The 1234-dah!\nhttps://t.co/sVkXLFuCFl"
+	},
+	{
 		"created_at": "Thu Feb 10 07:11:16 +0000 2022",
 		"id": 1491662119837732900,
 		"id_str": "1491671092318064640",
@@ -198,7 +339,7 @@ Grailbird.data.tweets_2022_02 =
 		"created_at": "Thu Feb 10 07:06:26 +0000 2022",
 		"id": 1491669872270532600,
 		"id_str": "1491669872270532609",
-		"full_text": "#유니버설뮤직코리아 #Universal_Music_Korea\n당신의 이상형은? 이모걸👩‍🎤 | 머신 건 켈리 (Machine Gun Kelly) - emo girl ft. WILLOW (가사/한글/해석)\nhttps://t.co/pUT9diQiKS",
+		"full_text": "#유니버설뮤직코리아 #Universal_Music_Korea\n당신의 이상형은? 이모걸👩🎤 | 머신 건 켈리 (Machine Gun Kelly) - emo girl ft. WILLOW (가사/한글/해석)\nhttps://t.co/pUT9diQiKS",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -333,7 +474,7 @@ Grailbird.data.tweets_2022_02 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "#유니버설뮤직코리아 #Universal_Music_Korea\n당신의 이상형은? 이모걸👩‍🎤 | 머신 건 켈리 (Machine Gun Kelly) - emo girl ft. WILLOW (가사/한글/해석)\nhttps://t.co/pUT9diQiKS"
+		"text": "#유니버설뮤직코리아 #Universal_Music_Korea\n당신의 이상형은? 이모걸👩🎤 | 머신 건 켈리 (Machine Gun Kelly) - emo girl ft. WILLOW (가사/한글/해석)\nhttps://t.co/pUT9diQiKS"
 	},
 	{
 		"created_at": "Thu Feb 10 06:29:46 +0000 2022",
