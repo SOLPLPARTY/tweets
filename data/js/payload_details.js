2eeaf7262063749b65774cd85fb3ef21899a219f
솔플파티 <solplparty@gmail.com>
2022-02-17 22:17:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 58234,
-	"created_at": "Thu Feb 17 12:17:35 +0000 2022",
+	"tweets": 58235,
+	"created_at": "Thu Feb 17 13:17:37 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,334 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Thu Feb 17 13:11:16 +0000 2022",
+		"id": 1494295578259300400,
+		"id_str": "1494298403680968707",
+		"full_text": "📢 2/18 공연 현장 안내\n⠀\n&lt;박소은 단독 콘서트 ‘너만 있으면’：롤링 27주년 기념 공연&gt;\n· 티켓수령 : 7시 (현장구매 소량 가능)\n· 입장 : 7시 30분 \n· 공연시작 : 8시\n· 출연 : 박소은\n\n#박소은 #단독콘서트 #너만있으면 \n#롤링홀 #ROLLINGHALL #롤링27주년기념공연 \n#홍대 #공연 #인디 #멜론티켓 https://t.co/5B0EnUpmn0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			192
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "박소은",
+					"indices": [
+						124,
+						128
+					]
+				},
+				{
+					"text": "단독콘서트",
+					"indices": [
+						129,
+						135
+					]
+				},
+				{
+					"text": "너만있으면",
+					"indices": [
+						136,
+						142
+					]
+				},
+				{
+					"text": "롤링홀",
+					"indices": [
+						144,
+						148
+					]
+				},
+				{
+					"text": "ROLLINGHALL",
+					"indices": [
+						149,
+						161
+					]
+				},
+				{
+					"text": "롤링27주년기념공연",
+					"indices": [
+						162,
+						173
+					]
+				},
+				{
+					"text": "홍대",
+					"indices": [
+						175,
+						178
+					]
+				},
+				{
+					"text": "공연",
+					"indices": [
+						179,
+						182
+					]
+				},
+				{
+					"text": "인디",
+					"indices": [
+						183,
+						186
+					]
+				},
+				{
+					"text": "멜론티켓",
+					"indices": [
+						187,
+						192
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1493848185680597000,
+					"id_str": "1493848185680596992",
+					"indices": [
+						193,
+						216
+					],
+					"media_url": "http://pbs.twimg.com/media/FLs3ApfakAA7wLi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLs3ApfakAA7wLi.jpg",
+					"url": "https://t.co/5B0EnUpmn0",
+					"display_url": "pic.twitter.com/5B0EnUpmn0",
+					"expanded_url": "https://twitter.com/rollinghall1995/status/1494295578259300352/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1753,
+							"h": 1240,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 481,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 849,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1493848185680597000,
+					"id_str": "1493848185680596992",
+					"indices": [
+						193,
+						216
+					],
+					"media_url": "http://pbs.twimg.com/media/FLs3ApfakAA7wLi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLs3ApfakAA7wLi.jpg",
+					"url": "https://t.co/5B0EnUpmn0",
+					"display_url": "pic.twitter.com/5B0EnUpmn0",
+					"expanded_url": "https://twitter.com/rollinghall1995/status/1494295578259300352/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1753,
+							"h": 1240,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 481,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 849,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1493848243301920800,
+					"id_str": "1493848243301920775",
+					"indices": [
+						193,
+						216
+					],
+					"media_url": "http://pbs.twimg.com/media/FLs3EAJaMAcytPg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLs3EAJaMAcytPg.jpg",
+					"url": "https://t.co/5B0EnUpmn0",
+					"display_url": "pic.twitter.com/5B0EnUpmn0",
+					"expanded_url": "https://twitter.com/rollinghall1995/status/1494295578259300352/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 481,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 849,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1753,
+							"h": 1240,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1493848243507429400,
+					"id_str": "1493848243507429379",
+					"indices": [
+						193,
+						216
+					],
+					"media_url": "http://pbs.twimg.com/media/FLs3EA6aAAMejki.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLs3EA6aAAMejki.jpg",
+					"url": "https://t.co/5B0EnUpmn0",
+					"display_url": "pic.twitter.com/5B0EnUpmn0",
+					"expanded_url": "https://twitter.com/rollinghall1995/status/1494295578259300352/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 481,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1753,
+							"h": 1240,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 849,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1291327050578456600,
+			"id_str": "1291327050578456578",
+			"name": "롤링홀",
+			"screen_name": "rollinghall1995",
+			"location": "서울시 마포구 어울마당로 35 신보빌딩 지하1층",
+			"description": "홍대/합정에 위치한 음악 공연장 #롤링홀 입니다٩( ᐛ )و\nConcert live hall in South Korea\n-\n👇🏻👇🏻멜론티켓 롤링홀 기획공연 바로가기.\nhttps://t.co/K14VaPTNnq",
+			"url": "https://t.co/C9YRGggByu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/C9YRGggByu",
+							"expanded_url": "http://www.rollinghall.co.kr/default/",
+							"display_url": "rollinghall.co.kr/default/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/K14VaPTNnq",
+							"expanded_url": "http://bit.ly/3C4QrHt",
+							"display_url": "bit.ly/3C4QrHt",
+							"indices": [
+								93,
+								116
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 375,
+			"friends_count": 3,
+			"listed_count": 3,
+			"created_at": "Thu Aug 06 10:56:17 +0000 2020",
+			"favourites_count": 133,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 420,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1291327708689862662/9hOLVWB2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1291327708689862662/9hOLVWB2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1291327050578456578/1596712102",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "📢 2/18 공연 현장 안내\n⠀\n&lt;박소은 단독 콘서트 ‘너만 있으면’：롤링 27주년 기념 공연&gt;\n· 티켓수령 : 7시 (현장구매 소량 가능)\n· 입장 : 7시 30분 \n· 공연시작 : 8시\n· 출연 : 박소은\n\n#박소은 #단독콘서트 #너만있으면 \n#롤링홀 #ROLLINGHALL #롤링27주년기념공연 \n#홍대 #공연 #인디 #멜론티켓 https://t.co/5B0EnUpmn0"
+	},
+	{
 		"created_at": "Thu Feb 17 11:29:24 +0000 2022",
 		"id": 1494269143930916900,
 		"id_str": "1494272767910461440",
