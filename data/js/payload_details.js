480c4044a3ed6508303d6443d46fa82058c8f526
솔플파티 <solplparty@gmail.com>
2022-02-21 16:17:39 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 58327,
-	"created_at": "Mon Feb 21 05:17:37 +0000 2022",
+	"tweets": 58328,
+	"created_at": "Mon Feb 21 07:17:38 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,193 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Mon Feb 21 06:29:24 +0000 2022",
+		"id": 1495635265230155800,
+		"id_str": "1495646822148280324",
+		"full_text": "딥한 보컬과 유연한 베이스라인이 그려내는 멜랑콜릭 사운드 \n\n#Dekova &amp; #LizzyWang ✨𝐈'𝐦 𝐈𝐧𝐭𝐨 𝐘𝐨𝐮✨\n\n🎧 https://t.co/dgyetQZl0D https://t.co/coX668pUml",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			100
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Dekova",
+					"indices": [
+						34,
+						41
+					]
+				},
+				{
+					"text": "LizzyWang",
+					"indices": [
+						48,
+						58
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/dgyetQZl0D",
+					"expanded_url": "https://youtu.be/rcX_aI6rNnI",
+					"display_url": "youtu.be/rcX_aI6rNnI",
+					"indices": [
+						77,
+						100
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1495635254987665400,
+					"id_str": "1495635254987665410",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/FMGQVxcXMAICVDV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FMGQVxcXMAICVDV.jpg",
+					"url": "https://t.co/coX668pUml",
+					"display_url": "pic.twitter.com/coX668pUml",
+					"expanded_url": "https://twitter.com/Warner_Music_Kr/status/1495635265230155776/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1495635254987665400,
+					"id_str": "1495635254987665410",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/FMGQVxcXMAICVDV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FMGQVxcXMAICVDV.jpg",
+					"url": "https://t.co/coX668pUml",
+					"display_url": "pic.twitter.com/coX668pUml",
+					"expanded_url": "https://twitter.com/Warner_Music_Kr/status/1495635265230155776/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.meltwater.com/\" rel=\"nofollow\">Meltwater Social</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 206117108,
+			"id_str": "206117108",
+			"name": "Warner Music Korea",
+			"screen_name": "Warner_Music_Kr",
+			"location": "Seoul, Korea",
+			"description": "워너뮤직코리아 공식 트위터",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 54399,
+			"friends_count": 494,
+			"listed_count": 198,
+			"created_at": "Fri Oct 22 09:32:47 +0000 2010",
+			"favourites_count": 1620,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 11462,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1267377694565490688/FD-WdFum_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1267377694565490688/FD-WdFum_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/206117108/1636703401",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "023BE5",
+			"profile_text_color": "2566FC",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 21,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "딥한 보컬과 유연한 베이스라인이 그려내는 멜랑콜릭 사운드 \n\n#Dekova &amp; #LizzyWang ✨𝐈'𝐦 𝐈𝐧𝐭𝐨 𝐘𝐨𝐮✨\n\n🎧 https://t.co/dgyetQZl0D https://t.co/coX668pUml"
+	},
+	{
 		"created_at": "Mon Feb 21 04:29:28 +0000 2022",
 		"id": 1495605184424456200,
 		"id_str": "1495616636749062148",
