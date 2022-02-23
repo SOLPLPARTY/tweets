b8908a6b24619a66928623108ae020d4203ad53e
솔플파티 <solplparty@gmail.com>
2022-02-23 22:17:37 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 58413,
-	"created_at": "Wed Feb 23 12:17:38 +0000 2022",
+	"tweets": 58414,
+	"created_at": "Wed Feb 23 13:17:36 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,163 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Wed Feb 23 12:29:25 +0000 2022",
+		"id": 1496452649633587200,
+		"id_str": "1496462195944734721",
+		"full_text": "[VIDEO] [#인순이] #부캐전성시대\n\n시작부터 머리끄덩이;; 악연의 끝판 인순이X이휘재 같은 파티가 되어버렸다💦 부캐전성시대\n\n👉 https://t.co/73LgfZfY99\n\n#INSOONI #휴맵컨텐츠 #HUMAPCONTENTS #TV조선 #TV조선조이",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			145
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "인순이",
+					"indices": [
+						9,
+						13
+					]
+				},
+				{
+					"text": "부캐전성시대",
+					"indices": [
+						15,
+						22
+					]
+				},
+				{
+					"text": "INSOONI",
+					"indices": [
+						101,
+						109
+					]
+				},
+				{
+					"text": "휴맵컨텐츠",
+					"indices": [
+						110,
+						116
+					]
+				},
+				{
+					"text": "HUMAPCONTENTS",
+					"indices": [
+						117,
+						131
+					]
+				},
+				{
+					"text": "TV조선",
+					"indices": [
+						132,
+						137
+					]
+				},
+				{
+					"text": "TV조선조이",
+					"indices": [
+						138,
+						145
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/73LgfZfY99",
+					"expanded_url": "https://youtu.be/lzlWMaXCudE",
+					"display_url": "youtu.be/lzlWMaXCudE",
+					"indices": [
+						76,
+						99
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1219506842935513000,
+			"id_str": "1219506842935513088",
+			"name": "휴맵컨텐츠(HUMAP)",
+			"screen_name": "humap_official",
+			"location": "",
+			"description": "휴맵컨텐츠(HUMAP CONTENTS) Official Twitter",
+			"url": "https://t.co/fBZZK3SmX6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fBZZK3SmX6",
+							"expanded_url": "http://www.humapcontents.com/",
+							"display_url": "humapcontents.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 391,
+			"friends_count": 2,
+			"listed_count": 2,
+			"created_at": "Tue Jan 21 06:28:27 +0000 2020",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 628,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1219506842935513088/1579755898",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[VIDEO] [#인순이] #부캐전성시대\n\n시작부터 머리끄덩이;; 악연의 끝판 인순이X이휘재 같은 파티가 되어버렸다💦 부캐전성시대\n\n👉 https://t.co/73LgfZfY99\n\n#INSOONI #휴맵컨텐츠 #HUMAPCONTENTS #TV조선 #TV조선조이"
+	},
+	{
 		"created_at": "Wed Feb 23 11:29:39 +0000 2022",
 		"id": 1496446713946685400,
 		"id_str": "1496447154872848387",
