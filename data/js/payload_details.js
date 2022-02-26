7650705ae60feac60fd65daaa2bb86268746d27c
솔플파티 <solplparty@gmail.com>
2022-02-26 19:17:37 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 58490,
-	"created_at": "Sat Feb 26 09:17:38 +0000 2022",
+	"tweets": 58491,
+	"created_at": "Sat Feb 26 10:17:36 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,265 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Sat Feb 26 09:30:04 +0000 2022",
+		"id": 1497500133826900000,
+		"id_str": "1497504226620186630",
+		"full_text": "[#Teaser] #적재\n\nTrip:Tape # 01\n적재 - 손을 잡는다거나, 같이 걷는다거나\n\n2022. 02. 27. 6PM Release\n \n🔗 https://t.co/8fQuU9M7dm\n\n#Jukjae #트립테이프\n#손을잡는다거나 #같이걷는다거나 https://t.co/CHyRLEYDCg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			142
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Teaser",
+					"indices": [
+						1,
+						8
+					]
+				},
+				{
+					"text": "적재",
+					"indices": [
+						10,
+						13
+					]
+				},
+				{
+					"text": "Jukjae",
+					"indices": [
+						110,
+						117
+					]
+				},
+				{
+					"text": "트립테이프",
+					"indices": [
+						118,
+						124
+					]
+				},
+				{
+					"text": "손을잡는다거나",
+					"indices": [
+						125,
+						133
+					]
+				},
+				{
+					"text": "같이걷는다거나",
+					"indices": [
+						134,
+						142
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/8fQuU9M7dm",
+					"expanded_url": "https://youtu.be/_3F0Tm5E8cw",
+					"display_url": "youtu.be/_3F0Tm5E8cw",
+					"indices": [
+						85,
+						108
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1497500057045995500,
+					"id_str": "1497500057045995521",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1497500057045995521/pu/img/P5U7bPB2z5pz4WLv.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1497500057045995521/pu/img/P5U7bPB2z5pz4WLv.jpg",
+					"url": "https://t.co/CHyRLEYDCg",
+					"display_url": "pic.twitter.com/CHyRLEYDCg",
+					"expanded_url": "https://twitter.com/antennamusic/status/1497500133826899968/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1497500057045995500,
+					"id_str": "1497500057045995521",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1497500057045995521/pu/img/P5U7bPB2z5pz4WLv.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1497500057045995521/pu/img/P5U7bPB2z5pz4WLv.jpg",
+					"url": "https://t.co/CHyRLEYDCg",
+					"display_url": "pic.twitter.com/CHyRLEYDCg",
+					"expanded_url": "https://twitter.com/antennamusic/status/1497500133826899968/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 49621,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1497500057045995521/pu/vid/480x270/YXOcS_yFEbbmaDxa.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1497500057045995521/pu/pl/KeqOm1acoJuamBNF.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1497500057045995521/pu/vid/640x360/FJ5JB6ibUdY6Xphz.mp4?tag=12"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1497500057045995521/pu/vid/1280x720/EKdnSCsy_Qms5l_F.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 266004552,
+			"id_str": "266004552",
+			"name": "안테나 Antenna",
+			"screen_name": "antennamusic",
+			"location": "대한민국 서울",
+			"description": "유희열(Toy), 유재석, 정재형, 루시드폴, 페퍼톤스(신재평&이장원), 박새별, 샘김, 이진아, 권진아, 정승환, 윤석철, 적재, 이미주, 서동환, 황현조 Official / 윤석철트리오 EP '익숙하고 일정한’ 2022. 02. 21. 12PM Release",
+			"url": "https://t.co/XSWWwEPbf8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XSWWwEPbf8",
+							"expanded_url": "http://www.antenna.co.kr",
+							"display_url": "antenna.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 40389,
+			"friends_count": 10,
+			"listed_count": 386,
+			"created_at": "Mon Mar 14 13:49:27 +0000 2011",
+			"favourites_count": 14,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 4672,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266004552/1645092104",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 74,
+		"favorite_count": 80,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#Teaser] #적재\n\nTrip:Tape # 01\n적재 - 손을 잡는다거나, 같이 걷는다거나\n\n2022. 02. 27. 6PM Release\n \n🔗 https://t.co/8fQuU9M7dm\n\n#Jukjae #트립테이프\n#손을잡는다거나 #같이걷는다거나 https://t.co/CHyRLEYDCg"
+	},
+	{
 		"created_at": "Sat Feb 26 08:29:25 +0000 2022",
 		"id": 1497481564866834400,
 		"id_str": "1497488961601785856",
