af2a929b92e0c5661b6228f9c93143c37f956e3d
솔플파티 <solplparty@gmail.com>
2022-03-01 20:17:33 +0900

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 58567,
-	"created_at": "Tue Mar 01 10:17:32 +0000 2022",
+	"tweets": 58570,
+	"created_at": "Tue Mar 01 11:17:32 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,553 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Tue Mar 01 10:29:49 +0000 2022",
+		"id": 1498602762283597800,
+		"id_str": "1498606424456404995",
+		"full_text": "[#NEWS] '올 탁구나' 손태진, 릴레이 랠리 1인자..'탁10' 랭킹전 출격\n\n▶️ https://t.co/YG96wSDpvG\n\n#손태진 #tvN #올탁구나",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			90
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "손태진",
+					"indices": [
+						75,
+						79
+					]
+				},
+				{
+					"text": "tvN",
+					"indices": [
+						80,
+						84
+					]
+				},
+				{
+					"text": "올탁구나",
+					"indices": [
+						85,
+						90
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/YG96wSDpvG",
+					"expanded_url": "http://naver.me/xyu18Rgt",
+					"display_url": "naver.me/xyu18Rgt",
+					"indices": [
+						50,
+						73
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/pgQyM3XZDx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pgQyM3XZDx",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 33843,
+			"friends_count": 15,
+			"listed_count": 273,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6767,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1645607843",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 16,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] '올 탁구나' 손태진, 릴레이 랠리 1인자..'탁10' 랭킹전 출격\n\n▶️ https://t.co/YG96wSDpvG\n\n#손태진 #tvN #올탁구나"
+	},
+	{
+		"created_at": "Tue Mar 01 10:29:34 +0000 2022",
+		"id": 1498594716681588700,
+		"id_str": "1498606364314198018",
+		"full_text": "Dance crew ‘Team Auspicious’ will be joining the show with their high-quality dance performance. \n\n1st ASIA 22°03’04’’ 9PM (KST)\n2nd AMERICA 22°03’04’’ 9PM (PST)\n      ↘︎22°03’05’’ 2PM (KST)\n3rd EUROPE 22°03’05’’ 9PM (CET)\n      ↘︎22°03’06’’ 5AM (KST)\n\n#auspicious_crew https://t.co/3hFX6mkhzT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			269
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "auspicious_crew",
+					"indices": [
+						253,
+						269
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1498594713640734700,
+					"id_str": "1498594713640734720",
+					"indices": [
+						270,
+						293
+					],
+					"media_url": "http://pbs.twimg.com/media/FMwT88DVkAAfGws.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FMwT88DVkAAfGws.jpg",
+					"url": "https://t.co/3hFX6mkhzT",
+					"display_url": "pic.twitter.com/3hFX6mkhzT",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1498594716681588738/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1498594713640734700,
+					"id_str": "1498594713640734720",
+					"indices": [
+						270,
+						293
+					],
+					"media_url": "http://pbs.twimg.com/media/FMwT88DVkAAfGws.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FMwT88DVkAAfGws.jpg",
+					"url": "https://t.co/3hFX6mkhzT",
+					"display_url": "pic.twitter.com/3hFX6mkhzT",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1498594716681588738/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1498594704673239000,
+		"in_reply_to_status_id_str": "1498594704673239041",
+		"in_reply_to_user_id": 884378714,
+		"in_reply_to_user_id_str": "884378714",
+		"in_reply_to_screen_name": "SMTOWNGLOBAL",
+		"user": {
+			"id": 884378714,
+			"id_str": "884378714",
+			"name": "SMTOWN",
+			"screen_name": "SMTOWNGLOBAL",
+			"location": "",
+			"description": "SMEntertainment Group Official Twitter",
+			"url": "https://t.co/zjb7YWlJE5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zjb7YWlJE5",
+							"expanded_url": "http://www.smtown.com",
+							"display_url": "smtown.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10644961,
+			"friends_count": 56,
+			"listed_count": 11903,
+			"created_at": "Tue Oct 16 12:49:45 +0000 2012",
+			"favourites_count": 2026,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 11869,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "F08BA0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1477835872729206786/4DNyIiTo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1477835872729206786/4DNyIiTo_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/884378714/1641178515",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 363,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Dance crew ‘Team Auspicious’ will be joining the show with their high-quality dance performance. \n\n1st ASIA 22°03’04’’ 9PM (KST)\n2nd AMERICA 22°03’04’’ 9PM (PST)\n      ↘︎22°03’05’’ 2PM (KST)\n3rd EUROPE 22°03’05’’ 9PM (CET)\n      ↘︎22°03’06’’ 5AM (KST)\n\n#auspicious_crew https://t.co/3hFX6mkhzT"
+	},
+	{
+		"created_at": "Tue Mar 01 10:29:30 +0000 2022",
+		"id": 1498594704673239000,
+		"id_str": "1498606347075604480",
+		"full_text": "ScreaM TOGETHER!\n\nThe March 4th SHOWME’s #009 DJ is producer/DJ ‘DJ https://t.co/ZYOXlwefat’, who’s been showcasing iconic sound throughout his career and proved his talent by playing at Boiler Room party held in Seoul.\n\n#SHOWME #쇼미 \n#코커\n#ScreaMRecords\n#PinkBlood \n#BeyondLIVE https://t.co/SlZXe8f8VG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SHOWME",
+					"indices": [
+						221,
+						228
+					]
+				},
+				{
+					"text": "쇼미",
+					"indices": [
+						229,
+						232
+					]
+				},
+				{
+					"text": "코커",
+					"indices": [
+						234,
+						237
+					]
+				},
+				{
+					"text": "ScreaMRecords",
+					"indices": [
+						238,
+						252
+					]
+				},
+				{
+					"text": "PinkBlood",
+					"indices": [
+						253,
+						263
+					]
+				},
+				{
+					"text": "BeyondLIVE",
+					"indices": [
+						265,
+						276
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ZYOXlwefat",
+					"expanded_url": "http://co.kr",
+					"display_url": "co.kr",
+					"indices": [
+						68,
+						91
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1498594702060228600,
+					"id_str": "1498594702060228611",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/FMwT8Q6VEAMKvxG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FMwT8Q6VEAMKvxG.jpg",
+					"url": "https://t.co/SlZXe8f8VG",
+					"display_url": "pic.twitter.com/SlZXe8f8VG",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1498594704673239041/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1498594702060228600,
+					"id_str": "1498594702060228611",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/FMwT8Q6VEAMKvxG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FMwT8Q6VEAMKvxG.jpg",
+					"url": "https://t.co/SlZXe8f8VG",
+					"display_url": "pic.twitter.com/SlZXe8f8VG",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1498594704673239041/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 884378714,
+			"id_str": "884378714",
+			"name": "SMTOWN",
+			"screen_name": "SMTOWNGLOBAL",
+			"location": "",
+			"description": "SMEntertainment Group Official Twitter",
+			"url": "https://t.co/zjb7YWlJE5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zjb7YWlJE5",
+							"expanded_url": "http://www.smtown.com",
+							"display_url": "smtown.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10644961,
+			"friends_count": 56,
+			"listed_count": 11903,
+			"created_at": "Tue Oct 16 12:49:45 +0000 2012",
+			"favourites_count": 2026,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 11869,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "F08BA0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1477835872729206786/4DNyIiTo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1477835872729206786/4DNyIiTo_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/884378714/1641178515",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 123,
+		"favorite_count": 1577,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "ScreaM TOGETHER!\n\nThe March 4th SHOWME’s #009 DJ is producer/DJ ‘DJ https://t.co/ZYOXlwefat’, who’s been showcasing iconic sound throughout his career and proved his talent by playing at Boiler Room party held in Seoul.\n\n#SHOWME #쇼미 \n#코커\n#ScreaMRecords\n#PinkBlood \n#BeyondLIVE https://t.co/SlZXe8f8VG"
+	},
+	{
 		"created_at": "Tue Mar 01 09:29:29 +0000 2022",
 		"id": 1498585081069269000,
 		"id_str": "1498591244561391624",
