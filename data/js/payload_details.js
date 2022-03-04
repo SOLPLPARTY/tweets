707c4c2cd87fe0d2892b06edcc6a368bc3f81f1d
솔플파티 <solplparty@gmail.com>
2022-03-04 14:17:34 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 58646,
-	"created_at": "Fri Mar 04 04:17:33 +0000 2022",
+	"tweets": 58647,
+	"created_at": "Fri Mar 04 05:17:33 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Fri Mar 04 04:29:24 +0000 2022",
+		"id": 1499597050266095600,
+		"id_str": "1499602889987272705",
+		"full_text": "🔥Out Now🔥\n에디전 Eddie Chun (Beautyhandsome) New Single \n'다음 생에 그댈 찾을게요' I’ll Find You In the Next Life\n\n\"이루어질 수 없는 현실에서의 사랑을 다음 생에서라도 꼭 찾겠다는 애절한 곡\"\n2022.03.04.Fri.12pm\n( ｡･_･｡)人(｡･_･｡ )\n\n🎧 Streaming 🎧 https://t.co/2njeW2Or6E",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			198
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1499596974265229300,
+					"id_str": "1499596974265229312",
+					"indices": [
+						199,
+						222
+					],
+					"media_url": "http://pbs.twimg.com/media/FM-jgL1UYAAvwbC.png",
+					"media_url_https": "https://pbs.twimg.com/media/FM-jgL1UYAAvwbC.png",
+					"url": "https://t.co/2njeW2Or6E",
+					"display_url": "pic.twitter.com/2njeW2Or6E",
+					"expanded_url": "https://twitter.com/sugar_records/status/1499597050266095621/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1499596974265229300,
+					"id_str": "1499596974265229312",
+					"indices": [
+						199,
+						222
+					],
+					"media_url": "http://pbs.twimg.com/media/FM-jgL1UYAAvwbC.png",
+					"media_url_https": "https://pbs.twimg.com/media/FM-jgL1UYAAvwbC.png",
+					"url": "https://t.co/2njeW2Or6E",
+					"display_url": "pic.twitter.com/2njeW2Or6E",
+					"expanded_url": "https://twitter.com/sugar_records/status/1499597050266095621/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 94343878,
+			"id_str": "94343878",
+			"name": "슈가레코드",
+			"screen_name": "sugar_records",
+			"location": "Seoul, KR",
+			"description": "#달에닿아 #뷰티핸섬 #리썬 #모브닝 #엔분의일 #당기시오 #우리같은사람들 슈가레코드",
+			"url": "https://t.co/MaZwhWbQNT",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/MaZwhWbQNT",
+							"expanded_url": "http://sugar-records.com",
+							"display_url": "sugar-records.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 577,
+			"friends_count": 38,
+			"listed_count": 9,
+			"created_at": "Thu Dec 03 14:51:49 +0000 2009",
+			"favourites_count": 449,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7577,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1087640124270075904/QfkBjdmp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1087640124270075904/QfkBjdmp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/94343878/1548148600",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "DFDFDF",
+			"profile_sidebar_fill_color": "F3F3F3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "🔥Out Now🔥\n에디전 Eddie Chun (Beautyhandsome) New Single \n'다음 생에 그댈 찾을게요' I’ll Find You In the Next Life\n\n\"이루어질 수 없는 현실에서의 사랑을 다음 생에서라도 꼭 찾겠다는 애절한 곡\"\n2022.03.04.Fri.12pm\n( ｡･_･｡)人(｡･_･｡ )\n\n🎧 Streaming 🎧 https://t.co/2njeW2Or6E"
+	},
+	{
 		"created_at": "Fri Mar 04 03:29:34 +0000 2022",
 		"id": 1499586553404084200,
 		"id_str": "1499587830451949600",
