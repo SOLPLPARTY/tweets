8a2024a1c764e638ed86333353ac0f4b1b0833a7
솔플파티 <solplparty@gmail.com>
2022-03-05 15:17:34 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 58676,
-	"created_at": "Sat Mar 05 05:17:33 +0000 2022",
+	"tweets": 58677,
+	"created_at": "Sat Mar 05 06:17:33 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,289 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Sat Mar 05 06:11:17 +0000 2022",
+		"id": 1499986170935406600,
+		"id_str": "1499990913950617603",
+		"full_text": "13일(일) 공연을 보실분들은 아래와 같이 예매해주세요\n일시/3월13(일) 18:00\n장소/클럽빵\n입장료/ 3만원\n예약방법/ 입금(하나_771-910167-21407_최현정) 뒤에 문자로 입금자이름발송( 010-8910-1089) https://t.co/IWiAU7Hshd",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			128
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1499986165587669000,
+					"id_str": "1499986165587668996",
+					"indices": [
+						129,
+						152
+					],
+					"media_url": "http://pbs.twimg.com/media/FNEFeGfacAQqV27.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNEFeGfacAQqV27.jpg",
+					"url": "https://t.co/IWiAU7Hshd",
+					"display_url": "pic.twitter.com/IWiAU7Hshd",
+					"expanded_url": "https://twitter.com/club_bbang/status/1499986170935406595/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1499986165587669000,
+					"id_str": "1499986165587668996",
+					"indices": [
+						129,
+						152
+					],
+					"media_url": "http://pbs.twimg.com/media/FNEFeGfacAQqV27.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNEFeGfacAQqV27.jpg",
+					"url": "https://t.co/IWiAU7Hshd",
+					"display_url": "pic.twitter.com/IWiAU7Hshd",
+					"expanded_url": "https://twitter.com/club_bbang/status/1499986170935406595/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1499986165638008800,
+					"id_str": "1499986165638008840",
+					"indices": [
+						129,
+						152
+					],
+					"media_url": "http://pbs.twimg.com/media/FNEFeGrakAgj3QK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNEFeGrakAgj3QK.jpg",
+					"url": "https://t.co/IWiAU7Hshd",
+					"display_url": "pic.twitter.com/IWiAU7Hshd",
+					"expanded_url": "https://twitter.com/club_bbang/status/1499986170935406595/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1499986165608624000,
+					"id_str": "1499986165608624128",
+					"indices": [
+						129,
+						152
+					],
+					"media_url": "http://pbs.twimg.com/media/FNEFeGkaMAAk0HC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNEFeGkaMAAk0HC.jpg",
+					"url": "https://t.co/IWiAU7Hshd",
+					"display_url": "pic.twitter.com/IWiAU7Hshd",
+					"expanded_url": "https://twitter.com/club_bbang/status/1499986170935406595/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1499986165596061700,
+					"id_str": "1499986165596061701",
+					"indices": [
+						129,
+						152
+					],
+					"media_url": "http://pbs.twimg.com/media/FNEFeGhagAUaUY7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNEFeGhagAUaUY7.jpg",
+					"url": "https://t.co/IWiAU7Hshd",
+					"display_url": "pic.twitter.com/IWiAU7Hshd",
+					"expanded_url": "https://twitter.com/club_bbang/status/1499986170935406595/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1387140522,
+			"id_str": "1387140522",
+			"name": "클럽 빵",
+			"screen_name": "club_bbang",
+			"location": "서울 마포 서교동 327-18 지하",
+			"description": "모던록 라이브클럽 빵, 공연일정을 알려드려요! 문의 010-8910-1089, cafebbang@hanmail.net",
+			"url": "https://t.co/ZROqsiLHaK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ZROqsiLHaK",
+							"expanded_url": "http://cafe.daum.net/cafebbang",
+							"display_url": "cafe.daum.net/cafebbang",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2524,
+			"friends_count": 874,
+			"listed_count": 35,
+			"created_at": "Sun Apr 28 14:57:43 +0000 2013",
+			"favourites_count": 77,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1844,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3617429436/49aa1ea87a3bdf4e12ef8ab826e7b229_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3617429436/49aa1ea87a3bdf4e12ef8ab826e7b229_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1387140522/1367815826",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "13일(일) 공연을 보실분들은 아래와 같이 예매해주세요\n일시/3월13(일) 18:00\n장소/클럽빵\n입장료/ 3만원\n예약방법/ 입금(하나_771-910167-21407_최현정) 뒤에 문자로 입금자이름발송( 010-8910-1089) https://t.co/IWiAU7Hshd"
+	},
+	{
 		"created_at": "Sat Mar 05 05:05:15 +0000 2022",
 		"id": 1499974297456107500,
 		"id_str": "1499974297456107522",
