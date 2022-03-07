72a59cc3172c28e353e20062d73e6f348f0eda15
솔플파티 <solplparty@gmail.com>
2022-03-07 16:17:35 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 58711,
-	"created_at": "Mon Mar 07 05:17:33 +0000 2022",
+	"tweets": 58713,
+	"created_at": "Mon Mar 07 07:17:34 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,397 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Mon Mar 07 07:11:09 +0000 2022",
+		"id": 1500730756179574800,
+		"id_str": "1500730756179574785",
+		"full_text": "#유니버설뮤직코리아 #Universal_Music_Korea\n🎂해리포터와 존 윌리엄스의 생일을 “추카”합니다🥳 | 해리포터 레터링 케이크 만들기 | 유니키친\nhttps://t.co/lkoCARtp84",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			111
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "유니버설뮤직코리아",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"text": "Universal_Music_Korea",
+					"indices": [
+						11,
+						33
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/lkoCARtp84",
+					"expanded_url": "https://youtu.be/X_beUaiT49E",
+					"display_url": "youtu.be/X_beUaiT49E",
+					"indices": [
+						88,
+						111
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.powerapps.com\" rel=\"nofollow\">Microsoft Power Platform</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 968706450779463700,
+			"id_str": "968706450779463682",
+			"name": "솔플파티",
+			"screen_name": "SOLPLPARTY",
+			"location": "문의: contact@solplparty.com",
+			"description": "혼자서도 즐겁게! \n문화예술 커뮤니티, 솔플파티 🤘 \n\n▶ 참가하기 \nhttps://t.co/Uq1kQ79SRI \n\n❓ ABOUT\nhttps://t.co/OtYqQtyXgi ",
+			"url": "https://t.co/L6Ap35US8u",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/L6Ap35US8u",
+							"expanded_url": "https://solplparty.com",
+							"display_url": "solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/Uq1kQ79SRI",
+							"expanded_url": "http://join.solpl.party",
+							"display_url": "join.solpl.party",
+							"indices": [
+								39,
+								62
+							]
+						},
+						{
+							"url": "https://t.co/OtYqQtyXgi",
+							"expanded_url": "http://about.solpl.party",
+							"display_url": "about.solpl.party",
+							"indices": [
+								73,
+								96
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 252,
+			"friends_count": 467,
+			"listed_count": 3,
+			"created_at": "Wed Feb 28 04:36:30 +0000 2018",
+			"favourites_count": 240,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 80277,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/968706450779463682/1519792691",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#유니버설뮤직코리아 #Universal_Music_Korea\n🎂해리포터와 존 윌리엄스의 생일을 “추카”합니다🥳 | 해리포터 레터링 케이크 만들기 | 유니키친\nhttps://t.co/lkoCARtp84"
+	},
+	{
+		"created_at": "Mon Mar 07 06:29:32 +0000 2022",
+		"id": 1500713632509743000,
+		"id_str": "1500720283635920896",
+		"full_text": "[#Playlist]\n#NIve(#니브)의 ‘what is life?’가 #TIDAL'R&amp;B x K-Pop\n’ 플레이리스트 커버를 장식하였습니다😎✨\n\n🎶https://t.co/y1Gs6iZsba\n\n#warnerkpop #warnermusic #KPOP #NIve #니브 #whatislife #proLOG https://t.co/nxutsUnXdB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			174
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Playlist",
+					"indices": [
+						1,
+						10
+					]
+				},
+				{
+					"text": "NIve",
+					"indices": [
+						12,
+						17
+					]
+				},
+				{
+					"text": "니브",
+					"indices": [
+						18,
+						21
+					]
+				},
+				{
+					"text": "TIDAL",
+					"indices": [
+						41,
+						47
+					]
+				},
+				{
+					"text": "warnerkpop",
+					"indices": [
+						114,
+						125
+					]
+				},
+				{
+					"text": "warnermusic",
+					"indices": [
+						126,
+						138
+					]
+				},
+				{
+					"text": "KPOP",
+					"indices": [
+						139,
+						144
+					]
+				},
+				{
+					"text": "NIve",
+					"indices": [
+						145,
+						150
+					]
+				},
+				{
+					"text": "니브",
+					"indices": [
+						151,
+						154
+					]
+				},
+				{
+					"text": "whatislife",
+					"indices": [
+						155,
+						166
+					]
+				},
+				{
+					"text": "proLOG",
+					"indices": [
+						167,
+						174
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/y1Gs6iZsba",
+					"expanded_url": "http://TIDAL.link/KPopRnB",
+					"display_url": "TIDAL.link/KPopRnB",
+					"indices": [
+						89,
+						112
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1500713383040991200,
+					"id_str": "1500713383040991232",
+					"indices": [
+						175,
+						198
+					],
+					"media_url": "http://pbs.twimg.com/media/FNOa3unVQAAId_4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNOa3unVQAAId_4.jpg",
+					"url": "https://t.co/nxutsUnXdB",
+					"display_url": "pic.twitter.com/nxutsUnXdB",
+					"expanded_url": "https://twitter.com/Warner_Music_Kr/status/1500713632509743107/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1500713383040991200,
+					"id_str": "1500713383040991232",
+					"indices": [
+						175,
+						198
+					],
+					"media_url": "http://pbs.twimg.com/media/FNOa3unVQAAId_4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNOa3unVQAAId_4.jpg",
+					"url": "https://t.co/nxutsUnXdB",
+					"display_url": "pic.twitter.com/nxutsUnXdB",
+					"expanded_url": "https://twitter.com/Warner_Music_Kr/status/1500713632509743107/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 206117108,
+			"id_str": "206117108",
+			"name": "Warner Music Korea",
+			"screen_name": "Warner_Music_Kr",
+			"location": "Seoul, Korea",
+			"description": "워너뮤직코리아 공식 트위터",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 54521,
+			"friends_count": 494,
+			"listed_count": 199,
+			"created_at": "Fri Oct 22 09:32:47 +0000 2010",
+			"favourites_count": 1619,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 11479,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1267377694565490688/FD-WdFum_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1267377694565490688/FD-WdFum_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/206117108/1636703401",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "023BE5",
+			"profile_text_color": "2566FC",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 15,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#Playlist]\n#NIve(#니브)의 ‘what is life?’가 #TIDAL'R&amp;B x K-Pop\n’ 플레이리스트 커버를 장식하였습니다😎✨\n\n🎶https://t.co/y1Gs6iZsba\n\n#warnerkpop #warnermusic #KPOP #NIve #니브 #whatislife #proLOG https://t.co/nxutsUnXdB"
+	},
+	{
 		"created_at": "Mon Mar 07 05:12:47 +0000 2022",
 		"id": 1500700970782761000,
 		"id_str": "1500700970782760963",
