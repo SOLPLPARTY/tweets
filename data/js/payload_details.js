9ccd1179416483710a1d82520af0472a2879b5d0
솔플파티 <solplparty@gmail.com>
2022-03-12 22:17:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 58840,
-	"created_at": "Sat Mar 12 09:17:34 +0000 2022",
+	"tweets": 58841,
+	"created_at": "Sat Mar 12 13:17:35 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,334 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Sat Mar 12 13:11:16 +0000 2022",
+		"id": 1502630493342224400,
+		"id_str": "1502633325269340164",
+		"full_text": "📢 3/13 공연 현장 안내\n⠀\n&lt;9001 단독 콘서트：롤링 27주년 기념 공연&gt;\n· 티켓수령 : 4시 (현장구매 가능)\n· 입장 : 4시 30분 \n· 공연시작 : 5시\n· 출연 : 9001\n⠀\n#9001 #단독콘서트 #NINETYOONE\n#롤링홀 #ROLLINGHALL #롤링27주년 #롤링27주년기념공연 \n#홍대 #공연 #인디 #멜론티켓 https://t.co/3vZhqVQRhB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			196
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "단독콘서트",
+					"indices": [
+						121,
+						127
+					]
+				},
+				{
+					"text": "NINETYOONE",
+					"indices": [
+						128,
+						139
+					]
+				},
+				{
+					"text": "롤링홀",
+					"indices": [
+						140,
+						144
+					]
+				},
+				{
+					"text": "ROLLINGHALL",
+					"indices": [
+						145,
+						157
+					]
+				},
+				{
+					"text": "롤링27주년",
+					"indices": [
+						158,
+						165
+					]
+				},
+				{
+					"text": "롤링27주년기념공연",
+					"indices": [
+						166,
+						177
+					]
+				},
+				{
+					"text": "홍대",
+					"indices": [
+						179,
+						182
+					]
+				},
+				{
+					"text": "공연",
+					"indices": [
+						183,
+						186
+					]
+				},
+				{
+					"text": "인디",
+					"indices": [
+						187,
+						190
+					]
+				},
+				{
+					"text": "멜론티켓",
+					"indices": [
+						191,
+						196
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1501450616056725500,
+					"id_str": "1501450616056725507",
+					"indices": [
+						197,
+						220
+					],
+					"media_url": "http://pbs.twimg.com/media/FNY5YVnaQAMW9OF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNY5YVnaQAMW9OF.jpg",
+					"url": "https://t.co/3vZhqVQRhB",
+					"display_url": "pic.twitter.com/3vZhqVQRhB",
+					"expanded_url": "https://twitter.com/rollinghall1995/status/1502630493342224385/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 481,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 849,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 849,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1501450616056725500,
+					"id_str": "1501450616056725507",
+					"indices": [
+						197,
+						220
+					],
+					"media_url": "http://pbs.twimg.com/media/FNY5YVnaQAMW9OF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNY5YVnaQAMW9OF.jpg",
+					"url": "https://t.co/3vZhqVQRhB",
+					"display_url": "pic.twitter.com/3vZhqVQRhB",
+					"expanded_url": "https://twitter.com/rollinghall1995/status/1502630493342224385/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 481,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 849,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 849,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1501450616081883100,
+					"id_str": "1501450616081883137",
+					"indices": [
+						197,
+						220
+					],
+					"media_url": "http://pbs.twimg.com/media/FNY5YVtaIAEpYyk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNY5YVtaIAEpYyk.jpg",
+					"url": "https://t.co/3vZhqVQRhB",
+					"display_url": "pic.twitter.com/3vZhqVQRhB",
+					"expanded_url": "https://twitter.com/rollinghall1995/status/1502630493342224385/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 848,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 481,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 848,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1501450616094474200,
+					"id_str": "1501450616094474248",
+					"indices": [
+						197,
+						220
+					],
+					"media_url": "http://pbs.twimg.com/media/FNY5YVwaQAgvm2t.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNY5YVwaQAgvm2t.jpg",
+					"url": "https://t.co/3vZhqVQRhB",
+					"display_url": "pic.twitter.com/3vZhqVQRhB",
+					"expanded_url": "https://twitter.com/rollinghall1995/status/1502630493342224385/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1053,
+							"h": 789,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1053,
+							"h": 789,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1291327050578456600,
+			"id_str": "1291327050578456578",
+			"name": "롤링홀",
+			"screen_name": "rollinghall1995",
+			"location": "서울시 마포구 어울마당로 35 신보빌딩 지하1층",
+			"description": "홍대/합정에 위치한 음악 공연장 #롤링홀 입니다٩( ᐛ )و\nConcert live hall in South Korea\n-\n👇🏻👇🏻멜론티켓 롤링홀 기획공연 바로가기.\nhttps://t.co/K14VaPTNnq",
+			"url": "https://t.co/C9YRGggByu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/C9YRGggByu",
+							"expanded_url": "http://www.rollinghall.co.kr/default/",
+							"display_url": "rollinghall.co.kr/default/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/K14VaPTNnq",
+							"expanded_url": "http://bit.ly/3C4QrHt",
+							"display_url": "bit.ly/3C4QrHt",
+							"indices": [
+								93,
+								116
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 419,
+			"friends_count": 3,
+			"listed_count": 4,
+			"created_at": "Thu Aug 06 10:56:17 +0000 2020",
+			"favourites_count": 133,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 453,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1291327708689862662/9hOLVWB2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1291327708689862662/9hOLVWB2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1291327050578456578/1596712102",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "📢 3/13 공연 현장 안내\n⠀\n&lt;9001 단독 콘서트：롤링 27주년 기념 공연&gt;\n· 티켓수령 : 4시 (현장구매 가능)\n· 입장 : 4시 30분 \n· 공연시작 : 5시\n· 출연 : 9001\n⠀\n#9001 #단독콘서트 #NINETYOONE\n#롤링홀 #ROLLINGHALL #롤링27주년 #롤링27주년기념공연 \n#홍대 #공연 #인디 #멜론티켓 https://t.co/3vZhqVQRhB"
+	},
+	{
 		"created_at": "Sat Mar 12 09:09:50 +0000 2022",
 		"id": 1502572565469954000,
 		"id_str": "1502572565469954050",
