404f4ebed1c68e6e1d628ce7c7288f37b7f6c283
솔플파티 <solplparty@gmail.com>
2022-03-13 21:17:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 58848,
-	"created_at": "Sun Mar 13 10:17:34 +0000 2022",
+	"tweets": 58849,
+	"created_at": "Sun Mar 13 12:17:35 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,197 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Sun Mar 13 11:29:25 +0000 2022",
+		"id": 1502962677190054000,
+		"id_str": "1502970078127697922",
+		"full_text": "D-5\nBIGONE (빅원) New Single Coming.\n2022.03.18 6PM\n\n빅원의 새로운 싱글이 3월 18일 (금) 오후 6시에 발매됩니다.\n\n#BIGONE #빅원\n#TheDialMusic #더다이얼뮤직 https://t.co/qK85VG5VWp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			122
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BIGONE",
+					"indices": [
+						89,
+						96
+					]
+				},
+				{
+					"text": "빅원",
+					"indices": [
+						97,
+						100
+					]
+				},
+				{
+					"text": "TheDialMusic",
+					"indices": [
+						101,
+						114
+					]
+				},
+				{
+					"text": "더다이얼뮤직",
+					"indices": [
+						115,
+						122
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1502918694833639400,
+					"id_str": "1502918694833639431",
+					"indices": [
+						123,
+						146
+					],
+					"media_url": "http://pbs.twimg.com/media/FNtwlxPaUAcaa-C.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNtwlxPaUAcaa-C.jpg",
+					"url": "https://t.co/qK85VG5VWp",
+					"display_url": "pic.twitter.com/qK85VG5VWp",
+					"expanded_url": "https://twitter.com/Warner_Music_Kr/status/1502962677190053892/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1638,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1502918694833639400,
+					"id_str": "1502918694833639431",
+					"indices": [
+						123,
+						146
+					],
+					"media_url": "http://pbs.twimg.com/media/FNtwlxPaUAcaa-C.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNtwlxPaUAcaa-C.jpg",
+					"url": "https://t.co/qK85VG5VWp",
+					"display_url": "pic.twitter.com/qK85VG5VWp",
+					"expanded_url": "https://twitter.com/Warner_Music_Kr/status/1502962677190053892/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1638,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 206117108,
+			"id_str": "206117108",
+			"name": "Warner Music Korea",
+			"screen_name": "Warner_Music_Kr",
+			"location": "Seoul, Korea",
+			"description": "워너뮤직코리아 공식 트위터",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 54540,
+			"friends_count": 494,
+			"listed_count": 199,
+			"created_at": "Fri Oct 22 09:32:47 +0000 2010",
+			"favourites_count": 1619,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 11484,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1267377694565490688/FD-WdFum_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1267377694565490688/FD-WdFum_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/206117108/1636703401",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "023BE5",
+			"profile_text_color": "2566FC",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 12,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "D-5\nBIGONE (빅원) New Single Coming.\n2022.03.18 6PM\n\n빅원의 새로운 싱글이 3월 18일 (금) 오후 6시에 발매됩니다.\n\n#BIGONE #빅원\n#TheDialMusic #더다이얼뮤직 https://t.co/qK85VG5VWp"
+	},
+	{
 		"created_at": "Sun Mar 13 09:29:24 +0000 2022",
 		"id": 1502936245365735400,
 		"id_str": "1502939878568382469",
