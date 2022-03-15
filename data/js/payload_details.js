93d992252e39c9d746e17cf2bf838203e443b5ff
솔플파티 <solplparty@gmail.com>
2022-03-15 10:17:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 58871,
-	"created_at": "Mon Mar 14 16:17:36 +0000 2022",
+	"tweets": 58872,
+	"created_at": "Tue Mar 15 01:17:35 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,238 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Tue Mar 15 01:11:17 +0000 2022",
+		"id": 1503538893635088400,
+		"id_str": "1503539295877017603",
+		"full_text": "이번 빵동행프로젝트는 일렉트릭뮤즈와 함께 합니다 뛰어난 인디레이블 일렉트릭뮤즈와 함께 봄의 시간으로 쑥 들어가봐요 #일렉트릭뮤즈 #김목인 #다정 #박연두 #민수홍 #클럽빵동행프로젝트 #클럽빵  #봄을만들어요 https://t.co/sV096dIyOn",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			115
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "일렉트릭뮤즈",
+					"indices": [
+						64,
+						71
+					]
+				},
+				{
+					"text": "김목인",
+					"indices": [
+						72,
+						76
+					]
+				},
+				{
+					"text": "다정",
+					"indices": [
+						77,
+						80
+					]
+				},
+				{
+					"text": "박연두",
+					"indices": [
+						81,
+						85
+					]
+				},
+				{
+					"text": "민수홍",
+					"indices": [
+						86,
+						90
+					]
+				},
+				{
+					"text": "클럽빵동행프로젝트",
+					"indices": [
+						91,
+						101
+					]
+				},
+				{
+					"text": "클럽빵",
+					"indices": [
+						102,
+						106
+					]
+				},
+				{
+					"text": "봄을만들어요",
+					"indices": [
+						108,
+						115
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1503538886181802000,
+					"id_str": "1503538886181801989",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/FN2kpqDaUAUnBF8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FN2kpqDaUAUnBF8.jpg",
+					"url": "https://t.co/sV096dIyOn",
+					"display_url": "pic.twitter.com/sV096dIyOn",
+					"expanded_url": "https://twitter.com/club_bbang/status/1503538893635088387/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1503538886181802000,
+					"id_str": "1503538886181801989",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/FN2kpqDaUAUnBF8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FN2kpqDaUAUnBF8.jpg",
+					"url": "https://t.co/sV096dIyOn",
+					"display_url": "pic.twitter.com/sV096dIyOn",
+					"expanded_url": "https://twitter.com/club_bbang/status/1503538893635088387/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1387140522,
+			"id_str": "1387140522",
+			"name": "클럽 빵",
+			"screen_name": "club_bbang",
+			"location": "서울 마포 서교동 327-18 지하",
+			"description": "모던록 라이브클럽 빵, 공연일정을 알려드려요! 문의 010-8910-1089, cafebbang@hanmail.net",
+			"url": "https://t.co/ZROqsiLHaK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ZROqsiLHaK",
+							"expanded_url": "http://cafe.daum.net/cafebbang",
+							"display_url": "cafe.daum.net/cafebbang",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2522,
+			"friends_count": 872,
+			"listed_count": 35,
+			"created_at": "Sun Apr 28 14:57:43 +0000 2013",
+			"favourites_count": 77,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1847,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3617429436/49aa1ea87a3bdf4e12ef8ab826e7b229_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3617429436/49aa1ea87a3bdf4e12ef8ab826e7b229_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1387140522/1367815826",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "이번 빵동행프로젝트는 일렉트릭뮤즈와 함께 합니다 뛰어난 인디레이블 일렉트릭뮤즈와 함께 봄의 시간으로 쑥 들어가봐요 #일렉트릭뮤즈 #김목인 #다정 #박연두 #민수홍 #클럽빵동행프로젝트 #클럽빵  #봄을만들어요 https://t.co/sV096dIyOn"
+	},
+	{
 		"created_at": "Mon Mar 14 16:11:16 +0000 2022",
 		"id": 1503395568235675600,
 		"id_str": "1503403399135514624",
