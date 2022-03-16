8cdc88e2a040cda50d30a4855a569c238e35944f
솔플파티 <solplparty@gmail.com>
2022-03-16 11:17:35 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 58890,
-	"created_at": "Tue Mar 15 16:17:35 +0000 2022",
+	"tweets": 58891,
+	"created_at": "Wed Mar 16 02:17:34 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,156 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Wed Mar 16 01:29:33 +0000 2022",
+		"id": 1503902617529102300,
+		"id_str": "1503906280964083718",
+		"full_text": "[NEWS] ‘부전시’ AVA인자벨라(#인순이), 깜짝 등장 이유는? 무대 촬영 소식 발표\n\n👉 https://t.co/9gdeWKiZKx\n\n#INSOONI #휴맵컨텐츠 #HUMAPCONTENTS #부캐전성시대 #TV조선",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			123
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "인순이",
+					"indices": [
+						21,
+						25
+					]
+				},
+				{
+					"text": "INSOONI",
+					"indices": [
+						79,
+						87
+					]
+				},
+				{
+					"text": "휴맵컨텐츠",
+					"indices": [
+						88,
+						94
+					]
+				},
+				{
+					"text": "HUMAPCONTENTS",
+					"indices": [
+						95,
+						109
+					]
+				},
+				{
+					"text": "부캐전성시대",
+					"indices": [
+						110,
+						117
+					]
+				},
+				{
+					"text": "TV조선",
+					"indices": [
+						118,
+						123
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/9gdeWKiZKx",
+					"expanded_url": "http://naver.me/xGbdTFMj",
+					"display_url": "naver.me/xGbdTFMj",
+					"indices": [
+						54,
+						77
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1219506842935513000,
+			"id_str": "1219506842935513088",
+			"name": "휴맵컨텐츠(HUMAP)",
+			"screen_name": "humap_official",
+			"location": "",
+			"description": "휴맵컨텐츠(HUMAP CONTENTS) Official Twitter",
+			"url": "https://t.co/fBZZK3SmX6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fBZZK3SmX6",
+							"expanded_url": "http://www.humapcontents.com/",
+							"display_url": "humapcontents.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 392,
+			"friends_count": 2,
+			"listed_count": 2,
+			"created_at": "Tue Jan 21 06:28:27 +0000 2020",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 643,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1219506842935513088/1579755898",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[NEWS] ‘부전시’ AVA인자벨라(#인순이), 깜짝 등장 이유는? 무대 촬영 소식 발표\n\n👉 https://t.co/9gdeWKiZKx\n\n#INSOONI #휴맵컨텐츠 #HUMAPCONTENTS #부캐전성시대 #TV조선"
+	},
+	{
 		"created_at": "Tue Mar 15 15:29:24 +0000 2022",
 		"id": 1503747849363751000,
 		"id_str": "1503755251224092687",
