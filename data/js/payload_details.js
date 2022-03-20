9178702c2675f7729793e8bfb6dd0b6f4eb6fac4
솔플파티 <solplparty@gmail.com>
2022-03-20 22:17:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 59025,
-	"created_at": "Sun Mar 20 10:17:37 +0000 2022",
+	"tweets": 59026,
+	"created_at": "Sun Mar 20 13:17:37 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Sun Mar 20 13:11:17 +0000 2022",
+		"id": 1505515145266565000,
+		"id_str": "1505532429079126017",
+		"full_text": "[재미공작소 4월 팝업+]\n새앙북스 팝업숍 2022.4.9(토) PM 1:00-6:00\n——\n'재미공작소 팝업+'는 '재미공작소 팝업숍'의 새로운 프로젝트 명칭입니다. 더욱 다양한 호스트 창작자들과 더욱 다양한 팝업 행사로 만나요!  \n\nhttps://t.co/K5yTx6r4pP https://t.co/6dg0xpJJkF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			156
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/K5yTx6r4pP",
+					"expanded_url": "https://m.blog.naver.com/studiozemi/222677167400",
+					"display_url": "m.blog.naver.com/studiozemi/222…",
+					"indices": [
+						133,
+						156
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1505515088018432000,
+					"id_str": "1505515088018432009",
+					"indices": [
+						157,
+						180
+					],
+					"media_url": "http://pbs.twimg.com/media/FOSp_vlUYAk85Ba.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FOSp_vlUYAk85Ba.jpg",
+					"url": "https://t.co/6dg0xpJJkF",
+					"display_url": "pic.twitter.com/6dg0xpJJkF",
+					"expanded_url": "https://twitter.com/studio_zemi/status/1505515145266565120/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1890,
+							"h": 1890,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1505515088018432000,
+					"id_str": "1505515088018432009",
+					"indices": [
+						157,
+						180
+					],
+					"media_url": "http://pbs.twimg.com/media/FOSp_vlUYAk85Ba.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FOSp_vlUYAk85Ba.jpg",
+					"url": "https://t.co/6dg0xpJJkF",
+					"display_url": "pic.twitter.com/6dg0xpJJkF",
+					"expanded_url": "https://twitter.com/studio_zemi/status/1505515145266565120/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1890,
+							"h": 1890,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 282449618,
+			"id_str": "282449618",
+			"name": "재미공작소",
+			"screen_name": "studio_zemi",
+			"location": "서울시 영등포구 문래동 3가 58-84 1층",
+			"description": "재미공작소는 서울 문래동에 위치한 문화예술공간입니다. (Since 2011) 공연, 문학 행사, 팝업숍, 전시, 창작 워크숍, 출판, 캘린더 갤러리 등 '재미있는 일'을 기획하고 진행합니다. 재미공작소의 일정 및 예약 관련 정보는 아래 홈블로그 링크에서 바로 확인하실 수 있습니다.",
+			"url": "https://t.co/aLQQKEqovK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aLQQKEqovK",
+							"expanded_url": "http://blog.naver.com/studiozemi",
+							"display_url": "blog.naver.com/studiozemi",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8369,
+			"friends_count": 557,
+			"listed_count": 82,
+			"created_at": "Fri Apr 15 07:08:52 +0000 2011",
+			"favourites_count": 2333,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 14687,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1314827654/zemi02_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1314827654/zemi02_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/282449618/1398647875",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[재미공작소 4월 팝업+]\n새앙북스 팝업숍 2022.4.9(토) PM 1:00-6:00\n——\n'재미공작소 팝업+'는 '재미공작소 팝업숍'의 새로운 프로젝트 명칭입니다. 더욱 다양한 호스트 창작자들과 더욱 다양한 팝업 행사로 만나요!  \n\nhttps://t.co/K5yTx6r4pP https://t.co/6dg0xpJJkF"
+	},
+	{
 		"created_at": "Sun Mar 20 09:29:25 +0000 2022",
 		"id": 1505469517664641000,
 		"id_str": "1505476594365829123",
