eb717246db0d948584c89bb9cca7a88ab012f68c
솔플파티 <solplparty@gmail.com>
2022-03-24 13:17:39 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 59116,
-	"created_at": "Thu Mar 24 03:17:37 +0000 2022",
+	"tweets": 59117,
+	"created_at": "Thu Mar 24 04:17:39 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,252 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Thu Mar 24 03:29:24 +0000 2022",
+		"id": 1506828164945027000,
+		"id_str": "1506835547406626819",
+		"full_text": "[#손태진] 포르테 디 콰트로 정규4집 'METAPHONIC' 발매✔\n\n손태진이 참여한 포르테 디 콰트로의 정규 4집📀 'METAPHONIC'은 지금 바로 전 음원 사이트에서 확인🎼\n\n#SONTAEJIN #TJSON #포르테디콰트로 #포디콰 #FortediQuattro #FDQ #정규4집 #METAPHONIC #메타포닉 https://t.co/OGSD4fUmL5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			179
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "손태진",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "SONTAEJIN",
+					"indices": [
+						103,
+						113
+					]
+				},
+				{
+					"text": "TJSON",
+					"indices": [
+						114,
+						120
+					]
+				},
+				{
+					"text": "포르테디콰트로",
+					"indices": [
+						121,
+						129
+					]
+				},
+				{
+					"text": "포디콰",
+					"indices": [
+						130,
+						134
+					]
+				},
+				{
+					"text": "FortediQuattro",
+					"indices": [
+						135,
+						150
+					]
+				},
+				{
+					"text": "FDQ",
+					"indices": [
+						151,
+						155
+					]
+				},
+				{
+					"text": "정규4집",
+					"indices": [
+						156,
+						161
+					]
+				},
+				{
+					"text": "METAPHONIC",
+					"indices": [
+						162,
+						173
+					]
+				},
+				{
+					"text": "메타포닉",
+					"indices": [
+						174,
+						179
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1506828154186666000,
+					"id_str": "1506828154186665986",
+					"indices": [
+						180,
+						203
+					],
+					"media_url": "http://pbs.twimg.com/media/FOlUOQPakAI4KBG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FOlUOQPakAI4KBG.jpg",
+					"url": "https://t.co/OGSD4fUmL5",
+					"display_url": "pic.twitter.com/OGSD4fUmL5",
+					"expanded_url": "https://twitter.com/themysticstory/status/1506828164945027080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1506828154186666000,
+					"id_str": "1506828154186665986",
+					"indices": [
+						180,
+						203
+					],
+					"media_url": "http://pbs.twimg.com/media/FOlUOQPakAI4KBG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FOlUOQPakAI4KBG.jpg",
+					"url": "https://t.co/OGSD4fUmL5",
+					"display_url": "pic.twitter.com/OGSD4fUmL5",
+					"expanded_url": "https://twitter.com/themysticstory/status/1506828164945027080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/pgQyM3XZDx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pgQyM3XZDx",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 34514,
+			"friends_count": 15,
+			"listed_count": 280,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6793,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1645607843",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 20,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#손태진] 포르테 디 콰트로 정규4집 'METAPHONIC' 발매✔\n\n손태진이 참여한 포르테 디 콰트로의 정규 4집📀 'METAPHONIC'은 지금 바로 전 음원 사이트에서 확인🎼\n\n#SONTAEJIN #TJSON #포르테디콰트로 #포디콰 #FortediQuattro #FDQ #정규4집 #METAPHONIC #메타포닉 https://t.co/OGSD4fUmL5"
+	},
+	{
 		"created_at": "Thu Mar 24 03:14:17 +0000 2022",
 		"id": 1506831740005888000,
 		"id_str": "1506831740005888002",
