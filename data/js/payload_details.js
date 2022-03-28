da716841fd03070ac6f2d87e12eacfc86e70da6f
솔플파티 <solplparty@gmail.com>
2022-03-29 00:17:40 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 59247,
-	"created_at": "Mon Mar 28 12:17:41 +0000 2022",
+	"tweets": 59248,
+	"created_at": "Mon Mar 28 15:17:39 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Mon Mar 28 14:29:32 +0000 2022",
+		"id": 1508446331639312400,
+		"id_str": "1508451226828087297",
+		"full_text": "[#정진운] SBS 파워FM '딘딘의 뮤직하이' 스페셜 DJ 출연✔\n\n정진운이 스페셜 DJ로 '딘딘의 뮤직하이'와 함께합니다📻\nSBS 파워FM 107.7MHz, 보이는 라디오에서 만나요💛\n🔗 https://t.co/JlEC6SkVML\n\n#Jinwoon #SBS파워FM #딘딘의뮤직하이",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			158
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "정진운",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "Jinwoon",
+					"indices": [
+						132,
+						140
+					]
+				},
+				{
+					"text": "SBS파워FM",
+					"indices": [
+						141,
+						149
+					]
+				},
+				{
+					"text": "딘딘의뮤직하이",
+					"indices": [
+						150,
+						158
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/JlEC6SkVML",
+					"expanded_url": "https://bit.ly/36TuShD",
+					"display_url": "bit.ly/36TuShD",
+					"indices": [
+						107,
+						130
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/pgQyM3XZDx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pgQyM3XZDx",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 34631,
+			"friends_count": 15,
+			"listed_count": 282,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6803,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1645607843",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#정진운] SBS 파워FM '딘딘의 뮤직하이' 스페셜 DJ 출연✔\n\n정진운이 스페셜 DJ로 '딘딘의 뮤직하이'와 함께합니다📻\nSBS 파워FM 107.7MHz, 보이는 라디오에서 만나요💛\n🔗 https://t.co/JlEC6SkVML\n\n#Jinwoon #SBS파워FM #딘딘의뮤직하이"
+	},
+	{
 		"created_at": "Mon Mar 28 11:29:24 +0000 2022",
 		"id": 1508398505324277800,
 		"id_str": "1508405894169649153",
