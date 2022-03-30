bbda2b652ccbadad8110777ec3f6a701e0751618
솔플파티 <solplparty@gmail.com>
2022-03-30 10:17:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 59280,
-	"created_at": "Wed Mar 30 00:17:39 +0000 2022",
+	"tweets": 59281,
+	"created_at": "Wed Mar 30 01:17:37 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,272 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Wed Mar 30 01:11:17 +0000 2022",
+		"id": 1508973557036220400,
+		"id_str": "1508975113269661700",
+		"full_text": "‼️이번주 금요일‼️\n여러분들과 드디어 만날 수 있는 시간을 기대하며, 불고기디스코 분들의 영상이 도착했습니다💌\n\n- 타이틀 : 티키타카 콘서트 : 불고기디스코\n- 일시 : 2022년 4월 1일 금요일 오후 7시30분 (입장 : 6시50분)\n\nhttps://t.co/geQue6YWoM\n\n#프리즘홀 #prismhall #불고기디스코 https://t.co/3BdB85Xwc7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			185
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "프리즘홀",
+					"indices": [
+						161,
+						166
+					]
+				},
+				{
+					"text": "prismhall",
+					"indices": [
+						167,
+						177
+					]
+				},
+				{
+					"text": "불고기디스코",
+					"indices": [
+						178,
+						185
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/geQue6YWoM",
+					"expanded_url": "https://ticket.melon.com/performance/index.htm?prodId=206633",
+					"display_url": "ticket.melon.com/performance/in…",
+					"indices": [
+						136,
+						159
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1508973506473906200,
+					"id_str": "1508973506473906176",
+					"indices": [
+						186,
+						209
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1508973506473906176/pu/img/pqwHGbO6ZGK-tqZ2.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1508973506473906176/pu/img/pqwHGbO6ZGK-tqZ2.jpg",
+					"url": "https://t.co/3BdB85Xwc7",
+					"display_url": "pic.twitter.com/3BdB85Xwc7",
+					"expanded_url": "https://twitter.com/prismhall/status/1508973557036220420/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 406,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 406,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1508973506473906200,
+					"id_str": "1508973506473906176",
+					"indices": [
+						186,
+						209
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1508973506473906176/pu/img/pqwHGbO6ZGK-tqZ2.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1508973506473906176/pu/img/pqwHGbO6ZGK-tqZ2.jpg",
+					"url": "https://t.co/3BdB85Xwc7",
+					"display_url": "pic.twitter.com/3BdB85Xwc7",
+					"expanded_url": "https://twitter.com/prismhall/status/1508973557036220420/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 406,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 406,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							360,
+							203
+						],
+						"duration_millis": 57066,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1508973506473906176/pu/vid/720x406/HOKx5WfniF_7rgyQ.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1508973506473906176/pu/pl/aiG1Qi8nIrZTYvjc.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1508973506473906176/pu/vid/478x270/YF7bwtv3uXTnWh_T.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 491231175,
+			"id_str": "491231175",
+			"name": "PrismHall",
+			"screen_name": "prismhall",
+			"location": "Mapo Seokyo-Dong",
+			"description": "최상급 무대장비와 시설. 엄청난 높이의 천고와 광활한 객석. 각종 파티,힙합,댄스,밴드 등 all_round concert, 실내페스티벌 등 모든 게 가능한 공연장입니다!",
+			"url": "https://t.co/VTKFWeLxvh",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/VTKFWeLxvh",
+							"expanded_url": "http://www.prismhall.com",
+							"display_url": "prismhall.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2045,
+			"friends_count": 1115,
+			"listed_count": 0,
+			"created_at": "Mon Feb 13 12:03:08 +0000 2012",
+			"favourites_count": 71,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1997,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/942698829089869824/bkVw2sYC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/942698829089869824/bkVw2sYC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/491231175/1513591891",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "1104338421d4e001",
+			"url": "https://api.twitter.com/1.1/geo/id/1104338421d4e001.json",
+			"place_type": "poi",
+			"name": "프리즘홀",
+			"full_name": "프리즘홀",
+			"country_code": "KR",
+			"country": "대한민국",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							126.917463,
+							37.551686
+						],
+						[
+							126.917463,
+							37.551686
+						],
+						[
+							126.917463,
+							37.551686
+						],
+						[
+							126.917463,
+							37.551686
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "‼️이번주 금요일‼️\n여러분들과 드디어 만날 수 있는 시간을 기대하며, 불고기디스코 분들의 영상이 도착했습니다💌\n\n- 타이틀 : 티키타카 콘서트 : 불고기디스코\n- 일시 : 2022년 4월 1일 금요일 오후 7시30분 (입장 : 6시50분)\n\nhttps://t.co/geQue6YWoM\n\n#프리즘홀 #prismhall #불고기디스코 https://t.co/3BdB85Xwc7"
+	},
+	{
 		"created_at": "Tue Mar 29 23:58:47 +0000 2022",
 		"id": 1508955938115711000,
 		"id_str": "1508956871482347522",
