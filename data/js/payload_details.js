c1857399e10d6d2bfcf9a0ce54ed090edd3bdd58
솔플파티 <solplparty@gmail.com>
2022-04-01 16:17:33 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 59352,
-	"created_at": "Fri Apr 01 06:17:32 +0000 2022",
+	"tweets": 59353,
+	"created_at": "Fri Apr 01 07:17:32 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,170 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Fri Apr 01 06:29:24 +0000 2022",
+		"id": 1509776437808574500,
+		"id_str": "1509779949334536196",
+		"full_text": "[VIDEO] [#인순이] #토크콘서트\n\n[동명대학교]2022 동기유발학기 토크콘서트 “THE JUMP” 라이브 방송\n\n👉 https://t.co/501xBsNaJk\n\n#인순이 #INSOONI #휴맵컨텐츠 #HUMAPCONTENTS #동명대학교 #THE_JUMP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			146
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "인순이",
+					"indices": [
+						9,
+						13
+					]
+				},
+				{
+					"text": "토크콘서트",
+					"indices": [
+						15,
+						21
+					]
+				},
+				{
+					"text": "인순이",
+					"indices": [
+						94,
+						98
+					]
+				},
+				{
+					"text": "INSOONI",
+					"indices": [
+						99,
+						107
+					]
+				},
+				{
+					"text": "휴맵컨텐츠",
+					"indices": [
+						108,
+						114
+					]
+				},
+				{
+					"text": "HUMAPCONTENTS",
+					"indices": [
+						115,
+						129
+					]
+				},
+				{
+					"text": "동명대학교",
+					"indices": [
+						130,
+						136
+					]
+				},
+				{
+					"text": "THE_JUMP",
+					"indices": [
+						137,
+						146
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/501xBsNaJk",
+					"expanded_url": "https://youtu.be/mMAuCt_sLPo",
+					"display_url": "youtu.be/mMAuCt_sLPo",
+					"indices": [
+						69,
+						92
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1219506842935513000,
+			"id_str": "1219506842935513088",
+			"name": "휴맵컨텐츠(HUMAP)",
+			"screen_name": "humap_official",
+			"location": "",
+			"description": "휴맵컨텐츠(HUMAP CONTENTS) Official Twitter",
+			"url": "https://t.co/fBZZK3SmX6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fBZZK3SmX6",
+							"expanded_url": "http://www.humapcontents.com/",
+							"display_url": "humapcontents.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 397,
+			"friends_count": 2,
+			"listed_count": 2,
+			"created_at": "Tue Jan 21 06:28:27 +0000 2020",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 671,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1219506842935513088/1579755898",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[VIDEO] [#인순이] #토크콘서트\n\n[동명대학교]2022 동기유발학기 토크콘서트 “THE JUMP” 라이브 방송\n\n👉 https://t.co/501xBsNaJk\n\n#인순이 #INSOONI #휴맵컨텐츠 #HUMAPCONTENTS #동명대학교 #THE_JUMP"
+	},
+	{
 		"created_at": "Fri Apr 01 05:29:41 +0000 2022",
 		"id": 1509751369879941000,
 		"id_str": "1509764919880822803",
