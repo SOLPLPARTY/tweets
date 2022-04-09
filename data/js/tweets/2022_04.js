e335677770d6b6221d6ad7936dc2c568c81f70f4
솔플파티 <solplparty@gmail.com>
2022-04-09 14:17:35 +0900

    Added 1 tweets

--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,300 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Sat Apr 09 05:11:17 +0000 2022",
+		"id": 1512644596383158300,
+		"id_str": "1512659389815140354",
+		"full_text": "[재미공작소 4월 팝업+]\n\n새앙북스(@Birdang_Books) 팝업숍 오픈했습니다! 새앙북스의 책들과 귀여운 굿즈를 만나보실 수 있어요. 오늘(4.9) 오후 1시부터 6시까지 진행됩니다. \n\n팝업숍 방문길에 근처 문래공원에서 벚꽃산책도 함께 즐겨보세요😉🌸 https://t.co/KtXyj01jhB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			144
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Birdang_Books",
+					"name": "새앙북스",
+					"id": 1138686692448014300,
+					"id_str": "1138686692448014336",
+					"indices": [
+						21,
+						35
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1512644564821098500,
+					"id_str": "1512644564821098497",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/FP3-N6WWQAECNuu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FP3-N6WWQAECNuu.jpg",
+					"url": "https://t.co/KtXyj01jhB",
+					"display_url": "pic.twitter.com/KtXyj01jhB",
+					"expanded_url": "https://twitter.com/studio_zemi/status/1512644596383158279/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1512644564821098500,
+					"id_str": "1512644564821098497",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/FP3-N6WWQAECNuu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FP3-N6WWQAECNuu.jpg",
+					"url": "https://t.co/KtXyj01jhB",
+					"display_url": "pic.twitter.com/KtXyj01jhB",
+					"expanded_url": "https://twitter.com/studio_zemi/status/1512644596383158279/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1512644564967993300,
+					"id_str": "1512644564967993346",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/FP3-N65XsAICYHL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FP3-N65XsAICYHL.jpg",
+					"url": "https://t.co/KtXyj01jhB",
+					"display_url": "pic.twitter.com/KtXyj01jhB",
+					"expanded_url": "https://twitter.com/studio_zemi/status/1512644596383158279/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1512644565375008800,
+					"id_str": "1512644565375008768",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/FP3-N8aaQAA3KvD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FP3-N8aaQAA3KvD.jpg",
+					"url": "https://t.co/KtXyj01jhB",
+					"display_url": "pic.twitter.com/KtXyj01jhB",
+					"expanded_url": "https://twitter.com/studio_zemi/status/1512644596383158279/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1512644565223841800,
+					"id_str": "1512644565223841794",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/FP3-N72XoAIEd8G.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FP3-N72XoAIEd8G.jpg",
+					"url": "https://t.co/KtXyj01jhB",
+					"display_url": "pic.twitter.com/KtXyj01jhB",
+					"expanded_url": "https://twitter.com/studio_zemi/status/1512644596383158279/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 282449618,
+			"id_str": "282449618",
+			"name": "재미공작소",
+			"screen_name": "studio_zemi",
+			"location": "서울시 영등포구 문래동 3가 58-84 1층",
+			"description": "재미공작소는 서울 문래동에 위치한 문화예술공간입니다. (Since 2011) 공연, 문학 행사, 팝업숍, 전시, 창작 워크숍, 출판, 캘린더 갤러리 등 '재미있는 일'을 기획하고 진행합니다. 재미공작소의 일정 및 예약 관련 정보는 아래 홈블로그 링크에서 바로 확인하실 수 있습니다.",
+			"url": "https://t.co/aLQQKEqovK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aLQQKEqovK",
+							"expanded_url": "http://blog.naver.com/studiozemi",
+							"display_url": "blog.naver.com/studiozemi",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8369,
+			"friends_count": 557,
+			"listed_count": 82,
+			"created_at": "Fri Apr 15 07:08:52 +0000 2011",
+			"favourites_count": 2340,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 14689,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1314827654/zemi02_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1314827654/zemi02_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/282449618/1398647875",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[재미공작소 4월 팝업+]\n\n새앙북스(@Birdang_Books) 팝업숍 오픈했습니다! 새앙북스의 책들과 귀여운 굿즈를 만나보실 수 있어요. 오늘(4.9) 오후 1시부터 6시까지 진행됩니다. \n\n팝업숍 방문길에 근처 문래공원에서 벚꽃산책도 함께 즐겨보세요😉🌸 https://t.co/KtXyj01jhB"
+	},
+	{
 		"created_at": "Sat Apr 09 01:29:33 +0000 2022",
 		"id": 1512602406592614400,
 		"id_str": "1512603589566472193",
