ef04436e915f182a7d5e00a134500765ac21f629
솔플파티 <solplparty@gmail.com>
2022-04-13 16:17:38 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 59722,
-	"created_at": "Wed Apr 13 04:17:36 +0000 2022",
+	"tweets": 59724,
+	"created_at": "Wed Apr 13 07:17:38 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,341 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Wed Apr 13 07:11:17 +0000 2022",
+		"id": 1514130635811483600,
+		"id_str": "1514139141012967424",
+		"full_text": "어린 시절, 처음으로 목욕탕에 갔던 날을 기억하시나요? 손강 작가님의 소설 &lt; 인류 최후의 목욕탕 &gt; 이 공상온도 온라인스토어와 오프라인에 모두 입고되었어요! 지금 당장 공상온도 스토어에서 만나보세요!!!\n\n.\nhttps://t.co/d7i5lMqQTc\nhttps://t.co/XipgX8L2sV\n— https://t.co/nC9EKlkNoa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			173
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/d7i5lMqQTc",
+					"expanded_url": "http://www.gongsangondo.com",
+					"display_url": "gongsangondo.com",
+					"indices": [
+						124,
+						147
+					]
+				},
+				{
+					"url": "https://t.co/XipgX8L2sV",
+					"expanded_url": "http://smartstore.naver.com/gongsangondo",
+					"display_url": "smartstore.naver.com/gongsangondo",
+					"indices": [
+						148,
+						171
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1514130630488912000,
+					"id_str": "1514130630488911874",
+					"indices": [
+						174,
+						197
+					],
+					"media_url": "http://pbs.twimg.com/media/FQNFyUXUcAInWB1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQNFyUXUcAInWB1.jpg",
+					"url": "https://t.co/nC9EKlkNoa",
+					"display_url": "pic.twitter.com/nC9EKlkNoa",
+					"expanded_url": "https://twitter.com/gongsangondo/status/1514130635811483651/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1514130630488912000,
+					"id_str": "1514130630488911874",
+					"indices": [
+						174,
+						197
+					],
+					"media_url": "http://pbs.twimg.com/media/FQNFyUXUcAInWB1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQNFyUXUcAInWB1.jpg",
+					"url": "https://t.co/nC9EKlkNoa",
+					"display_url": "pic.twitter.com/nC9EKlkNoa",
+					"expanded_url": "https://twitter.com/gongsangondo/status/1514130635811483651/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 720862057243914200,
+			"id_str": "720862057243914240",
+			"name": "공상온도",
+			"screen_name": "gongsangondo",
+			"location": "대한민국 마포구 동교로23길 40",
+			"description": "카페를 기반으로 한 대안공간,갤러리,공연장,아트마켓의 복합문화공간! 서울시 마포구 동교로23길 40 아트마켓 입점,대관 : gongsangondo@gmail.com {ARTIST지원} 아티스트 증명시, 제조음료 30%할인",
+			"url": "https://t.co/nDAiuVnllp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/nDAiuVnllp",
+							"expanded_url": "http://www.gongsangondo.com",
+							"display_url": "gongsangondo.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 398,
+			"friends_count": 214,
+			"listed_count": 7,
+			"created_at": "Fri Apr 15 06:31:22 +0000 2016",
+			"favourites_count": 1141,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3127,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/720863690283622400/Rh9cF7Fa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/720863690283622400/Rh9cF7Fa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/720862057243914240/1460702295",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "어린 시절, 처음으로 목욕탕에 갔던 날을 기억하시나요? 손강 작가님의 소설 &lt; 인류 최후의 목욕탕 &gt; 이 공상온도 온라인스토어와 오프라인에 모두 입고되었어요! 지금 당장 공상온도 스토어에서 만나보세요!!!\n\n.\nhttps://t.co/d7i5lMqQTc\nhttps://t.co/XipgX8L2sV\n— https://t.co/nC9EKlkNoa"
+	},
+	{
+		"created_at": "Wed Apr 13 07:01:58 +0000 2022",
+		"id": 1514136795952210000,
+		"id_str": "1514136795952209922",
+		"full_text": "#유니버설뮤직코리아 #Universal_Music_Korea\n너라는 햇살에 그을린 흔적은 영원할 테니까🌞 |  올모스트 먼데이(almost monday) - sunburn (가사/한글/해석)\nhttps://t.co/QOcael6act",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			130
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "유니버설뮤직코리아",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"text": "Universal_Music_Korea",
+					"indices": [
+						11,
+						33
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/QOcael6act",
+					"expanded_url": "https://youtu.be/J491CrxULXM",
+					"display_url": "youtu.be/J491CrxULXM",
+					"indices": [
+						107,
+						130
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.powerapps.com\" rel=\"nofollow\">Microsoft Power Platform</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 968706450779463700,
+			"id_str": "968706450779463682",
+			"name": "솔플파티",
+			"screen_name": "SOLPLPARTY",
+			"location": "문의: contact@solplparty.com",
+			"description": "혼자서도 즐겁게! \n문화예술 커뮤니티, 솔플파티 🤘 \n\n▶ 참가하기 \nhttps://t.co/Uq1kQ79SRI \n\n❓ ABOUT\nhttps://t.co/OtYqQtyXgi ",
+			"url": "https://t.co/L6Ap35US8u",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/L6Ap35US8u",
+							"expanded_url": "https://solplparty.com",
+							"display_url": "solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/Uq1kQ79SRI",
+							"expanded_url": "http://join.solpl.party",
+							"display_url": "join.solpl.party",
+							"indices": [
+								39,
+								62
+							]
+						},
+						{
+							"url": "https://t.co/OtYqQtyXgi",
+							"expanded_url": "http://about.solpl.party",
+							"display_url": "about.solpl.party",
+							"indices": [
+								73,
+								96
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 249,
+			"friends_count": 464,
+			"listed_count": 4,
+			"created_at": "Wed Feb 28 04:36:30 +0000 2018",
+			"favourites_count": 239,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 81544,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035643238504251394/wHw9i07J_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/968706450779463682/1519792691",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#유니버설뮤직코리아 #Universal_Music_Korea\n너라는 햇살에 그을린 흔적은 영원할 테니까🌞 |  올모스트 먼데이(almost monday) - sunburn (가사/한글/해석)\nhttps://t.co/QOcael6act"
+	},
+	{
 		"created_at": "Wed Apr 13 03:58:34 +0000 2022",
 		"id": 1514076247051038700,
 		"id_str": "1514090643311714304",
@@ -663,7 +998,7 @@ Grailbird.data.tweets_2022_04 =
 		"created_at": "Wed Apr 13 03:29:48 +0000 2022",
 		"id": 1514079719280427000,
 		"id_str": "1514083403901116416",
-		"full_text": "담예, NEW SINGLE OUT NOW!!👨🏻‍⚕️\n\n담예(DAMYE)의 새 싱글 'Phenylethylamine (feat. Shammah) (#002_side a)'이 모든 음원사이트에 발매되었습니다. 담예가 직접 3D 애니메이션 작업으로 참여한 뮤직비디오는 인플래닛 유튜브 채널을 통해서 감상하실 수 있습니다.\n\nMV ▷ https://t.co/cvxi8lyBVx https://t.co/zVDDM5ipIp",
+		"full_text": "담예, NEW SINGLE OUT NOW!!👨🏻⚕️\n\n담예(DAMYE)의 새 싱글 'Phenylethylamine (feat. Shammah) (#002_side a)'이 모든 음원사이트에 발매되었습니다. 담예가 직접 3D 애니메이션 작업으로 참여한 뮤직비디오는 인플래닛 유튜브 채널을 통해서 감상하실 수 있습니다.\n\nMV ▷ https://t.co/cvxi8lyBVx https://t.co/zVDDM5ipIp",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -849,7 +1184,7 @@ Grailbird.data.tweets_2022_04 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "담예, NEW SINGLE OUT NOW!!👨🏻‍⚕️\n\n담예(DAMYE)의 새 싱글 'Phenylethylamine (feat. Shammah) (#002_side a)'이 모든 음원사이트에 발매되었습니다. 담예가 직접 3D 애니메이션 작업으로 참여한 뮤직비디오는 인플래닛 유튜브 채널을 통해서 감상하실 수 있습니다.\n\nMV ▷ https://t.co/cvxi8lyBVx https://t.co/zVDDM5ipIp"
+		"text": "담예, NEW SINGLE OUT NOW!!👨🏻⚕️\n\n담예(DAMYE)의 새 싱글 'Phenylethylamine (feat. Shammah) (#002_side a)'이 모든 음원사이트에 발매되었습니다. 담예가 직접 3D 애니메이션 작업으로 참여한 뮤직비디오는 인플래닛 유튜브 채널을 통해서 감상하실 수 있습니다.\n\nMV ▷ https://t.co/cvxi8lyBVx https://t.co/zVDDM5ipIp"
 	},
 	{
 		"created_at": "Wed Apr 13 03:29:42 +0000 2022",
