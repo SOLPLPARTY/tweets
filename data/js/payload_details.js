3a68fe1cc4a9d226b9dc99a6a59ce9d52e024218
솔플파티 <solplparty@gmail.com>
2022-05-01 19:20:22 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 60447,
-	"created_at": "Sun May 01 09:17:32 +0000 2022",
+	"tweets": 60448,
+	"created_at": "Sun May 01 10:20:22 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,170 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Sun May 01 09:29:25 +0000 2022",
+		"id": 1520690438969380900,
+		"id_str": "1520696884515516417",
+		"full_text": "[#NEWS] 차세대 솔로퀸 ‘민서’, 인기가요서 새 싱글 타이틀곡 선보여\n\n▶️ https://t.co/Ksc3u4d6Fo\n\n#MINSEO #민서 #MNSO #mOS #내맘대로 #SBS #인기가요",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			110
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "MINSEO",
+					"indices": [
+						71,
+						78
+					]
+				},
+				{
+					"text": "민서",
+					"indices": [
+						79,
+						82
+					]
+				},
+				{
+					"text": "MNSO",
+					"indices": [
+						83,
+						88
+					]
+				},
+				{
+					"text": "mOS",
+					"indices": [
+						89,
+						93
+					]
+				},
+				{
+					"text": "내맘대로",
+					"indices": [
+						94,
+						99
+					]
+				},
+				{
+					"text": "SBS",
+					"indices": [
+						100,
+						104
+					]
+				},
+				{
+					"text": "인기가요",
+					"indices": [
+						105,
+						110
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Ksc3u4d6Fo",
+					"expanded_url": "http://naver.me/F0c2y7KF",
+					"display_url": "naver.me/F0c2y7KF",
+					"indices": [
+						46,
+						69
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/pgQyM3XZDx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pgQyM3XZDx",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 34931,
+			"friends_count": 15,
+			"listed_count": 285,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6884,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1650879304",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 12,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] 차세대 솔로퀸 ‘민서’, 인기가요서 새 싱글 타이틀곡 선보여\n\n▶️ https://t.co/Ksc3u4d6Fo\n\n#MINSEO #민서 #MNSO #mOS #내맘대로 #SBS #인기가요"
+	},
+	{
 		"created_at": "Sun May 01 08:29:24 +0000 2022",
 		"id": 1520675325113299000,
 		"id_str": "1520681783699849218",
