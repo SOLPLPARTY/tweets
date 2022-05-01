f8e01c2e5f4637790a5d4ad452d4e901cdd9b0e5
솔플파티 <solplparty@gmail.com>
2022-05-01 12:17:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 60440,
-	"created_at": "Sat Apr 30 15:17:49 +0000 2022",
+	"tweets": 60441,
+	"created_at": "Sun May 01 03:17:37 +0000 2022",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2022_05.js
@@ -0,0 +1,293 @@
+Grailbird.data.tweets_2022_05 = 
+[
+	{
+		"created_at": "Sun May 01 02:29:25 +0000 2022",
+		"id": 1520590059233169400,
+		"id_str": "1520591188377673728",
+		"full_text": "[#손태진] TBS eFM &lt;Sweet Rendezvous&gt; 2주년 축하💐\n\nDJ 손태진의 &lt;Sweet Rendezvous&gt;가 어느덧 2주년을 맞이했습니다💖\n2주년 특집 버스킹으로 인사드릴 수 있어 더욱 의미 깊었던 시간🎵\n매주 주말 아침, 달콤한 만남 계속 함께해요🥰\n\n#SONTAEJIN #TJSON #TBSeFM #스윗랑데부 #SweetRendezvous https://t.co/0FzNMxdS0f",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			213
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "손태진",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "SONTAEJIN",
+					"indices": [
+						164,
+						174
+					]
+				},
+				{
+					"text": "TJSON",
+					"indices": [
+						175,
+						181
+					]
+				},
+				{
+					"text": "TBSeFM",
+					"indices": [
+						182,
+						189
+					]
+				},
+				{
+					"text": "스윗랑데부",
+					"indices": [
+						190,
+						196
+					]
+				},
+				{
+					"text": "SweetRendezvous",
+					"indices": [
+						197,
+						213
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1520590005466386400,
+					"id_str": "1520590005466386433",
+					"indices": [
+						214,
+						237
+					],
+					"media_url": "http://pbs.twimg.com/media/FRo4jbTVUAEQ4Kf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRo4jbTVUAEQ4Kf.jpg",
+					"url": "https://t.co/0FzNMxdS0f",
+					"display_url": "pic.twitter.com/0FzNMxdS0f",
+					"expanded_url": "https://twitter.com/themysticstory/status/1520590059233169408/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1520590005466386400,
+					"id_str": "1520590005466386433",
+					"indices": [
+						214,
+						237
+					],
+					"media_url": "http://pbs.twimg.com/media/FRo4jbTVUAEQ4Kf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRo4jbTVUAEQ4Kf.jpg",
+					"url": "https://t.co/0FzNMxdS0f",
+					"display_url": "pic.twitter.com/0FzNMxdS0f",
+					"expanded_url": "https://twitter.com/themysticstory/status/1520590059233169408/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1520590005457940500,
+					"id_str": "1520590005457940480",
+					"indices": [
+						214,
+						237
+					],
+					"media_url": "http://pbs.twimg.com/media/FRo4jbRUcAA6_4x.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRo4jbRUcAA6_4x.jpg",
+					"url": "https://t.co/0FzNMxdS0f",
+					"display_url": "pic.twitter.com/0FzNMxdS0f",
+					"expanded_url": "https://twitter.com/themysticstory/status/1520590059233169408/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1520590005474713600,
+					"id_str": "1520590005474713600",
+					"indices": [
+						214,
+						237
+					],
+					"media_url": "http://pbs.twimg.com/media/FRo4jbVUYAAhNv3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRo4jbVUYAAhNv3.jpg",
+					"url": "https://t.co/0FzNMxdS0f",
+					"display_url": "pic.twitter.com/0FzNMxdS0f",
+					"expanded_url": "https://twitter.com/themysticstory/status/1520590059233169408/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/pgQyM3XZDx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pgQyM3XZDx",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 34927,
+			"friends_count": 15,
+			"listed_count": 282,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6881,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1650879304",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 21,
+		"favorite_count": 29,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#손태진] TBS eFM &lt;Sweet Rendezvous&gt; 2주년 축하💐\n\nDJ 손태진의 &lt;Sweet Rendezvous&gt;가 어느덧 2주년을 맞이했습니다💖\n2주년 특집 버스킹으로 인사드릴 수 있어 더욱 의미 깊었던 시간🎵\n매주 주말 아침, 달콤한 만남 계속 함께해요🥰\n\n#SONTAEJIN #TJSON #TBSeFM #스윗랑데부 #SweetRendezvous https://t.co/0FzNMxdS0f"
+	}
+]
