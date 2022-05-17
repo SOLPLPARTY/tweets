2187c11ba6f556dd474f2e82e89cbe5810b987cb
솔플파티 <solplparty@gmail.com>
2022-05-17 13:17:41 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 61119,
-	"created_at": "Tue May 17 03:17:40 +0000 2022",
+	"tweets": 61121,
+	"created_at": "Tue May 17 04:17:41 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,285 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Tue May 17 03:29:29 +0000 2022",
+		"id": 1526398420558090200,
+		"id_str": "1526404513191567362",
+		"full_text": "[보라미유 - '느낌대로' 바로듣기]\n⠀\n벅스 https://t.co/meKJK27gP1\n멜론 https://t.co/gbuEiIIDY9\n지니 https://t.co/Jc3R9xHDXW\n플로 https://t.co/F3H6i8juew\n바이브 https://t.co/elHirqowwb\n\n#보라미유 #Boramiyu #느낌대로 #CAMPUS_LIVE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			194
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "보라미유",
+					"indices": [
+						160,
+						165
+					]
+				},
+				{
+					"text": "Boramiyu",
+					"indices": [
+						166,
+						175
+					]
+				},
+				{
+					"text": "느낌대로",
+					"indices": [
+						176,
+						181
+					]
+				},
+				{
+					"text": "CAMPUS_LIVE",
+					"indices": [
+						182,
+						194
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/meKJK27gP1",
+					"expanded_url": "https://bit.ly/3yHk16G",
+					"display_url": "bit.ly/3yHk16G",
+					"indices": [
+						26,
+						49
+					]
+				},
+				{
+					"url": "https://t.co/gbuEiIIDY9",
+					"expanded_url": "https://bit.ly/3MokFtJ",
+					"display_url": "bit.ly/3MokFtJ",
+					"indices": [
+						53,
+						76
+					]
+				},
+				{
+					"url": "https://t.co/Jc3R9xHDXW",
+					"expanded_url": "https://bit.ly/3yHd6dx",
+					"display_url": "bit.ly/3yHd6dx",
+					"indices": [
+						80,
+						103
+					]
+				},
+				{
+					"url": "https://t.co/F3H6i8juew",
+					"expanded_url": "https://bit.ly/3wyl1XL",
+					"display_url": "bit.ly/3wyl1XL",
+					"indices": [
+						107,
+						130
+					]
+				},
+				{
+					"url": "https://t.co/elHirqowwb",
+					"expanded_url": "https://bit.ly/3MnBlBy",
+					"display_url": "bit.ly/3MnBlBy",
+					"indices": [
+						135,
+						158
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1526397093035786200,
+		"in_reply_to_status_id_str": "1526397093035786240",
+		"in_reply_to_user_id": 265792818,
+		"in_reply_to_user_id_str": "265792818",
+		"in_reply_to_screen_name": "shofar_ent",
+		"user": {
+			"id": 265792818,
+			"id_str": "265792818",
+			"name": "쇼파르엔터테인먼트",
+			"screen_name": "shofar_ent",
+			"location": "",
+			"description": "SHOFAR ENTERTAINMENT Official Twitter",
+			"url": "https://t.co/zzQk2izsAi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zzQk2izsAi",
+							"expanded_url": "http://shofar-ent.com/",
+							"display_url": "shofar-ent.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7709,
+			"friends_count": 5,
+			"listed_count": 63,
+			"created_at": "Mon Mar 14 04:31:00 +0000 2011",
+			"favourites_count": 53,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4708,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1380084076573417474/i-NclciR_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1380084076573417474/i-NclciR_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/265792818/1651584080",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[보라미유 - '느낌대로' 바로듣기]\n⠀\n벅스 https://t.co/meKJK27gP1\n멜론 https://t.co/gbuEiIIDY9\n지니 https://t.co/Jc3R9xHDXW\n플로 https://t.co/F3H6i8juew\n바이브 https://t.co/elHirqowwb\n\n#보라미유 #Boramiyu #느낌대로 #CAMPUS_LIVE"
+	},
+	{
+		"created_at": "Tue May 17 03:29:27 +0000 2022",
+		"id": 1526397860912083000,
+		"id_str": "1526404504295546880",
+		"full_text": "Lilyhimei [How to be friends] https://t.co/v3h1koZMwB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			53
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/v3h1koZMwB",
+					"expanded_url": "http://blog.naver.com/chili7272/222735637888",
+					"display_url": "blog.naver.com/chili7272/2227…",
+					"indices": [
+						30,
+						53
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.naver.com\" rel=\"nofollow\">Naver</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 114111001,
+			"id_str": "114111001",
+			"name": "칠리뮤직코리아",
+			"screen_name": "chilimusickorea",
+			"location": "Seoul, Korea",
+			"description": "음반제작 및 배급전문/국내 및 해외사이트 음원유통/매니지먼트 담당//\n허클베리 핀/에이프릴 세컨드/트레봉봉/오드트리/Steve Barakett/LasseLindh/Sioen 등",
+			"url": "https://t.co/qZzCfFQtXO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qZzCfFQtXO",
+							"expanded_url": "http://www.facebook.com/chilimusickorea1",
+							"display_url": "facebook.com/chilimusickore…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2083,
+			"friends_count": 2341,
+			"listed_count": 61,
+			"created_at": "Sun Feb 14 05:12:40 +0000 2010",
+			"favourites_count": 690,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5366,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1526066762969935872/M-LQZSu0_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1526066762969935872/M-LQZSu0_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/114111001/1652680037",
+			"profile_link_color": "FC420A",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "C0B8C4",
+			"profile_text_color": "9FFEE3",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Lilyhimei [How to be friends] https://t.co/v3h1koZMwB"
+	},
+	{
 		"created_at": "Tue May 17 03:08:47 +0000 2022",
 		"id": 1526399301424803800,
 		"id_str": "1526399301424803840",
