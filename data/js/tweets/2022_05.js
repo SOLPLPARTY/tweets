f656f2d46b99fa7211d2e8c8a20ec49bbd20265d
솔플파티 <solplparty@gmail.com>
2022-05-19 00:17:41 +0900

    Added 1 tweets

--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,288 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Wed May 18 15:11:17 +0000 2022",
+		"id": 1526938638860157000,
+		"id_str": "1526943511714594821",
+		"full_text": "이번주 클럽FF 공연 안내 \n\n5/19 (목) 7:30pm - 굿우즈,파주,업스타,페일블루\n5/20 (금) 7:30pm - 투지,앤든,프랭클리,캐치더영\n5/21 (토) 6pm - 묘리,뷰렛,아디오스오디오,코토바,에이치얼랏\n5/22 (일) 5pm - 브리니클레인,신스네이크,뉴클리어이디엇츠,바닐레어 https://t.co/Wg1gtrjnr8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			165
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1526938627397193700,
+					"id_str": "1526938627397193728",
+					"indices": [
+						166,
+						189
+					],
+					"media_url": "http://pbs.twimg.com/media/FTDGl2_VEAACjRh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTDGl2_VEAACjRh.jpg",
+					"url": "https://t.co/Wg1gtrjnr8",
+					"display_url": "pic.twitter.com/Wg1gtrjnr8",
+					"expanded_url": "https://twitter.com/clubFF_/status/1526938638860156929/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1334,
+							"h": 1334,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1526938627397193700,
+					"id_str": "1526938627397193728",
+					"indices": [
+						166,
+						189
+					],
+					"media_url": "http://pbs.twimg.com/media/FTDGl2_VEAACjRh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTDGl2_VEAACjRh.jpg",
+					"url": "https://t.co/Wg1gtrjnr8",
+					"display_url": "pic.twitter.com/Wg1gtrjnr8",
+					"expanded_url": "https://twitter.com/clubFF_/status/1526938638860156929/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1334,
+							"h": 1334,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1526938627384627200,
+					"id_str": "1526938627384627205",
+					"indices": [
+						166,
+						189
+					],
+					"media_url": "http://pbs.twimg.com/media/FTDGl28VUAUn-0r.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTDGl28VUAUn-0r.jpg",
+					"url": "https://t.co/Wg1gtrjnr8",
+					"display_url": "pic.twitter.com/Wg1gtrjnr8",
+					"expanded_url": "https://twitter.com/clubFF_/status/1526938638860156929/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1334,
+							"h": 1334,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1526938627392954400,
+					"id_str": "1526938627392954368",
+					"indices": [
+						166,
+						189
+					],
+					"media_url": "http://pbs.twimg.com/media/FTDGl2-UYAAg-TT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTDGl2-UYAAg-TT.jpg",
+					"url": "https://t.co/Wg1gtrjnr8",
+					"display_url": "pic.twitter.com/Wg1gtrjnr8",
+					"expanded_url": "https://twitter.com/clubFF_/status/1526938638860156929/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1334,
+							"h": 1334,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1526938627380375600,
+					"id_str": "1526938627380375552",
+					"indices": [
+						166,
+						189
+					],
+					"media_url": "http://pbs.twimg.com/media/FTDGl27UcAARK9u.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTDGl27UcAARK9u.jpg",
+					"url": "https://t.co/Wg1gtrjnr8",
+					"display_url": "pic.twitter.com/Wg1gtrjnr8",
+					"expanded_url": "https://twitter.com/clubFF_/status/1526938638860156929/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 546,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 964,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1072,
+							"h": 1334,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 455224965,
+			"id_str": "455224965",
+			"name": "클럽에프에프",
+			"screen_name": "clubFF_",
+			"location": "서울 마포구 서교동 407-8 지하1층",
+			"description": "홍대 최고의 NO.1 Indie experience 라이브 & 댄스클럽 에프에프입니다!! 엪엪의 대관 및 오디션 문의(프로필,음원,연락처 필수)는  010-9025-3407 과 ianbrown@naver.com 으로 .",
+			"url": "https://t.co/dmU2vJP8pS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dmU2vJP8pS",
+							"expanded_url": "http://clubff.modoo.at",
+							"display_url": "clubff.modoo.at",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6126,
+			"friends_count": 8,
+			"listed_count": 0,
+			"created_at": "Wed Jan 04 21:41:48 +0000 2012",
+			"favourites_count": 559,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3829,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2263880419/image_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2263880419/image_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "이번주 클럽FF 공연 안내 \n\n5/19 (목) 7:30pm - 굿우즈,파주,업스타,페일블루\n5/20 (금) 7:30pm - 투지,앤든,프랭클리,캐치더영\n5/21 (토) 6pm - 묘리,뷰렛,아디오스오디오,코토바,에이치얼랏\n5/22 (일) 5pm - 브리니클레인,신스네이크,뉴클리어이디엇츠,바닐레어 https://t.co/Wg1gtrjnr8"
+	},
+	{
 		"created_at": "Wed May 18 13:29:36 +0000 2022",
 		"id": 1526914890442903600,
 		"id_str": "1526917924627394560",
