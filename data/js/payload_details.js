81db64fe463a8784e892614996e129c1d35ebfa7
솔플파티 <solplparty@gmail.com>
2022-06-01 21:17:33 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 61671,
-	"created_at": "Wed Jun 01 11:17:32 +0000 2022",
+	"tweets": 61673,
+	"created_at": "Wed Jun 01 12:17:32 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,344 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Wed Jun 01 11:29:35 +0000 2022",
+		"id": 1531954489506537500,
+		"id_str": "1531961149704527873",
+		"full_text": "[#인순이] #수니버스 #수니잼\n\n▶#GoodAsHell - #Lizzo  cover by 인순이, 박세인◀\n\n당당하고 멋진✨ \n언니들과 함께라면 상처입은 나도 이 구역의 힙쟁이💘\n\n👉 https://t.co/jSpE4S1i2g\n\n#INSOONI #휴맵컨텐츠 #HUMAPCONTENTS #sooniverse #cover",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			178
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "인순이",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "수니버스",
+					"indices": [
+						7,
+						12
+					]
+				},
+				{
+					"text": "수니잼",
+					"indices": [
+						13,
+						17
+					]
+				},
+				{
+					"text": "GoodAsHell",
+					"indices": [
+						20,
+						31
+					]
+				},
+				{
+					"text": "Lizzo",
+					"indices": [
+						34,
+						40
+					]
+				},
+				{
+					"text": "INSOONI",
+					"indices": [
+						129,
+						137
+					]
+				},
+				{
+					"text": "휴맵컨텐츠",
+					"indices": [
+						138,
+						144
+					]
+				},
+				{
+					"text": "HUMAPCONTENTS",
+					"indices": [
+						145,
+						159
+					]
+				},
+				{
+					"text": "sooniverse",
+					"indices": [
+						160,
+						171
+					]
+				},
+				{
+					"text": "cover",
+					"indices": [
+						172,
+						178
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/jSpE4S1i2g",
+					"expanded_url": "https://youtu.be/-A6bB-7Uwhw",
+					"display_url": "youtu.be/-A6bB-7Uwhw",
+					"indices": [
+						104,
+						127
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1219506842935513000,
+			"id_str": "1219506842935513088",
+			"name": "휴맵컨텐츠(HUMAP)",
+			"screen_name": "humap_official",
+			"location": "",
+			"description": "휴맵컨텐츠(HUMAP CONTENTS) Official Twitter",
+			"url": "https://t.co/fBZZK3SmX6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fBZZK3SmX6",
+							"expanded_url": "http://www.humapcontents.com/",
+							"display_url": "humapcontents.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 390,
+			"friends_count": 2,
+			"listed_count": 2,
+			"created_at": "Tue Jan 21 06:28:27 +0000 2020",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 754,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1219506842935513088/1579755898",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#인순이] #수니버스 #수니잼\n\n▶#GoodAsHell - #Lizzo  cover by 인순이, 박세인◀\n\n당당하고 멋진✨ \n언니들과 함께라면 상처입은 나도 이 구역의 힙쟁이💘\n\n👉 https://t.co/jSpE4S1i2g\n\n#INSOONI #휴맵컨텐츠 #HUMAPCONTENTS #sooniverse #cover"
+	},
+	{
+		"created_at": "Wed Jun 01 11:29:28 +0000 2022",
+		"id": 1531946467849834500,
+		"id_str": "1531961119832686592",
+		"full_text": "@le_petit_piano @pptnznoshel \n#JungJaeHyung\n#페퍼톤스 #Peppertones\n#이효리\n#오늘도삽질 #스튜디오훜",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			81
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "JungJaeHyung",
+					"indices": [
+						30,
+						43
+					]
+				},
+				{
+					"text": "페퍼톤스",
+					"indices": [
+						44,
+						49
+					]
+				},
+				{
+					"text": "Peppertones",
+					"indices": [
+						50,
+						62
+					]
+				},
+				{
+					"text": "이효리",
+					"indices": [
+						63,
+						67
+					]
+				},
+				{
+					"text": "오늘도삽질",
+					"indices": [
+						68,
+						74
+					]
+				},
+				{
+					"text": "스튜디오훜",
+					"indices": [
+						75,
+						81
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "le_petit_piano",
+					"name": "J.H.JUNG",
+					"id": 120697779,
+					"id_str": "120697779",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "pptnznoshel",
+					"name": "이장원 (Edward J Lee)",
+					"id": 192668164,
+					"id_str": "192668164",
+					"indices": [
+						16,
+						28
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1531946463282217000,
+		"in_reply_to_status_id_str": "1531946463282216960",
+		"in_reply_to_user_id": 266004552,
+		"in_reply_to_user_id_str": "266004552",
+		"in_reply_to_screen_name": "antennamusic",
+		"user": {
+			"id": 266004552,
+			"id_str": "266004552",
+			"name": "안테나 Antenna",
+			"screen_name": "antennamusic",
+			"location": "대한민국 서울",
+			"description": "유희열(Toy), 유재석, 정재형, 루시드폴, 페퍼톤스(신재평&이장원), 박새별, 샘김, 이진아, 권진아, 정승환, 윤석철, 적재, 이미주, 서동환, 황현조 Official / 루시드폴 DS ‘목소리와 기타’ 2022. 05. 23. 6PM Release",
+			"url": "https://t.co/XSWWwEPbf8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XSWWwEPbf8",
+							"expanded_url": "http://www.antenna.co.kr",
+							"display_url": "antenna.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 40375,
+			"friends_count": 11,
+			"listed_count": 382,
+			"created_at": "Mon Mar 14 13:49:27 +0000 2011",
+			"favourites_count": 14,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 5056,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266004552/1653296979",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "und",
+		"text": "@le_petit_piano @pptnznoshel \n#JungJaeHyung\n#페퍼톤스 #Peppertones\n#이효리\n#오늘도삽질 #스튜디오훜"
+	},
+	{
 		"created_at": "Wed Jun 01 10:29:24 +0000 2022",
 		"id": 1531945878831112200,
 		"id_str": "1531946007092658177",
