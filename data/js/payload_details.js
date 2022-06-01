74e1ccdd77eca6cffa68d958cdf2aa613e7d6150
솔플파티 <solplparty@gmail.com>
2022-06-01 15:17:32 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 61663,
-	"created_at": "Wed Jun 01 04:17:32 +0000 2022",
+	"tweets": 61664,
+	"created_at": "Wed Jun 01 06:17:32 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,241 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Wed Jun 01 05:29:25 +0000 2022",
+		"id": 1531867092009427000,
+		"id_str": "1531870511344586758",
+		"full_text": "Girls' Generation 소녀시대 'Gee' MV\n\nhttps://t.co/3yD8ug2Q2h\n\n#SM_Remastering_Project #RemasterMV\n#리마스터링프로젝트\n#GirlsGeneration #소녀시대\n#Gee\n#SMSTATION https://t.co/m7p0FD74DK",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			143
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SM_Remastering_Project",
+					"indices": [
+						58,
+						81
+					]
+				},
+				{
+					"text": "RemasterMV",
+					"indices": [
+						82,
+						93
+					]
+				},
+				{
+					"text": "리마스터링프로젝트",
+					"indices": [
+						94,
+						104
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						105,
+						121
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						122,
+						127
+					]
+				},
+				{
+					"text": "Gee",
+					"indices": [
+						128,
+						132
+					]
+				},
+				{
+					"text": "SMSTATION",
+					"indices": [
+						133,
+						143
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/3yD8ug2Q2h",
+					"expanded_url": "https://youtu.be/U7mPqycQ0tQ",
+					"display_url": "youtu.be/U7mPqycQ0tQ",
+					"indices": [
+						33,
+						56
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1531867082534420500,
+					"id_str": "1531867082534420480",
+					"indices": [
+						144,
+						167
+					],
+					"media_url": "http://pbs.twimg.com/media/FUJI_tGUAAAtjuC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUJI_tGUAAAtjuC.jpg",
+					"url": "https://t.co/m7p0FD74DK",
+					"display_url": "pic.twitter.com/m7p0FD74DK",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1531867092009426945/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1531867082534420500,
+					"id_str": "1531867082534420480",
+					"indices": [
+						144,
+						167
+					],
+					"media_url": "http://pbs.twimg.com/media/FUJI_tGUAAAtjuC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUJI_tGUAAAtjuC.jpg",
+					"url": "https://t.co/m7p0FD74DK",
+					"display_url": "pic.twitter.com/m7p0FD74DK",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1531867092009426945/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 884378714,
+			"id_str": "884378714",
+			"name": "SMTOWN",
+			"screen_name": "SMTOWNGLOBAL",
+			"location": "",
+			"description": "SMEntertainment Group Official Twitter",
+			"url": "https://t.co/zjb7YWlJE5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zjb7YWlJE5",
+							"expanded_url": "http://www.smtown.com",
+							"display_url": "smtown.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10861904,
+			"friends_count": 56,
+			"listed_count": 12239,
+			"created_at": "Tue Oct 16 12:49:45 +0000 2012",
+			"favourites_count": 2026,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 12289,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "F08BA0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1477835872729206786/4DNyIiTo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1477835872729206786/4DNyIiTo_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/884378714/1641178515",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4501,
+		"favorite_count": 8194,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "Girls' Generation 소녀시대 'Gee' MV\n\nhttps://t.co/3yD8ug2Q2h\n\n#SM_Remastering_Project #RemasterMV\n#리마스터링프로젝트\n#GirlsGeneration #소녀시대\n#Gee\n#SMSTATION https://t.co/m7p0FD74DK"
+	},
+	{
 		"created_at": "Wed Jun 01 03:29:35 +0000 2022",
 		"id": 1531834020082110500,
 		"id_str": "1531840353095061510",
