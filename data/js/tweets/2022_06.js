11b8328f7b3fddc80a473dc6e9e51be03c13f8c5
솔플파티 <solplparty@gmail.com>
2022-06-02 22:17:33 +0900

    Added 1 tweets

--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,242 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Thu Jun 02 12:29:30 +0000 2022",
+		"id": 1532331198399271000,
+		"id_str": "1532338615971467264",
+		"full_text": "[📹]\nGSoul (지소울) Europe Tour Recap Ep. 03\n\n▶️https://t.co/GE7y4p6EGs\n\n#GSoul #지소울 #Natural \n@Gsoullove https://t.co/UmPs3EnuRl",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			101
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GSoul",
+					"indices": [
+						69,
+						75
+					]
+				},
+				{
+					"text": "지소울",
+					"indices": [
+						76,
+						80
+					]
+				},
+				{
+					"text": "Natural",
+					"indices": [
+						81,
+						89
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Gsoullove",
+					"name": "G",
+					"id": 121589749,
+					"id_str": "121589749",
+					"indices": [
+						91,
+						101
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/GE7y4p6EGs",
+					"expanded_url": "https://youtu.be/pMHgup8FVkA",
+					"display_url": "youtu.be/pMHgup8FVkA",
+					"indices": [
+						44,
+						67
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1532291891618009000,
+					"id_str": "1532291891618009088",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1532291891618009088/pu/img/78MXJhK0eCVZPYFb.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1532291891618009088/pu/img/78MXJhK0eCVZPYFb.jpg",
+					"url": "https://t.co/UmPs3EnuRl",
+					"display_url": "pic.twitter.com/UmPs3EnuRl",
+					"expanded_url": "https://twitter.com/Warner_Music_Kr/status/1532331198399270912/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1532291891618009000,
+					"id_str": "1532291891618009088",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1532291891618009088/pu/img/78MXJhK0eCVZPYFb.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1532291891618009088/pu/img/78MXJhK0eCVZPYFb.jpg",
+					"url": "https://t.co/UmPs3EnuRl",
+					"display_url": "pic.twitter.com/UmPs3EnuRl",
+					"expanded_url": "https://twitter.com/Warner_Music_Kr/status/1532331198399270912/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 55096,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1532291891618009088/pu/vid/1280x720/yo3DCtChYHgaAQ00.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1532291891618009088/pu/pl/MtQjPljD6lI8QTFH.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1532291891618009088/pu/vid/480x270/C0_8nPzHMeQ4FO7X.mp4?tag=12"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1532291891618009088/pu/vid/640x360/0BBgDS58eRr9-m45.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 206117108,
+			"id_str": "206117108",
+			"name": "Warner Music Korea",
+			"screen_name": "Warner_Music_Kr",
+			"location": "Seoul, Korea",
+			"description": "워너뮤직코리아 공식 트위터",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 84882,
+			"friends_count": 493,
+			"listed_count": 209,
+			"created_at": "Fri Oct 22 09:32:47 +0000 2010",
+			"favourites_count": 1594,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 11699,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1267377694565490688/FD-WdFum_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1267377694565490688/FD-WdFum_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/206117108/1636703401",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "023BE5",
+			"profile_text_color": "2566FC",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 19,
+		"favorite_count": 78,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[📹]\nGSoul (지소울) Europe Tour Recap Ep. 03\n\n▶️https://t.co/GE7y4p6EGs\n\n#GSoul #지소울 #Natural \n@Gsoullove https://t.co/UmPs3EnuRl"
+	},
+	{
 		"created_at": "Thu Jun 02 12:11:16 +0000 2022",
 		"id": 1532325168617783300,
 		"id_str": "1532334030410203138",
