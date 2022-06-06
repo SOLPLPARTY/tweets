b579effd0852841d4635c2af8eb41493c17bae2b
솔플파티 <solplparty@gmail.com>
2022-06-06 16:17:33 +0900

    Added 1 tweets

--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,184 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Mon Jun 06 06:29:25 +0000 2022",
+		"id": 1533691756482789400,
+		"id_str": "1533697549412012032",
+		"full_text": "[#MTS] 미스틱스토리 Monday To Sunday🔔\n\n🔗https://t.co/F5AhNNmo75\n\n#걍나와 #윤종신 #Billlie #신재휘 #LUCY #김강민 #김영철 #LISTENSTAGE #월간윤종신",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			118
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MTS",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "걍나와",
+					"indices": [
+						59,
+						63
+					]
+				},
+				{
+					"text": "윤종신",
+					"indices": [
+						64,
+						68
+					]
+				},
+				{
+					"text": "Billlie",
+					"indices": [
+						69,
+						77
+					]
+				},
+				{
+					"text": "신재휘",
+					"indices": [
+						78,
+						82
+					]
+				},
+				{
+					"text": "LUCY",
+					"indices": [
+						83,
+						88
+					]
+				},
+				{
+					"text": "김강민",
+					"indices": [
+						89,
+						93
+					]
+				},
+				{
+					"text": "김영철",
+					"indices": [
+						94,
+						98
+					]
+				},
+				{
+					"text": "LISTENSTAGE",
+					"indices": [
+						99,
+						111
+					]
+				},
+				{
+					"text": "월간윤종신",
+					"indices": [
+						112,
+						118
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/F5AhNNmo75",
+					"expanded_url": "https://bit.ly/3zl4fhY",
+					"display_url": "bit.ly/3zl4fhY",
+					"indices": [
+						34,
+						57
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/pgQyM3XZDx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pgQyM3XZDx",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 35117,
+			"friends_count": 15,
+			"listed_count": 290,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6978,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1650879304",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 14,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#MTS] 미스틱스토리 Monday To Sunday🔔\n\n🔗https://t.co/F5AhNNmo75\n\n#걍나와 #윤종신 #Billlie #신재휘 #LUCY #김강민 #김영철 #LISTENSTAGE #월간윤종신"
+	},
+	{
 		"created_at": "Mon Jun 06 03:10:49 +0000 2022",
 		"id": 1533647570886533000,
 		"id_str": "1533647570886533121",
