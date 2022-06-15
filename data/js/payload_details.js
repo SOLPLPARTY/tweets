2c6c292c1f93b54bf310d84fc2bac6a1909d3b0e
솔플파티 <solplparty@gmail.com>
2022-06-16 01:17:37 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 62065,
-	"created_at": "Wed Jun 15 14:17:35 +0000 2022",
+	"tweets": 62066,
+	"created_at": "Wed Jun 15 16:17:36 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Wed Jun 15 15:29:25 +0000 2022",
+		"id": 1537087588833427500,
+		"id_str": "1537094935513550848",
+		"full_text": "HAPPY BIRTHDAY GSoul🎂🥳\nGSoul(@Gsoullove)의 생일을 축하합니다🎉\n⠀\n#GSoul #지소울 #HAPPY_BIRTHDAY_GSoul https://t.co/sBLcUHhoMT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			88
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GSoul",
+					"indices": [
+						55,
+						61
+					]
+				},
+				{
+					"text": "지소울",
+					"indices": [
+						62,
+						66
+					]
+				},
+				{
+					"text": "HAPPY_BIRTHDAY_GSoul",
+					"indices": [
+						67,
+						88
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Gsoullove",
+					"name": "G",
+					"id": 121589749,
+					"id_str": "121589749",
+					"indices": [
+						29,
+						39
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1537087584916357000,
+					"id_str": "1537087584916357127",
+					"indices": [
+						89,
+						112
+					],
+					"media_url": "http://pbs.twimg.com/media/FVTVA7-aQAcN5JR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVTVA7-aQAcN5JR.jpg",
+					"url": "https://t.co/sBLcUHhoMT",
+					"display_url": "pic.twitter.com/sBLcUHhoMT",
+					"expanded_url": "https://twitter.com/Warner_Music_Kr/status/1537087588833427456/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1350,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1537087584916357000,
+					"id_str": "1537087584916357127",
+					"indices": [
+						89,
+						112
+					],
+					"media_url": "http://pbs.twimg.com/media/FVTVA7-aQAcN5JR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVTVA7-aQAcN5JR.jpg",
+					"url": "https://t.co/sBLcUHhoMT",
+					"display_url": "pic.twitter.com/sBLcUHhoMT",
+					"expanded_url": "https://twitter.com/Warner_Music_Kr/status/1537087588833427456/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1350,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 206117108,
+			"id_str": "206117108",
+			"name": "Warner Music Korea",
+			"screen_name": "Warner_Music_Kr",
+			"location": "Seoul, Korea",
+			"description": "워너뮤직코리아 공식 트위터",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 86463,
+			"friends_count": 492,
+			"listed_count": 211,
+			"created_at": "Fri Oct 22 09:32:47 +0000 2010",
+			"favourites_count": 1594,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 11729,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1267377694565490688/FD-WdFum_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1267377694565490688/FD-WdFum_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/206117108/1636703401",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "023BE5",
+			"profile_text_color": "2566FC",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 52,
+		"favorite_count": 110,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "HAPPY BIRTHDAY GSoul🎂🥳\nGSoul(@Gsoullove)의 생일을 축하합니다🎉\n⠀\n#GSoul #지소울 #HAPPY_BIRTHDAY_GSoul https://t.co/sBLcUHhoMT"
+	},
+	{
 		"created_at": "Wed Jun 15 14:11:16 +0000 2022",
 		"id": 1537061035101737000,
 		"id_str": "1537075271756652544",
