dee350a0b198601604b90844b4df0d06d39ea035
솔플파티 <solplparty@gmail.com>
2022-06-15 22:17:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 62062,
-	"created_at": "Wed Jun 15 12:17:35 +0000 2022",
+	"tweets": 62063,
+	"created_at": "Wed Jun 15 13:17:37 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,209 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Wed Jun 15 12:29:25 +0000 2022",
+		"id": 1537042581078564900,
+		"id_str": "1537049636803862530",
+		"full_text": "YDE 🪞Old Her🪞\n아티스트로 변신한 YDE의 신곡 공개!\n🎧음원듣기 https://t.co/8pf9ezf68N https://t.co/VLOXubbNWA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			65
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/8pf9ezf68N",
+					"expanded_url": "https://wmk.lnk.to/qB0LwUXK",
+					"display_url": "wmk.lnk.to/qB0LwUXK",
+					"indices": [
+						42,
+						65
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1537042312592863200,
+					"id_str": "1537042312592863233",
+					"indices": [
+						66,
+						89
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1537042312592863233/pu/img/VpW-8i8TVXYen_bz.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1537042312592863233/pu/img/VpW-8i8TVXYen_bz.jpg",
+					"url": "https://t.co/VLOXubbNWA",
+					"display_url": "pic.twitter.com/VLOXubbNWA",
+					"expanded_url": "https://twitter.com/Warner_Music_Kr/status/1537042581078564864/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1537042312592863200,
+					"id_str": "1537042312592863233",
+					"indices": [
+						66,
+						89
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1537042312592863233/pu/img/VpW-8i8TVXYen_bz.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1537042312592863233/pu/img/VpW-8i8TVXYen_bz.jpg",
+					"url": "https://t.co/VLOXubbNWA",
+					"display_url": "pic.twitter.com/VLOXubbNWA",
+					"expanded_url": "https://twitter.com/Warner_Music_Kr/status/1537042581078564864/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 30864,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1537042312592863233/pu/pl/KViCyaVGVTiVlXg6.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1537042312592863233/pu/vid/320x320/JsAgL1zaL31Fu8SJ.mp4?tag=12"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1537042312592863233/pu/vid/540x540/49a9s3Q-7VgArQ4A.mp4?tag=12"
+							},
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1537042312592863233/pu/vid/720x720/sDb2qFEelMZB4Yzp.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.meltwater.com/\" rel=\"nofollow\">Meltwater Social</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 206117108,
+			"id_str": "206117108",
+			"name": "Warner Music Korea",
+			"screen_name": "Warner_Music_Kr",
+			"location": "Seoul, Korea",
+			"description": "워너뮤직코리아 공식 트위터",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 86448,
+			"friends_count": 492,
+			"listed_count": 212,
+			"created_at": "Fri Oct 22 09:32:47 +0000 2010",
+			"favourites_count": 1594,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 11728,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1267377694565490688/FD-WdFum_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1267377694565490688/FD-WdFum_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/206117108/1636703401",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "023BE5",
+			"profile_text_color": "2566FC",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 21,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "YDE 🪞Old Her🪞\n아티스트로 변신한 YDE의 신곡 공개!\n🎧음원듣기 https://t.co/8pf9ezf68N https://t.co/VLOXubbNWA"
+	},
+	{
 		"created_at": "Wed Jun 15 11:29:41 +0000 2022",
 		"id": 1537034106223341600,
 		"id_str": "1537034605521424384",
