fc33f5c227e933ca11e8047fb1569d74c690da9a
솔플파티 <solplparty@gmail.com>
2022-06-20 00:17:39 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 62149,
-	"created_at": "Sun Jun 19 10:17:37 +0000 2022",
+	"tweets": 62150,
+	"created_at": "Sun Jun 19 15:17:38 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,239 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Sun Jun 19 14:29:25 +0000 2022",
+		"id": 1538525632036229000,
+		"id_str": "1538529388215549952",
+		"full_text": "[#홍자] 제1회 드림콘서트 트롯 완료 📸\n\n✨가장 찬란했던 시절✨의 한 페이지로 기억될 오늘💕\n홍자와 함께해주시고 응원해주셔서 고마워요🥰👏\n\n#HONGJA #드림콘서트트롯 https://t.co/wx8iiLqIXo",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			96
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "홍자",
+					"indices": [
+						1,
+						4
+					]
+				},
+				{
+					"text": "HONGJA",
+					"indices": [
+						80,
+						87
+					]
+				},
+				{
+					"text": "드림콘서트트롯",
+					"indices": [
+						88,
+						96
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1538525619881144300,
+					"id_str": "1538525619881144320",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/FVnw5lracAA4qb1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVnw5lracAA4qb1.jpg",
+					"url": "https://t.co/wx8iiLqIXo",
+					"display_url": "pic.twitter.com/wx8iiLqIXo",
+					"expanded_url": "https://twitter.com/themysticstory/status/1538525632036229120/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 509,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 899,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1534,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1538525619881144300,
+					"id_str": "1538525619881144320",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/FVnw5lracAA4qb1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVnw5lracAA4qb1.jpg",
+					"url": "https://t.co/wx8iiLqIXo",
+					"display_url": "pic.twitter.com/wx8iiLqIXo",
+					"expanded_url": "https://twitter.com/themysticstory/status/1538525632036229120/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 509,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 899,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1534,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1538525619885346800,
+					"id_str": "1538525619885346816",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/FVnw5lsakAAKrHR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVnw5lsakAAKrHR.jpg",
+					"url": "https://t.co/wx8iiLqIXo",
+					"display_url": "pic.twitter.com/wx8iiLqIXo",
+					"expanded_url": "https://twitter.com/themysticstory/status/1538525632036229120/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/pgQyM3XZDx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pgQyM3XZDx",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 35169,
+			"friends_count": 15,
+			"listed_count": 293,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 7005,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1650879304",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 18,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#홍자] 제1회 드림콘서트 트롯 완료 📸\n\n✨가장 찬란했던 시절✨의 한 페이지로 기억될 오늘💕\n홍자와 함께해주시고 응원해주셔서 고마워요🥰👏\n\n#HONGJA #드림콘서트트롯 https://t.co/wx8iiLqIXo"
+	},
+	{
 		"created_at": "Sun Jun 19 10:13:15 +0000 2022",
 		"id": 1538464920647311400,
 		"id_str": "1538464920647311365",
