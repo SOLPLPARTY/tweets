048d077944db5417966f4f8bbf89cb903f97cc62
솔플파티 <solplparty@gmail.com>
2022-06-19 12:17:39 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 62144,
-	"created_at": "Sat Jun 18 18:17:37 +0000 2022",
+	"tweets": 62145,
+	"created_at": "Sun Jun 19 03:17:38 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,231 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Sun Jun 19 02:29:25 +0000 2022",
+		"id": 1538340795555586000,
+		"id_str": "1538348193414512640",
+		"full_text": "iScreaM Vol.16 : 버퍼링 (Glitch Mode) Remix\n\n➫ 2022.06.21 6PM KST  \n\n#NCTDREAM #GlitchMode \n#NCTDREAM_GlitchMode #Remix\n#iScreaM\n#ScreaMRecords \n#SMTOWN https://t.co/hd10JGZO69",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			149
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NCTDREAM",
+					"indices": [
+						66,
+						75
+					]
+				},
+				{
+					"text": "GlitchMode",
+					"indices": [
+						76,
+						87
+					]
+				},
+				{
+					"text": "NCTDREAM_GlitchMode",
+					"indices": [
+						89,
+						109
+					]
+				},
+				{
+					"text": "Remix",
+					"indices": [
+						110,
+						116
+					]
+				},
+				{
+					"text": "iScreaM",
+					"indices": [
+						117,
+						125
+					]
+				},
+				{
+					"text": "ScreaMRecords",
+					"indices": [
+						126,
+						140
+					]
+				},
+				{
+					"text": "SMTOWN",
+					"indices": [
+						142,
+						149
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1537411689100247000,
+					"id_str": "1537411689100247040",
+					"indices": [
+						150,
+						173
+					],
+					"media_url": "http://pbs.twimg.com/media/FVX7ySJagAAi89I.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVX7ySJagAAi89I.jpg",
+					"url": "https://t.co/hd10JGZO69",
+					"display_url": "pic.twitter.com/hd10JGZO69",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1538340795555586048/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1537411689100247000,
+					"id_str": "1537411689100247040",
+					"indices": [
+						150,
+						173
+					],
+					"media_url": "http://pbs.twimg.com/media/FVX7ySJagAAi89I.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVX7ySJagAAi89I.jpg",
+					"url": "https://t.co/hd10JGZO69",
+					"display_url": "pic.twitter.com/hd10JGZO69",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1538340795555586048/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 884378714,
+			"id_str": "884378714",
+			"name": "SMTOWN",
+			"screen_name": "SMTOWNGLOBAL",
+			"location": "",
+			"description": "SMEntertainment Group Official Twitter",
+			"url": "https://t.co/zjb7YW3ApX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zjb7YW3ApX",
+							"expanded_url": "http://www.smtown.com",
+							"display_url": "smtown.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10914919,
+			"friends_count": 56,
+			"listed_count": 12312,
+			"created_at": "Tue Oct 16 12:49:45 +0000 2012",
+			"favourites_count": 2026,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 12346,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "F08BA0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1537993544555560963/XHzYSZjH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1537993544555560963/XHzYSZjH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/884378714/1655521211",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5048,
+		"favorite_count": 17202,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "iScreaM Vol.16 : 버퍼링 (Glitch Mode) Remix\n\n➫ 2022.06.21 6PM KST  \n\n#NCTDREAM #GlitchMode \n#NCTDREAM_GlitchMode #Remix\n#iScreaM\n#ScreaMRecords \n#SMTOWN https://t.co/hd10JGZO69"
+	},
+	{
 		"created_at": "Sat Jun 18 17:29:34 +0000 2022",
 		"id": 1538174698898436000,
 		"id_str": "1538212337475264514",
