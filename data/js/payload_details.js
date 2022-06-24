7ee4a37e6d2de144c8ebb129e7070bcf910356c6
솔플파티 <solplparty@gmail.com>
2022-06-24 22:17:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 62308,
-	"created_at": "Fri Jun 24 12:17:40 +0000 2022",
+	"tweets": 62309,
+	"created_at": "Fri Jun 24 13:17:37 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,177 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Fri Jun 24 12:29:24 +0000 2022",
+		"id": 1540303488747720700,
+		"id_str": "1540311126805815296",
+		"full_text": "[#인순이] #수니버스 #순티크\n\n▶[메이킹] 화려한 불빛🪅에 나를 맡길 준비가 되었다면?👑 |  순티크 EP.02◀\n\n사이키조명에도 지지 않는 디바의 품격⚜️\n메이킹필름에서 만나보세요! \n\n👉 https://t.co/Qt4gGnNHnr\n\n#INSOONI #휴맵컨텐츠 #HUMAPCONTENTS #sooniverse #메이크오버 #makeover",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			193
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "인순이",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "수니버스",
+					"indices": [
+						7,
+						12
+					]
+				},
+				{
+					"text": "순티크",
+					"indices": [
+						13,
+						17
+					]
+				},
+				{
+					"text": "INSOONI",
+					"indices": [
+						134,
+						142
+					]
+				},
+				{
+					"text": "휴맵컨텐츠",
+					"indices": [
+						143,
+						149
+					]
+				},
+				{
+					"text": "HUMAPCONTENTS",
+					"indices": [
+						150,
+						164
+					]
+				},
+				{
+					"text": "sooniverse",
+					"indices": [
+						165,
+						176
+					]
+				},
+				{
+					"text": "메이크오버",
+					"indices": [
+						177,
+						183
+					]
+				},
+				{
+					"text": "makeover",
+					"indices": [
+						184,
+						193
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Qt4gGnNHnr",
+					"expanded_url": "https://youtu.be/nF2o9SBZbIw",
+					"display_url": "youtu.be/nF2o9SBZbIw",
+					"indices": [
+						109,
+						132
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1219506842935513000,
+			"id_str": "1219506842935513088",
+			"name": "휴맵컨텐츠(HUMAP)",
+			"screen_name": "humap_official",
+			"location": "",
+			"description": "휴맵컨텐츠(HUMAP CONTENTS) Official Twitter",
+			"url": "https://t.co/fBZZK3SmX6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fBZZK3SmX6",
+							"expanded_url": "http://www.humapcontents.com/",
+							"display_url": "humapcontents.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 388,
+			"friends_count": 2,
+			"listed_count": 2,
+			"created_at": "Tue Jan 21 06:28:27 +0000 2020",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 776,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1220210788956168192/AkHOuBLd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1219506842935513088/1579755898",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#인순이] #수니버스 #순티크\n\n▶[메이킹] 화려한 불빛🪅에 나를 맡길 준비가 되었다면?👑 |  순티크 EP.02◀\n\n사이키조명에도 지지 않는 디바의 품격⚜️\n메이킹필름에서 만나보세요! \n\n👉 https://t.co/Qt4gGnNHnr\n\n#INSOONI #휴맵컨텐츠 #HUMAPCONTENTS #sooniverse #메이크오버 #makeover"
+	},
+	{
 		"created_at": "Fri Jun 24 12:10:24 +0000 2022",
 		"id": 1540306345232019500,
 		"id_str": "1540306345232019456",
