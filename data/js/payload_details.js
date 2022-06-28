b8a2d92dfa84cd5fc6e943da6c01715329eb7027
솔플파티 <solplparty@gmail.com>
2022-06-28 11:20:32 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 62380,
-	"created_at": "Mon Jun 27 21:17:40 +0000 2022",
+	"tweets": 62381,
+	"created_at": "Tue Jun 28 02:20:31 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,366 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Tue Jun 28 02:11:16 +0000 2022",
+		"id": 1541594302567968800,
+		"id_str": "1541605120387022854",
+		"full_text": "이번주 토요일은 #빵파티 오랜만에 빵에서 파티해요 많이 준비했어요 함께 해요~  #총체적파티 #전다인밴드 #민수홍 #파라란 #이지구 #김연구 #단막극 #설치퍼포먼스 #제8극장 #스프링스 #펑크온파이어 #미미키스 #김동용 #클럽빵 ##kindie https://t.co/gExcz1BF2n",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			136
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "빵파티",
+					"indices": [
+						9,
+						13
+					]
+				},
+				{
+					"text": "총체적파티",
+					"indices": [
+						45,
+						51
+					]
+				},
+				{
+					"text": "전다인밴드",
+					"indices": [
+						52,
+						58
+					]
+				},
+				{
+					"text": "민수홍",
+					"indices": [
+						59,
+						63
+					]
+				},
+				{
+					"text": "파라란",
+					"indices": [
+						64,
+						68
+					]
+				},
+				{
+					"text": "이지구",
+					"indices": [
+						69,
+						73
+					]
+				},
+				{
+					"text": "김연구",
+					"indices": [
+						74,
+						78
+					]
+				},
+				{
+					"text": "단막극",
+					"indices": [
+						79,
+						83
+					]
+				},
+				{
+					"text": "설치퍼포먼스",
+					"indices": [
+						84,
+						91
+					]
+				},
+				{
+					"text": "제8극장",
+					"indices": [
+						92,
+						97
+					]
+				},
+				{
+					"text": "스프링스",
+					"indices": [
+						98,
+						103
+					]
+				},
+				{
+					"text": "펑크온파이어",
+					"indices": [
+						104,
+						111
+					]
+				},
+				{
+					"text": "미미키스",
+					"indices": [
+						112,
+						117
+					]
+				},
+				{
+					"text": "김동용",
+					"indices": [
+						118,
+						122
+					]
+				},
+				{
+					"text": "클럽빵",
+					"indices": [
+						123,
+						127
+					]
+				},
+				{
+					"text": "kindie",
+					"indices": [
+						129,
+						136
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1541594287535583200,
+					"id_str": "1541594287535583233",
+					"indices": [
+						137,
+						160
+					],
+					"media_url": "http://pbs.twimg.com/media/FWTX1jhUcAE8e35.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWTX1jhUcAE8e35.jpg",
+					"url": "https://t.co/gExcz1BF2n",
+					"display_url": "pic.twitter.com/gExcz1BF2n",
+					"expanded_url": "https://twitter.com/club_bbang/status/1541594302567968768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1541594287535583200,
+					"id_str": "1541594287535583233",
+					"indices": [
+						137,
+						160
+					],
+					"media_url": "http://pbs.twimg.com/media/FWTX1jhUcAE8e35.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWTX1jhUcAE8e35.jpg",
+					"url": "https://t.co/gExcz1BF2n",
+					"display_url": "pic.twitter.com/gExcz1BF2n",
+					"expanded_url": "https://twitter.com/club_bbang/status/1541594302567968768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1541594287543943200,
+					"id_str": "1541594287543943168",
+					"indices": [
+						137,
+						160
+					],
+					"media_url": "http://pbs.twimg.com/media/FWTX1jjUAAABdwc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWTX1jjUAAABdwc.jpg",
+					"url": "https://t.co/gExcz1BF2n",
+					"display_url": "pic.twitter.com/gExcz1BF2n",
+					"expanded_url": "https://twitter.com/club_bbang/status/1541594302567968768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1541594287539773400,
+					"id_str": "1541594287539773440",
+					"indices": [
+						137,
+						160
+					],
+					"media_url": "http://pbs.twimg.com/media/FWTX1jiUYAASSNC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWTX1jiUYAASSNC.jpg",
+					"url": "https://t.co/gExcz1BF2n",
+					"display_url": "pic.twitter.com/gExcz1BF2n",
+					"expanded_url": "https://twitter.com/club_bbang/status/1541594302567968768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1387140522,
+			"id_str": "1387140522",
+			"name": "클럽 빵",
+			"screen_name": "club_bbang",
+			"location": "서울 마포 서교동 327-18 지하",
+			"description": "모던록 라이브클럽 빵, 공연일정을 알려드려요! 문의 010-8910-1089, cafebbang@hanmail.net",
+			"url": "https://t.co/ZROqsiLHaK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ZROqsiLHaK",
+							"expanded_url": "http://cafe.daum.net/cafebbang",
+							"display_url": "cafe.daum.net/cafebbang",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2527,
+			"friends_count": 908,
+			"listed_count": 35,
+			"created_at": "Sun Apr 28 14:57:43 +0000 2013",
+			"favourites_count": 89,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1878,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3617429436/49aa1ea87a3bdf4e12ef8ab826e7b229_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3617429436/49aa1ea87a3bdf4e12ef8ab826e7b229_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1387140522/1367815826",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "이번주 토요일은 #빵파티 오랜만에 빵에서 파티해요 많이 준비했어요 함께 해요~  #총체적파티 #전다인밴드 #민수홍 #파라란 #이지구 #김연구 #단막극 #설치퍼포먼스 #제8극장 #스프링스 #펑크온파이어 #미미키스 #김동용 #클럽빵 ##kindie https://t.co/gExcz1BF2n"
+	},
+	{
 		"created_at": "Mon Jun 27 20:58:15 +0000 2022",
 		"id": 1541524062937415700,
 		"id_str": "1541526343304847364",
