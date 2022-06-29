de958abe43bf22a2ec36851caac64f02a15a795f
솔플파티 <solplparty@gmail.com>
2022-06-29 12:17:42 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 62419,
-	"created_at": "Wed Jun 29 02:17:38 +0000 2022",
+	"tweets": 62420,
+	"created_at": "Wed Jun 29 03:17:41 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,288 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Wed Jun 29 02:29:25 +0000 2022",
+		"id": 1541965578826526700,
+		"id_str": "1541972073010044933",
+		"full_text": "SM ‘STATION’’s newtro ballad single ‘Cause You’ by Brown Eyed Soul Young Jun X Lee Ye Joon (produced by KANGTA) is out today (6/29)!\n\n#영준 #YoungJun\n#브라운아이드소울 #BrownEyedSoul\n#이예준 #LeeYeJoon\n#강타 #KANGTA\n#혼잣말이많아졌어 #CauseYou https://t.co/UvND1E9y3g",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			220
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "영준",
+					"indices": [
+						134,
+						137
+					]
+				},
+				{
+					"text": "YoungJun",
+					"indices": [
+						138,
+						147
+					]
+				},
+				{
+					"text": "브라운아이드소울",
+					"indices": [
+						148,
+						157
+					]
+				},
+				{
+					"text": "BrownEyedSoul",
+					"indices": [
+						158,
+						172
+					]
+				},
+				{
+					"text": "이예준",
+					"indices": [
+						173,
+						177
+					]
+				},
+				{
+					"text": "LeeYeJoon",
+					"indices": [
+						178,
+						188
+					]
+				},
+				{
+					"text": "강타",
+					"indices": [
+						189,
+						192
+					]
+				},
+				{
+					"text": "KANGTA",
+					"indices": [
+						193,
+						200
+					]
+				},
+				{
+					"text": "혼잣말이많아졌어",
+					"indices": [
+						201,
+						210
+					]
+				},
+				{
+					"text": "CauseYou",
+					"indices": [
+						211,
+						220
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1541965539852947500,
+					"id_str": "1541965539852947457",
+					"indices": [
+						221,
+						244
+					],
+					"media_url": "http://pbs.twimg.com/media/FWYpfSLUEAEU8w3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWYpfSLUEAEU8w3.jpg",
+					"url": "https://t.co/UvND1E9y3g",
+					"display_url": "pic.twitter.com/UvND1E9y3g",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1541965578826526720/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1400,
+							"h": 1400,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1541965539852947500,
+					"id_str": "1541965539852947457",
+					"indices": [
+						221,
+						244
+					],
+					"media_url": "http://pbs.twimg.com/media/FWYpfSLUEAEU8w3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWYpfSLUEAEU8w3.jpg",
+					"url": "https://t.co/UvND1E9y3g",
+					"display_url": "pic.twitter.com/UvND1E9y3g",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1541965578826526720/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1400,
+							"h": 1400,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1541965539878154200,
+					"id_str": "1541965539878154240",
+					"indices": [
+						221,
+						244
+					],
+					"media_url": "http://pbs.twimg.com/media/FWYpfSRUsAAWpWu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWYpfSRUsAAWpWu.jpg",
+					"url": "https://t.co/UvND1E9y3g",
+					"display_url": "pic.twitter.com/UvND1E9y3g",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1541965578826526720/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 801,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1250,
+							"h": 1873,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 454,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 884378714,
+			"id_str": "884378714",
+			"name": "SMTOWN",
+			"screen_name": "SMTOWNGLOBAL",
+			"location": "",
+			"description": "SMEntertainment Group Official Twitter",
+			"url": "https://t.co/zjb7YW3ApX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zjb7YW3ApX",
+							"expanded_url": "http://www.smtown.com",
+							"display_url": "smtown.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10942548,
+			"friends_count": 56,
+			"listed_count": 12350,
+			"created_at": "Tue Oct 16 12:49:45 +0000 2012",
+			"favourites_count": 2026,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 12405,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "F08BA0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1537993544555560963/XHzYSZjH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1537993544555560963/XHzYSZjH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/884378714/1655521211",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 142,
+		"favorite_count": 1320,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "SM ‘STATION’’s newtro ballad single ‘Cause You’ by Brown Eyed Soul Young Jun X Lee Ye Joon (produced by KANGTA) is out today (6/29)!\n\n#영준 #YoungJun\n#브라운아이드소울 #BrownEyedSoul\n#이예준 #LeeYeJoon\n#강타 #KANGTA\n#혼잣말이많아졌어 #CauseYou https://t.co/UvND1E9y3g"
+	},
+	{
 		"created_at": "Wed Jun 29 01:29:37 +0000 2022",
 		"id": 1541956764362748000,
 		"id_str": "1541957024531054592",
