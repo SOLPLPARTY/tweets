23b6acbd81dceb4c73ec863a6455b6bc839665fb
솔플파티 <solplparty@gmail.com>
2022-07-01 13:17:32 +0900

    Added 1 tweets

--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,253 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Fri Jul 01 03:29:25 +0000 2022",
+		"id": 1542700790111842300,
+		"id_str": "1542711946918727681",
+		"full_text": "SM X Seoul Philharmonic Orchestra’s ‘Feel My Rhythm’ Orchestra Ver. will be out today at 6PM (KST)! \n\nThe Seoul Philharmonic Orchestra's magnificent performance music video will be released simultaneously!\n\n#Feel_My_Rhythm \n#RedVelvet\n#서울시립교향악단 #seoulphil\n#smclassics https://t.co/CAWAVaHF1H",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			267
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Feel_My_Rhythm",
+					"indices": [
+						207,
+						222
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						224,
+						234
+					]
+				},
+				{
+					"text": "서울시립교향악단",
+					"indices": [
+						235,
+						244
+					]
+				},
+				{
+					"text": "seoulphil",
+					"indices": [
+						245,
+						255
+					]
+				},
+				{
+					"text": "smclassics",
+					"indices": [
+						256,
+						267
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1542700780037124000,
+					"id_str": "1542700780037124096",
+					"indices": [
+						268,
+						291
+					],
+					"media_url": "http://pbs.twimg.com/media/FWjGL5TUsAA_8rR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWjGL5TUsAA_8rR.jpg",
+					"url": "https://t.co/CAWAVaHF1H",
+					"display_url": "pic.twitter.com/CAWAVaHF1H",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1542700790111842304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1728,
+							"h": 1728,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1542700780037124000,
+					"id_str": "1542700780037124096",
+					"indices": [
+						268,
+						291
+					],
+					"media_url": "http://pbs.twimg.com/media/FWjGL5TUsAA_8rR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWjGL5TUsAA_8rR.jpg",
+					"url": "https://t.co/CAWAVaHF1H",
+					"display_url": "pic.twitter.com/CAWAVaHF1H",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1542700790111842304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1728,
+							"h": 1728,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1542700780028719000,
+					"id_str": "1542700780028719104",
+					"indices": [
+						268,
+						291
+					],
+					"media_url": "http://pbs.twimg.com/media/FWjGL5RUcAA9Z9h.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWjGL5RUcAA9Z9h.jpg",
+					"url": "https://t.co/CAWAVaHF1H",
+					"display_url": "pic.twitter.com/CAWAVaHF1H",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1542700790111842304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 884378714,
+			"id_str": "884378714",
+			"name": "SMTOWN",
+			"screen_name": "SMTOWNGLOBAL",
+			"location": "",
+			"description": "SMEntertainment Group Official Twitter",
+			"url": "https://t.co/zjb7YW3ApX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zjb7YW3ApX",
+							"expanded_url": "http://www.smtown.com",
+							"display_url": "smtown.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10951812,
+			"friends_count": 56,
+			"listed_count": 12349,
+			"created_at": "Tue Oct 16 12:49:45 +0000 2012",
+			"favourites_count": 2026,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 12428,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "F08BA0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1537993544555560963/XHzYSZjH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1537993544555560963/XHzYSZjH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/884378714/1655521211",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1190,
+		"favorite_count": 3715,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "SM X Seoul Philharmonic Orchestra’s ‘Feel My Rhythm’ Orchestra Ver. will be out today at 6PM (KST)! \n\nThe Seoul Philharmonic Orchestra's magnificent performance music video will be released simultaneously!\n\n#Feel_My_Rhythm \n#RedVelvet\n#서울시립교향악단 #seoulphil\n#smclassics https://t.co/CAWAVaHF1H"
+	},
+	{
 		"created_at": "Fri Jul 01 02:29:25 +0000 2022",
 		"id": 1542685322911162400,
 		"id_str": "1542696849450192897",
