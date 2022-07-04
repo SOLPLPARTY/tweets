c01bb183202f045a217757ffbcafd060ab7e197d
솔플파티 <solplparty@gmail.com>
2022-07-04 17:17:33 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 62590,
-	"created_at": "Mon Jul 04 07:17:32 +0000 2022",
+	"tweets": 62591,
+	"created_at": "Mon Jul 04 08:17:33 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,241 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Mon Jul 04 07:29:25 +0000 2022",
+		"id": 1543849204136611800,
+		"id_str": "1543859509692604416",
+		"full_text": "[나왔습니다] ‘레인보우 노트’와 '브레이브걸스’의 롤린, 운전만해를 프로듀싱한 ‘투챔프’, 프로듀서 ‘디웨일’, ‘All day on’이 함께한 #새벽공기\n🌈지금 듣기 https://t.co/nDQyeLywo6\n-\n🌈레인보우노트 나오는 페스티벌 '스마일러브위크엔드'도 놓치지마세요\n📌 예매사이트 https://t.co/aCU9r7DXq3 https://t.co/iRWjhOx3SN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			189
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "새벽공기",
+					"indices": [
+						82,
+						87
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/nDQyeLywo6",
+					"expanded_url": "http://linktr.ee/newrubymusic",
+					"display_url": "linktr.ee/newrubymusic",
+					"indices": [
+						95,
+						118
+					]
+				},
+				{
+					"url": "https://t.co/aCU9r7DXq3",
+					"expanded_url": "https://linktr.ee/smileloveweekend",
+					"display_url": "linktr.ee/smileloveweeke…",
+					"indices": [
+						166,
+						189
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1543849199925506000,
+					"id_str": "1543849199925506049",
+					"indices": [
+						190,
+						213
+					],
+					"media_url": "http://pbs.twimg.com/media/FWzaquxUsAEw4kv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWzaquxUsAEw4kv.jpg",
+					"url": "https://t.co/iRWjhOx3SN",
+					"display_url": "pic.twitter.com/iRWjhOx3SN",
+					"expanded_url": "https://twitter.com/ruby_records/status/1543849204136611840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1543849199925506000,
+					"id_str": "1543849199925506049",
+					"indices": [
+						190,
+						213
+					],
+					"media_url": "http://pbs.twimg.com/media/FWzaquxUsAEw4kv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWzaquxUsAEw4kv.jpg",
+					"url": "https://t.co/iRWjhOx3SN",
+					"display_url": "pic.twitter.com/iRWjhOx3SN",
+					"expanded_url": "https://twitter.com/ruby_records/status/1543849204136611840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 110930534,
+			"id_str": "110930534",
+			"name": "Ruby records",
+			"screen_name": "ruby_records",
+			"location": "대한민국 서울",
+			"description": "💎 루비레코드 #루비튜브 Rubytube #인천여관X루비살롱 rubysalon #모트 Motte #레인보우노트 RBN #우예린 yerin",
+			"url": "https://t.co/vLFrhW6ba0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vLFrhW6ba0",
+							"expanded_url": "http://youtube.com/user/rubysalon",
+							"display_url": "youtube.com/user/rubysalon",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7587,
+			"friends_count": 8221,
+			"listed_count": 196,
+			"created_at": "Wed Feb 03 07:13:24 +0000 2010",
+			"favourites_count": 1010,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7666,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1403718575479681030/-FWGVqIS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1403718575479681030/-FWGVqIS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/110930534/1623507563",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "01685eee514bfb4a",
+			"url": "https://api.twitter.com/1.1/geo/id/01685eee514bfb4a.json",
+			"place_type": "city",
+			"name": "중구",
+			"full_name": "대한민국 중구",
+			"country_code": "KR",
+			"country": "대한민국",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							126.592646397111,
+							37.4294897539181
+						],
+						[
+							126.644132161358,
+							37.4294897539181
+						],
+						[
+							126.644132161358,
+							37.4926495711362
+						],
+						[
+							126.592646397111,
+							37.4926495711362
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[나왔습니다] ‘레인보우 노트’와 '브레이브걸스’의 롤린, 운전만해를 프로듀싱한 ‘투챔프’, 프로듀서 ‘디웨일’, ‘All day on’이 함께한 #새벽공기\n🌈지금 듣기 https://t.co/nDQyeLywo6\n-\n🌈레인보우노트 나오는 페스티벌 '스마일러브위크엔드'도 놓치지마세요\n📌 예매사이트 https://t.co/aCU9r7DXq3 https://t.co/iRWjhOx3SN"
+	},
+	{
 		"created_at": "Mon Jul 04 07:05:34 +0000 2022",
 		"id": 1543853507119181800,
 		"id_str": "1543853507119181825",
