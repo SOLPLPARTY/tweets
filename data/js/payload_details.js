10629eda6b58be3bcf846d30f942c44955ddd2b9
솔플파티 <solplparty@gmail.com>
2022-07-11 02:17:34 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 62766,
-	"created_at": "Sun Jul 10 16:17:35 +0000 2022",
+	"tweets": 62767,
+	"created_at": "Sun Jul 10 17:17:34 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Sun Jul 10 16:29:25 +0000 2022",
+		"id": 1546154780552138800,
+		"id_str": "1546169731991863296",
+		"full_text": "[#장원형] 2022.07.11 WONHYOUNG DAY🎉\n\nHappy Birthday to WONHYOUNG ZANG🎂\n미스틱스토리 배우 장원형의 생일을 축하합니다🎁💙\n\n#220711 #happybirthday #birthday #생일 https://t.co/VuewUZfzkD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			132
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "장원형",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "happybirthday",
+					"indices": [
+						104,
+						118
+					]
+				},
+				{
+					"text": "birthday",
+					"indices": [
+						119,
+						128
+					]
+				},
+				{
+					"text": "생일",
+					"indices": [
+						129,
+						132
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1545313409046282200,
+					"id_str": "1545313409046282240",
+					"indices": [
+						133,
+						156
+					],
+					"media_url": "http://pbs.twimg.com/media/FXIOW6zaAAA7tWk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXIOW6zaAAA7tWk.jpg",
+					"url": "https://t.co/VuewUZfzkD",
+					"display_url": "pic.twitter.com/VuewUZfzkD",
+					"expanded_url": "https://twitter.com/themysticstory/status/1546154780552138753/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 667,
+							"h": 667,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 667,
+							"h": 667,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 667,
+							"h": 667,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1545313409046282200,
+					"id_str": "1545313409046282240",
+					"indices": [
+						133,
+						156
+					],
+					"media_url": "http://pbs.twimg.com/media/FXIOW6zaAAA7tWk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXIOW6zaAAA7tWk.jpg",
+					"url": "https://t.co/VuewUZfzkD",
+					"display_url": "pic.twitter.com/VuewUZfzkD",
+					"expanded_url": "https://twitter.com/themysticstory/status/1546154780552138753/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 667,
+							"h": 667,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 667,
+							"h": 667,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 667,
+							"h": 667,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/pgQyM4g8RF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pgQyM4g8RF",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 36690,
+			"friends_count": 15,
+			"listed_count": 293,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 7057,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1656860618",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 20,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#장원형] 2022.07.11 WONHYOUNG DAY🎉\n\nHappy Birthday to WONHYOUNG ZANG🎂\n미스틱스토리 배우 장원형의 생일을 축하합니다🎁💙\n\n#220711 #happybirthday #birthday #생일 https://t.co/VuewUZfzkD"
+	},
+	{
 		"created_at": "Sun Jul 10 16:11:16 +0000 2022",
 		"id": 1546154200815480800,
 		"id_str": "1546165167095955456",
