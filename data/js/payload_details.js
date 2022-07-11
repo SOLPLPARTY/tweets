88635aac5a15cfe67b3e6ce21ebdca738b33ce43
솔플파티 <solplparty@gmail.com>
2022-07-12 00:17:36 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 62792,
-	"created_at": "Mon Jul 11 13:17:35 +0000 2022",
+	"tweets": 62793,
+	"created_at": "Mon Jul 11 15:17:35 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Mon Jul 11 14:29:25 +0000 2022",
+		"id": 1546491479224623000,
+		"id_str": "1546501920386420736",
+		"full_text": "[#News] #윤석철\n\n윤석철트리오, 16일 컴필레이션 앨범 발표..13년 음악 기록 담았다\n\n(출처: 헤럴드POP | 네이버 TV연예)\n\n✔️ https://t.co/XGdgrBbxht\n\n@SCJAZZY \n#YunSeokCheol #윤석철트리오 #YUNSEOKCHEOL_TRIO\n#SOUNDSGOOD #VANS #WAFFLERECORDSTORE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			194
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "News",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "윤석철",
+					"indices": [
+						8,
+						12
+					]
+				},
+				{
+					"text": "YunSeokCheol",
+					"indices": [
+						117,
+						130
+					]
+				},
+				{
+					"text": "윤석철트리오",
+					"indices": [
+						131,
+						138
+					]
+				},
+				{
+					"text": "YUNSEOKCHEOL_TRIO",
+					"indices": [
+						139,
+						157
+					]
+				},
+				{
+					"text": "SOUNDSGOOD",
+					"indices": [
+						158,
+						169
+					]
+				},
+				{
+					"text": "VANS",
+					"indices": [
+						170,
+						175
+					]
+				},
+				{
+					"text": "WAFFLERECORDSTORE",
+					"indices": [
+						176,
+						194
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SCJAZZY",
+					"name": "윤석철",
+					"id": 129854311,
+					"id_str": "129854311",
+					"indices": [
+						107,
+						115
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/XGdgrBbxht",
+					"expanded_url": "http://naver.me/51QhiyJR",
+					"display_url": "naver.me/51QhiyJR",
+					"indices": [
+						82,
+						105
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 266004552,
+			"id_str": "266004552",
+			"name": "안테나 Antenna",
+			"screen_name": "antennamusic",
+			"location": "대한민국 서울",
+			"description": "유희열(Toy), 유재석, 정재형, 루시드폴, 페퍼톤스(신재평&이장원), 박새별, 샘김, 이진아, 권진아, 정승환, 윤석철, 적재, 이미주, 서동환, 황현조 Official / 루시드폴 DS ‘목소리와 기타’ 2022. 05. 23. 6PM Release",
+			"url": "https://t.co/XSWWwEPbf8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XSWWwEPbf8",
+							"expanded_url": "http://www.antenna.co.kr",
+							"display_url": "antenna.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 40517,
+			"friends_count": 12,
+			"listed_count": 382,
+			"created_at": "Mon Mar 14 13:49:27 +0000 2011",
+			"favourites_count": 14,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 5207,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266004552/1653296979",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#News] #윤석철\n\n윤석철트리오, 16일 컴필레이션 앨범 발표..13년 음악 기록 담았다\n\n(출처: 헤럴드POP | 네이버 TV연예)\n\n✔️ https://t.co/XGdgrBbxht\n\n@SCJAZZY \n#YunSeokCheol #윤석철트리오 #YUNSEOKCHEOL_TRIO\n#SOUNDSGOOD #VANS #WAFFLERECORDSTORE"
+	},
+	{
 		"created_at": "Mon Jul 11 12:29:27 +0000 2022",
 		"id": 1546470824869126100,
 		"id_str": "1546471730184683521",
