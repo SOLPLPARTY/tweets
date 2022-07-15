def1b249ec71f96a630d96be4c08009e9766df9d
솔플파티 <solplparty@gmail.com>
2022-07-16 00:17:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 62935,
-	"created_at": "Fri Jul 15 13:17:37 +0000 2022",
+	"tweets": 62936,
+	"created_at": "Fri Jul 15 15:17:37 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,248 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Fri Jul 15 14:29:25 +0000 2022",
+		"id": 1547936522527068200,
+		"id_str": "1547951472863617024",
+		"full_text": "[#윤종신 #Billlie] KBS2 '유희열의 스케치북' 출연✔\n\n🔥여름밤 무더위를 탈출하는 방법\n1️⃣ 잠시 후 11시 30분, KBS2를 켠다!\n2️⃣ '유희열의 스케치북'을 본방사수 한다📺\n\n🎧 팥빙수 PLAY \n🔗 https://t.co/7Yx5hedvYA\n\n#track_by_YOON #트랙바이윤 #YoonJongshin #빌리 #팥빙수 #Patbingsu https://t.co/FN3ddp4Cod",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			205
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "윤종신",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "Billlie",
+					"indices": [
+						6,
+						14
+					]
+				},
+				{
+					"text": "track_by_YOON",
+					"indices": [
+						150,
+						164
+					]
+				},
+				{
+					"text": "트랙바이윤",
+					"indices": [
+						165,
+						171
+					]
+				},
+				{
+					"text": "YoonJongshin",
+					"indices": [
+						172,
+						185
+					]
+				},
+				{
+					"text": "빌리",
+					"indices": [
+						186,
+						189
+					]
+				},
+				{
+					"text": "팥빙수",
+					"indices": [
+						190,
+						194
+					]
+				},
+				{
+					"text": "Patbingsu",
+					"indices": [
+						195,
+						205
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/7Yx5hedvYA",
+					"expanded_url": "http://kko.to/rWOQcIhQi",
+					"display_url": "kko.to/rWOQcIhQi",
+					"indices": [
+						125,
+						148
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1547850993383387100,
+					"id_str": "1547850993383387136",
+					"indices": [
+						206,
+						229
+					],
+					"media_url": "http://pbs.twimg.com/media/FXsSRxHaMAAa_1S.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXsSRxHaMAAa_1S.jpg",
+					"url": "https://t.co/FN3ddp4Cod",
+					"display_url": "pic.twitter.com/FN3ddp4Cod",
+					"expanded_url": "https://twitter.com/themysticstory/status/1547936522527068164/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1547850993383387100,
+					"id_str": "1547850993383387136",
+					"indices": [
+						206,
+						229
+					],
+					"media_url": "http://pbs.twimg.com/media/FXsSRxHaMAAa_1S.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXsSRxHaMAAa_1S.jpg",
+					"url": "https://t.co/FN3ddp4Cod",
+					"display_url": "pic.twitter.com/FN3ddp4Cod",
+					"expanded_url": "https://twitter.com/themysticstory/status/1547936522527068164/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/pgQyM4g8RF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pgQyM4g8RF",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 36923,
+			"friends_count": 15,
+			"listed_count": 293,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 7071,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1656860618",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 462,
+		"favorite_count": 1750,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#윤종신 #Billlie] KBS2 '유희열의 스케치북' 출연✔\n\n🔥여름밤 무더위를 탈출하는 방법\n1️⃣ 잠시 후 11시 30분, KBS2를 켠다!\n2️⃣ '유희열의 스케치북'을 본방사수 한다📺\n\n🎧 팥빙수 PLAY \n🔗 https://t.co/7Yx5hedvYA\n\n#track_by_YOON #트랙바이윤 #YoonJongshin #빌리 #팥빙수 #Patbingsu https://t.co/FN3ddp4Cod"
+	},
+	{
 		"created_at": "Fri Jul 15 13:11:17 +0000 2022",
 		"id": 1547928974570840000,
 		"id_str": "1547931810318860290",
