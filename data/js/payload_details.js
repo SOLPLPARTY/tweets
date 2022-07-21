b21012c968a3479028f8f9231627fb200da33d50
솔플파티 <solplparty@gmail.com>
2022-07-21 12:17:40 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 63090,
-	"created_at": "Thu Jul 21 02:17:39 +0000 2022",
+	"tweets": 63091,
+	"created_at": "Thu Jul 21 03:17:39 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,163 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Thu Jul 21 02:29:25 +0000 2022",
+		"id": 1549942183255412700,
+		"id_str": "1549944605688332293",
+		"full_text": "[#NEWS] 민서, 신생팀 'FC 발라드림' 합류…김병지·하석주도 놀란 축구 실력 ('골때녀')\n\n▶️https://t.co/ID0RWJcO3c\n\n#민서 #MINSEO #MNSO #SBS #골때리는그녀들 #골때녀",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			119
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "민서",
+					"indices": [
+						83,
+						86
+					]
+				},
+				{
+					"text": "MINSEO",
+					"indices": [
+						87,
+						94
+					]
+				},
+				{
+					"text": "MNSO",
+					"indices": [
+						95,
+						100
+					]
+				},
+				{
+					"text": "SBS",
+					"indices": [
+						101,
+						105
+					]
+				},
+				{
+					"text": "골때리는그녀들",
+					"indices": [
+						106,
+						114
+					]
+				},
+				{
+					"text": "골때녀",
+					"indices": [
+						115,
+						119
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ID0RWJcO3c",
+					"expanded_url": "https://naver.me/G1IToO2v",
+					"display_url": "naver.me/G1IToO2v",
+					"indices": [
+						58,
+						81
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/pgQyM4g8RF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pgQyM4g8RF",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 36981,
+			"friends_count": 14,
+			"listed_count": 292,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 7079,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1656860618",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 11,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] 민서, 신생팀 'FC 발라드림' 합류…김병지·하석주도 놀란 축구 실력 ('골때녀')\n\n▶️https://t.co/ID0RWJcO3c\n\n#민서 #MINSEO #MNSO #SBS #골때리는그녀들 #골때녀"
+	},
+	{
 		"created_at": "Thu Jul 21 02:04:20 +0000 2022",
 		"id": 1549938294636523500,
 		"id_str": "1549938294636523521",
