a78db09a222287853116e7078dec8fe488161d93
솔플파티 <solplparty@gmail.com>
2022-07-22 12:17:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 63124,
-	"created_at": "Fri Jul 22 02:17:38 +0000 2022",
+	"tweets": 63125,
+	"created_at": "Fri Jul 22 03:17:37 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,250 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Fri Jul 22 02:58:22 +0000 2022",
+		"id": 1550300894771548200,
+		"id_str": "1550314280280539136",
+		"full_text": "[Kep1er CHECK IN]\nKep1ian 울고 싶어질 때도~ 미소짓게 만드는 마법~🍓 Kep1er 파티시에👩‍🍳가 케이크에 숨겨놓은 메시지 찾으러 갈 Kep1ian~🎂💙💚\n\n#KCON #KCONUSA #KCON2022LA @official_kep1er https://t.co/fboYfRWTTt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			143
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "KCON",
+					"indices": [
+						100,
+						105
+					]
+				},
+				{
+					"text": "KCONUSA",
+					"indices": [
+						106,
+						114
+					]
+				},
+				{
+					"text": "KCON2022LA",
+					"indices": [
+						115,
+						126
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "official_kep1er",
+					"name": "Kep1er",
+					"id": 1448627067856179200,
+					"id_str": "1448627067856179203",
+					"indices": [
+						127,
+						143
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1550300787426832400,
+					"id_str": "1550300787426832384",
+					"indices": [
+						144,
+						167
+					],
+					"media_url": "http://pbs.twimg.com/media/FYPGWjGVsAA4hr3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FYPGWjGVsAA4hr3.jpg",
+					"url": "https://t.co/fboYfRWTTt",
+					"display_url": "pic.twitter.com/fboYfRWTTt",
+					"expanded_url": "https://twitter.com/KCON_official/status/1550300894771548160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1550300787426832400,
+					"id_str": "1550300787426832384",
+					"indices": [
+						144,
+						167
+					],
+					"media_url": "http://pbs.twimg.com/media/FYPGWjGVsAA4hr3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FYPGWjGVsAA4hr3.jpg",
+					"url": "https://t.co/fboYfRWTTt",
+					"display_url": "pic.twitter.com/fboYfRWTTt",
+					"expanded_url": "https://twitter.com/KCON_official/status/1550300894771548160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1550300787405758500,
+					"id_str": "1550300787405758464",
+					"indices": [
+						144,
+						167
+					],
+					"media_url": "http://pbs.twimg.com/media/FYPGWjBUIAAzcW4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FYPGWjBUIAAzcW4.jpg",
+					"url": "https://t.co/fboYfRWTTt",
+					"display_url": "pic.twitter.com/fboYfRWTTt",
+					"expanded_url": "https://twitter.com/KCON_official/status/1550300894771548160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1550300883144958000,
+		"in_reply_to_status_id_str": "1550300883144957953",
+		"in_reply_to_user_id": 135447868,
+		"in_reply_to_user_id_str": "135447868",
+		"in_reply_to_screen_name": "KCON_official",
+		"user": {
+			"id": 135447868,
+			"id_str": "135447868",
+			"name": "KCON official",
+			"screen_name": "KCON_official",
+			"location": "Mnet, Seoul, Republic of KOREA",
+			"description": "KCON OFFICIAL TWITTER / KCON 2022 LA AUG 19-21",
+			"url": "https://t.co/IDnJhWG279",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/IDnJhWG279",
+							"expanded_url": "http://kconofficial.com",
+							"display_url": "kconofficial.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 449159,
+			"friends_count": 76,
+			"listed_count": 1119,
+			"created_at": "Wed Apr 21 09:56:48 +0000 2010",
+			"favourites_count": 136,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 12512,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1537723180315316224/3hS4BbEJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1537723180315316224/3hS4BbEJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/135447868/1655163084",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 121,
+		"favorite_count": 298,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[Kep1er CHECK IN]\nKep1ian 울고 싶어질 때도~ 미소짓게 만드는 마법~🍓 Kep1er 파티시에👩‍🍳가 케이크에 숨겨놓은 메시지 찾으러 갈 Kep1ian~🎂💙💚\n\n#KCON #KCONUSA #KCON2022LA @official_kep1er https://t.co/fboYfRWTTt"
+	},
+	{
 		"created_at": "Fri Jul 22 01:58:15 +0000 2022",
 		"id": 1550284564550955000,
 		"id_str": "1550299150545260545",
