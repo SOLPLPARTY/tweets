1055783fc58d23d3c699441c549082ecb0da6219
솔플파티 <solplparty@gmail.com>
2022-07-31 15:17:42 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 63405,
-	"created_at": "Sun Jul 31 04:17:42 +0000 2022",
+	"tweets": 63406,
+	"created_at": "Sun Jul 31 06:17:42 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2022_07.js",
 		"year": 2022,
 		"var_name": "tweets_2022_07",
-		"tweet_count": 893,
+		"tweet_count": 894,
 		"month": 7
 	},
 	{
