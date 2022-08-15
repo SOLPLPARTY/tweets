4186cdb9d8e254211de810e4c1a84e49a13806d0
솔플파티 <solplparty@gmail.com>
2022-08-15 22:17:38 +0900

    Added 1 tweets

--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Mon Aug 15 12:29:25 +0000 2022",
+		"id": 1559153246748283000,
+		"id_str": "1559155297989574657",
+		"full_text": "[#홍자] 네이버 NOW. 뮤지컬 라이브쇼 &lt;커튼콜&gt; 완료📸\n\n송화홍자 뮤지컬에 이어 &lt;커튼콜&gt;도 접수❣\n\n송화홍자는 💜8월 20일, 8월 24일\n9월 4일, 9월 7일, 9월 18일💜\n광림아트센터 BBCH홀에서 만날 수 있습니다🌸\n\n#HONGJA #커튼콜 https://t.co/Co3rWGrnnV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			155
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "홍자",
+					"indices": [
+						1,
+						4
+					]
+				},
+				{
+					"text": "HONGJA",
+					"indices": [
+						143,
+						150
+					]
+				},
+				{
+					"text": "커튼콜",
+					"indices": [
+						151,
+						155
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1559153230869037000,
+					"id_str": "1559153230869037056",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/FaM5mkxaAAA_R4h.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FaM5mkxaAAA_R4h.jpg",
+					"url": "https://t.co/Co3rWGrnnV",
+					"display_url": "pic.twitter.com/Co3rWGrnnV",
+					"expanded_url": "https://twitter.com/themysticstory/status/1559153246748282881/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1559153230869037000,
+					"id_str": "1559153230869037056",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/FaM5mkxaAAA_R4h.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FaM5mkxaAAA_R4h.jpg",
+					"url": "https://t.co/Co3rWGrnnV",
+					"display_url": "pic.twitter.com/Co3rWGrnnV",
+					"expanded_url": "https://twitter.com/themysticstory/status/1559153246748282881/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/pgQyM4g8RF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pgQyM4g8RF",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 37136,
+			"friends_count": 14,
+			"listed_count": 299,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 7129,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1660014455",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 11,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#홍자] 네이버 NOW. 뮤지컬 라이브쇼 &lt;커튼콜&gt; 완료📸\n\n송화홍자 뮤지컬에 이어 &lt;커튼콜&gt;도 접수❣\n\n송화홍자는 💜8월 20일, 8월 24일\n9월 4일, 9월 7일, 9월 18일💜\n광림아트센터 BBCH홀에서 만날 수 있습니다🌸\n\n#HONGJA #커튼콜 https://t.co/Co3rWGrnnV"
+	},
+	{
 		"created_at": "Mon Aug 15 11:58:33 +0000 2022",
 		"id": 1559140594177687600,
 		"id_str": "1559147529991749632",
