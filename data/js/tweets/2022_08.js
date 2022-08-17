0f0b4dd3be5288c94f7ace8c38afcc76790d4bd9
솔플파티 <solplparty@gmail.com>
2022-08-17 22:17:38 +0900

    Added 1 tweets

--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,226 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Wed Aug 17 12:29:30 +0000 2022",
+		"id": 1559865372370665500,
+		"id_str": "1559880096302989313",
+		"full_text": "#리조 💕2 Be Loved (Am I Ready)💕\n내가 내 자신을 사랑하게 되면 준비 끝🥰\n🎬뮤비보기 https://t.co/G0PJ20Tvnt\n🎧음원듣기 https://t.co/yOdVSlMBFM https://t.co/Jf1pl5mmZV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			112
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "리조",
+					"indices": [
+						0,
+						3
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/G0PJ20Tvnt",
+					"expanded_url": "https://youtu.be/krdQLzzAeZU",
+					"display_url": "youtu.be/krdQLzzAeZU",
+					"indices": [
+						59,
+						82
+					]
+				},
+				{
+					"url": "https://t.co/yOdVSlMBFM",
+					"expanded_url": "https://WMK.lnk.to/SNLXJjW5",
+					"display_url": "WMK.lnk.to/SNLXJjW5",
+					"indices": [
+						89,
+						112
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1559799590710562800,
+					"id_str": "1559799590710562817",
+					"indices": [
+						113,
+						136
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1559799590710562817/pu/img/hVXFpkoQayWjJ5mT.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1559799590710562817/pu/img/hVXFpkoQayWjJ5mT.jpg",
+					"url": "https://t.co/Jf1pl5mmZV",
+					"display_url": "pic.twitter.com/Jf1pl5mmZV",
+					"expanded_url": "https://twitter.com/Warner_Music_Kr/status/1559865372370665474/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1559799590710562800,
+					"id_str": "1559799590710562817",
+					"indices": [
+						113,
+						136
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1559799590710562817/pu/img/hVXFpkoQayWjJ5mT.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1559799590710562817/pu/img/hVXFpkoQayWjJ5mT.jpg",
+					"url": "https://t.co/Jf1pl5mmZV",
+					"display_url": "pic.twitter.com/Jf1pl5mmZV",
+					"expanded_url": "https://twitter.com/Warner_Music_Kr/status/1559865372370665474/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							9,
+							16
+						],
+						"duration_millis": 26538,
+						"variants": [
+							{
+								"bitrate": 950000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1559799590710562817/pu/vid/480x852/wXraHJp_mYAqeoEf.mp4?tag=12"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1559799590710562817/pu/vid/320x568/HXTd_rae-LgnTrIZ.mp4?tag=12"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1559799590710562817/pu/vid/720x1280/RFqTjvjok283nUtC.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1559799590710562817/pu/pl/cLopjoVPD_6bw2bH.m3u8?tag=12&container=fmp4"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 206117108,
+			"id_str": "206117108",
+			"name": "Warner Music Korea",
+			"screen_name": "Warner_Music_Kr",
+			"location": "Seoul, Korea",
+			"description": "워너뮤직코리아 공식 트위터",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 89488,
+			"friends_count": 493,
+			"listed_count": 219,
+			"created_at": "Fri Oct 22 09:32:47 +0000 2010",
+			"favourites_count": 1579,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 11893,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1267377694565490688/FD-WdFum_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1267377694565490688/FD-WdFum_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/206117108/1636703401",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "023BE5",
+			"profile_text_color": "2566FC",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 17,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#리조 💕2 Be Loved (Am I Ready)💕\n내가 내 자신을 사랑하게 되면 준비 끝🥰\n🎬뮤비보기 https://t.co/G0PJ20Tvnt\n🎧음원듣기 https://t.co/yOdVSlMBFM https://t.co/Jf1pl5mmZV"
+	},
+	{
 		"created_at": "Wed Aug 17 12:11:17 +0000 2022",
 		"id": 1559846813208719400,
 		"id_str": "1559875511106654211",
