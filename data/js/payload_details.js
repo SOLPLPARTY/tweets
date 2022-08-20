e4ee08cde1731bfd71712d1873784146c79c812b
솔플파티 <solplparty@gmail.com>
2022-08-20 13:17:41 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 64121,
-	"created_at": "Sat Aug 20 03:17:39 +0000 2022",
+	"tweets": 64122,
+	"created_at": "Sat Aug 20 04:17:41 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Sat Aug 20 03:50:35 +0000 2022",
+		"id": 1560533315354087400,
+		"id_str": "1560836668331986944",
+		"full_text": "EVERYONE STAY CALM BUT WE ARE COMING TO PLAY SHOWS IN OUR FAVOURITE PART OF THE WORLD.\n\nAsia tour baby!! Kuala Lumpur, Jakarta, Manila and Busan - SEE YOU IN 35 DAYS!!!! Get your tickets y'allllll! 🇲🇾💖🇮🇩💖🇵🇭💖🇰🇷\n\nhttps://t.co/fLUw3TBD5H https://t.co/Rc7r9GJY8c",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			234
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/fLUw3TBD5H",
+					"expanded_url": "https://www.ohwondermusic.com/tour/",
+					"display_url": "ohwondermusic.com/tour/",
+					"indices": [
+						211,
+						234
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1560533312657129500,
+					"id_str": "1560533312657129472",
+					"indices": [
+						235,
+						258
+					],
+					"media_url": "http://pbs.twimg.com/media/Faggx6EXgAAECc6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Faggx6EXgAAECc6.jpg",
+					"url": "https://t.co/Rc7r9GJY8c",
+					"display_url": "pic.twitter.com/Rc7r9GJY8c",
+					"expanded_url": "https://twitter.com/OhWonderMusic/status/1560533315354087428/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1350,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1560533312657129500,
+					"id_str": "1560533312657129472",
+					"indices": [
+						235,
+						258
+					],
+					"media_url": "http://pbs.twimg.com/media/Faggx6EXgAAECc6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Faggx6EXgAAECc6.jpg",
+					"url": "https://t.co/Rc7r9GJY8c",
+					"display_url": "pic.twitter.com/Rc7r9GJY8c",
+					"expanded_url": "https://twitter.com/OhWonderMusic/status/1560533315354087428/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1350,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2740435740,
+			"id_str": "2740435740",
+			"name": "Oh Wonder",
+			"screen_name": "OhWonderMusic",
+			"location": "London",
+			"description": "New album \"22 Make\" out October 7th! ❤️‍🔥",
+			"url": "https://t.co/aWDekOEjgr",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aWDekOEjgr",
+							"expanded_url": "https://linktr.ee/ohwondermusic",
+							"display_url": "linktr.ee/ohwondermusic",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 165951,
+			"friends_count": 204,
+			"listed_count": 421,
+			"created_at": "Sun Aug 17 20:45:13 +0000 2014",
+			"favourites_count": 3612,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 5434,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1501967196595048451/sXXwT4h1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1501967196595048451/sXXwT4h1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2740435740/1652968649",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 113,
+		"favorite_count": 377,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "EVERYONE STAY CALM BUT WE ARE COMING TO PLAY SHOWS IN OUR FAVOURITE PART OF THE WORLD.\n\nAsia tour baby!! Kuala Lumpur, Jakarta, Manila and Busan - SEE YOU IN 35 DAYS!!!! Get your tickets y'allllll! 🇲🇾💖🇮🇩💖🇵🇭💖🇰🇷\n\nhttps://t.co/fLUw3TBD5H https://t.co/Rc7r9GJY8c"
+	},
+	{
 		"created_at": "Sat Aug 20 03:11:16 +0000 2022",
 		"id": 1560826773822001200,
 		"id_str": "1560826773822001152",
