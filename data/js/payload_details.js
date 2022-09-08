5dee4f0a47be42ef085e6c3acb2dbfd84e237bbe
솔플파티 <solplparty@gmail.com>
2022-09-08 20:17:35 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 64901,
-	"created_at": "Thu Sep 08 10:17:34 +0000 2022",
+	"tweets": 64902,
+	"created_at": "Thu Sep 08 11:17:34 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Thu Sep 08 10:29:25 +0000 2022",
+		"id": 1567808691147272200,
+		"id_str": "1567822406928011265",
+		"full_text": "너드커넥션(Nerd Connection) - 파블로(Pablo)\nOfficial Lyric Video 🥁with DRUMS\n\n너드커넥션 싱글 ‘파블로’의 가사 영상이 공개되었습니다!\n파블로를 카피해보고 싶었던 분들이 계시다면 어서 오세요‼️\n\n🔗 https://t.co/crHmAPuhvg (너드커넥션 공식 유튜브)\n\n#너드커넥션 #파블로 #드럼 #가사영상",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			199
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "너드커넥션",
+					"indices": [
+						178,
+						184
+					]
+				},
+				{
+					"text": "파블로",
+					"indices": [
+						185,
+						189
+					]
+				},
+				{
+					"text": "드럼",
+					"indices": [
+						190,
+						193
+					]
+				},
+				{
+					"text": "가사영상",
+					"indices": [
+						194,
+						199
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/crHmAPuhvg",
+					"expanded_url": "https://c11.kr/1547y",
+					"display_url": "c11.kr/1547y",
+					"indices": [
+						138,
+						161
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 404720294,
+			"id_str": "404720294",
+			"name": "유어썸머 your summer",
+			"screen_name": "YoursummerKR",
+			"location": "한국",
+			"description": "유어썸머(Your Summer)는 cott(콧), 계피, 김사월, 김사월X김해원, 너드커넥션, 랄라스윗, 못(Mot), 박소은, 슬릭, 아마도이자람밴드, 우주왕복선싸이드미러, 이랑, 이하윤, 전기뱀장어와 함께합니다.",
+			"url": "https://t.co/M1QKGb4pGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/M1QKGb4pGi",
+							"expanded_url": "https://linktr.ee/yoursummer",
+							"display_url": "linktr.ee/yoursummer",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2291,
+			"friends_count": 20,
+			"listed_count": 11,
+			"created_at": "Fri Nov 04 09:50:32 +0000 2011",
+			"favourites_count": 635,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6534,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1509898501031079936/2_2E7tDV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1509898501031079936/2_2E7tDV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/404720294/1615202264",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "너드커넥션(Nerd Connection) - 파블로(Pablo)\nOfficial Lyric Video 🥁with DRUMS\n\n너드커넥션 싱글 ‘파블로’의 가사 영상이 공개되었습니다!\n파블로를 카피해보고 싶었던 분들이 계시다면 어서 오세요‼️\n\n🔗 https://t.co/crHmAPuhvg (너드커넥션 공식 유튜브)\n\n#너드커넥션 #파블로 #드럼 #가사영상"
+	},
+	{
 		"created_at": "Thu Sep 08 10:15:43 +0000 2022",
 		"id": 1567818961479401500,
 		"id_str": "1567818961479401475",
