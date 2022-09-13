bbf63df7399e434b3b1a65473084bede145d2110
솔플파티 <solplparty@gmail.com>
2022-09-13 23:17:35 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65010,
-	"created_at": "Tue Sep 13 13:17:35 +0000 2022",
+	"tweets": 65011,
+	"created_at": "Tue Sep 13 14:17:34 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,246 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Tue Sep 13 14:11:17 +0000 2022",
+		"id": 1569686129804443600,
+		"id_str": "1569690182647185416",
+		"full_text": "오랜 시간 인디씬에서 활동하고 있는 뮤지션 셋이 함께 공연합니다 참 멋지게 활동하고 있는 음악가들을 한자리에서 만나는 자리입니다 #소히 #이장혁 #빅베이비드라이버 #클럽빵 https://t.co/V9gTXRh3Z1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			95
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "소히",
+					"indices": [
+						72,
+						75
+					]
+				},
+				{
+					"text": "이장혁",
+					"indices": [
+						76,
+						80
+					]
+				},
+				{
+					"text": "빅베이비드라이버",
+					"indices": [
+						81,
+						90
+					]
+				},
+				{
+					"text": "클럽빵",
+					"indices": [
+						91,
+						95
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1569686120572792800,
+					"id_str": "1569686120572792832",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/media/FcilNcmaMAA_0LB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcilNcmaMAA_0LB.jpg",
+					"url": "https://t.co/V9gTXRh3Z1",
+					"display_url": "pic.twitter.com/V9gTXRh3Z1",
+					"expanded_url": "https://twitter.com/club_bbang/status/1569686129804443653/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1569686120572792800,
+					"id_str": "1569686120572792832",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/media/FcilNcmaMAA_0LB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcilNcmaMAA_0LB.jpg",
+					"url": "https://t.co/V9gTXRh3Z1",
+					"display_url": "pic.twitter.com/V9gTXRh3Z1",
+					"expanded_url": "https://twitter.com/club_bbang/status/1569686129804443653/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1569686121080320000,
+					"id_str": "1569686121080320004",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/media/FcilNefacAQp7k_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcilNefacAQp7k_.jpg",
+					"url": "https://t.co/V9gTXRh3Z1",
+					"display_url": "pic.twitter.com/V9gTXRh3Z1",
+					"expanded_url": "https://twitter.com/club_bbang/status/1569686129804443653/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1387140522,
+			"id_str": "1387140522",
+			"name": "클럽 빵",
+			"screen_name": "club_bbang",
+			"location": "서울 마포 서교동 327-18 지하",
+			"description": "모던록 라이브클럽 빵, 공연일정을 알려드려요! 문의 010-8910-1089, cafebbang@hanmail.net",
+			"url": "https://t.co/ZROqsiLHaK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ZROqsiLHaK",
+							"expanded_url": "http://cafe.daum.net/cafebbang",
+							"display_url": "cafe.daum.net/cafebbang",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2534,
+			"friends_count": 903,
+			"listed_count": 34,
+			"created_at": "Sun Apr 28 14:57:43 +0000 2013",
+			"favourites_count": 90,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1902,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3617429436/49aa1ea87a3bdf4e12ef8ab826e7b229_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3617429436/49aa1ea87a3bdf4e12ef8ab826e7b229_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1387140522/1367815826",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "오랜 시간 인디씬에서 활동하고 있는 뮤지션 셋이 함께 공연합니다 참 멋지게 활동하고 있는 음악가들을 한자리에서 만나는 자리입니다 #소히 #이장혁 #빅베이비드라이버 #클럽빵 https://t.co/V9gTXRh3Z1"
+	},
+	{
 		"created_at": "Tue Sep 13 12:13:59 +0000 2022",
 		"id": 1569660664062578700,
 		"id_str": "1569660664062578690",
