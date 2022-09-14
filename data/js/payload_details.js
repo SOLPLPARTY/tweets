f1cd80ab514c28302106438e4aa68444661ec466
솔플파티 <solplparty@gmail.com>
2022-09-14 23:17:37 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65066,
-	"created_at": "Wed Sep 14 12:17:43 +0000 2022",
+	"tweets": 65067,
+	"created_at": "Wed Sep 14 14:17:36 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Wed Sep 14 13:29:26 +0000 2022",
+		"id": 1570034640521494500,
+		"id_str": "1570042036014776320",
+		"full_text": "[#양재웅] MBC '라디오스타' 출연✔️\n\n잠시 후 10시 30분, 양재웅 원장이 \n'라디오스타'에 출연합니다🌕\n양재웅이 알려주는 호감 시그널부터 \n유쾌한 입담까지 모두 만나보세요☺️\n\n#YangJewoong #MBC #라디오스타 https://t.co/KZzgYpoW2I",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			129
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "양재웅",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "YangJewoong",
+					"indices": [
+						105,
+						117
+					]
+				},
+				{
+					"text": "MBC",
+					"indices": [
+						118,
+						122
+					]
+				},
+				{
+					"text": "라디오스타",
+					"indices": [
+						123,
+						129
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1569973573057679400,
+					"id_str": "1569973573057679365",
+					"indices": [
+						130,
+						153
+					],
+					"media_url": "http://pbs.twimg.com/media/FcmqpYjakAUYoo1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcmqpYjakAUYoo1.jpg",
+					"url": "https://t.co/KZzgYpoW2I",
+					"display_url": "pic.twitter.com/KZzgYpoW2I",
+					"expanded_url": "https://twitter.com/themysticstory/status/1570034640521494529/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1569973573057679400,
+					"id_str": "1569973573057679365",
+					"indices": [
+						130,
+						153
+					],
+					"media_url": "http://pbs.twimg.com/media/FcmqpYjakAUYoo1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcmqpYjakAUYoo1.jpg",
+					"url": "https://t.co/KZzgYpoW2I",
+					"display_url": "pic.twitter.com/KZzgYpoW2I",
+					"expanded_url": "https://twitter.com/themysticstory/status/1570034640521494529/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 486655058,
+			"id_str": "486655058",
+			"name": "미스틱스토리 MYSTIC STORY",
+			"screen_name": "themysticstory",
+			"location": "Seoul, Korea",
+			"description": "MYSTIC STORY Official Twitter",
+			"url": "https://t.co/pgQyM4fB27",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pgQyM4fB27",
+							"expanded_url": "http://youtube.com/welcomeMYSTIC89",
+							"display_url": "youtube.com/welcomeMYSTIC89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 37488,
+			"friends_count": 14,
+			"listed_count": 304,
+			"created_at": "Wed Feb 08 14:33:50 +0000 2012",
+			"favourites_count": 11,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 7223,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225690398372184064/x98FrzSM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/486655058/1661267008",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 12,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#양재웅] MBC '라디오스타' 출연✔️\n\n잠시 후 10시 30분, 양재웅 원장이 \n'라디오스타'에 출연합니다🌕\n양재웅이 알려주는 호감 시그널부터 \n유쾌한 입담까지 모두 만나보세요☺️\n\n#YangJewoong #MBC #라디오스타 https://t.co/KZzgYpoW2I"
+	},
+	{
 		"created_at": "Wed Sep 14 12:11:22 +0000 2022",
 		"id": 1569976043695673300,
 		"id_str": "1570022393996886016",
