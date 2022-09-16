cc247270407df6f3ceafc63d7d11e8037220002d
솔플파티 <solplparty@gmail.com>
2022-09-16 14:17:39 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65132,
-	"created_at": "Fri Sep 16 04:17:38 +0000 2022",
+	"tweets": 65133,
+	"created_at": "Fri Sep 16 05:17:38 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,252 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Fri Sep 16 04:29:25 +0000 2022",
+		"id": 1570616589296472000,
+		"id_str": "1570630915314819072",
+		"full_text": "DJ 스트리밍 쇼 ‘SHOWME’ 시즌2, 이메스-코지훈 공연 오늘 오후 10시 공개!\n\nDJ Streaming Show ‘SHOWME’ Season 2 with EMESS-COZYHOON is today at 10PM (KST)!\n\n#SHOWME #쇼미\n#DJ #EMESS \n#COZYHOON \n#auspicious_crew \n#ScreaMRecords \n#PinkBlood \n#SMTOWN \n#BeyondLIVE https://t.co/9tGyirrZuW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			232
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SHOWME",
+					"indices": [
+						131,
+						138
+					]
+				},
+				{
+					"text": "쇼미",
+					"indices": [
+						139,
+						142
+					]
+				},
+				{
+					"text": "DJ",
+					"indices": [
+						143,
+						146
+					]
+				},
+				{
+					"text": "EMESS",
+					"indices": [
+						147,
+						153
+					]
+				},
+				{
+					"text": "COZYHOON",
+					"indices": [
+						155,
+						164
+					]
+				},
+				{
+					"text": "auspicious_crew",
+					"indices": [
+						166,
+						182
+					]
+				},
+				{
+					"text": "ScreaMRecords",
+					"indices": [
+						184,
+						198
+					]
+				},
+				{
+					"text": "PinkBlood",
+					"indices": [
+						200,
+						210
+					]
+				},
+				{
+					"text": "SMTOWN",
+					"indices": [
+						212,
+						219
+					]
+				},
+				{
+					"text": "BeyondLIVE",
+					"indices": [
+						221,
+						232
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1570616581604151300,
+					"id_str": "1570616581604151297",
+					"indices": [
+						233,
+						256
+					],
+					"media_url": "http://pbs.twimg.com/media/FcvzdaDakAERGkF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcvzdaDakAERGkF.jpg",
+					"url": "https://t.co/9tGyirrZuW",
+					"display_url": "pic.twitter.com/9tGyirrZuW",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1570616589296472064/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1570616581604151300,
+					"id_str": "1570616581604151297",
+					"indices": [
+						233,
+						256
+					],
+					"media_url": "http://pbs.twimg.com/media/FcvzdaDakAERGkF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcvzdaDakAERGkF.jpg",
+					"url": "https://t.co/9tGyirrZuW",
+					"display_url": "pic.twitter.com/9tGyirrZuW",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1570616589296472064/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 884378714,
+			"id_str": "884378714",
+			"name": "SMTOWN",
+			"screen_name": "SMTOWNGLOBAL",
+			"location": "",
+			"description": "SMEntertainment Group Official Twitter",
+			"url": "https://t.co/zjb7YW3ApX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zjb7YW3ApX",
+							"expanded_url": "http://www.smtown.com",
+							"display_url": "smtown.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11176739,
+			"friends_count": 55,
+			"listed_count": 12609,
+			"created_at": "Tue Oct 16 12:49:45 +0000 2012",
+			"favourites_count": 2027,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 12893,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "F08BA0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1537993544555560963/XHzYSZjH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1537993544555560963/XHzYSZjH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/884378714/1655521211",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 76,
+		"favorite_count": 898,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "DJ 스트리밍 쇼 ‘SHOWME’ 시즌2, 이메스-코지훈 공연 오늘 오후 10시 공개!\n\nDJ Streaming Show ‘SHOWME’ Season 2 with EMESS-COZYHOON is today at 10PM (KST)!\n\n#SHOWME #쇼미\n#DJ #EMESS \n#COZYHOON \n#auspicious_crew \n#ScreaMRecords \n#PinkBlood \n#SMTOWN \n#BeyondLIVE https://t.co/9tGyirrZuW"
+	},
+	{
 		"created_at": "Fri Sep 16 03:29:48 +0000 2022",
 		"id": 1570615701559447600,
 		"id_str": "1570615912960782338",
