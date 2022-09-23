a6027fd862f2bb2d2b865af3048cd8b81b4f62ef
솔플파티 <solplparty@gmail.com>
2022-09-23 23:17:43 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65441,
-	"created_at": "Fri Sep 23 13:17:41 +0000 2022",
+	"tweets": 65442,
+	"created_at": "Fri Sep 23 14:17:42 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Fri Sep 23 14:11:17 +0000 2022",
+		"id": 1573281980770357200,
+		"id_str": "1573314060174647303",
+		"full_text": "D-10 [시공간집 5 : 시 놀이공원] \n\nQ: 시를 잘 몰라도 [시공간집] 전시가 흥미로울까요?\n\nA: [시공간집]에서 필사를 시작하는 순간 ‘시를 잘 모른다’는 생각은 사라질 것입니다. 이미 온몸으로 시를 만나고 있으니까요. https://t.co/kZ24n7DfJP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			128
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1573281920997200000,
+					"id_str": "1573281920997199872",
+					"indices": [
+						129,
+						152
+					],
+					"media_url": "http://pbs.twimg.com/media/FdVrklCVEAANB8s.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FdVrklCVEAANB8s.jpg",
+					"url": "https://t.co/kZ24n7DfJP",
+					"display_url": "pic.twitter.com/kZ24n7DfJP",
+					"expanded_url": "https://twitter.com/studio_zemi/status/1573281980770357250/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2000,
+							"h": 2000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1573281920997200000,
+					"id_str": "1573281920997199872",
+					"indices": [
+						129,
+						152
+					],
+					"media_url": "http://pbs.twimg.com/media/FdVrklCVEAANB8s.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FdVrklCVEAANB8s.jpg",
+					"url": "https://t.co/kZ24n7DfJP",
+					"display_url": "pic.twitter.com/kZ24n7DfJP",
+					"expanded_url": "https://twitter.com/studio_zemi/status/1573281980770357250/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2000,
+							"h": 2000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1569959584055586800,
+		"in_reply_to_status_id_str": "1569959584055586816",
+		"in_reply_to_user_id": 282449618,
+		"in_reply_to_user_id_str": "282449618",
+		"in_reply_to_screen_name": "studio_zemi",
+		"user": {
+			"id": 282449618,
+			"id_str": "282449618",
+			"name": "재미공작소",
+			"screen_name": "studio_zemi",
+			"location": "서울시 영등포구 문래동 3가 58-84 1층",
+			"description": "재미공작소는 서울 문래동에 위치한 문화예술공간입니다. (Since 2011) 공연, 문학 행사, 팝업숍, 전시, 창작 워크숍, 출판, 캘린더 갤러리 등 '재미있는 일'을 기획하고 진행합니다. 재미공작소의 일정 및 예약 관련 정보는 아래 홈블로그 링크에서 바로 확인하실 수 있습니다.",
+			"url": "https://t.co/aLQQKEqovK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aLQQKEqovK",
+							"expanded_url": "http://blog.naver.com/studiozemi",
+							"display_url": "blog.naver.com/studiozemi",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8320,
+			"friends_count": 548,
+			"listed_count": 75,
+			"created_at": "Fri Apr 15 07:08:52 +0000 2011",
+			"favourites_count": 2408,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 14834,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1314827654/zemi02_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1314827654/zemi02_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/282449618/1398647875",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "D-10 [시공간집 5 : 시 놀이공원] \n\nQ: 시를 잘 몰라도 [시공간집] 전시가 흥미로울까요?\n\nA: [시공간집]에서 필사를 시작하는 순간 ‘시를 잘 모른다’는 생각은 사라질 것입니다. 이미 온몸으로 시를 만나고 있으니까요. https://t.co/kZ24n7DfJP"
+	},
+	{
 		"created_at": "Fri Sep 23 12:58:15 +0000 2022",
 		"id": 1573281024821321700,
 		"id_str": "1573295680973672448",
