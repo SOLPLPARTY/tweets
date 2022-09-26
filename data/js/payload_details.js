af476cf360942ab104cce361a34a212149cd5cf5
솔플파티 <solplparty@gmail.com>
2022-09-26 12:17:39 +0900

    Added 7 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65505,
-	"created_at": "Mon Sep 26 02:17:43 +0000 2022",
+	"tweets": 65512,
+	"created_at": "Mon Sep 26 03:17:38 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2022_09.js",
 		"year": 2022,
 		"var_name": "tweets_2022_09",
-		"tweet_count": 875,
+		"tweet_count": 882,
 		"month": 9
 	},
 	{
