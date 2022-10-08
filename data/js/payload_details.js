37331c5014e3ea438c6a491051297597a3ba838b
솔플파티 <solplparty@gmail.com>
2022-10-08 17:17:37 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66126,
-	"created_at": "Sat Oct 08 07:17:35 +0000 2022",
+	"tweets": 66127,
+	"created_at": "Sat Oct 08 08:17:36 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,230 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Sat Oct 08 07:58:15 +0000 2022",
+		"id": 1578641343718490000,
+		"id_str": "1578656000105668608",
+		"full_text": "[KCON 2022 SAUDI ARABIA | DAY 2]\nCLOSING w/‘POPPIA’ Signature Song😎🎵\n\nOur thrilling day at KCON 2022 SAUDI ARABIA💎\nThank you KCON-ers for joining us💚\n\nSEE YOU AT KCON 2022 JAPAN❤️\n\n#KCON #KCON2022SAUDIARABIA https://t.co/HUwJtsJev5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			207
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "KCON",
+					"indices": [
+						181,
+						186
+					]
+				},
+				{
+					"text": "KCON2022SAUDIARABIA",
+					"indices": [
+						187,
+						207
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1577988562774986800,
+					"id_str": "1577988562774986752",
+					"indices": [
+						208,
+						231
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1577988562774986752/img/UK2U6gKvd0k-9X9S.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1577988562774986752/img/UK2U6gKvd0k-9X9S.jpg",
+					"url": "https://t.co/HUwJtsJev5",
+					"display_url": "pic.twitter.com/HUwJtsJev5",
+					"expanded_url": "https://twitter.com/KCON_official/status/1578641343718490113/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1577988562774986800,
+					"id_str": "1577988562774986752",
+					"indices": [
+						208,
+						231
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1577988562774986752/img/UK2U6gKvd0k-9X9S.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1577988562774986752/img/UK2U6gKvd0k-9X9S.jpg",
+					"url": "https://t.co/HUwJtsJev5",
+					"display_url": "pic.twitter.com/HUwJtsJev5",
+					"expanded_url": "https://twitter.com/KCON_official/status/1578641343718490113/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 235001,
+						"variants": [
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1577988562774986752/vid/480x270/fbQo5dFzH2vCz7Lx.mp4?tag=14"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1577988562774986752/pl/XMp4j2dLV6xetzvJ.m3u8?tag=14&container=fmp4"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1577988562774986752/vid/640x360/VrMims_9TjrsfCCl.mp4?tag=14"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1577988562774986752/vid/1280x720/TVRrUxnnQFk7OomC.mp4?tag=14"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 135447868,
+			"id_str": "135447868",
+			"name": "KCON official",
+			"screen_name": "KCON_official",
+			"location": "Mnet, Seoul, Republic of KOREA",
+			"description": "KCON OFFICIAL TWITTER\n📍KCON 2022 JAPAN 10/14 ~ 10/16 (JST)",
+			"url": "https://t.co/IDnJhWoZ59",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/IDnJhWoZ59",
+							"expanded_url": "http://kconofficial.com",
+							"display_url": "kconofficial.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 468418,
+			"friends_count": 76,
+			"listed_count": 1137,
+			"created_at": "Wed Apr 21 09:56:48 +0000 2010",
+			"favourites_count": 191,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13273,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1567787455188987905/sAYoOtFz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1567787455188987905/sAYoOtFz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/135447868/1664852417",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 387,
+		"favorite_count": 994,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "[KCON 2022 SAUDI ARABIA | DAY 2]\nCLOSING w/‘POPPIA’ Signature Song😎🎵\n\nOur thrilling day at KCON 2022 SAUDI ARABIA💎\nThank you KCON-ers for joining us💚\n\nSEE YOU AT KCON 2022 JAPAN❤️\n\n#KCON #KCON2022SAUDIARABIA https://t.co/HUwJtsJev5"
+	},
+	{
 		"created_at": "Sat Oct 08 06:58:27 +0000 2022",
 		"id": 1578633793413546000,
 		"id_str": "1578640952629002240",
