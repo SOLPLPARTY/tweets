6d593cba7a1b841fed1a6b4988e6dba32a4dc70d
솔플파티 <solplparty@gmail.com>
2022-10-09 15:17:37 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66136,
-	"created_at": "Sun Oct 09 03:17:36 +0000 2022",
+	"tweets": 66137,
+	"created_at": "Sun Oct 09 06:17:36 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,138 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Sun Oct 09 05:29:31 +0000 2022",
+		"id": 1578971252899053600,
+		"id_str": "1578980959885488128",
+		"full_text": "🎼 #윤석철트리오 SET LIST\n\n01. 우주를 건너 \n02. 춘곤\n03. 어쩌려고 그런 말을 해\n04. 익숙하고 일정한\n05. 독백이라 착각하기 쉽다 \n06. 한국전래동화\n07. Giant Steps\n08. Love Is A Song\n+ 둘의 대화\n\n#윤석철 #YunSeokCheol\n#YUNSEOKCHEOL_TRIO\n#JAZZKOREA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			190
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "윤석철트리오",
+					"indices": [
+						2,
+						9
+					]
+				},
+				{
+					"text": "윤석철",
+					"indices": [
+						142,
+						146
+					]
+				},
+				{
+					"text": "YunSeokCheol",
+					"indices": [
+						147,
+						160
+					]
+				},
+				{
+					"text": "YUNSEOKCHEOL_TRIO",
+					"indices": [
+						161,
+						179
+					]
+				},
+				{
+					"text": "JAZZKOREA",
+					"indices": [
+						180,
+						190
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1578971248117559300,
+		"in_reply_to_status_id_str": "1578971248117559298",
+		"in_reply_to_user_id": 266004552,
+		"in_reply_to_user_id_str": "266004552",
+		"in_reply_to_screen_name": "antennamusic",
+		"user": {
+			"id": 266004552,
+			"id_str": "266004552",
+			"name": "안테나 Antenna",
+			"screen_name": "antennamusic",
+			"location": "대한민국 서울",
+			"description": "유희열(Toy), 유재석, 정재형, 루시드폴, 페퍼톤스(신재평&이장원), 박새별, 샘김, 이진아, 권진아, 정승환, 윤석철, 적재, 이미주, 서동환, 황현조 Official / 권진아 DS ‘진심이었던 사람만 바보가 돼’ 2022. 10. 13. 6PM Release",
+			"url": "https://t.co/XSWWwEx210",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XSWWwEx210",
+							"expanded_url": "http://www.antenna.co.kr",
+							"display_url": "antenna.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 40490,
+			"friends_count": 12,
+			"listed_count": 376,
+			"created_at": "Mon Mar 14 13:49:27 +0000 2011",
+			"favourites_count": 19,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 5431,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675611168992419840/3DJUZI4Z_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/266004552/1664877607",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "🎼 #윤석철트리오 SET LIST\n\n01. 우주를 건너 \n02. 춘곤\n03. 어쩌려고 그런 말을 해\n04. 익숙하고 일정한\n05. 독백이라 착각하기 쉽다 \n06. 한국전래동화\n07. Giant Steps\n08. Love Is A Song\n+ 둘의 대화\n\n#윤석철 #YunSeokCheol\n#YUNSEOKCHEOL_TRIO\n#JAZZKOREA"
+	},
+	{
 		"created_at": "Sun Oct 09 03:14:37 +0000 2022",
 		"id": 1578947008965902300,
 		"id_str": "1578947008965902337",
