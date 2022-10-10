e4364c22f2bfb8a9be54394e0db0378d1f3b0815
솔플파티 <solplparty@gmail.com>
2022-10-10 22:17:37 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66155,
-	"created_at": "Mon Oct 10 12:17:36 +0000 2022",
+	"tweets": 66156,
+	"created_at": "Mon Oct 10 13:17:36 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,124 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Mon Oct 10 12:29:34 +0000 2022",
+		"id": 1579440711178784800,
+		"id_str": "1579449055691780096",
+		"full_text": "10/15 (토) 오후 5시 @ 난지한강공원 젊음의 광장 일대\n[#박소은] 어스어스 페스티벌\n-\n10/15 (토) 오후 5시 20분 @ 경인아라뱃길 아라마리나\n[#너드커넥션] 경기인디뮤직페스티벌 2022\n-\n10/15 (토) @ 영덕해파랑공원\n[#우주왕복선싸이드미러] 해파랑프리마켓 공연",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			160
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "박소은",
+					"indices": [
+						36,
+						40
+					]
+				},
+				{
+					"text": "너드커넥션",
+					"indices": [
+						90,
+						96
+					]
+				},
+				{
+					"text": "우주왕복선싸이드미러",
+					"indices": [
+						137,
+						148
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1579440546489458700,
+		"in_reply_to_status_id_str": "1579440546489458688",
+		"in_reply_to_user_id": 404720294,
+		"in_reply_to_user_id_str": "404720294",
+		"in_reply_to_screen_name": "YoursummerKR",
+		"user": {
+			"id": 404720294,
+			"id_str": "404720294",
+			"name": "유어썸머 your summer",
+			"screen_name": "YoursummerKR",
+			"location": "한국",
+			"description": "유어썸머(Your Summer)는 cott(콧), 계피, 김사월, 김사월X김해원, 너드커넥션, 랄라스윗, 못(Mot), 박소은, 슬릭, 아마도이자람밴드, 우주왕복선싸이드미러, 이랑, 이하윤, 전기뱀장어와 함께합니다.",
+			"url": "https://t.co/M1QKGb4pGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/M1QKGb4pGi",
+							"expanded_url": "https://linktr.ee/yoursummer",
+							"display_url": "linktr.ee/yoursummer",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2299,
+			"friends_count": 20,
+			"listed_count": 12,
+			"created_at": "Fri Nov 04 09:50:32 +0000 2011",
+			"favourites_count": 637,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6601,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1509898501031079936/2_2E7tDV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1509898501031079936/2_2E7tDV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/404720294/1615202264",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "10/15 (토) 오후 5시 @ 난지한강공원 젊음의 광장 일대\n[#박소은] 어스어스 페스티벌\n-\n10/15 (토) 오후 5시 20분 @ 경인아라뱃길 아라마리나\n[#너드커넥션] 경기인디뮤직페스티벌 2022\n-\n10/15 (토) @ 영덕해파랑공원\n[#우주왕복선싸이드미러] 해파랑프리마켓 공연"
+	},
+	{
 		"created_at": "Mon Oct 10 12:11:17 +0000 2022",
 		"id": 1579418192891760600,
 		"id_str": "1579444455282614272",
