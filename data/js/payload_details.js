1cbe6edf9d666fd50027c8eb57d10fe6aa7be4f7
솔플파티 <solplparty@gmail.com>
2022-10-14 21:17:43 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66358,
-	"created_at": "Fri Oct 14 11:17:39 +0000 2022",
+	"tweets": 66360,
+	"created_at": "Fri Oct 14 12:17:42 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,313 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Fri Oct 14 12:11:24 +0000 2022",
+		"id": 1580883352697335800,
+		"id_str": "1580894035329265665",
+		"full_text": "[시공간집 5 : 시 놀이공원]\n2022.10.3~13.\n\n기획 및 진행 | 재미공작소(이재림, 이세미)\n후원 | 영등포문화재단 2022예술생태랩\n\n🎡전시 참여 시인 \n강보원, 강혜빈, 김선오, 김연덕, 안태운, 안희연, 이기리, 임지은, 최지은, 최재원 \n\n🎩시 도슨트 \n김현, 김복희 \n\n📝시꾸러미 클럽 진행 \n이혜미",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			179
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1580882939835199500,
+		"in_reply_to_status_id_str": "1580882939835199488",
+		"in_reply_to_user_id": 282449618,
+		"in_reply_to_user_id_str": "282449618",
+		"in_reply_to_screen_name": "studio_zemi",
+		"user": {
+			"id": 282449618,
+			"id_str": "282449618",
+			"name": "재미공작소",
+			"screen_name": "studio_zemi",
+			"location": "서울시 영등포구 문래동 3가 58-84 1층 왼편",
+			"description": "재미공작소는 서울 문래동에 위치한 문화예술공간입니다. (Since 2011) 공연, 문학 행사, 팝업숍, 전시, 창작 워크숍, 출판, 캘린더 갤러리 등 '재미있는 일'을 기획하고 진행합니다. 재미공작소의 일정 및 예약 관련 정보는 아래 홈블로그 링크에서 바로 확인하실 수 있습니다.",
+			"url": "https://t.co/aLQQKE9ltK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aLQQKE9ltK",
+							"expanded_url": "http://blog.naver.com/studiozemi",
+							"display_url": "blog.naver.com/studiozemi",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8303,
+			"friends_count": 548,
+			"listed_count": 75,
+			"created_at": "Fri Apr 15 07:08:52 +0000 2011",
+			"favourites_count": 2413,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 14869,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1314827654/zemi02_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1314827654/zemi02_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/282449618/1398647875",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "[시공간집 5 : 시 놀이공원]\n2022.10.3~13.\n\n기획 및 진행 | 재미공작소(이재림, 이세미)\n후원 | 영등포문화재단 2022예술생태랩\n\n🎡전시 참여 시인 \n강보원, 강혜빈, 김선오, 김연덕, 안태운, 안희연, 이기리, 임지은, 최지은, 최재원 \n\n🎩시 도슨트 \n김현, 김복희 \n\n📝시꾸러미 클럽 진행 \n이혜미"
+	},
+	{
+		"created_at": "Fri Oct 14 12:11:17 +0000 2022",
+		"id": 1580882939835199500,
+		"id_str": "1580894005130276864",
+		"full_text": "[시공간집 5 : 시 놀이공원]\n\n감사 인사🙇🏻🙇🏻‍♀️\n\n열흘 동안 진행된 다섯 번째 시공간집에 함께해주신 모든 시인님들, 찾아주신 관객 여러분께 감사 인사 전합니다. 우리 내년에도 꼭 만날 수 있길✍️\n\n#재미공작소 #시공간집 #시공간집5\n#시공간집5_시놀이공원 #2022예술생태랩 https://t.co/ojpcnMaQHu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			159
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "재미공작소",
+					"indices": [
+						116,
+						122
+					]
+				},
+				{
+					"text": "시공간집",
+					"indices": [
+						123,
+						128
+					]
+				},
+				{
+					"text": "시공간집5",
+					"indices": [
+						129,
+						135
+					]
+				},
+				{
+					"text": "시공간집5_시놀이공원",
+					"indices": [
+						136,
+						148
+					]
+				},
+				{
+					"text": "2022예술생태랩",
+					"indices": [
+						149,
+						159
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1580882914409009200,
+					"id_str": "1580882914409009152",
+					"indices": [
+						160,
+						183
+					],
+					"media_url": "http://pbs.twimg.com/media/FfBsooDVUAAcaf7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FfBsooDVUAAcaf7.jpg",
+					"url": "https://t.co/ojpcnMaQHu",
+					"display_url": "pic.twitter.com/ojpcnMaQHu",
+					"expanded_url": "https://twitter.com/studio_zemi/status/1580882939835199488/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1712,
+							"h": 1712,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1580882914409009200,
+					"id_str": "1580882914409009152",
+					"indices": [
+						160,
+						183
+					],
+					"media_url": "http://pbs.twimg.com/media/FfBsooDVUAAcaf7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FfBsooDVUAAcaf7.jpg",
+					"url": "https://t.co/ojpcnMaQHu",
+					"display_url": "pic.twitter.com/ojpcnMaQHu",
+					"expanded_url": "https://twitter.com/studio_zemi/status/1580882939835199488/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1712,
+							"h": 1712,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1580585191655231500,
+		"in_reply_to_status_id_str": "1580585191655231489",
+		"in_reply_to_user_id": 282449618,
+		"in_reply_to_user_id_str": "282449618",
+		"in_reply_to_screen_name": "studio_zemi",
+		"user": {
+			"id": 282449618,
+			"id_str": "282449618",
+			"name": "재미공작소",
+			"screen_name": "studio_zemi",
+			"location": "서울시 영등포구 문래동 3가 58-84 1층 왼편",
+			"description": "재미공작소는 서울 문래동에 위치한 문화예술공간입니다. (Since 2011) 공연, 문학 행사, 팝업숍, 전시, 창작 워크숍, 출판, 캘린더 갤러리 등 '재미있는 일'을 기획하고 진행합니다. 재미공작소의 일정 및 예약 관련 정보는 아래 홈블로그 링크에서 바로 확인하실 수 있습니다.",
+			"url": "https://t.co/aLQQKE9ltK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aLQQKE9ltK",
+							"expanded_url": "http://blog.naver.com/studiozemi",
+							"display_url": "blog.naver.com/studiozemi",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8303,
+			"friends_count": 548,
+			"listed_count": 75,
+			"created_at": "Fri Apr 15 07:08:52 +0000 2011",
+			"favourites_count": 2413,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 14869,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1314827654/zemi02_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1314827654/zemi02_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/282449618/1398647875",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[시공간집 5 : 시 놀이공원]\n\n감사 인사🙇🏻🙇🏻‍♀️\n\n열흘 동안 진행된 다섯 번째 시공간집에 함께해주신 모든 시인님들, 찾아주신 관객 여러분께 감사 인사 전합니다. 우리 내년에도 꼭 만날 수 있길✍️\n\n#재미공작소 #시공간집 #시공간집5\n#시공간집5_시놀이공원 #2022예술생태랩 https://t.co/ojpcnMaQHu"
+	},
+	{
 		"created_at": "Fri Oct 14 10:58:44 +0000 2022",
 		"id": 1580869020298772500,
 		"id_str": "1580875748700667905",
