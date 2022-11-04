56516a7089c41a1622a19a545fe3c95504737eb7
솔플파티 <solplparty@gmail.com>
2022-11-04 12:17:34 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 67036,
-	"created_at": "Fri Nov 04 01:17:33 +0000 2022",
+	"tweets": 67037,
+	"created_at": "Fri Nov 04 03:17:33 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,147 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Fri Nov 04 03:06:26 +0000 2022",
+		"id": 1588367037755060200,
+		"id_str": "1588367037755060224",
+		"full_text": "#워너뮤직코리아 #Warner_Music_Korea\nILYS - Raspberry [Live Clip]\nhttps://t.co/TKtocaahTb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			81
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "워너뮤직코리아",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"text": "Warner_Music_Korea",
+					"indices": [
+						9,
+						28
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/TKtocaahTb",
+					"expanded_url": "https://youtu.be/1ao689ygPzM",
+					"display_url": "youtu.be/1ao689ygPzM",
+					"indices": [
+						58,
+						81
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.powerapps.com\" rel=\"nofollow\">Microsoft Power Platform</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 968706450779463700,
+			"id_str": "968706450779463682",
+			"name": "솔플파티",
+			"screen_name": "SOLPLPARTY",
+			"location": "문의: contact@solplparty.com",
+			"description": "혼자서도 즐겁게! \n문화예술 커뮤니티, 솔플파티 🤘 \n\n▶ 참가하기 \nhttps://t.co/Uq1kQ79SRI \n\n❓ ABOUT\nhttps://t.co/OtYqQtyXgi ",
+			"url": "https://t.co/L6Ap35US8u",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/L6Ap35US8u",
+							"expanded_url": "https://solplparty.com",
+							"display_url": "solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/Uq1kQ79SRI",
+							"expanded_url": "http://join.solpl.party",
+							"display_url": "join.solpl.party",
+							"indices": [
+								39,
+								62
+							]
+						},
+						{
+							"url": "https://t.co/OtYqQtyXgi",
+							"expanded_url": "http://about.solpl.party",
+							"display_url": "about.solpl.party",
+							"indices": [
+								73,
+								96
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 258,
+			"friends_count": 460,
+			"listed_count": 5,
+			"created_at": "Wed Feb 28 04:36:30 +0000 2018",
+			"favourites_count": 234,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 90459,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1585495630180683776/k-0BAl3I_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1585495630180683776/k-0BAl3I_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/968706450779463682/1519792691",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#워너뮤직코리아 #Warner_Music_Korea\nILYS - Raspberry [Live Clip]\nhttps://t.co/TKtocaahTb"
+	},
+	{
 		"created_at": "Fri Nov 04 01:10:16 +0000 2022",
 		"id": 1588337801954615300,
 		"id_str": "1588337801954615296",
@@ -2555,7 +2696,7 @@ Grailbird.data.tweets_2022_11 =
 		"created_at": "Fri Nov 04 01:10:16 +0000 2022",
 		"id": 1588337800276938800,
 		"id_str": "1588337800276938753",
-		"full_text": "#소니뮤직코리아 #Sony_Music_Korea\n더 키드 라로이 내한공연 STAY 라이브...! 현장 분위기 🙆‍♂️🙆‍♀️\nhttps://t.co/pRynwa01w8",
+		"full_text": "#소니뮤직코리아 #Sony_Music_Korea\n더 키드 라로이 내한공연 STAY 라이브...! 현장 분위기 🙆♂️🙆♀️\nhttps://t.co/pRynwa01w8",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -2690,7 +2831,7 @@ Grailbird.data.tweets_2022_11 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "#소니뮤직코리아 #Sony_Music_Korea\n더 키드 라로이 내한공연 STAY 라이브...! 현장 분위기 🙆‍♂️🙆‍♀️\nhttps://t.co/pRynwa01w8"
+		"text": "#소니뮤직코리아 #Sony_Music_Korea\n더 키드 라로이 내한공연 STAY 라이브...! 현장 분위기 🙆♂️🙆♀️\nhttps://t.co/pRynwa01w8"
 	},
 	{
 		"created_at": "Fri Nov 04 01:10:16 +0000 2022",
@@ -3887,7 +4028,7 @@ Grailbird.data.tweets_2022_11 =
 		"created_at": "Fri Nov 04 01:10:15 +0000 2022",
 		"id": 1588337799572295700,
 		"id_str": "1588337799572295680",
-		"full_text": "#소니뮤직코리아 #Sony_Music_Korea\n페더 깜짝 버스킹 첫번째 영상 'Loving You Girl'!🙆‍♀️\nhttps://t.co/bxz8e0AQ1p",
+		"full_text": "#소니뮤직코리아 #Sony_Music_Korea\n페더 깜짝 버스킹 첫번째 영상 'Loving You Girl'!🙆♀️\nhttps://t.co/bxz8e0AQ1p",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -4022,7 +4163,7 @@ Grailbird.data.tweets_2022_11 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "#소니뮤직코리아 #Sony_Music_Korea\n페더 깜짝 버스킹 첫번째 영상 'Loving You Girl'!🙆‍♀️\nhttps://t.co/bxz8e0AQ1p"
+		"text": "#소니뮤직코리아 #Sony_Music_Korea\n페더 깜짝 버스킹 첫번째 영상 'Loving You Girl'!🙆♀️\nhttps://t.co/bxz8e0AQ1p"
 	},
 	{
 		"created_at": "Fri Nov 04 01:10:15 +0000 2022",
